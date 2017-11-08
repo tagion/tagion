9f7cff5045cd2151be2a7b4a69fbec3f947760ed
cbleser <bitcuits@gmail.com>
2017-11-08 18:20:24 +0200

    Working on ScriptElement

--- src/lib-funnel/bakery/script/ScriptElement.d
+++ src/lib-funnel/bakery/script/ScriptElement.d
@@ -1,4 +1,6 @@
 
+module bakery.script.ScriptElement;
+
 @safe
 class Value {
     immutable(ulong[]) data;
@@ -17,46 +19,107 @@ class Value {
 
 @safe
 class ScriptContext {
-    private const(Value)[] stack;
-    private uint stack_index;
-    this(uint stack_size) {
-        stack=new Value[stack_size];
+    private const(Value)[] data_stack;
+    private const(ScriptElement)[] return_stack;
+    private uint data_stack_index;
+    private uint return_stack_index;
+    private uint iteration_count;
+    this(const uint data_stack_size, const uint return_data_size ) {
+        data_stack=new Value[data_stack_size];
+        return_stack=new Value[return_stack_size];
     }
-    void push(const(Value) v) {
-        if ( stack_index < stack_size) {
-            stack[stack_index++]=v;
+    void data_push(const(Value) v) {
+        if ( data_stack_index < data_stack.length) {
+            data_stack[data_stack_index++]=v;
         }
         else {
             throw new ScriptException("Stack overflow");
         }
     }
-    const(Value) pop() {
-        if ( stack_index > 0 ) {
-            stack_index--;
+    const(Value) data_pop() {
+        if ( data_stack_index > 0 ) {
+            data_stack_index--;
         }
         else {
             throw new ScriptException("Stack empty");
         }
-        return stack[stack_index];
+        return data_stack[data_stack_index];
 
+    }
+    const(ScriptElement) return_pop() {
+        if ( return_stack_index > 0 ) {
+            return_stack_index--;
+        }
+        else {
+            throw new ScriptException("Return stack empty");
+        }
+        return return_stack[return_stack_index];
+
+    }
+    void return_push(const(ScriptElement) v) {
+        if ( data_stack_index < data_stack.length) {
+            data_stack[stack_index++]=v;
+        }
+        else {
+            throw new ScriptException("Return stack overflow");
+        }
+    }
+    void check_jump() {
+        if ( iteration_count == 0 ) {
+            throw new ScriptException("Iteration limit");
+        }
+        iteration_count--;
     }
 }
 
 
-class ScriptElement {
-    private const ScriptElement next;
-    this(const ScriptElement next) {
-        this.next=next;
-    }
-    ScriptElement doit(const ScriptContext) {
-        throw new ScriptException("Undefined script element");
+@safe
+abstract class ScriptElement {
+    private ScriptElement _next;
+    private bool touched; // Set to true if the function has been executed
+    immutable uint runlevel;
+    ScriptElement doit(const Script s, ScriptContext sc)
+        in {
+            assert(sc !is null);
+        }
+    body {
         return next;
     }
+    package void next(ScriptElement n) {
+        _next = n;
+        return _next;
+    }
+    const ScriptElement next() {
+        return _next;
+    }
+    void check(const ScriptContext sc)
+        in {
+            assert( sc !is null);
+        }
+    body {
+
+        if ( runlevel > sc.runlevel ) {
+            throw new ScriptException("Opcode not allowed in this runlevel");
+        }
+        else if ( sc.runlevel < 2 ) {
+            if ( touched ) {
+                throw new ScriptException("Opcode has already been executed (loop not allowed in this runlevel)");
+            }
+        }
+        touched = true;
+    }
 }
 
-class ScriptConditial : ScriptElement {
-    private ScriptElement jump;
-    override ScriptElement doit(const ScriptContext sc) {
+@safe
+class ScriptConditional : ScriptElement {
+    private const ScriptElement jump;
+    this(const ScriptElement jump) {
+        runlevel=0;
+        this.next=next;
+        this.jump=jump;
+    }
+    override ScriptElement doit(const Script s, ScriptContext sc) {
+        check(sc);
         if ( sc.pop.isTrue ) {
             return next;
         }
@@ -66,51 +129,79 @@ class ScriptConditial : ScriptElement {
     }
 }
 
+@safe
 class ScriptJump : ScriptElement {
-    private bool turing complete;
-    override ScriptElement doit(const ScriptContext sc) {
+    private bool turing_complete;
+    this(const ScriptElement next) {
+        runlevel=2;
+        this.next=next;
+    }
+    override ScriptElement doit(const Script s, ScriptContext sc) {
+        check(sc);
         if ( turing_complete ) {
-            throw new ScriptException("Illigal ");
+            if ( !sc.turing_complete) {
+                throw new ScriptException("Illigal command ");
+            }
         }
+        sc.check_jump();
         return next; // Points to the jump position
     }
 }
 
-class ScriptJump : ScriptElement {
-    override ScriptElement doit(const ScriptContext sc) {
-        if ( tu
-        return next; // Points to the jump position
+@safe
+class ScriptCall : ScriptElement {
+    private const ScriptElement call;
+    this(const ScriptElement next, const ScriptElement call, immutable uint runlevel=2)
+    in {
+        assert( runlevel>=2 );
+    }
+    body {
+
+        this.next=next;
+        this.call=call;
+        this.runlevel=2;
+    }
+    override ScriptElement doit(const Script s, ScriptContext sc) {
+        check(s,sc);
+        sc.return_push(next);
+        return call;
     }
 }
 
 
-
-class ScriptConditial : ScriptElement {
-    private ScriptElement jump;
-    override ScriptElement doit(const ScriptContext sc) {
-        if ( sc.pop.isTrue ) {
-            return next;
-        }
-        else {
-            return jump;
-        }
-    }
-}
-
-
-
+/*
 class ScriptAdd : ScriptElement {
-    ScriptElemet
+
+    override ScriptElement doit(const ScriptContext sc) {
+
+    }
 }
 
 class PushLiteral : ScriptElement {
 
 }
+*/
 
 class Script {
-    private const ScriptElement root;
-    this(const ScriptElement root) {
-
+    private ScriptElement root, last;
+    immutable uint runlevel;
+    private ScriptContext sc;
+    this(ScriptElement root, ScriptContext sc, immutable uint runlevel) {
+        this.runlevel=runlevel;
+        this.sc=sc;
+        this.root=root;
     }
-    void append(const
+    void run() {
+        for(const ScriptElement current=root; current !is null; current=current.doit(this, context) ) {
+            /* empty */
+        }
+    }
+    void append(ScriptElement e) {
+        if ( root is null ) {
+            root = last = e;
+        }
+        else {
+            last.next =
+        }
+    }
 }
