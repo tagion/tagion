d96a33116e133dbd3b50dbdcc67a0cecec41ada2
cbleser <bitcuits@gmail.com>
2018-02-05 21:55:47 +0200

    Work on hashgraph.EventBody

--- src/lib-hashgraph/bakery/hashgraph/HashGraph.d
+++ src/lib-hashgraph/bakery/hashgraph/HashGraph.d
@@ -70,7 +70,7 @@ class HashGraph {
         GBSON toBSON() {
             auto bson=new GBSON;
             foreach(i, m; this.tupleof) {
-                enum name=EventBody.tupleof[i].stringof;
+                enum name=this.tupleof[i].stringof["this.".length..$];
                 static if ( __traits(compiles, m.toBSON) ) {
                     bson[name]=m.toBSON;
                 }
