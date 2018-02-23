9264d622127f9ef4248a9618f8b2b5d6d954e770
cbleser <bitcuits@gmail.com>
2018-02-23 17:51:26 +0200

    GossipNet.d added

---
+++ src/lib-hashgraph/bakery/hashgraph/GossipNet.d
@@ -0,0 +1,10 @@
+module bakery.hashgraph.GossipNet.
+
+import bakery.hashgraph.Event;
+
+class GossipNet {
+    void receive(ref immutable(EventPackage) epack) {
+    }
+    void send(ref immutable(EventPackage) epack) {
+    }
+}
