749860e4afcfefc3a95f93a1aa7e2b5fd27af7f5
cbleser <bitcuits@gmail.com>
2018-02-26 14:22:07 +0200

    Merge pull request #6 from tagion/tsimonsen

--- src/lib-hashgraph/bakery/hashgraph/GossipNet.d
+++
@@ -1,8 +0,0 @@
-module bakery.hashgraph.GossipNet.
-
-import bakery.hashgraph.Event;
-
-interface GossipNet {
-    void receive(ref immutable(EventPackage) epack);
-    void send(ref immutable(EventPackage) epack);
-}
