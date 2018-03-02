302b83ef68f5b1d867ebaf086763846b1b8d1278
cbleser <bitcuits@gmail.com>
2018-03-02 13:23:25 +0200

    Makefile is chacked to support static link library

--- src/lib-hashgraph/bakery/hashgraph/GossipNet.d
+++ src/lib-hashgraph/bakery/hashgraph/GossipNet.d
@@ -1,8 +1,9 @@
-module bakery.hashgraph.GossipNet.
+module bakery.hashgraph.GossipNet;
 
-import bakery.hashgraph.Event;
+import bakery.hashgraph.HashGraph;
 
 interface GossipNet {
+    alias HashGraph.EventPackage EventPackage;
     void receive(ref immutable(EventPackage) epack);
     void send(ref immutable(EventPackage) epack);
 }
