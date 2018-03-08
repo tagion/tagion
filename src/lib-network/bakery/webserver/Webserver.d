6c41bd140707ff9ef99736b3525ebdd1e372ed47
cbleser <bitcuits@gmail.com>
2018-03-08 10:31:03 +0200

    Work on testprogram event sort

--- src/lib-network/bakery/webserver/Webserver.d
+++ src/lib-network/bakery/webserver/Webserver.d
@@ -1,11 +1,11 @@
-module bakery.backend.Backend;
+module bakery.webserver.Webserver;
 
-import vibe.core.core : sleep;
-import vibe.core.log;
-import vibe.http.fileserver : serveStaticFiles;
-import vibe.http.router : URLRouter;
-import vibe.http.server;
-import vibe.http.websockets : WebSocket, handleWebSockets;
+// import vibe.core.core : sleep;
+// import vibe.core.log;
+// import vibe.http.fileserver : serveStaticFiles;
+// import vibe.http.router : URLRouter;
+// import vibe.http.server;
+// import vibe.http.websockets : WebSocket, handleWebSockets;
 
 // import core.time;
 // import std.conv : to;
