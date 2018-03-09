57fef7e6ccf7f6311c25eef25f827c97fdbf2432
cbleser <bitcuits@gmail.com>
2018-03-09 17:32:44 +0200

    Merge branch master into cbleser

--- src/lib-network/bakery/backend/Backend.d
+++ src/lib-network/bakery/backend/Backend.d
@@ -7,32 +7,87 @@ import vibe.http.router : URLRouter;
 import vibe.http.server;
 import vibe.http.websockets : WebSocket, handleWebSockets;
 
-// import core.time;
-// import std.conv : to;
+import core.time;
+import std.concurrency;
+import std.conv : to;
+import std.stdio : writeln;
 
-// void startWebserver() {
-//     auto router = new URLRouter;
-// 	router.get("/", staticRedirect("/index.html"));
-// 	router.get("/ws", handleWebSockets(&handleWebSocketConnection));
-// 	router.get("*", serveStaticFiles("public/"));
+import bakery.hashgraph.Event;
 
-// 	auto settings = new HTTPServerSettings;
-// 	settings.port = 8080;
-// 	settings.bindAddresses = ["::1", "127.0.0.1"];
-// 	listenHTTP(settings, router);
-// }
+enum EventProperty {
+	STRONGLY_SEEING,
+	IS_FAMOUS,
+	IS_WITNESS
+};
+
+immutable struct InterfaceEventUpdate {
+    uint id;
+	EventProperty property;
+	bool value;
+
+    this(uint id, EventProperty property, bool value) {
+        this.id = id;
+        this.property = property;
+        this.value = value;
+    }
+}
+
+immutable struct InterfaceEventBody {
+    uint id;
+    uint motherId;
+	uint fatherId;
+	ubyte[] payload;
+
+    this(immutable(uint) id, 
+	immutable(ubyte[]) payload,
+	immutable(uint) motherId = 0, 
+	immutable(uint) fatherId = 0
+	) {
+        this.id = id;
+        this.motherId = motherId;
+		this.fatherId = fatherId;
+		this.payload = payload;
+    }
+}
+
+void startWebserver() {
+    auto router = new URLRouter;
+	router.get("/", staticRedirect("/index.html"));
+	router.get("/ws", handleWebSockets(&handleWebSocketConnection));
+	router.get("*", serveStaticFiles("../../backend_tools/public/"));
+
+	auto settings = new HTTPServerSettings;
+	settings.port = 8080;
+	settings.bindAddresses = ["::1", "127.0.0.1"];
+	auto listener = listenHTTP(settings, router);
+
+	scope(exit) listener.stopListening;
+
+    for (;;) {
+        receive(
+            (string msg) {
+             writeln("Received the message: " , msg);
+            }
+			
+        );
+    }
+
+    
+
+}
 
 
-// void handleWebSocketConnection(scope WebSocket socket)
-// {
-// 	int counter = 0;
-// 	logInfo("Got new web socket connection.");
-// 	while (true) {
-// 		sleep(1.seconds);
-// 		if (!socket.connected) break;
-// 		counter++;
-// 		logInfo("Sending '%s'.", counter);
-// 		socket.send(counter.to!string);
-// 	}
-// 	logInfo("Client disconnected.");
-// }
+
+void handleWebSocketConnection(scope WebSocket socket)
+{
+	int counter = 0;
+	logInfo("Got new web socket connection.");
+	while (true) {
+		sleep(1.seconds);
+		if (!socket.connected) break;
+		counter++;
+		logInfo("Sending '%s'.", counter);
+		socket.send(counter.to!string);
+	}
+	logInfo("Client disconnected.");
+}
