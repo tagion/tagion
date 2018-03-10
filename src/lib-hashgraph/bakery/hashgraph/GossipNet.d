3f1da62fd8d13738f8f936d4617561d7b41b393a
cbleser <bitcuits@gmail.com>
2018-03-10 18:19:31 +0200

    Work on Gossip Test

--- src/lib-hashgraph/bakery/hashgraph/GossipNet.d
+++ src/lib-hashgraph/bakery/hashgraph/GossipNet.d
@@ -6,4 +6,9 @@ interface GossipNet {
     alias HashGraph.EventPackage EventPackage;
     void receive(ref immutable(EventPackage) epack);
     void send(ref immutable(EventPackage) epack);
+    alias bool delegate(immutable(ubyte[])) Request;
+    // Request a missing event from the network
+    // add
+    void request(immutable(ubyte[]) event_hash, Request add);
+
 }
