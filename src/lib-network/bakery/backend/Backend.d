391ff938cb56f6857c8cc528d681c8ecfd3633d0
cbleser <bitcuits@gmail.com>
2018-03-10 18:19:31 +0200

    Work on Gossip Test

--- src/lib-network/bakery/backend/Backend.d
+++ src/lib-network/bakery/backend/Backend.d
@@ -13,7 +13,42 @@ import std.conv : to;
 import std.stdio : writeln;
 
 import bakery.hashgraph.Event;
-import bakery.Base;
+
+enum EventProperty {
+	STRONGLY_SEEING,
+	IS_FAMOUS,
+	IS_WITNESS
+};
+
+immutable struct InterfaceEventUpdate {
+    uint id;
+	EventProperty property;
+	bool value;
+
+    this(uint id, EventProperty property, bool value) {
+        this.id = id;
+        this.property = property;
+        this.value = value;
+    }
+}
+
+immutable struct InterfaceEventBody {
+    uint id;
+    uint motherId;
+	uint fatherId;
+	ubyte[] payload;
+
+    this(immutable(uint) id, 
+	immutable(ubyte[]) payload,
+	immutable(uint) motherId = 0, 
+	immutable(uint) fatherId = 0
+	) {
+        this.id = id;
+        this.motherId = motherId;
+		this.fatherId = fatherId;
+		this.payload = payload;
+    }
+}
 
 void startWebserver() {
     auto router = new URLRouter;
@@ -28,31 +63,11 @@ void startWebserver() {
 
 	scope(exit) listener.stopListening;
 
-	bool runBackend = true;
-
-	void handleState (SetThreadState ts) {
-		with(SetThreadState) final switch(ts) {
-					case KILL:
-						writeln("Kill webserver");
-						runBackend = false;
-					break;
-
-					case LIVE:
-						runBackend = true;
-					break;
-				}
-	}
-
-    while(runBackend) {
+    for (;;) {
         receive(
-			//Control the thread
-			&handleState,
-
-			(string msg) {
+            (string msg) {
              writeln("Received the message: " , msg);
             }
-
-
 			
         );
     }
