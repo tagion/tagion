1d1690efd6fc2e5f3a6a6314477bb4da7c040c02
tsimonsen <theissimonsen@gmail.com>
2018-03-12 10:13:21 +0200

    backend public path and env.

--- src/lib-network/bakery/backend/Backend.d
+++ src/lib-network/bakery/backend/Backend.d
@@ -13,81 +13,85 @@ import std.conv : to;
 import std.stdio : writeln;
 
 import bakery.hashgraph.Event;
+import bakery.Base;
+import tango.io.FilePath;
 
-enum EventProperty {
-	STRONGLY_SEEING,
-	IS_FAMOUS,
-	IS_WITNESS
-};
+class Backend {
 
-immutable struct InterfaceEventUpdate {
-    uint id;
-	EventProperty property;
-	bool value;
-
-    this(uint id, EventProperty property, bool value) {
-        this.id = id;
-        this.property = property;
-        this.value = value;
-    }
-}
-
-immutable struct InterfaceEventBody {
-    uint id;
-    uint motherId;
-	uint fatherId;
-	ubyte[] payload;
-
-    this(immutable(uint) id, 
-	immutable(ubyte[]) payload,
-	immutable(uint) motherId = 0, 
-	immutable(uint) fatherId = 0
-	) {
-        this.id = id;
-        this.motherId = motherId;
-		this.fatherId = fatherId;
-		this.payload = payload;
-    }
-}
-
-void startWebserver() {
-    auto router = new URLRouter;
-	router.get("/", staticRedirect("/index.html"));
-	router.get("/ws", handleWebSockets(&handleWebSocketConnection));
-	router.get("*", serveStaticFiles("../../backend_tools/public/"));
-
-	auto settings = new HTTPServerSettings;
-	settings.port = 8080;
-	settings.bindAddresses = ["::1", "127.0.0.1"];
-	auto listener = listenHTTP(settings, router);
-
-	scope(exit) listener.stopListening;
-
-    for (;;) {
-        receive(
-            (string msg) {
-             writeln("Received the message: " , msg);
-            }
-			
-        );
-    }
-
-    
-
-}
+	immutable FilePath public_repository;
 
 
-
-void handleWebSocketConnection(scope WebSocket socket)
-{
-	int counter = 0;
-	logInfo("Got new web socket connection.");
-	while (true) {
-		sleep(1.seconds);
-		if (!socket.connected) break;
-		counter++;
-		logInfo("Sending '%s'.", counter);
-		socket.send(counter.to!string);
+	this(FilePath public_repository ) {
+		this.public_repository = cast(immutable)public_repository.dup;
 	}
-	logInfo("Client disconnected.");
+
+
+	void startWebserver(SetThreadState threadState) {	
+		auto router = new URLRouter;
+		router.get("/", staticRedirect("/index.html"));
+		router.get("/ws", handleWebSockets(&handleWebSocketConnection));
+		router.get("*", serveStaticFiles(public_repository.toString));
+
+		auto settings = new HTTPServerSettings;
+		settings.port = 8080;
+		settings.bindAddresses = ["::1", "127.0.0.1"];
+		auto listener = listenHTTP(settings, router);
+
+		scope(exit) listener.stopListening;
+
+		bool runBackend = false;
+		void handleState (SetThreadState ts) {
+			with(SetThreadState) final switch(ts) {
+						case KILL:
+							writeln("Kill webserver");
+							runBackend = false;
+						break;
+
+						case LIVE:
+							runBackend = true;
+						break;
+					}
+		}
+		
+		handleState(threadState);
+
+		while(runBackend) {
+			receive(
+				//Control the thread
+				&handleState,
+
+				(string msg) {
+				writeln("Received the message: " , msg);
+				},
+
+				(immutable(InterfaceEventBody) event) {
+					writeln("Event recieved: ", event.id);
+				}
+				
+			);
+		}
+
+		
+
+	}
+
+
+
+	void handleWebSocketConnection(scope WebSocket socket)
+	{
+		int counter = 0;
+		logInfo("Got new web socket connection.");
+		while (true) {
+			sleep(1.seconds);
+			if (!socket.connected) break;
+			counter++;
+			logInfo("Sending '%s'.", counter);
+			socket.send(counter.to!string);
+		}
+		logInfo("Client disconnected.");
+	}
+
+
+
 }
+
