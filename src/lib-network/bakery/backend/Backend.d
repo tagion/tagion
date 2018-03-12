1122889aede06b1d083e9e22330ff005d2f05bb0
cbleser <bitcuits@gmail.com>
2018-03-12 21:37:22 +0200

    Strongly seeing work with fork

--- src/lib-network/bakery/backend/Backend.d
+++ src/lib-network/bakery/backend/Backend.d
@@ -13,55 +13,81 @@ import std.conv : to;
 import std.stdio : writeln;
 
 import bakery.hashgraph.Event;
-import bakery.Base;
-import tango.io.FilePath;
 
-class Backend {
+enum EventProperty {
+	STRONGLY_SEEING,
+	IS_FAMOUS,
+	IS_WITNESS
+};
 
-	immutable(FilePath) public_repository;
-	private ThreadState _thread_state;
-	private HTTPListener _listener;
+immutable struct InterfaceEventUpdate {
+    uint id;
+	EventProperty property;
+	bool value;
 
-	this(immutable(FilePath) public_repository) {
-		this.public_repository = public_repository;
-		writeln("Public path to webserver files: ", public_repository.toString);
-	}
+    this(uint id, EventProperty property, bool value) {
+        this.id = id;
+        this.property = property;
+        this.value = value;
+    }
+}
 
+immutable struct InterfaceEventBody {
+    uint id;
+    uint motherId;
+	uint fatherId;
+	ubyte[] payload;
 
-	void startWebserver() {	
-		auto router = new URLRouter;
-		router.get("/", staticRedirect("/index.html"));
-		router.get("/ws", handleWebSockets(&handleWebSocketConnection));
-		router.get("*", serveStaticFiles(public_repository.toString));
+    this(immutable(uint) id, 
+	immutable(ubyte[]) payload,
+	immutable(uint) motherId = 0, 
+	immutable(uint) fatherId = 0
+	) {
+        this.id = id;
+        this.motherId = motherId;
+		this.fatherId = fatherId;
+		this.payload = payload;
+    }
+}
 
-		auto settings = new HTTPServerSettings;
-		settings.port = 8080;
-		settings.bindAddresses = ["::1", "127.0.0.1"];
-		_listener = listenHTTP(settings, router);
+void startWebserver() {
+    auto router = new URLRouter;
+	router.get("/", staticRedirect("/index.html"));
+	router.get("/ws", handleWebSockets(&handleWebSocketConnection));
+	router.get("*", serveStaticFiles("../../backend_tools/public/"));
 
-	}
+	auto settings = new HTTPServerSettings;
+	settings.port = 8080;
+	settings.bindAddresses = ["::1", "127.0.0.1"];
+	auto listener = listenHTTP(settings, router);
 
-	void stopWebserver() {
-		_listener.stopListening;
-	}
+	scope(exit) listener.stopListening;
 
+    for (;;) {
+        receive(
+            (string msg) {
+             writeln("Received the message: " , msg);
+            }
+			
+        );
+    }
 
-
-	void handleWebSocketConnection(scope WebSocket socket)
-	{
-		int counter = 0;
-		logInfo("Got new web socket connection.");
-		while (true) {
-			sleep(1.seconds);
-			if (!socket.connected) break;
-			counter++;
-			logInfo("Sending '%s'.", counter);
-			socket.send(counter.to!string);
-		}
-		logInfo("Client disconnected.");
-	}
-
-
+    
 
 }
 
+
+
+void handleWebSocketConnection(scope WebSocket socket)
+{
+	int counter = 0;
+	logInfo("Got new web socket connection.");
+	while (true) {
+		sleep(1.seconds);
+		if (!socket.connected) break;
+		counter++;
+		logInfo("Sending '%s'.", counter);
+		socket.send(counter.to!string);
+	}
+	logInfo("Client disconnected.");
+}
