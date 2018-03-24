826cfd72fc89c102ef298c07060864eb2279a775
cbleser <bitcuits@gmail.com>
2018-03-24 09:06:10 +0200

    The interface Package in side GossipNet modules has been rename to EventPackage

--- src/lib-hashgraph/bakery/hashgraph/GossipNet.d
+++ src/lib-hashgraph/bakery/hashgraph/GossipNet.d
@@ -3,13 +3,13 @@ module bakery.hashgraph.GossipNet;
 import bakery.hashgraph.HashGraph;
 import bakery.hashgraph.Event;
 
-@safe interface Package {
+@safe interface EventPackage {
 }
 @safe
 interface GossipNet {
 //    alias HashGraph.EventPackage EventPackage;
-    void receive(ref immutable(Package) epack);
-    void send(ref immutable(Package) epack);
+    void receive(ref immutable(EventPackage) epack);
+    void send(ref immutable(EventPackage) epack);
     alias bool delegate(immutable(ubyte[])) Request;
     // Request a missing event from the network
     // add
