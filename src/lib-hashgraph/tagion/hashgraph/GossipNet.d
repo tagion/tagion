6e4ba1748e20c112236974dc04ff3054449e801a
cbleser <bitcuits@gmail.com>
2018-04-07 16:31:05 +0300

    work on tagionwave

--- src/lib-hashgraph/tagion/hashgraph/GossipNet.d
+++ src/lib-hashgraph/tagion/hashgraph/GossipNet.d
@@ -5,28 +5,25 @@ import tagion.hashgraph.Event;
 import tagion.utils.BSON : R_BSON=BSON, Document;
 alias R_BSON!true GBSON;
 
-@safe interface EventPackage {
-}
-
 @safe
 interface GossipNet {
     alias immutable(ubyte)[] Pubkey;
     alias immutable(ubyte)[] Privkey;
     alias immutable(ubyte)[] HashPointer;
 //    alias HashGraph.EventPackage EventPackage;
-    void receive(ref immutable(EventPackage) epack);
-    void send(ref immutable(EventPackage) epack);
+    void receive(immutable(ubyte[]) data);
+    void send(immutable(ubyte[]) data);
     alias bool delegate(immutable(ubyte[])) Request;
     // Request a missing event from the network
     // add
     void request(HashGraph h, immutable(ubyte[]) event_hash);
     // This function is call by the HashGraph.whatIsNotKnowBy
-    // and is use to collect node to be send to another node
+    // and is use to collect node to be send to anotehr node
     bool collect(Event e, immutable uint depth);
     HashPointer eventHashFromId(immutable uint id);
     HashPointer calcHash(immutable(HashPointer) hash_pointer) inout;
 
-    immutable(ubyte[]) evaPackage();
+//    immutable(ubyte[]) evaPackage();
 
     void buildPackage(HashGraph hashgraph, GBSON bson, Event event, immutable uint node_id);
 
