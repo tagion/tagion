9a20b54535c676a70eed2a4f982d3adcce291346
cbleser <bitcuits@gmail.com>
2018-04-11 01:57:05 +0300

    in tagionave the Keywords enum is now generated from string list

--- src/lib-basic/tagion/Base.d
+++ src/lib-basic/tagion/Base.d
@@ -5,6 +5,9 @@ import tagion.crypto.Hash;
 enum this_dot="this.";
 
 import std.conv;
+/**
+   Return the position of first '.' in string and
+ */
 template find_dot(string str, size_t index=0) {
     static if ( index >= str.length ) {
         enum zero_index=0;
@@ -57,6 +60,31 @@ unittest {
     something.check();
 }
 
+template EnumText(string name, string[] list, bool first=true) {
+    static if ( first ) {
+        enum begin="enum "~name~"{";
+        alias EnumText!(begin, list, false) EnumText;
+    }
+    else static if ( list.length > 0 ) {
+        enum k=list[0];
+        enum code=name~k~" = "~'"'~k~'"'~',';
+        alias EnumText!(code, list[1..$], false) EnumText;
+    }
+    else {
+        enum code=name~"}";
+        alias code EnumText;
+    }
+}
+
+unittest {
+    enum list=["red", "green", "blue"];
+//    pragma(msg, EnumText!("Colour", list));
+    mixin(EnumText!("Colour", list));
+    static assert(Colour.red == list[0]);
+    static assert(Colour.green == list[1]);
+    static assert(Colour.blue == list[2]);
+
+}
 enum ThreadState {
     KILL = 9,
     LIVE = 1
