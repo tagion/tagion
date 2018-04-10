be10d25f228a478832a03b93002f8a3b705a0b66
cbleser <bitcuits@gmail.com>
2018-04-10 21:41:42 +0300

    Work on Base.basename

--- src/lib-basic/tagion/Base.d
+++ src/lib-basic/tagion/Base.d
@@ -4,6 +4,22 @@ import tagion.crypto.Hash;
 
 enum this_dot="this.";
 
+import std.conv;
+template find_dot(string str, size_t index=0) {
+    static if ( index >= str.length ) {
+        enum zero_index=0;
+        alias zero_index find_dot;
+    }
+    else static if (str[index] == '.') {
+        enum index_plus_one=index+1;
+        static assert(index_plus_one < str.length, "Static name ends with a dot");
+        alias index_plus_one find_dot;
+    }
+    else {
+        alias find_dot!(str, index+1) find_dot;
+    }
+}
+
 /**
    Template function for removing the "this." prefix
  */
@@ -15,22 +31,31 @@ template basename(alias K) {
         alias name[this_dot.length..$] basename;
     }
     else {
-        alias name basename;
+        enum dot_pos=find_dot!(name);
+        static if ( dot_pos > 0 ) {
+            enum suffix=name[dot_pos..$];
+            alias suffix basename;
+        }
+        else {
+            alias name basename;
+        }
     }
 }
-// immutable(char[]) basename(immutable(char[]) fullname) {
-//     enum _fullname=fullname;
-//     static if (
-//         (_fullname.length > this_dot) &&
-//         (_fullname[0..this_dot.length] == this_dot) ) {
-//         return _fullname[this_dot.length..$];
-//     }
-//     else {
-//         return _fullname;
-//     }
-// }
 
-//Common components for tagion
+unittest {
+    enum name_another="another";
+    struct Something {
+        mixin("int "~name_another~";");
+        void check() {
+            assert(find_dot!(another.stringof) == this_dot.length);
+            assert(basename!(this.another) == name_another);
+        }
+    }
+    Something something;
+    static assert(find_dot!((something.another).stringof) == something.stringof.length+1);
+    static assert(basename!(something.another) == name_another);
+    something.check();
+}
 
 enum ThreadState {
     KILL = 9,
