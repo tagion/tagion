4b129b1db89825d4332dea770e8cf4bf2faacad9
tsimonsen <theissimonsen@gmail.com>
2018-04-12 14:10:07 +0300

    Small changes to load of test-data for status without success of loading large files

--- src/lib-hashgraph/tagion/hashgraph/GossipNet.d
+++ src/lib-hashgraph/tagion/hashgraph/GossipNet.d
@@ -5,27 +5,30 @@ import tagion.hashgraph.Event;
 import tagion.utils.BSON : R_BSON=BSON, Document;
 alias R_BSON!true GBSON;
 
+@safe interface EventPackage {
+}
+
 @safe
 interface GossipNet {
     alias immutable(ubyte)[] Pubkey;
     alias immutable(ubyte)[] Privkey;
     alias immutable(ubyte)[] HashPointer;
 //    alias HashGraph.EventPackage EventPackage;
-    void receive(immutable(ubyte[]) data);
-    void send(immutable(ubyte[]) data);
+    void receive(ref immutable(EventPackage) epack);
+    void send(ref immutable(EventPackage) epack);
     alias bool delegate(immutable(ubyte[])) Request;
     // Request a missing event from the network
     // add
     void request(HashGraph h, immutable(ubyte[]) event_hash);
     // This function is call by the HashGraph.whatIsNotKnowBy
     // and is use to collect node to be send to anotehr node
-//    bool collect(Event e, immutable uint depth);
+    bool collect(Event e, immutable uint depth);
     HashPointer eventHashFromId(immutable uint id);
     HashPointer calcHash(immutable(HashPointer) hash_pointer) inout;
 
-//    immutable(ubyte[]) evaPackage();
+    immutable(ubyte[]) evaPackage(Pubkey pubkey);
 
-//    void buildPackage(HashGraph hashgraph, GBSON bson, Event event, immutable uint node_id);
+    void buildPackage(HashGraph hashgraph, GBSON bson, Event event);
 
     ulong time();
 
@@ -36,7 +39,6 @@ interface GossipNet {
     // The message is a hash of the 'real' message
     immutable(ubyte[]) sign(immutable(ubyte[]) message);
 
-    Pubkey pubkey() pure const nothrow;
 
 //    HashPointer calcHash(const(Event) e);
 }
