c74b8145915d806a8e58543c57d9c0302c80eacb
tsimonsen <theissimonsen@gmail.com>
2018-04-20 17:04:59 +0300

    Impl. bsontype

--- src/lib-basic/tagion/Base.d
+++ src/lib-basic/tagion/Base.d
@@ -1,12 +1,14 @@
 module tagion.Base;
-
+import tagion.utils.BSON : R_BSON=BSON, Document;
+alias R_BSON!true GBSON;
 import tagion.crypto.Hash;
+import tagion.BSONType;
+import std.string : format;
+import std.stdio : writefln, writeln;
 
 enum this_dot="this.";
 
 import std.conv;
-
-
 /**
    Return the position of first '.' in string and
  */
@@ -52,7 +54,7 @@ unittest {
     struct Something {
         mixin("int "~name_another~";");
         void check() {
-            assert(find_dot!(another.stringof) == this_dot.length);
+            assert(find_dot!(this.another.stringof) == this_dot.length);
             assert(basename!(this.another) == name_another);
         }
     }
@@ -96,55 +98,207 @@ enum EventProperty {
 	IS_STRONGLY_SEEING,
 	IS_FAMOUS,
 	IS_WITNESS
-};
+}
 
-enum EventType {
-    EVENT_BODY,
-    EVENT_UPDATE
-};
-
-struct InterfaceEventUpdate {
-    EventType eventType;
+@safe
+struct EventUpdateMessage {
+    immutable uint bson_type_code=bsonType!(typeof(this));
     uint id;
 	EventProperty property;
 	bool value;
 
     this (const uint id, const EventProperty property, const bool value) {
-        this.eventType = EventType.EVENT_UPDATE;
         this.id = id;
         this.property = property;
         this.value = value;
     }
+
+    this(immutable(ubyte)[] data) inout {
+        auto doc=Document(data);
+        this(doc);
+    }
+
+    this(Document doc) inout {
+        foreach(i, ref m; this.tupleof) {
+            alias typeof(m) type;
+            writeln("Type for member: ", type.stringof);
+            enum name=basename!(this.tupleof[i]);
+            //CheckBSON!(cc, name, doc);
+            if ( doc.hasElement(name) ) {
+                static if(is(type == enum)) {
+                    auto value = doc[name].get!uint;
+                    if(value <= type.max) {
+                        this.tupleof[i]=cast(type)value;
+                    }
+                    else {
+                        throw new BsonCastException("The chosen enum element is out of range");
+                    }
+                }
+                else {
+                    writefln("Inserting value for : %s with the value: %s and doc type: %s", name, doc[name], doc[name].type);
+                    this.tupleof[i]=doc[name].get!type;
+                }
+            }
+        }
+    }
+
+    GBSON toBSON () const {
+        auto bson = new GBSON();
+        foreach(i, m; this.tupleof) {
+            enum name = basename!(this.tupleof[i]);
+            static if ( __traits(compiles, m.toBSON) ) {
+                bson[name] = m.toBSON;
+                //pragma(msg, format("Associated member type %s implements toBSON." , name));
+            }
+
+            bool include_member = true;
+
+            static if ( __traits(compiles, m.length ) ) {
+                include_member = m.length != 0;
+                //pragma(msg, format("The member %s is an array type", name) );
+            }
+
+            enum member_is_enum = is(typeof (m) == enum );
+
+            if( include_member ) {
+                static if(member_is_enum) {
+                    bson[name] = cast(uint)m;
+                }
+                else {
+                    bson[name] = m;
+                }
+
+                //pragma(msg, format("Member %s included.", name) );
+            }
+        }
+        return bson;
+    }
+
+    immutable(ubyte)[] serialize() const {
+        return toBSON().serialize;
+    }
 }
 
-struct InterfaceEventBody {
-    EventType eventType;
+unittest { // Serialize and unserialize EventCreateMessage
+
+    auto seed_body=EventUpdateMessage(1, EventProperty.IS_FAMOUS, true);
+    writefln("Event id: %s,  Event property: %s : %s  ,   bson_type_code: %s", seed_body.id, seed_body.property.stringof, seed_body.value, seed_body.bson_type_code);
+    auto raw=seed_body.serialize;
+
+    auto replicate_body=EventUpdateMessage(raw);
+
+    // Raw and repicate shoud be the same
+    assert(seed_body == replicate_body);
+}
+
+@safe
+struct EventCreateMessage {
     uint id;
     uint mother_id;
 	uint father_id;
-	//immutable(ubyte)[] payload;
+	immutable(ubyte)[] payload;
     uint node_id;
     bool witness;
 
     this(const(uint) id,
-	/*immutable(ubyte)[] payload,*/
+	immutable(ubyte)[] payload,
     const(uint) node_id,
 	const(uint) mother_id,
 	const(uint) father_id,
     const(bool) witness
 	) inout {
-        this.eventType = EventType.EVENT_BODY;
         this.id = id;
         this.mother_id = mother_id;
 		this.father_id = father_id;
-		//this.payload = payload;
+		this.payload = payload;
         this.node_id = node_id;
         this.witness = witness;
     }
+
+    this(immutable(ubyte)[] data) inout {
+        auto doc=Document(data);
+        this(doc);
+    }
+
+    this(Document doc) inout {
+        foreach(i, ref m; this.tupleof) {
+            alias typeof(m) type;
+            writeln("Type for member: ", type.stringof);
+            enum name=basename!(this.tupleof[i]);
+            if ( doc.hasElement(name) ) {
+                static if(is(type == enum)) {
+                    auto value = doc[name].get!uint;
+                    if(value <= type.max) {
+                        this.tupleof[i]=cast(type)value;
+                    }
+                    else {
+                        throw new BsonCastException("The chosen enum element is out of range");
+                    }
+                }
+                else {
+                    writefln("Inserting value for : %s with the value: %s and doc type: %s", name, doc[name], doc[name].type);
+                    this.tupleof[i]=doc[name].get!type;
+                }
+            }
+        }
+    }
+
+    GBSON toBSON () const {
+        auto bson = new GBSON();
+        foreach(i, m; this.tupleof) {
+            enum name = basename!(this.tupleof[i]);
+            static if ( __traits(compiles, m.toBSON) ) {
+                bson[name] = m.toBSON;
+                //pragma(msg, format("Associated member type %s implements toBSON." , name));
+            }
+
+            bool include_member = true;
+
+            static if ( __traits(compiles, m.length ) ) {
+                include_member = m.length != 0;
+                //pragma(msg, format("The member %s is an array type", name) );
+            }
+
+            enum member_is_enum = is(typeof (m) == enum );
+
+            if( include_member ) {
+                static if(member_is_enum) {
+                    bson[name] = cast(uint)m;
+                }
+                else {
+                    bson[name] = m;
+                }
+
+                //pragma(msg, format("Member %s included.", name) );
+            }
+        }
+        return bson;
+    }
+
+    immutable(ubyte)[] serialize() const {
+        return toBSON().serialize;
+    }
+}
+
+unittest {
+    auto payload = cast(immutable(ubyte)[])"Test payload";
+    auto seed_body = EventCreateMessage(1, payload, 2, 3, 5, false);
+    auto raw = seed_body.serialize;
+
+    auto replicate_body = EventCreateMessage(raw);
+    assert(replicate_body == seed_body);
+
 }
 
 @safe
 immutable(Hash) hfuncSHA256(immutable(ubyte)[] data) {
     import tagion.crypto.SHA256;
     return SHA256(data);
+}
+
+@safe
+class BsonCastException : Exception {
+    this( immutable(char)[] msg, string file = __FILE__, size_t line = __LINE__ ) {
+        super( msg, file, line );
+    }
 }
