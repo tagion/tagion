e8fad88d44168390ff8f97df24cf23af43a4a305
cbleser <bitcuits@gmail.com>
2018-04-22 11:16:46 +0300

    Merge branch master into cbleser

--- src/lib-basic/tagion/Base.d
+++ src/lib-basic/tagion/Base.d
@@ -1,12 +1,12 @@
 module tagion.Base;
 
 import tagion.crypto.Hash;
-import tagion.bson.BSONType;
-import std.string : format;
 
 enum this_dot="this.";
 
 import std.conv;
+
+
 /**
    Return the position of first '.' in string and
  */
@@ -52,7 +52,7 @@ unittest {
     struct Something {
         mixin("int "~name_another~";");
         void check() {
-            assert(find_dot!(this.another.stringof) == this_dot.length);
+            assert(find_dot!(another.stringof) == this_dot.length);
             assert(basename!(this.another) == name_another);
         }
     }
@@ -92,10 +92,59 @@ enum ThreadState {
     LIVE = 1
 }
 
+enum EventProperty {
+	IS_STRONGLY_SEEING,
+	IS_FAMOUS,
+	IS_WITNESS
+};
+
+enum EventType {
+    EVENT_BODY,
+    EVENT_UPDATE
+};
+
+struct InterfaceEventUpdate {
+    EventType eventType;
+    uint id;
+	EventProperty property;
+	bool value;
+
+    this (const uint id, const EventProperty property, const bool value) {
+        this.eventType = EventType.EVENT_UPDATE;
+        this.id = id;
+        this.property = property;
+        this.value = value;
+    }
+}
+
+struct InterfaceEventBody {
+    EventType eventType;
+    uint id;
+    uint mother_id;
+	uint father_id;
+	//immutable(ubyte)[] payload;
+    uint node_id;
+    bool witness;
+
+    this(const(uint) id,
+	/*immutable(ubyte)[] payload,*/
+    const(uint) node_id,
+	const(uint) mother_id,
+	const(uint) father_id,
+    const(bool) witness
+	) inout {
+        this.eventType = EventType.EVENT_BODY;
+        this.id = id;
+        this.mother_id = mother_id;
+		this.father_id = father_id;
+		//this.payload = payload;
+        this.node_id = node_id;
+        this.witness = witness;
+    }
+}
+
 @safe
 immutable(Hash) hfuncSHA256(immutable(ubyte)[] data) {
     import tagion.crypto.SHA256;
     return SHA256(data);
 }
-
-
