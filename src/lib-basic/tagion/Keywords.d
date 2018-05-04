8296c1a2d98ba0b1402ffbc63acbd7ea4ba95801
cbleser <bitcuits@gmail.com>
2018-05-04 09:32:10 +0300

    The enum Keyword is moved to Keyword.d

---
+++ src/lib-basic/tagion/Keywords.d
@@ -0,0 +1,19 @@
+module tagion.Keyword;
+
+private import tagion.Base : EnumText;
+
+// Keyword list for the BSON packages
+enum _keywords = [
+    "pubkey",       // Pubkey
+    "sig",        // signature of the block
+    "altitude",   // altitude
+    "tidewave",
+    "wavefront",  // Wave front is the list of events hashs
+    "ebody",      // Event body
+//        "events",     // List of event
+    "type",       // Package type
+    "block"     // block
+    ];
+
+// Generated the Keywords and enum string list
+mixin(EnumText!("Keywords", _keywords));
