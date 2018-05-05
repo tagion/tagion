3cdcd477b05adef30d45526a4365346b658126c9
cbleser <bitcuits@gmail.com>
2018-05-05 20:37:41 +0300

    LRU now support struct as a reference pointer

--- src/lib-hashgraph/tagion/hashgraph/Event.d
+++ src/lib-hashgraph/tagion/hashgraph/Event.d
@@ -13,6 +13,7 @@ import std.stdio;
 import std.format;
 
 import tagion.Base : this_dot, basename;
+import tagion.Keywords;
 
 @safe
 void check(bool flag, ConsensusFailCode code, string file = __FILE__, size_t line = __LINE__) {
@@ -55,24 +56,29 @@ struct EventBody {
         this(doc);
     }
 
-    @trusted
+//    @trusted
     this(Document doc, RequestNet gossipnet=null) inout {
         foreach(i, ref m; this.tupleof) {
             alias typeof(m) type;
-            enum name=this.tupleof[i].stringof[this_dot.length..$];
+            enum name=basename!(this.tupleof[i]);
             if ( doc.hasElement(name) ) {
                 static if ( name == mother.stringof || name == father.stringof ) {
                     if ( gossipnet ) {
                         immutable event_id=doc[name].get!uint;
-                        pragma(msg, "Name "~name~" type "~type.stringof);
                         this.tupleof[i]=gossipnet.eventHashFromId(event_id);
+                    }
+                    else {
+                        this.tupleof[i]=(doc[name].get!type).idup;
+                    }
+                }
+                else {
+                    pragma(msg, "EventBody " ~ name ~ " type=" ~is(type : immutable(ubyte[])).to!string);
+                    static if ( is(type : immutable(ubyte[])) ) {
+                        this.tupleof[i]=(doc[name].get!type).idup;
                     }
                     else {
                         this.tupleof[i]=doc[name].get!type;
                     }
-                }
-                else {
-                    this.tupleof[i]=doc[name].get!type;
                 }
             }
         }
@@ -100,10 +106,11 @@ struct EventBody {
 //json encoding of body only
 //    version(none)
     // @use_event_will used evnet-ids instead of hashs
-    HBSON toBSON(const(Event) use_event=null) const {
+    HBSON toBSON(File fout, const(Event) use_event=null) const {
         auto bson=new HBSON;
         foreach(i, m; this.tupleof) {
             enum name=basename!(this.tupleof[i]);
+            fout.writefln("EventBody %s", name);
             static if ( __traits(compiles, m.toBSON) ) {
                 bson[name]=m.toBSON;
             }
@@ -130,7 +137,7 @@ struct EventBody {
 
     @trusted
     immutable(ubyte[]) serialize(const(Event) use_event=null) const {
-        return toBSON(use_event).serialize;
+        return toBSON(stdout, use_event).serialize;
     }
 
 }
@@ -300,6 +307,30 @@ class Event {
         }
         return id_count;
     }
+
+    HBSON toBSON(File fout) const {
+//        check(_event_body !is null, ConsensusFailCode.EVENT_MISSING_BODY);
+        auto bson=new HBSON;
+        foreach(i, m; this.tupleof) {
+            enum member_name=basename!(this.tupleof[i]);
+            fout.writefln("Member %s", member_name);
+            static if ( member_name == basename!(_event_body) ) {
+                enum name=Keywords.ebody;
+            }
+            else {
+                enum name=member_name;
+            }
+            static if ( name[0] != '_' ) {
+                static if ( __traits(compiles, m.toBSON) ) {
+                    bson[name]=m.toBSON(fout);
+                }
+                else {
+                    bson[name]=m;
+                }
+            }
+        }
+        return bson;
+   }
 
     void round(Round round)
         in {
@@ -705,6 +736,12 @@ class Event {
         return !motherExists && !fatherExists;
     }
 
+
+
+    ref immutable(EventBody) eventBody() const {
+//        check(_event_body !is null, ConsensusFailCode.EVENT_MISSING_BODY);
+        return *_event_body;
+    }
 
     immutable(HashPointer) toCryptoHash() const pure nothrow
     in {
