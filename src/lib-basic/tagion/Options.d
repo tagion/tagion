a3e73fd57e008264bb1f31fc7a30cd30ff4c352d
cbleser <bitcuits@gmail.com>
2018-06-02 20:26:56 +0300

    ConsensusException now supports arguments via Base.consensusCheckArguments template

--- src/lib-basic/tagion/Options.d
+++ src/lib-basic/tagion/Options.d
@@ -26,8 +26,6 @@ struct Options {
     bool trace_gossip;
     // Directory for the trace files
     string tmp;
-    //Port for network socket
-    ushort network_socket_port;
 }
 
 __gshared static Options options;
