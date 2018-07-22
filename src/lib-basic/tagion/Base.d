df0976bb868b01c91e7cf8b207839365ab1248fd
cbleser <cr@tagion.com>
2018-07-22 17:52:38 +0300

    Pubkey add but causes runtime failures

--- src/lib-basic/tagion/Base.d
+++ src/lib-basic/tagion/Base.d
@@ -12,6 +12,46 @@ enum this_dot="this.";
 
 import std.conv;
 
+import std.typecons : Typedef, TypedefType;
+
+enum BufferType {
+    PUBKEY,
+    PRIVKEY,
+    SIGNATURE,
+    HASHPOINTER,
+    MESSAGE
+}
+
+
+alias Buffer=immutable(ubyte)[];
+alias Pubkey     =Typedef!(Buffer, null, BufferType.PUBKEY.stringof);
+version(none) {
+alias Privkey    =Typedef!(Buffer, null, BufferType.PRIVKEY.stringof);
+alias Signature  =Typedef!(Buffer, null, BufferType.SIGNATURE.stringof);
+alias Message    =Typedef!(Buffer, null, BufferType.SIGNATURE.stringof);
+alias HashPointer=Typedef!(Buffer, null, BufferType.HASHPOINTER.stringof);
+
+}
+//template isBufferType(T) {
+//    alias isBuffer=true;
+enum isBufferType(T)=is(T : immutable(ubyte[]) ) || is(TypedefType!T : immutable(ubyte[]) );
+
+static unittest {
+    static assert(isBufferType!(immutable(ubyte[])));
+    static assert(isBufferType!(immutable(ubyte)[]));
+    static assert(isBufferType!(Pubkey));
+    pragma(msg, TypedefType!int);
+}
+
+unittest {
+    immutable buf=cast(Buffer)"Hello";
+    immutable pkey=Pubkey(buf);
+}
+
+BUF buf_idup(BUF)(immutable(Buffer) buffer) {
+    return cast(BUF)(buffer.idup);
+}
+
 
 /**
    Return the position of first '.' in string and
@@ -156,8 +196,6 @@ template consensusCheckArguments(Consensus) {
         }
     }
 }
-
-private import tagion.hashgraph.GossipNet : isBufferType;
 
 @safe
 string cutHex(BUF)(BUF buf) if ( isBufferType!BUF )  {
