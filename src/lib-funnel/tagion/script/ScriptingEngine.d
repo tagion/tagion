61aecb3fbbca91f987121a0bc1145655b6867fac
tsimonsen <ts@tagion.com>
2018-10-26 13:07:29 +0300

    Read and accept are in two different threads. Needs non-blocking fiber for accept

--- src/lib-funnel/tagion/script/ScriptingEngine.d
+++ src/lib-funnel/tagion/script/ScriptingEngine.d
@@ -28,144 +28,137 @@ struct ScriptingEngineContext {
     Thread scripting_engine_tread;
 }
 
-struct ScriptingEngine {
-
-    synchronized
-    class SharedClients {
-        private shared (SSocket[uint])* locate_clients;
-        private shared(uint) client_counter;
+synchronized
+class SharedClients {
+    private shared (SSocket[uint])* locate_clients;
+    private shared(uint) client_counter;
 
 
-        this(ref SSocket[uint] _clients)
-        in {
-            assert(locate_clients is null);
-            assert(_clients !is null);
-        }
-        out {
-            assert(locate_clients !is null);
-            client_counter=cast(uint)_clients.length;
-        }
-        do {
-            locate_clients = cast(typeof(locate_clients))&_clients;
-        }
+    this(ref SSocket[uint] _clients)
+    in {
+        assert(locate_clients is null);
+    }
+    out {
+        assert(locate_clients !is null);
+        client_counter=cast(uint)_clients.length;
+    }
+    do {
+        locate_clients = cast(typeof(locate_clients))&_clients;
+    }
 
-        bool active() const pure {
-            return (locate_clients !is null);
-        }
+    bool active() const pure {
+        return (locate_clients !is null);
+    }
 
-        uint length() pure const {
-            auto clients = cast(SSocket[uint]) *locate_clients;
-            return cast(uint)clients.length;
-        }
+    uint length() pure const {
+        auto clients = cast(SSocket[uint]) *locate_clients;
+        return cast(uint)clients.length;
+    }
 
-        void add(ref SSocket client)
-        in {
-            assert(locate_clients !is null);
-            assert(client !is null);
-            assert(client_counter <= client_counter.max);
-        }
-        body {
-            auto clients = cast(SSocket[uint]) *locate_clients;
-            clients[client_counter] = client;
-            client_counter = client_counter +1;
-        }
+    void add(ref SSocket client)
+    in {
+        assert(locate_clients !is null);
+        assert(client !is null);
+        assert(client_counter <= client_counter.max);
+    }
+    body {
+        auto clients = cast(SSocket[uint]) *locate_clients;
+        clients[client_counter] = client;
+        client_counter = client_counter +1;
+    }
 
-        void removeClient(uint key)
-        in {
-            assert(locate_clients !is null);
-            assert(key in *locate_clients);
-        }
-        out{
-            assert(key !in *locate_clients);
-        }
-        body{
-            auto clients = cast(SSocket[uint]) *locate_clients;
-            clients.remove(key);
-        }
-
-
-        void closeAll() {
-            if ( active ) {
-                auto clients = cast(SSocket[uint]) *locate_clients;
-                foreach ( key, client; clients ) {
-                    client.disconnect;
-                }
-                locate_clients = null;
-                client_counter = 0;
-            }
-        }
-
-        void addClientsToSocketSet(ref SocketSet socket_set)
-        in {
-            assert(socket_set !is null);
-            assert(active);
-        }
-        body {
-            auto clients = cast(SSocket[uint]) *locate_clients;
-            foreach(client; clients) {
-                socket_set.add(client);
-            }
-        }
-
-        void readDataAllClients(ref SocketSet socket_set) {
-            auto clients = cast(SSocket[uint]) *locate_clients;
-            foreach ( key, client; clients ) {
-                if( socket_set.isSet(client) )  {
-                    char[1024] buffer;
-                    auto data_length = client.receive( buffer[] );
-
-                    if ( data_length == Socket.ERROR ) {
-                        writeln( "Connection error" );
-                    }
-
-                    else if ( data_length != 0) {
-                        writefln ( "Received %d bytes from %s: \"%s\"", data_length, client.remoteAddress.toString, buffer[0..data_length] );
-                        client.send(buffer[0..data_length] );
-
-                        //Check dataformat
-                        //Call scripting engine
-                        //Send response back
-                    }
-
-                    else {
-                        try {
-                            writefln("Connection from %s closed.", client.remoteAddress().toString());
-                        }
-                        catch ( SocketException ) {
-                            writeln("Connection closed.");
-                        }
-                    }
-
-                    client.disconnect();
-
-                    this.removeClient(key);
-
-                    writefln("\tTotal connections: %d", this.length);
-                }
-
-                else if ( !client.isAlive ) {
-                    client.disconnect();
-
-                    this.removeClient(key);
-
-                    writefln("\tTotal connections: %d", this.length);
-                }
-            }
-        }
+    void removeClient(uint key)
+    in {
+        assert(locate_clients !is null);
+        assert(key in *locate_clients);
+    }
+    out{
+        assert(key !in *locate_clients);
+    }
+    body{
+        auto clients = cast(SSocket[uint]) *locate_clients;
+        clients.remove(key);
     }
 
 
-private:
-    SSocket[uint] clients;
-    shared(SharedClients) shared_clients;
-    immutable char[] _listener_ip_address;
-    immutable ushort _listener_port;
-    immutable uint _max_connections;
-    immutable uint _listener_max_queue_length;
-    OpenSslSocket _listener;
-    enum _buffer_size = 1024;
+    void closeAll() {
+        if ( active ) {
+            auto clients = cast(SSocket[uint]) *locate_clients;
+            foreach ( key, client; clients ) {
+                client.disconnect;
+            }
+            locate_clients = null;
+            client_counter = 0;
+        }
+    }
 
-    uint numberOfClients() pure const{
+    void addClientsToSocketSet(ref SocketSet socket_set)
+    in {
+        assert(socket_set !is null);
+        assert(active);
+    }
+    body {
+        auto clients = cast(SSocket[uint]) *locate_clients;
+        foreach(client; clients) {
+            socket_set.add(client);
+        }
+    }
+
+    void readDataAllClients(ref SocketSet socket_set) {
+        auto clients = cast(SSocket[uint]) *locate_clients;
+        foreach ( key, client; clients) {
+            if( socket_set.isSet(client) )  {
+                char[1024] buffer;
+                auto data_length = client.receive( buffer[] );
+
+                if ( data_length == Socket.ERROR ) {
+                    writeln( "Connection error" );
+                }
+
+                else if ( data_length != 0) {
+                    writefln ( "Received %d bytes from %s: \"%s\"", data_length, client.remoteAddress.toString, buffer[0..data_length] );
+                    client.send(buffer[0..data_length] );
+
+                    //Check dataformat
+                    //Call scripting engine
+                    //Send response back
+                }
+
+                else {
+                    try {
+                        writefln("Connection from %s closed.", client.remoteAddress().toString());
+                    }
+                    catch ( SocketException ) {
+                        writeln("Connection closed.");
+                    }
+                }
+
+                client.disconnect();
+
+                this.removeClient(key);
+
+                writefln("\tTotal connections: %d", this.length);
+            }
+
+            else if ( !client.isAlive ) {
+                client.disconnect();
+
+                this.removeClient(key);
+
+                writefln("\tTotal connections: %d", this.length);
+            }
+        }
+    }
+}
+
+struct SharedClientAccess {
+private:
+    static shared(SharedClients) shared_clients;
+    SSocket[uint] clients;
+
+public:
+
+    uint numberOfClients() const{
         uint res;
         if ( shared_clients !is null ) {
             res = shared_clients.length;
@@ -173,7 +166,7 @@ private:
          return res;
     }
 
-    bool active() pure const {
+    bool active() const {
         return (shared_clients !is null) && shared_clients.active;
     }
 
@@ -210,6 +203,70 @@ private:
             shared_clients.readDataAllClients(socket_set);
         }
     }
+}
+
+ScriptingEngineWorkerContext startScriptingEngineWorker () {
+    auto s_e_w_c = ScriptingEngineWorkerContext();
+
+    s_e_w_c.scripting_engine_worker = ScriptingEngineWorker();
+
+    void delegate() scr_eng_del;
+    scr_eng_del.funcptr = &ScriptingEngineWorker.run;
+    scr_eng_del.ptr = &s_e_w_c.scripting_engine_worker;
+    s_e_w_c.scripting_engine_worker_thread = new Thread ( scr_eng_del ).start();
+
+    return s_e_w_c;
+}
+
+struct ScriptingEngineWorkerContext {
+    ScriptingEngineWorker scripting_engine_worker;
+    Thread scripting_engine_worker_thread;
+}
+
+struct ScriptingEngineWorker {
+private:
+    enum _buffer_size = 1024;
+    SharedClientAccess shared_client_access = SharedClientAccess();
+    alias clients = shared_client_access;
+    bool run_scripting_engine_worker = true;
+
+public:
+
+    void stop () {
+        writeln("Stops scripting engine worker");
+        run_scripting_engine_worker = false;
+    }
+
+    auto socket_set = new SocketSet();
+
+    void run() {
+        writeln("Startet scripting engine worker.");
+        while (run_scripting_engine_worker) {
+            clients.addClientsToSocketSet(socket_set);
+            Socket.select(socket_set, null, null, dur!"msecs"(50));
+
+            clients.readDataAllClients(socket_set);
+            socket_set.reset;
+        }
+
+        scope(exit) {
+            writeln("Closing scripting engine worker.");
+        }
+    }
+}
+
+struct ScriptingEngine {
+
+private:
+    SharedClientAccess shared_clients_access = SharedClientAccess();
+    alias clients = this.shared_clients_access;
+    immutable char[] _listener_ip_address;
+    immutable ushort _listener_port;
+    immutable uint _max_connections;
+    immutable uint _listener_max_queue_length;
+    OpenSslSocket _listener;
+    enum _buffer_size = 1024;
+
 
 public:
 
@@ -239,16 +296,6 @@ public:
 
     void run () {
 
-        scope ( exit ) {
-            writefln( "Shutdown of listener socket. Is there an listener: %s and active: %s", _listener !is null, (_listener !is null &&_listener.isAlive));
-            _listener.shutdown(SocketShutdown.BOTH);
-            _listener.disconnect();
-            Thread.sleep( dur!("seconds") (2));
-            writefln( "Destroy of listener socket. Is there an listener: %s and active: %s", _listener !is null, (_listener !is null &&_listener.isAlive));
-            _listener.destroy();
-            Thread.sleep( dur!("seconds") (2));
-        }
-
         _listener = new SSocket(AddressFamily.INET, EndpointType.Server);
         assert(_listener.isAlive);
         _listener.configureContext("pem_files/domain.pem", "pem_files/domain.key.pem");
@@ -259,27 +306,27 @@ public:
 
         auto socket_set = new SocketSet(_max_connections + 1);
 
+        auto s_e_w_c = startScriptingEngineWorker();
+
         while ( run_scripting_engine ) {
             socket_set.add( _listener );
 
-            this.addClientsToSocketSet(socket_set);
+            this.clients.addClientsToSocketSet(socket_set);
 
             Socket.select( socket_set, null, null);
-
-            this.readDataAllClients(socket_set);
 
             if (socket_set.isSet(_listener)) {     // connection request
                 try {
                     OpenSslSocket client = null;
-                    client = cast(OpenSslSocket)_listener.accept();
+                    client = cast(OpenSslSocket)_listener.acceptSsl();
                     assert( client.isAlive );
                     assert( _listener.isAlive );
 
-                    if ( this.numberOfClients < _max_connections )
+                    if ( clients.numberOfClients < _max_connections )
                     {
                         writefln( "Connection from %s established.", client.remoteAddress().toString() );
-                        this.addClient(client);
-                        writefln( "\tTotal connections: %d", this.numberOfClients );
+                        clients.addClient(client);
+                        writefln( "\tTotal connections: %d", clients.numberOfClients );
                     }
                     else
                     {
@@ -298,7 +345,18 @@ public:
 
         }
 
-        this.closeAll;
+        scope ( exit ) {
+            s_e_w_c.scripting_engine_worker.stop();
+            s_e_w_c.scripting_engine_worker_thread.join;
+            clients.closeAll;
+            writefln( "Shutdown of listener socket. Is there an listener: %s and active: %s", _listener !is null, (_listener !is null &&_listener.isAlive));
+            _listener.shutdown(SocketShutdown.BOTH);
+            _listener.disconnect();
+            Thread.sleep( dur!("seconds") (2));
+            writefln( "Destroy of listener socket. Is there an listener: %s and active: %s", _listener !is null, (_listener !is null &&_listener.isAlive));
+            _listener.destroy();
+            Thread.sleep( dur!("seconds") (2));
+        }
 
     }
 }
