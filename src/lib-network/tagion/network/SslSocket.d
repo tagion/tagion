1c00a01082fe9f8e258dd4c093aeaf6084b62faf
cbleser <cr@tagion.com>
2018-11-06 17:40:57 +0200

    Work on Eporch

--- src/lib-network/tagion/network/SslSocket.d
+++ src/lib-network/tagion/network/SslSocket.d
@@ -1,9 +1,9 @@
 module tagion.network.SslSocket;
 
-import std.stdio : writeln, writefln;
 import std.socket;
+import std.stdio:writeln;
 import core.stdc.stdio;
-import std.range.primitives : isBidirectionalRange;
+import std.exception : assumeUnique;
 
 enum EndpointType {Client, Server};
 
@@ -14,15 +14,12 @@ class SslSocketException : SocketException {
     }
 }
 
-debug {
-    void printDebugInformation (string msg) {
-        int i;
-        auto fmsg = msg ~ "\nPress a number to continue. \n";
-        printf(fmsg.ptr);
-        scanf("%d\n", i);
-    }
+void printDebugInformation (string msg) {
+    int i;
+    auto fmsg = msg ~ "\nPress a number to continue. \n";
+    printf(fmsg.ptr);
+    scanf("%d\n", i);
 }
-
 
 
 version=use_openssl;
@@ -49,7 +46,6 @@ version(use_openssl) {
         int SSL_accept(SSL*);
         int SSL_write(SSL*, const void*, int);
         int SSL_read(SSL*, void*, int);
-        int SSL_pending(SSL*);
 
         SSL_CTX* SSL_CTX_new(const SSL_METHOD* method);
         void SSL_CTX_free(SSL_CTX*);
@@ -62,38 +58,20 @@ version(use_openssl) {
         int SSL_CTX_check_private_key(SSL_CTX*);
 
         void ERR_print_errors_fp(FILE*);
-        int SSL_get_error(const SSL *ssl, int ret);
 
-    }
-
-    enum {
-        SSL_ERROR_NONE = 0,
-        SSL_ERROR_SSL = 1,
-        SSL_ERROR_WANT_READ = 2,
-        SSL_ERROR_WANT_WRITE = 3,
-        SSL_ERROR_WANT_X509_LOOKUP = 4,
-        SSL_ERROR_SYSCALL = 5,           /* look at error stack/return
-                                           * value/errno */
-        SSL_ERROR_ZERO_RETURN = 6,
-        SSL_ERROR_WANT_CONNECT = 7,
-        SSL_ERROR_WANT_ACCEPT = 8,
-        SSL_ERROR_WANT_ASYNC = 9,
-        SSL_ERROR_WANT_ASYNC_JOB = 10
     }
 
     class OpenSslSocket : Socket {
-
         private:
             debug{
                 pragma(msg,"Compiles SslSocket in debug mode" );
-                enum in_debugging_mode = true;
+               bool in_debugging_mode = true;
             }
 
             SSL* _ssl;
 
             SSL_CTX* _ctx;
 
-            uint accept_counter;
             //Static are used as default as context. A setter/argu. in cons. for the context
             //could be impl. if diff. contexts for diff SSL are needed.
             static SSL_CTX* client_ctx;
@@ -222,115 +200,24 @@ version(use_openssl) {
 
             if ( SSL_accept(ssl_client.getSsl) <= 0 ) {
                 ERR_print_errors_fp(stderr);
+                static if (__traits(hasMember, OpenSslSocket, "in_debugging_mode") ) {
+                    //TODO: debug!
+                       // printDebugInformation("Error in handsaking, accept");
+                }
 
                 client.shutdown(SocketShutdown.BOTH);
                 client.close();
+                writeln("Before exception");
 				throw new SslSocketException("ssl handsake, accept");
             }
             else {
-                return cast(Socket)ssl_client;
+                return ssl_client;
             }
-        }
-
-        //false=operation not complete and 1, operation complete.
-        bool acceptSslAsync(ref OpenSslSocket ssl_client) {
-            if ( ssl_client is null ) {
-                writeln("Accepting new client");
-                Socket client = super.accept();
-                if ( !client.isAlive ) {
-                    client.close;
-                    writeln("Could not establish conenction");
-                }
-                writeln("Set Non-blocking");
-                client.blocking = false;
-                writeln("Create new OpenSslSocket");
-                ssl_client = new OpenSslSocket(client.handle, EndpointType.Server, AddressFamily.INET);
-                SSL_set_fd(ssl_client.getSsl, client.handle);
-            }
-
-            auto c_ssl = ssl_client.getSsl;
-
-            const res = SSL_accept(c_ssl);
-
-            auto ssl_error = SSL_get_error(c_ssl, res);
-            bool accepted;
-            bool accept_blocked;
-
-            writefln("The ssl error code: %d", ssl_error);
-            switch(ssl_error) {
-                case SSL_ERROR_NONE:
-                    accepted = true;
-                    writeln("accepted new SSL connection");
-                    break;
-
-                case SSL_ERROR_ZERO_RETURN:
-                    accept_blocked = true;
-                    //throw an error;
-                    break;
-
-                case SSL_ERROR_WANT_READ:
-                    writefln("SSL_ERROR_WANT_READ");
-                    break;
-
-                case SSL_ERROR_WANT_WRITE:
-                    writefln("SSL_ERROR_WANT_WRITE");
-                    break;
-
-                default:
-                    accept_blocked = 1;
-                    //throw error;
-            }
-
-            auto result = false;
-
-            if( ( SSL_pending(c_ssl) || (!accepted && !accept_blocked) ) &&
-            accept_counter < 10 ) {
-                result = false;
-            } else {
-                result = true;
-            }
-
-            accept_counter++;
-
-            return result;
-        }
-
-        @trusted
-        void rejectClient () {
-            auto client = super.accept();
-            writefln( "Rejected connection from %s; too many connections.", client.remoteAddress().toString() );
-            this.disconnect();
-        }
-
-        @trusted
-        void disconnect() {
-            try
-            {
-                if ( _ssl !is null ) {
-                    SSL_free(_ssl);
-                }
-
-
-                if ((client_ctx !is null || server_ctx !is null) &&
-                    client_ctx != _ctx && server_ctx != _ctx && _ctx !is null) {
-
-                    SSL_CTX_free(_ctx);
-                }
-            } catch(Exception ex) {
-                writeln(ex);
-            }
-
-            super.close();
         }
 
         @trusted
         SSL* getSsl() {
             return this._ssl;
-        }
-
-        @safe
-        uint AcceptCounter() pure const {
-            return accept_counter;
         }
 
         this(AddressFamily af, EndpointType et,
@@ -344,10 +231,17 @@ version(use_openssl) {
             init(true, et);
         }
 
+        ~this() {
+            SSL_free(_ssl);
+            if ( client_ctx != _ctx && server_ctx != _ctx ) {
+                SSL_free(_ssl);
+                SSL_CTX_free(_ctx);
+            }
+        }
+
         static ~this() {
             if ( server_ctx !is null ) SSL_CTX_free(server_ctx);
             if ( client_ctx !is null ) SSL_CTX_free(client_ctx);
-            writeln("Executed static destructor");
         }
     }
 }
