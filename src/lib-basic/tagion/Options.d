fc0ec8ae2731bc587e3a5f29213e277e8227f438
cbleser <cr@tagion.com>
2018-11-06 17:40:57 +0200

    Work on Eporch

--- src/lib-basic/tagion/Options.d
+++ src/lib-basic/tagion/Options.d
@@ -40,15 +40,9 @@ struct Options {
         //Port
         ushort listener_port;
         //Listener max. incomming connection req. queue length
-        uint listener_max_queue_length;
+        uint listener_max_queue_lenght;
         //Max simultanious connections for the scripting engine
         uint max_connections;
-        //Max simultanious fibers for accepting incomming SSL connections.
-        uint max_number_of_accept_fibers;
-        //Min duration between a full call cycle for all fibers in milliseconds;
-        uint min_duration_full_fibers_cycle_ms;
-        //Number of times to reuse a fiber
-        uint max_number_of_fiber_reuse;
     }
 
     ScriptingEngine scripting_engine;
