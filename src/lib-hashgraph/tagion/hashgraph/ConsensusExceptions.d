7c82909de1be7ac41adc32e3f54167bcfdc44d95
cbleser <cr@tagion.com>
2018-11-10 14:02:40 +0200

    Initial DART version now compiles

--- src/lib-hashgraph/tagion/hashgraph/ConsensusExceptions.d
+++ src/lib-hashgraph/tagion/hashgraph/ConsensusExceptions.d
@@ -38,6 +38,9 @@ enum ConsensusFailCode {
     SECURITY_PUBLIC_KEY_COMPRESS_SIZE_FAULT,
     SECURITY_PUBLIC_KEY_UNCOMPRESS_SIZE_FAULT,
 
+    DART_ARCHIVE_ALREADY_ADDED,
+    DART_ARCHIVE_DOES_NOT_EXIST,
+
     NETWORK_BAD_PACKAGE_TYPE
 };
 
@@ -97,6 +100,14 @@ class HashGraphConsensusException : ConsensusException {
 }
 
 
+@safe
+class DARTConsensusException : ConsensusException {
+    this( ConsensusFailCode code, string file = __FILE__, size_t line = __LINE__ ) {
+        super( code, file, line );
+    }
+}
+
+
 @trusted
 static this() {
     with (ConsensusFailCode) {
@@ -133,6 +144,9 @@ static this() {
             SECURITY_PRIVATE_KEY_TWEAK_MULT_FAULT : "Failed to tweak mult private key",
             SECURITY_PUBLIC_KEY_TWEAK_ADD_FAULT   : "Failed to tweak add public key",
             SECURITY_PUBLIC_KEY_TWEAK_MULT_FAULT  : "Failed to tweak mult public key",
+
+            DART_ARCHIVE_ALREADY_ADDED            : "DART Failed archive is already added",
+            DART_ARCHIVE_DOES_NOT_EXIST           : "DART Failed archive does not exist",
 
             NETWORK_BAD_PACKAGE_TYPE : "Illegal package type",
             ];
