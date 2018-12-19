0e2fdc280ec23bab08714080d3260b6ba19c05bd
cbleser <cr@tagion.com>
2018-12-19 15:26:53 +0200

    Initial StdDARTNet added to DART

--- src/lib-dart/tagion/dart/StdDARTNet.d
+++ src/lib-dart/tagion/dart/StdDARTNet.d
@@ -1,6 +1,15 @@
 module tagion.dart.StdDARTNet;
 
+import tagion.hashgraph.Net : StdSecureNet;
+import tagion.hashgraph.GossipNet : DARTNet;
+
 class StdDARTNet : StdSecureNet, DARTNet {
+    import tagion.crypto.secp256k1.NativeSecp256k1 : NativeSecp256k1;
+
+    this(NativeSecp256k1 crypt) {
+        super(crypt);
+    }
+
     immutable(ubyte[]) load(const(ubyte[]) key) {
         return null;
     }
