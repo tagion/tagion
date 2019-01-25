861b286ec71601a6d2bab15d33928c42b5167fba
cbleser <cr@tagion.com>
2019-01-25 13:44:50 +0200

    BlockFile bugfix in recycle chain listed fixed

--- src/lib-basic/tagion/Base.d
+++ src/lib-basic/tagion/Base.d
@@ -26,11 +26,6 @@ enum BufferType {
     PAYLOAD
 }
 
-enum BillType {
-    Non_Usable,
-    Tagions,
-    Contracts
-}
 
 alias Buffer=immutable(ubyte)[];
 alias Pubkey     =Typedef!(Buffer, null, BufferType.PUBKEY.stringof);
@@ -280,4 +275,42 @@ string cutHex(BUF)(BUF buf) if ( isBufferType!BUF )  {
     else {
         return buf[0..LEN].toHexString;
     }
+}
+
+
+@safe
+void check(E)(bool flag, string msg, string file = __FILE__, size_t line = __LINE__) {
+    if (!flag) {
+        throw new E(msg, file, line);
+    }
+}
+
+@trusted
+int log2(ulong n) {
+    if ( n == 0 ) {
+        return -1;
+    }
+    import core.bitop : bsr;
+    return bsr(n);
+    // int i;
+    // void S(uint k=((ulong.sizeof*8)>>1))() {
+    //     static if ( k > 0 ) {
+    //         if (n >= (1uL << k)) {
+    //             i += k; n >>= k;
+    //         }
+    //         S!(k>>1);
+    //     }
+    // }
+    // S;
+    // return i;
+}
+
+
+    unittest {
+    // Undefined value returns -1
+    assert(log2(0) == -1);
+    assert(log2(17) == 4);
+    assert(log2(177) == 7);
+    assert(log2(0x1000_000_000) == 36);
+
 }
