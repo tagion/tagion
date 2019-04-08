661d8f912b70ee0150c6f4715fe65ef1fc79a29d
cbleser <cr@tagion.com>
2019-04-08 16:15:20 +0300

    Working on dividing GossipNet up into emulator network and socket network

---
+++ src/lib-services/tagion/services/HeartBeatNode.d
@@ -0,0 +1,140 @@
+module tagion.services.HeartBeatNode;
+
+import core.thread;
+import std.concurrency;
+
+import tagion.Options;
+import tagion.services.TagionLog;
+import tagion.utils.Random;
+
+import tagion.Base : Pubkey, Control;
+import tagion.services.TagionNode;
+import tagion.hashgraph.EmulatorGossipNet;
+
+void heart_beat() { //immutable uint count_from, immutable uint N, immutable uint seed, immutable uint delay, immutable uint timeout) {
+    immutable N=options.nodes;
+    immutable delay=options.delay;
+    immutable timeout=options.timeout;
+    immutable uint count_from=options.loops;
+
+//    auto main_tid=ownerTid;
+
+    Tid[] tids;
+//    Tid[] scription_api_tids;
+    Pubkey[]  pkeys;
+    immutable monitor_address = options.url; //"127.0.0.1";
+
+    version(Monitor) {
+        auto network_socket_thread_id = spawn(&createSocketThread, options.network_socket_port, monitor_address);
+     //spawn(&createSocketThread, ThreadState.LIVE, monitor_port, monitor_ip_address, true);
+
+        register(format("network_socket_thread %s", options.network_socket_port), network_socket_thread_id);
+    }
+
+    immutable transcript_enable=options.transcript.enable;
+
+    scope(exit) {
+        version(Monitor) {
+            if ( network_socket_thread_id != Tid.init ) {
+                log.writefln("Send prioritySend(Control.STOP) %s", options.network_socket_port);
+                network_socket_thread_id.send(Control.STOP);
+                auto control=receiveOnly!Control;
+                if ( control == Control.END ) {
+                    log.writeln("Closed network socket monitor.");
+                }
+                else {
+                    log.writefln("Closed network socket monitor with unexpect control command %s", control);
+                }
+            }
+        }
+
+        log.writeln("----- Stop all tasks -----");
+        foreach(i, ref tid; tids) {
+            log.writefln("Send stop to %d", i);
+            tid.prioritySend(Control.STOP);
+        }
+        log.writeln("----- Wait for all tasks -----");
+        foreach(i, ref tid; tids) {
+            auto control=receiveOnly!Control;
+            if ( control == Control.END ) {
+                log.writefln("Thread %d stopped %d", i, control);
+            }
+            else {
+                log.writefln("Thread %d stopped %d unexpected control %s", i, control);
+            }
+        }
+        log.writeln("----- Stop send to all -----");
+    }
+
+    foreach(i;0..N) {
+        ushort monitor_port;
+        if ( (!options.disable_sockets) && ((options.max_monitors == 0) || (i < options.max_monitors) ) ) {
+            monitor_port=cast(ushort)(options.port + i);
+        }
+        immutable setup=immutable(EmulatorGossipNet.Init)(timeout, i, N, monitor_address, monitor_port, 1234);
+        auto tid=spawn(&(tagionNode!EmulatorGossipNet), setup);
+        register(getname(i), tid);
+        tids~=tid;
+        pkeys~=receiveOnly!(Pubkey);
+        log.writefln("Start %d", pkeys.length);
+    }
+
+    log.writeln("----- Receive sync signal from nodes -----");
+
+    log.writefln("----- Send acknowlege signals  num of keys=%d -----", pkeys.length);
+
+    foreach(ref tid; tids) {
+        foreach(pkey; pkeys) {
+            tid.send(pkey);
+        }
+    }
+
+    uint count = count_from;
+
+    bool stop=false;
+
+    if ( options.sequential ) {
+        Thread.sleep(1.seconds);
+
+
+        log.writeln("Start the heart beat");
+        uint node_id;
+        uint time=delay;
+        Random!uint rand;
+        rand.seed(options.seed);
+        while(!stop) {
+            if ( !options.infinity ) {
+                log.writefln("count=%d", count);
+            }
+            Thread.sleep(delay.msecs);
+
+            tids[node_id].send(time, rand.value);
+            if ( !options.infinity ) {
+                log.writefln("send time=%d to  %d", time, node_id);
+            }
+
+            time+=delay;
+            node_id++;
+            if ( node_id >= tids.length ) {
+                node_id=0;
+            }
+
+            if ( !options.infinity ) {
+                stop=(count==0);
+                count--;
+            }
+        }
+    }
+    else {
+        while(!stop) {
+            if ( !options.infinity ) {
+                log.writefln("count=%d", count);
+            }
+            Thread.sleep(delay.msecs);
+            if ( !options.infinity ) {
+                stop=(count==0);
+                count--;
+            }
+        }
+    }
+}
---
+++ src/lib-services/tagion/services/TagionNode.d
@@ -0,0 +1,336 @@
+module tagion.services.TagionNode;
+
+import std.concurrency;
+import std.exception : assumeUnique;
+import std.stdio;
+import std.conv : to;
+import std.traits : hasMember;
+import core.thread;
+
+import tagion.utils.Miscellaneous: cutHex;
+import tagion.hashgraph.Event;
+import tagion.hashgraph.HashGraph;
+import tagion.hashgraph.ConsensusExceptions;
+import tagion.hashgraph.GossipNet;
+import tagion.hashgraph.EmulatorGossipNet : getname, getfilename;
+import tagion.services.ScriptingEngineNode;
+import tagion.services.TranscriptNode;
+import tagion.services.ScriptCallbacks;
+
+import tagion.communication.Monitor;
+import tagion.communication.HRPC;
+import tagion.Options;
+import tagion.Base : Pubkey, Payload, Control;
+import tagion.utils.BSON : HBSON;
+
+// If no monitor should be enable set the address to empty or the port below 6000.
+// void tagionNode(Net)(uint timeout, immutable uint node_id,
+//     immutable uint N,
+//     string monitor_ip_address,
+//     const ushort monitor_port)  {
+void tagionNode(Net)(immutable(Net.Init) setup) {
+    // timeout, immutable uint node_id,
+    // immutable uint N,
+    // string monitor_ip_address,
+    // const ushort monitor_port)  {
+//    HRPC hrpc;
+    import std.format;
+    import std.datetime.systime;
+    immutable node_name=getname(setup.node_id);
+    immutable filename=[node_name].getfilename;
+    Net.fout.open(filename, "w");
+    alias fout=Net.fout;
+    Event.fout=&fout;
+
+    fout.write("\n\n\n\n\n");
+    fout.writefln("##### Received %s #####", node_name);
+
+    Tid monitor_socket_tid;
+
+    auto hashgraph=new HashGraph();
+    // Create hash-graph
+    ScriptNet net;
+    net=new Net(hashgraph);
+//    hrpc.net=net;
+
+    immutable transcript_enable=options.transcript.enable;
+
+    debug {
+        net.node_name=node_name;
+    }
+    // Pseudo passpharse
+    immutable passphrase=node_name;
+    net.generateKeyPair(passphrase);
+
+
+    ownerTid.send(net.pubkey);
+    Pubkey[] received_pkeys; //=receiveOnly!(immutable(Pubkey[]));
+    foreach(i;0..setup.N) {
+        received_pkeys~=receiveOnly!(Pubkey);
+    }
+    immutable pkeys=assumeUnique(received_pkeys);
+
+    hashgraph.createNode(net.pubkey);
+    fout.writefln("Ownkey %s num=%d", net.pubkey.cutHex, pkeys.length);
+    foreach(i, p; pkeys) {
+        if ( hashgraph.createNode(p) ) {
+            fout.writefln("%d] %s", i, p.cutHex);
+        }
+    }
+    // All tasks is in sync
+    fout.writefln("All tasks are in sync %s", node_name);
+    // scope tids=new Tid[N];
+    // getTids(tids);
+    net.set(pkeys);
+
+    if ( (setup.monitor_ip_address != "") && (setup.monitor_port > 6000) ) {
+        monitor_socket_tid = spawn(&createSocketThread, setup.monitor_port, setup.monitor_ip_address);
+
+        Event.callbacks = new MonitorCallBacks(monitor_socket_tid, setup.node_id, net.globalNodeId(net.pubkey));
+    }
+
+    enum max_gossip=2;
+    uint gossip_count=max_gossip;
+    bool stop=false;
+    // // True of the network has been initialized;
+    // bool initialised=false;
+    enum timeout_end=10;
+    uint timeout_count;
+//    Event mother;
+    Event event;
+    auto own_node=hashgraph.getNode(net.pubkey);
+    writefln("Wait for some delay %s", node_name);
+    Thread.sleep(2.seconds);
+
+    alias has_random_seed=hasMember!(Net, "random.seed");
+    static if ( has_random_seed ) {
+        if ( !options.sequential ) {
+            net.random.seed(cast(uint)(Clock.currTime.toUnixTime!int));
+        }
+    }
+
+    //
+    // Start Script API task
+    //
+
+    if ( transcript_enable ) {
+        net.transcript_tid=spawn(&transcript!Net, setup);
+
+        auto scripting_engine_tid=spawn(&scripting_engine, setup.node_id);
+        Event.scriptcallbacks=new ScriptCallbacks(scripting_engine_tid);
+    }
+
+    scope(exit) {
+        fout.flush;
+        writefln("!!!==========!!!!!! Existing hasnode %s", node_name);
+        fout.writefln("Send stop to the transcript");
+        fout.flush;
+
+        if ( net.transcript_tid != net.transcript_tid.init ) {
+            fout.writeln("net.transcript_tid.prioritySend(Control.STOP)");
+            fout.flush;
+
+            net.transcript_tid.prioritySend(Control.STOP);
+            if ( receiveOnly!Control == Control.END ) {
+                fout.writeln("Scripting api end!!");
+            }
+        }
+
+        fout.writefln("Send stop to the engine");
+        fout.flush;
+        if ( Event.scriptcallbacks ) {
+            if ( Event.scriptcallbacks.stop && (receiveOnly!Control == Control.END) ) {
+                fout.writeln("Scripting engine end!!");
+            }
+        }
+
+        fout.writefln("Existing hasnode %s", node_name);
+        fout.flush;
+        if ( net.callbacks ) {
+            net.callbacks.exiting(hashgraph.getNode(net.pubkey));
+        }
+        fout.writefln("$$$$$$Closed monitor %s", node_name);
+        fout.flush;
+        // Thread.sleep(2.seconds);
+        if ( monitor_socket_tid != monitor_socket_tid.init ) {
+            fout.writefln("Send STOP %s", node_name);
+            fout.flush;
+            monitor_socket_tid.prioritySend(Control.STOP);
+
+            fout.writefln("after STOP %s", node_name);
+            fout.flush;
+            auto control=receiveOnly!Control;
+            fout.writefln("Control %s", control);
+            fout.flush;
+            if ( control == Control.END ) {
+                fout.writeln("Closed monitor thread");
+            }
+            else if ( control == Control.FAIL ) {
+                fout.writeln("Closed monitor thread with failure");
+            }
+            fout.flush;
+        }
+//        Thread.sleep(2.seconds);
+        //      version(none) {
+
+
+        // }
+        fout.writefln("prioritySend %s", node_name);
+        fout.writefln("End");
+        fout.close;
+        ownerTid.prioritySend(Control.END);
+    }
+
+    Payload empty_payload;
+
+    while(!stop) {
+        immutable(ubyte)[] data;
+        void receive_buffer(immutable(ubyte)[] buf) {
+            timeout_count=0;
+            net.time=net.time+100;
+            fout.write("*\n*\n*\n");
+            fout.writefln("******* receive %s [%s] %s", node_name, setup.node_id, buf.length);
+            auto own_node=hashgraph.getNode(net.pubkey);
+
+            Event register_leading_event(immutable(ubyte)[] father_fingerprint) @safe {
+                auto mother=own_node.event;
+                immutable ebody=immutable(EventBody)(empty_payload, mother.fingerprint,
+                    father_fingerprint, net.time, mother.altitude+1);
+                const pack=net.bulidEvent(ebody.toBSON);
+                //  immutable signature=net.sign(ebody);
+                return hashgraph.registerEvent(net, net.pubkey, pack.signature, ebody);
+            }
+            event=net.receive(buf, &register_leading_event);
+        }
+
+        void next_mother(Payload payload) {
+            auto own_node=hashgraph.getNode(net.pubkey);
+            if ( gossip_count >= max_gossip ) {
+                // fout.writeln("After build wave front");
+                if ( own_node.event is null ) {
+                    immutable ebody=immutable(EventBody)(net.evaPackage, null, null, net.time, net.eva_altitude);
+                    const pack=net.bulidEvent(ebody.toBSON);
+                    // immutable signature=net.sign(ebody);
+                    event=hashgraph.registerEvent(net,  net.pubkey, pack.signature, ebody);
+                }
+                else {
+                    auto mother=own_node.event;
+                    immutable mother_hash=mother.fingerprint;
+                    immutable ebody=immutable(EventBody)(payload, mother_hash, null, net.time, mother.altitude+1);
+                    const pack=net.bulidEvent(ebody.toBSON);
+                    //immutable signature=net.sign(ebody);
+                    event=hashgraph.registerEvent(net,  net.pubkey, pack.signature, ebody);
+                }
+                immutable send_channel=net.selectRandomNode;
+                auto send_node=hashgraph.getNode(send_channel);
+                if ( send_node.state is  ExchangeState.NONE ) {
+                    send_node.state = ExchangeState.INIT_TIDE;
+                    auto tidewave   = new HBSON;
+                    auto tides      = net.tideWave(tidewave, net.callbacks !is null);
+                    auto pack       = net.bulidEvent(tidewave, ExchangeState.TIDE_WAVE);
+
+                    net.send(send_channel, pack);
+                    if ( net.callbacks ) {
+                        net.callbacks.sent_tidewave(send_channel, tides);
+                    }
+                }
+                gossip_count=0;
+            }
+            else {
+                gossip_count++;
+            }
+        }
+
+        void receive_payload(Payload pload) {
+            fout.writefln("payload.length=%d", pload.length);
+            next_mother(pload);
+        }
+
+        void controller(Control ctrl) {
+            with(Control) switch(ctrl) {
+                case STOP:
+                    stop=true;
+                    fout.writefln("##### Stop %s", node_name);
+                    break;
+                default:
+                    fout.writefln("Unsupported control %s", ctrl);
+                }
+        }
+
+
+        static if (has_random_seed) {
+            auto net_random=cast(Net)net;
+            void sequential(uint time, uint random)
+                in {
+                    assert(options.sequential);
+                }
+            do {
+
+                immutable(ubyte[]) payload;
+                net_random.seed(random);
+                net_random.time=time;
+                next_mother(empty_payload);
+            }
+        }
+
+        try {
+            if ( has_random_seed && options.sequential ) {
+                static if (has_random_seed) {
+                    immutable message_received=receiveTimeout(
+                        setup.timeout.msecs,
+                        &receive_payload,
+                        &controller,
+                        &sequential,
+                        &receive_buffer,
+                        );
+                    if ( !message_received ) {
+                        fout.writeln("TIME OUT");
+                        timeout_count++;
+                        if ( !net.queue.empty ) {
+                            receive_buffer(net.queue.read);
+                        }
+                    }
+                }
+            }
+            else {
+                immutable message_received=receiveTimeout(
+                    setup.timeout.msecs,
+                    &receive_payload,
+                    &controller,
+                    // &sequential,
+                    &receive_buffer,
+                    );
+                if ( !message_received ) {
+                    fout.writeln("TIME OUT");
+                    timeout_count++;
+                    net.time=Clock.currTime.toUnixTime!long;
+                    if ( !net.queue.empty ) {
+                        receive_buffer(net.queue.read);
+                    }
+                    next_mother(empty_payload);
+                }
+            }
+        }
+        catch ( ConsensusException e ) {
+            fout.writefln("Consensus fail %s: %s. code=%s\n%s", node_name, e.msg, e.code, typeid(e));
+            stop=true;
+            if ( net.callbacks ) {
+                net.callbacks.consensus_failure(e);
+            }
+        }
+        catch ( Exception e ) {
+            auto msg=format("Error %s: %s\n%s", node_name, e.msg, typeid(e));
+            fout.writeln(msg);
+            writeln(msg);
+            stop=true;
+        }
+        catch ( Throwable t ) {
+            t.msg ~= " - From hashnode thread " ~ to!string(setup.node_id);
+            fout.writeln(t);
+            writeln(t);
+            stop=true;
+            throw t;
+        }
+
+    }
+}
