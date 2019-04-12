e620b008bff356a1de524ca155a029ba971f1600
cbleser <cr@tagion.org>
2019-04-12 08:42:19 +0300

    All TagionException is moved from Base module to TagionExceptions The ServiceNames modules is added to help set the task names TransactionService module added. This module is meant to handel incomming transaction from the user

---
+++ src/lib-basic/tagion/TagionExceptions.d
@@ -0,0 +1,69 @@
+module tagion.TagionExceptions;
+
+import std.exception;
+import tagion.hashgraph.ConsensusExceptions;
+import tagion.services.LoggerService;
+
+
+/++
+ + Exception used as a base exception class for all exceptions use in tagion project
+ +/
+@safe
+class TagionException : Exception {
+    immutable(string) task_name; /// Contains the name of the task when the execption has throw
+    this(string msg, string file = __FILE__, size_t line = __LINE__ ) {
+        task_name=log.task_name;
+        super( msg, file, line );
+    }
+}
+
+@safe
+template convertEnum(Enum, Consensus) {
+    const(Enum) convertEnum(uint enum_number, string file = __FILE__, size_t line = __LINE__) {
+        if ( enum_number <= Enum.max) {
+            return cast(Enum)enum_number;
+        }
+        throw new Consensus(ConsensusFailCode.NETWORK_BAD_PACKAGE_TYPE, file, line);
+        assert(0);
+    }
+}
+
+@safe
+template consensusCheck(Consensus) {
+    static if ( is(Consensus:ConsensusException) ) {
+        void consensusCheck(bool flag, ConsensusFailCode code, string file = __FILE__, size_t line = __LINE__) {
+            if (!flag) {
+                throw new Consensus(code, file, line);
+            }
+        }
+    }
+}
+
+@safe
+template consensusCheckArguments(Consensus) {
+    static if ( is(Consensus:ConsensusException) ) {
+        ref auto consensusCheckArguments(A...)(A args) {
+            struct Arguments {
+                A args;
+                void check(bool flag, ConsensusFailCode code, string file = __FILE__, size_t line = __LINE__) const {
+                    if ( !flag ) {
+                        immutable msg=format(consensus_error_messages[code], args);
+                        throw new Consensus(msg, code, file, line);
+                    }
+                }
+            }
+            return const(Arguments)(args);
+        }
+    }
+}
+
+/++
+ + Builds a check function out of a TagionExecption
++/
+@safe
+void Check(E)(bool flag, lazy string msg, string file = __FILE__, size_t line = __LINE__) {
+    static assert(is(E:TagionException));
+    if (!flag) {
+        throw new E(msg, file, line);
+    }
+}
