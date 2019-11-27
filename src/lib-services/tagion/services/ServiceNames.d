0a8c357fa9d89b857236b867ea929af80b8c6cd1
cbleser <cr@tagion.org>
2019-11-27 09:13:25 +0200

    ServiceNames update

--- src/lib-services/tagion/services/ServiceNames.d
+++ src/lib-services/tagion/services/ServiceNames.d
@@ -1,20 +1,21 @@
 module tagion.services.ServiceNames;
 
 import tagion.Options : Options;
-import std.format;
+import std.array : join;
+import std.conv : to;
 
-string node_task_name(const Options opt) pure {
-    return format("%s%s%d", opt.nodeprefix, opt.separator, opt.node_id);
+string node_task_name(const Options opt) pure nothrow {
+    return [opt.nodeprefix, opt.node_id.to!string].join(opt.separator);
 }
 
-string transaction_task_name(const Options opt) pure {
-    return format("%s%s%d", opt.transaction.prefix, opt.separator, opt.node_id);
+string transaction_task_name(const Options opt) pure nothrow {
+    return [opt.transaction.prefix, opt.node_id.to!string].join(opt.separator);
 }
 
-string transcript_task_name(const Options opt) pure {
-    return format("%s%s%d", opt.transcript.prefix, opt.separator, opt.node_id);
+string transcript_task_name(const Options opt) pure nothrow {
+    return [opt.transcript.prefix, opt.node_id.to!string].join(opt.separator);
 }
 
-string monitor_task_name(const Options opt) pure {
-    return format("%s%s%d", opt.monitor.prefix, opt.separator, opt.node_id);
+string monitor_task_name(const Options opt) pure nothrow {
+    return [opt.monitor.prefix, opt.node_id.to!string].join(opt.separator);
 }
