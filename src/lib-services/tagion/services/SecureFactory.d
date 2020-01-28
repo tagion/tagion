7c7754aadc17fe545503090fe92db93d070488a8
cbleser <cr@tagion.org>
2020-01-28 13:01:54 +0200

    Working on SecureNet drive tweak key

--- src/lib-services/tagion/services/SecureFactory.d
+++ src/lib-services/tagion/services/SecureFactory.d
@@ -5,29 +5,18 @@ import tagion.gossip.GossipNet;
 import tagion.crypto.secp256k1.NativeSecp256k1;
 
 @safe
-synchronized class SecureFactory : FactoryNet {
+class SecureFactory : FactoryNet {
     protected {
-        shared(StdSecureNet) net;
-        shared(bool) keyset;
+        StdSecureNet net;
     }
-    this(NativeSecp256k1 crypt) {
-        net = new shared(StdSecureNet)(crypt);
+    this(ref StdSecureNet net) {
+        this.net = net;
+        net = null;
     }
 
     HashNet hashnet() const {
-        return HashNet;
+        return new StdHashNet;
     }
-    SecureNet securenet() {
-
-    }
-
-    void generateKeyPair(string passphrase)
-        in {
-            assert(!keyset, "Passphase has already been initialized");
-        }
-    do {
-        net.generateKeyPair(passphrase);
-        keyset = true;
-    }
-
 }
+
+pragma(msg, typeof(SecureFactory.hashnet));
