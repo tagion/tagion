d050ee39456d86ae54b57469a9e15b3c8bff499d
carsten <cr@tagion.org>
2020-04-19 11:23:21 +0300

    Debug pragma printouts removed from WasmExpr

--- src/lib-wasm/wavm/WasmExpr.d
+++ src/lib-wasm/wavm/WasmExpr.d
@@ -31,9 +31,6 @@ struct WasmExpr {
                 static if (Args.length == 1) {
                     assert(isIntegral!(Args[0]), format("Args idx must be an integer for %s not %s",
                             instr.name, Args[0].stringof));
-                    pragma(msg, "opCall ARGS=", Args);
-                    pragma(msg, "opCall ARGS[0]=", Args[0]);
-                    pragma(msg, "opCall args=", typeof(args[0]));
                     static if (isIntegral!(Args[0])) {
                         bout.write(encode(args[0]));
                     }
@@ -41,46 +38,12 @@ struct WasmExpr {
                 break;
             case BRANCH_TABLE:
                 scope uint[] table;
-                pragma(msg, "BRANCH_TABLE >> ", Args);
-
                 static foreach(i, a; args) {
                     {
                         enum OK=is(Args[i]:const(uint)) || is(Args[i]:const(uint[])); // || isArray!(Args[i]); // && isIntegral!(ForeachType!(Args[i])));
                         assert(OK, format("Argument %d must be integer or uint[] of integer not %s", i, Args[i].stringof));
                         static if (OK) {
                             table~=a;
-                        }
-                    }
-                    version(none)
-                    {
-                        alias BaseT=Unqual!(Args[i]);
-                        pragma(msg, "BRANCH_TABLE BaseT=", BaseT, " ", isIntegral!BaseT);
-                        writefln("BRANCH_TABLE BaseT=%s %s isArray=%s", BaseT.stringof, isIntegral!BaseT, isArray!BaseT);
-                        static if (is(isArray!BaseT)) {
-                            writefln("\tinside isArray!BaseT is TRUE");
-                        }
-                        static if (is(isArray!BaseT)) {
-                            writefln("\tinside isArray!BaseT");
-                            assert(isIntegral!(ForeachType!BaseT),
-                                format("Type %s for as argument for instruction %s is not allowed",
-                                    Args[i], instr.name));
-                            static if (isIntegral!(ForeachType!BaseT)) {
-                                //foreach(e; args[i]) {
-                                table~=a;
-                                //}
-                            }
-                        }
-                        else {
-//                            static if (!isArray!BaseT) {
-                            writefln("BRANCH_TABLE %s Args[i]=%s", isArray!BaseT, Args[i].stringof);
-                            // assert(is(BaseT==uint) ,
-                            //     format("Bad type %s of argument %d for instruction %s",
-                            //         BaseT.stringof, i, instr.name));
-                            pragma(msg, "table~=a ", typeof(a));
-                            static if (is(BaseT==uint)) {
-                                table~=a;
-                            }
-//                            }
                         }
                     }
                 }
@@ -119,7 +82,6 @@ struct WasmExpr {
                 assert(Args.length == 1, format("Instruction %s one argument", instr.name));
                 static if (Args.length == 1) {
                     alias BaseArg0=Unqual!(Args[0]);
-                    pragma(msg, "CONST BaseArg0=", BaseArg0);
                     with(IR) {
                         switch (ir) {
                         case I32_CONST:
