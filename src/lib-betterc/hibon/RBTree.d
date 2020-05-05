03c99265c938dd9b6713d087d4c105bbedc5d0e9
carsten <cr@tagion.org>
2020-05-05 12:33:07 +0300

    Text module added

--- src/lib-betterc/hibon/RBTree.d
+++ src/lib-betterc/hibon/RBTree.d
@@ -5,35 +5,59 @@ This code is adopted from the code provided by
 Dinesh Khandelwal in comments **/
 //#include <bits/stdc++.h>
 //using namespace std;
-
-
+extern(C):
+import hibon.Memory;
+import hibon.Queue;
+import core.stdc.stdio;
 
 // Class to represent Red-Black Tree
-struct RBTree(T) {
+struct RBTree(K, V=void) {
     enum Color {RED, BLACK};
 
     struct Node {
-        T data;
+        K data;
         bool color;
         Node* left, right, parent;
-
+        static if (!is(V == void)) {
+            V value;
+        }
         // Constructor
-        this(T data) {
+        this(K data) {
             this.data = data;
             left = right = parent = null;
-            this.color = RED;
+            this.color = Color.RED;
         }
-    };
+        private void clean() {
+            static if (is(V == void)) {
+                static if (__traits(compiles, value.dispose)) {
+                    value.dispose;
+                }
+            }
+            static if (__traits(compiles, data.dispose)) {
+                data.dispose;
+            }
+        }
+    }
 
-private:
-    Node* root;
-// protected:
-//     void rotateLeft(ref Node *&, Node *&);
-//     void rotateRight(Node *&, Node *&);
-//     void fixViolation(Node *&, Node *&);
-public:
+    protected Node* root;
     // Constructor
-    this() { root = null; }
+    ~this() {
+        dispose;
+    }
+
+    void dispose() {
+        void _dispose(Node* current) {
+            if (current is null) {
+                _dispose(current.left);
+                _dispose(current.right);
+                current.clean;
+                current.dispose;
+            }
+        }
+        _dispose(root);
+        root=null;
+    }
+
     // void insert(const int &n);
     // void inorder();
     // void levelOrder();
@@ -55,19 +79,18 @@ public:
 in BST */
     static Node* BSTInsert(Node* root, Node *pt) {
 	/* If the tree is empty, return a new node */
-	if (root == NULL) {
+	if (root is null) {
             return pt;
         }
 
 	/* Otherwise, recur down the tree */
-	if (pt->data < root->data) {
-		root->left = BSTInsert(root->left, pt);
-		root->left->parent = root;
+	if (pt.data < root.data) {
+            root.left = BSTInsert(root.left, pt);
+            root.left.parent = root;
 	}
-	else if (pt->data > root->data)
-	{
-		root->right = BSTInsert(root->right, pt);
-		root->right->parent = root;
+	else if (pt.data > root.data) {
+            root.right = BSTInsert(root.right, pt);
+            root.right.parent = root;
 	}
 
 	/* return the (unchanged) node pointer */
@@ -75,179 +98,171 @@ in BST */
     }
 
 // Utility function to do level order traversal
-    static void levelOrderHelper(Node *root) {
-	if (root == NULL)
-		return;
+    static void levelOrderHelper(Node* root) {
+	if (root is null) {
+            return;
+        }
 
-	std::queue<Node *> q;
+        Queue!(Node*) q;
 	q.push(root);
 
-	while (!q.empty())
-	{
-		Node *temp = q.front();
-		cout << temp->data << " ";
-		q.pop();
+	while (!q.empty()) {
+            auto temp = q.front();
+            printf("%d ", temp.data);
+            q.popFront;
 
-		if (temp->left != NULL)
-			q.push(temp->left);
-
-		if (temp->right != NULL)
-			q.push(temp->right);
+            if (temp.left !is null) {
+                q.push(temp.left);
+            }
+            if (temp.right !is null) {
+                q.push(temp.right);
+            }
 	}
     }
 
-    void rotateLeft(Node *&root, Node *&pt) {
-	Node *pt_right = pt->right;
+    protected void rotateLeft(ref Node* root, ref Node* pt) {
+	Node* pt_right = pt.right;
 
-	pt->right = pt_right->left;
+	pt.right = pt_right.left;
 
-	if (pt->right != NULL)
-		pt->right->parent = pt;
+	if (pt.right !is null) {
+            pt.right.parent = pt;
+        }
 
-	pt_right->parent = pt->parent;
+	pt_right.parent = pt.parent;
 
-	if (pt->parent == NULL)
-		root = pt_right;
-
-	else if (pt == pt->parent->left)
-		pt->parent->left = pt_right;
-
-	else
-		pt->parent->right = pt_right;
-
-	pt_right->left = pt;
-	pt->parent = pt_right;
+	if (pt.parent is null) {
+            root = pt_right;
+        }
+	else if (pt is pt.parent.left) {
+            pt.parent.left = pt_right;
+        }
+	else {
+            pt.parent.right = pt_right;
+        }
+	pt_right.left = pt;
+	pt.parent = pt_right;
     }
 
-    void rotateRight(Node *&root, Node *&pt) {
-	Node *pt_left = pt->left;
+    protected void rotateRight(ref Node* root, Node* pt) {
+	Node* pt_left = pt.left;
 
-	pt->left = pt_left->right;
+	pt.left = pt_left.right;
 
-	if (pt->left != NULL)
-		pt->left->parent = pt;
+	if (pt.left != NULL)
+		pt.left.parent = pt;
 
-	pt_left->parent = pt->parent;
+	pt_left.parent = pt.parent;
 
-	if (pt->parent == NULL)
+	if (pt.parent == NULL)
 		root = pt_left;
 
-	else if (pt == pt->parent->left)
-		pt->parent->left = pt_left;
+	else if (pt == pt.parent.left)
+		pt.parent.left = pt_left;
 
 	else
-		pt->parent->right = pt_left;
+		pt.parent.right = pt_left;
 
-	pt_left->right = pt;
-	pt->parent = pt_left;
-}
+	pt_left.right = pt;
+	pt.parent = pt_left;
+    }
 
 // This function fixes violations caused by BST insertion
-    void fixViolation(Node *&root, Node *&pt) {
-	Node *parent_pt = NULL;
-	Node *grand_parent_pt = NULL;
+    protected void fixViolation(ref Node* root, ref Node* pt) {
+	Node* parent_pt = null;
+	Node* grand_parent_pt = null;
 
-	while ((pt != root) && (pt->color != BLACK) &&
-		(pt->parent->color == RED))
-	{
+	while ((pt !is root) && (pt.color !is Color.BLACK) &&
+            (pt.parent.color is Color.RED)) {
+            parent_pt = pt.parent;
+            grand_parent_pt = pt.parent.parent;
 
-		parent_pt = pt->parent;
-		grand_parent_pt = pt->parent->parent;
+            /* Case : A
+               Parent of pt is left child of Grand-parent of pt */
+            if (parent_pt is grand_parent_pt.left) {
+                Node* uncle_pt = grand_parent_pt.right;
 
-		/* Case : A
-			Parent of pt is left child of Grand-parent of pt */
-		if (parent_pt == grand_parent_pt->left)
-		{
+                /* Case : 1
+                   The uncle of pt is also red
+                   Only Recoloring required */
+                if (uncle_pt !is null && uncle_pt.color is Color.RED) {
+                    grand_parent_pt.color = Color.RED;
+                    parent_pt.color = Color.BLACK;
+                    uncle_pt.color = Color.BLACK;
+                    pt = grand_parent_pt;
+                }
+                else {
+                    /* Case : 2
+                       pt is right child of its parent
+                       Left-rotation required */
+                    if (pt is parent_pt.right) {
+                        rotateLeft(root, parent_pt);
+                        pt = parent_pt;
+                        parent_pt = pt.parent;
+                    }
 
-			Node *uncle_pt = grand_parent_pt->right;
+                    /* Case : 3
+                       pt is left child of its parent
+                       Right-rotation required */
+                    rotateRight(root, grand_parent_pt);
+                    swap(parent_pt.color, grand_parent_pt.color);
+                    pt = parent_pt;
+                }
+            }
 
-			/* Case : 1
-			The uncle of pt is also red
-			Only Recoloring required */
-			if (uncle_pt != NULL && uncle_pt->color == RED)
-			{
-				grand_parent_pt->color = RED;
-				parent_pt->color = BLACK;
-				uncle_pt->color = BLACK;
-				pt = grand_parent_pt;
-			}
+            /* Case : B
+               Parent of pt is right child of Grand-parent of pt */
+            else {
+                Node* uncle_pt = grand_parent_pt.left;
 
-			else
-			{
-				/* Case : 2
-				pt is right child of its parent
-				Left-rotation required */
-				if (pt == parent_pt->right)
-				{
-					rotateLeft(root, parent_pt);
-					pt = parent_pt;
-					parent_pt = pt->parent;
-				}
+                /* Case : 1
+                   The uncle of pt is also red
+                   Only Recoloring required */
+                if ((uncle_pt !is null) && (uncle_pt.color is Color.RED)) {
+                    grand_parent_pt.color = Color.RED;
+                    parent_pt.color = Color.BLACK;
+                    uncle_pt.color = Color.BLACK;
+                    pt = grand_parent_pt;
+                }
+                else {
+                    /* Case : 2
+                       pt is left child of its parent
+                       Right-rotation required */
+                    if (pt is parent_pt.left) {
+                        rotateRight(root, parent_pt);
+                        pt = parent_pt;
+                        parent_pt = pt.parent;
+                    }
 
-				/* Case : 3
-				pt is left child of its parent
-				Right-rotation required */
-				rotateRight(root, grand_parent_pt);
-				swap(parent_pt->color, grand_parent_pt->color);
-				pt = parent_pt;
-			}
-		}
-
-		/* Case : B
-		Parent of pt is right child of Grand-parent of pt */
-		else
-		{
-			Node *uncle_pt = grand_parent_pt->left;
-
-			/* Case : 1
-				The uncle of pt is also red
-				Only Recoloring required */
-			if ((uncle_pt != NULL) && (uncle_pt->color == RED))
-			{
-				grand_parent_pt->color = RED;
-				parent_pt->color = BLACK;
-				uncle_pt->color = BLACK;
-				pt = grand_parent_pt;
-			}
-			else
-			{
-				/* Case : 2
-				pt is left child of its parent
-				Right-rotation required */
-				if (pt == parent_pt->left)
-				{
-					rotateRight(root, parent_pt);
-					pt = parent_pt;
-					parent_pt = pt->parent;
-				}
-
-				/* Case : 3
-				pt is right child of its parent
-				Left-rotation required */
-				rotateLeft(root, grand_parent_pt);
-				swap(parent_pt->color, grand_parent_pt->color);
-				pt = parent_pt;
-			}
-		}
+                    /* Case : 3
+                       pt is right child of its parent
+                       Left-rotation required */
+                    rotateLeft(root, grand_parent_pt);
+                    swap(parent_pt.color, grand_parent_pt.color);
+                    pt = parent_pt;
+                }
+            }
 	}
+	root.color = Color.BLACK;
+    }
 
-	root->color = BLACK;
-}
 
 // Function to insert a new node with given data
-    void insert(const int &data) {
-	Node *pt = new Node(data);
-
+    void insert(ref const K data) {
+        Node* pt = create!Node(data);
 	// Do a normal BST insert
 	root = BSTInsert(root, pt);
-
 	// fix Red Black Tree violations
 	fixViolation(root, pt);
     }
 
 // Function to do inorder and level order traversals
-    void inorder()	 { inorderHelper(root);}
-    void levelOrder() { levelOrderHelper(root); }
+    void inorder() {
+        inorderHelper(root);
+    }
+    void levelOrder() {
+        levelOrderHelper(root);
+    }
 }
 // Driver Code
 unittest {
