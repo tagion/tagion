9bab21aa9182f5cd0f01691af49e437062b0b652
cbleser <cr@tagion.org>
2020-11-07 09:41:01 +0200

    Merged with tagionwave_nat

--- src/lib-basic/setup.mk
+++ src/lib-basic/setup.mk
@@ -11,17 +11,18 @@ endif
 DCFLAGS+=$(DIP1000) # Should support scope c= new C; // is(C == class)
 DCFLAGS+=$(DIP25)
 DCFLAGS+=$(DVERSION)=NO_MEMBER_LIST
+
+ifdef LOGGER
 DCFLAGS+=$(DVERSION)=LOGGER # Enables task name to be added for TagionExceptions
+endif
 
 SCRIPTROOT:=${REPOROOT}/scripts/
 
 
-WAMR_ROOT:=$(REPOROOT)/../wasm-micro-runtime/
-LIBS+=$(WAMR_ROOT)/wamr-compiler/build/libvmlib.a
 
 # DDOC Configuration
 #
-include ddoc.mk
+-include ddoc.mk
 
 BIN:=bin
 
@@ -36,11 +37,9 @@ REVISION:=$(REPOROOT)/$(SOURCE)/revision.di
 
 -include dstep.mk
 
-TAGION_CORE:=$(REPOROOT)/../tagion_core/
 
--include core_dfiles.mk
-TAGION_DFILES:=${addprefix $(TAGION_CORE), $(TAGION_DFILES)}
-INC+=$(TAGION_CORE)
+LIBS+=$(TAGION_BASIC)/bin/libtagion_basic.a
+
 INC+=$(REPOROOT)
 
 include unittest_setup.mk
