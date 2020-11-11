7bc6d7266c9314dbbecc64cb358aecec2ee57fea
cbleser <cr@tagion.org>
2020-11-11 14:57:59 +0200

    Makefile update

--- src/lib-hibon/setup.mk
+++ src/lib-hibon/setup.mk
@@ -5,3 +5,8 @@ include $(MAINROOT)/submodule_default_setup.mk
 
 LIBS+=${call GETLIB,tagion_basic}
 LIBS+=${call GETLIB,tagion_utils}
+#LIBS+=${call GETLIB,tagion_hibon}
+#LIBS+=${call GETLIB,tagion_hashgraph}
+#LIBS+=${call GETLIB,tagion_crypto}
+#LIBS+=${call GETLIB,tagion_services}
+#LIBS+=$(LIBSECP256K1)
