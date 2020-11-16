1c744850b5942884370ea7a48c9a5266535dbfcf
cbleser <cr@tagion.org>
2020-11-16 14:44:08 +0200

    setup.mk and unittest_setup.mk updated

--- src/lib-wasm/setup.mk
+++ src/lib-wasm/setup.mk
@@ -5,3 +5,11 @@ PACKAGE_MODULE?=tagion.vm.$(PACKAGE)
 include $(MAINROOT)/submodule_default_setup.mk
 SOURCE:=tagion/vm
 REVISION:=$(SOURCE)/$(PACKAGE)/revision.di
+
+LIBS+=${call GETLIB,tagion_basic}
+LIBS+=${call GETLIB,tagion_utils}
+#LIBS+=${call GETLIB,tagion_hibon}
+#LIBS+=${call GETLIB,tagion_hashgraph}
+#LIBS+=${call GETLIB,tagion_crypto}
+#LIBS+=${call GETLIB,tagion_services}
+#LIBS+=$(LIBSECP256K1)
--- src/lib-wasm/unittest_setup.mk
+++ src/lib-wasm/unittest_setup.mk
@@ -1,13 +1,5 @@
-UNITTEST:=$(BIN)/uinttest
+UNITTEST:=$(BINDIR)/uinttest_$(PACKAGE)
 
-#DFILES+=$(WAVM_DI)
-#TESTDCFLAGS+=$(WAVM_DI)
-#TESTDCFLAGS+=-I$(REPOROOT)/tests/basic/d/
 TESTDCFLAGS+=$(LIBS)
-#TESTDCFLAGS+=$(TAGION_CORE)/bin/libtagion.a
-TESTDCFLAGS+=$(TAGION_DFILES)
-#TESTDCFLAGS+=$(REPOROOT)/tests/basic/d/src/native_impl.d
-#TESTDCFLAGS+=$(REPOROOT)/tests/unittest.d
 TESTDCFLAGS+=-main
 
-vpath %.d tests/
