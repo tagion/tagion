94bb04e2b05f8c939c3fcfd10c230a996205b3b6
cbleser <cr@tagion.org>
2020-11-23 15:59:00 +0200

    WIP: attributes added to a number of member functions

--- src/lib-basic/tagion/basic/TagionExceptions.d
+++ src/lib-basic/tagion/basic/TagionExceptions.d
@@ -13,7 +13,7 @@ interface TagionExceptionInterface {
 @safe
 class TagionException : Exception, TagionExceptionInterface {
     string task_name; /// Contains the name of the task when the execption has throw
-    this(string msg, string file = __FILE__, size_t line = __LINE__ ) pure {
+    this(string msg, string file = __FILE__, size_t line = __LINE__ ) pure nothrow {
         super( msg, file, line );
     }
 
