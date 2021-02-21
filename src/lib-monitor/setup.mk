0b6211606519aa34941cae4f895446238388043e
cbleser <cr@tagion.org>
2021-02-21 11:14:29 +0200

    Makefile updated

--- src/lib-monitor/setup.mk
+++ src/lib-monitor/setup.mk
@@ -7,10 +7,11 @@ LIBS+=${call GETLIB,tagion_basic}
 LIBS+=${call GETLIB,tagion_utils}
 LIBS+=${call GETLIB,tagion_hibon}
 LIBS+=${call GETLIB,tagion_hashgraph}
-IBS+=${call GETLIB,tagion_gossip}
-IBS+=${call GETLIB,tagion_network}
-#LIBS+=${call GETLIB,tagion_crypto}
-#LIBS+=${call GETLIB,tagion_dart}
+LIBS+=${call GETLIB,tagion_gossip}
+LIBS+=${call GETLIB,tagion_network}
+LIBS+=${call GETLIB,tagion_crypto}
+LIBS+=${call GETLIB,tagion_dart}
+LIBS+=${call GETLIB,tagion_communication}
 
 LIBS+=$(LIBSECP256K1)
 LIBS+=$(LIBP2P)
@@ -18,3 +19,4 @@ LIBS+=$(LIBP2P_GO)
 
 LDCFLAGS+=$(LDCFLAGS_GMP)
 LDCFLAGS+=$(LDCFLAGS_CRYPT)
+LDCFLAGS+=$(LDCFLAGS_SSL)
