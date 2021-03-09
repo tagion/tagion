4e27fabaad2f3ffac84005544fd335c1f4e10589
alex <sgoleksandr@gmail.com>
2021-03-09 14:15:42 +0200

    Add tagionwallet basic func

---
+++ src/lib-wallet/tagion/wallet/Wallet.d
@@ -0,0 +1,213 @@
+module tagion_wallet.tagion.wallet.Wallet;
+
+import std.format;
+import std.string: representation;
+import std.algorithm : map, max, min;
+import std.array;
+import std.exception : assumeUnique;
+import core.time : MonoTime;
+
+import tagion.hibon.HiBON : HiBON;
+import tagion.hibon.Document : Document;
+import tagion.hibon.HiBONRecord;
+import tagion.hibon.HiBONJSON;
+
+import tagion.basic.Basic : basename, Buffer, Pubkey;
+import tagion.script.StandardRecords;
+import tagion.crypto.SecureNet : StdSecureNet, StdHashNet, scramble;
+// import tagion.gossip.GossipNet : StdSecureNet, StdHashNet, scramble;
+import tagion.wallet.KeyRecover;
+import tagion.basic.Message;
+import tagion.utils.Miscellaneous;
+import tagion.Keywords;
+import tagion_wallet.tagion.wallet.TagionCurrency;
+
+// @safe
+struct WalletWrapper {
+    protected Wallet _wallet;
+    protected Buffer[Pubkey] _account;
+    protected StandardBill[] _bills;
+    protected StdSecureNet net;
+
+    Buffer drive_state;
+
+    this(Wallet wallet, Buffer[Pubkey] account = null, Buffer drive_state = null, StandardBill[] bills = null){
+        _wallet = wallet;
+        _account = account;
+        _bills = bills;
+        this.drive_state = drive_state;
+    }
+
+    static WalletWrapper createWallet(const(string[]) questions, const(string[]) answers, uint confidence, Buffer pincode)
+    in{
+        assert(questions.length >3, "Minimal amount of answers is 3");
+        assert(questions.length is answers.length, "Amount of questions should be same as answers");
+    }
+    do{
+        auto hashnet = new StdHashNet;
+        auto recover = KeyRecover(hashnet);
+
+        if (confidence == questions.length) {
+            // Due to some bug in KeyRecover
+            confidence--;
+        }
+
+        recover.createKey(questions, answers, confidence);
+        StdSecureNet net; 
+        Wallet wallet;
+        {
+            net=new StdSecureNet;
+            auto R=new ubyte[net.hashSize];
+
+            recover.findSecret(R, questions, answers);
+            //writefln("R=0x%s", R.idup.hex);
+            // import std.string : representation;
+            auto pinhash=recover.checkHash(pincode);
+            //writefln("R.length=%d pinhash.length=%d", R.length, pinhash.length);
+            wallet.Y=xor(R, pinhash);
+            wallet.check=recover.checkHash(R);
+            net.createKeyPair(R);
+            wallet.pubkey=net.pubkey;
+            const seed_data=recover.toHiBON.serialize;
+            const seed_doc=Document(seed_data);
+            wallet.seed=KeyRecover.RecoverSeed(seed_doc);
+        }
+        return WalletWrapper(wallet);
+    }
+
+    bool isLogedin(){
+        return net !is null;
+    }
+    
+    void checkLogin() { assert(isLogedin(), "Need login first"); }
+
+    bool login(string pincode){
+        auto hashnet=new StdHashNet;
+        auto recover=new KeyRecover(hashnet);
+        auto pinhash=recover.checkHash(pincode.representation);
+        auto R=new ubyte[hashnet.hashSize];
+        xor(R, _wallet.Y, pinhash);
+        if (_wallet.check == recover.checkHash(R)) {
+            auto net=new StdSecureNet;
+            net.createKeyPair(R);
+            return true;
+        }
+        return false;
+    }
+
+    ulong total() nothrow{
+        return WalletWrapper.calcTotal(_bills);
+    }
+
+    void registerInvoice(ref Invoice invoice) 
+    in{
+        checkLogin;
+    }
+    do{
+        string current_time=MonoTime.currTime.toString;
+        scope seed=new ubyte[net.hashSize];
+        scramble(seed);
+        drive_state=net.calcHash(seed~drive_state~current_time.representation);
+        scramble(seed);
+        const pkey=net.derivePubkey(drive_state);
+        invoice.pkey=cast(Buffer)pkey;
+        _account[pkey]=drive_state;
+    }
+
+    static Invoice createInvoice(string label, TagionCurency tagions = TagionCurency.init){
+        Invoice new_invoice;
+        new_invoice.name=label;
+        if(tagions==TagionCurency.init){
+            new_invoice.amount=tagions.axios();
+        }
+        return new_invoice;
+    }   
+
+    bool payment(const(Invoice[]) orders, ref SignedContract result) 
+    in{
+        checkLogin;
+    } 
+    do{
+        ulong calcTotal(const(Invoice[]) invoices) {
+            ulong result;
+            foreach(b; invoices) {
+                result+=b.amount;
+            }
+            return result;
+        }
+        const topay=calcTotal(orders);
+
+        StandardBill[] contract_bills;
+        if (topay > 0) {
+            string source;
+            uint count;
+            foreach(o; orders) {
+                source=assumeUnique(format("%s %s", o.amount, source));
+                count++;
+            }
+
+            // Input
+            ulong amount=topay;
+
+
+            foreach(b; _bills) {
+                amount-=min(amount, b.value);
+                contract_bills~=b;
+                if (amount == 0) {
+                    break;
+                }
+            }
+            if(amount != 0){
+                return false;
+            }
+//        result.input=contract_bills; // Input _bills
+//        Buffer[] inputs;
+            foreach(b; contract_bills) {
+                result.contract.input~=net.calcHash(b.toHiBON.serialize);
+            }
+            const _total_input=WalletWrapper.calcTotal(contract_bills);
+            if ( _total_input >= topay) {
+                const _rest=_total_input-topay;
+                count++;
+                result.contract.script=assumeUnique(format("%s %s %d pay", source, _rest, count));
+                // output
+                Invoice money_back;
+                money_back.amount=_rest;
+                registerInvoice(money_back);
+                result.contract.output~=money_back.pkey;
+                foreach(o; orders) {
+                    result.contract.output~=o.pkey;
+                }
+            }
+            else {
+                return false;
+            }
+        }
+
+        // Sign all inputs
+        immutable message=net.calcHash(result.contract.toHiBON.serialize);
+        shared shared_net=cast(shared)net;
+        foreach(i, b; contract_bills) {
+            Pubkey pkey=b.owner;
+            if (pkey in _account) {
+                immutable tweak_code=_account[pkey];
+                auto bill_net=new StdSecureNet;
+                bill_net.derive(tweak_code, shared_net);
+                immutable signature=bill_net.sign(message);
+                result.signs~=signature;
+            }
+        }
+
+        return true;
+    }
+
+    static protected{
+        ulong calcTotal(const(StandardBill[]) bills) nothrow{
+            ulong result;
+            foreach(b; bills) {
+                result+=b.value;
+            }
+            return result;
+        }
+    }
+}
