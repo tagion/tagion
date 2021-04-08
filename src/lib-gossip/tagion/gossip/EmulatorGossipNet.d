7fdfd4411eb63462449a391f988730b096c82a50
alex <sgoleksandr@gmail.com>
2021-04-08 15:28:01 +0300

    Change random

--- src/lib-gossip/tagion/gossip/EmulatorGossipNet.d
+++ src/lib-gossip/tagion/gossip/EmulatorGossipNet.d
@@ -11,7 +11,7 @@ import tagion.Options;
 import tagion.basic.Basic : EnumText, Buffer, Pubkey, buf_idup,  basename, isBufferType;
 //import tagion.TagionExceptions : convertEnum, consensusCheck, consensusCheckArguments;
 import tagion.utils.Miscellaneous: cutHex;
-import tagion.utils.Random;
+// import tagion.utils.Random;
 import tagion.utils.LRU;
 import tagion.utils.Queue;
 //import tagion.Keywords;
@@ -31,6 +31,7 @@ import tagion.utils.StdTime;
 import tagion.communication.HiRPC;
 import tagion.crypto.secp256k1.NativeSecp256k1;
 import core.atomic;
+import std.random: Random, unpredictableSeed, uniform;
 
 @trusted
 static uint getTids(Tid[] tids) {
@@ -62,7 +63,10 @@ class EmulatorGossipNet : GossipNet {
     protected uint _send_node_id;
     protected sdt_t _current_time;
 
-    Random!uint random;
+    Random random;
+    this(){
+        this.random = Random(unpredictableSeed);
+    }
 
     void add_channel(const Pubkey channel) {
         _pkeys~=channel;
@@ -100,11 +104,13 @@ class EmulatorGossipNet : GossipNet {
     const(Pubkey) select_channel(ChannelFilter channel_filter) {
         import std.range : dropExactly;
         foreach(count; 0.._tids.length/2) {
-            const node_index=random.value(0, cast(uint)_tids.length);
+            const node_index=uniform(0, cast(uint)_tids.length, random);
+            // log("selected index: %d %d", node_index, _tids.length);
             const send_channel = _tids
                 .byKey
                 .dropExactly(node_index)
                 .front;
+            // log("trying to select: %s, valid?: %s", send_channel.cutHex, channel_filter(send_channel));
             if (channel_filter(send_channel)) {
                 return send_channel;
             }
