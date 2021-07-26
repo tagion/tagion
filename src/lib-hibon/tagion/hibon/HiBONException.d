57e06d087184b9e164742356f804b2a2a85130bd
cbleser <cr@tagion.org>
2021-07-26 15:26:04 +0300

    WIP: ./tagion/hibon/HiBONException.d formated with dfmt

--- src/lib-hibon/tagion/hibon/HiBONException.d
+++ src/lib-hibon/tagion/hibon/HiBONException.d
@@ -1,23 +1,21 @@
 module tagion.hibon.HiBONException;
 
-
 import tagion.basic.TagionExceptions;
+
 /++
  Exception type used by tagion.hibon.HiBON module
  +/
-@safe
-class HiBONException : TagionException {
-    this(string msg, string file = __FILE__, size_t line = __LINE__ ) pure {
-        super( msg, file, line );
+@safe class HiBONException : TagionException {
+    this(string msg, string file = __FILE__, size_t line = __LINE__) pure {
+        super(msg, file, line);
     }
 }
 
 /// check function used in the HiBON package
-alias check=Check!(HiBONException);
+alias check = Check!(HiBONException);
 
-@safe
-class HiBONRecordException : HiBONException {
-    this(string msg, string file = __FILE__, size_t line = __LINE__ ) pure {
-        super( msg, file, line );
+@safe class HiBONRecordException : HiBONException {
+    this(string msg, string file = __FILE__, size_t line = __LINE__) pure {
+        super(msg, file, line);
     }
 }
