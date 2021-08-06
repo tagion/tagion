a8dbf2a9707b73f2016a76f3060cbea7a91ba889
Vladislav Pazych <vladpazych@gmail.com>
2021-08-06 18:41:41 +0300

    Improve logs

--- tub/meta.mk
+++ tub/meta.mk
@@ -1,2 +1,5 @@
 meta/%:
+	${call log.header}
 	$(PRECMD)$(CP) $(DIR_TAGIL)/metas/$(@F).meta $(DIR_TAGIL_ROOT)/.meta
+	${call log.line, Initialized $(@F) .meta file at $(DIR_TAGIL_ROOT)/.meta}
+	${call log.close}
