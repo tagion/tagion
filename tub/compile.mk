b203a13dbdad63b945e9a2b0be290a28c373616c
Vlad Pazych <vladpazych@gmail.com>
2021-08-13 14:26:03 +0300

    Move add targets to separate file

--- tub/compile.mk
+++ tub/compile.mk
@@ -97,27 +97,6 @@ ways:
 	@$(MKDIR) -p $(DIR_BUILD)/tests
 
 # 
-# Source code cloning
-# 
-add/lib/%: $(DIR_LIBS)/%/context.mk
-	@
-
-$(DIR_LIBS)/%/context.mk:
-	$(PRECMD)git clone $(GIT_ORIGIN)/core-lib-$(*) $(DIR_LIBS)/$(*)	
-
-add/bin/%: $(DIR_BINS)/%/context.mk
-	@
-
-$(DIR_BINS)/%/context.mk:	
-	$(PRECMD)git clone $(GIT_ORIGIN)/core-bin-$(*) $(DIR_BINS)/$(*)
-
-add/wrap/%: $(DIR_WRAPS)/%/Makefile
-	@
-
-$(DIR_WRAPS)/%/Makefile:
-	$(PRECMD)git clone $(GIT_ORIGIN)/core-wrap-$(*) $(DIR_WRAPS)/$(*)
-
-# 
 # Compile targets to use
 # 
 bin/%: env-compiler ways ctx/bin/%
