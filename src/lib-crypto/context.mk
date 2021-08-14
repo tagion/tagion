3f799db303d239b3ae810ca91cf2245f25d8b108
Vlad Pazych <vladpazych@gmail.com>
2021-08-14 09:56:54 +0300

    Add tiny_aes support

--- src/lib-crypto/context.mk
+++ src/lib-crypto/context.mk
@@ -1,1 +1,8 @@
-ctx/lib/crypto: ctx/lib/hibon ctx/wrap/secp256k1 ctx/wrap/openssl
+ifndef TINY_AES
+SOURCEFLAGS+=-a -not -path "*/tiny_aes/*"
+else
+SOURCEFLAGS+=-a -not -path "*/openssl_aes/*"
+DCFLAGS+=$(DVERSION)=TINY_AES
+endif
+
+ctx/lib/crypto: ctx/lib/hibon ctx/wrap/secp256k1
