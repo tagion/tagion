eba94312a3b5c7f7c764f2d6e560aca5e0c28dcb
Vlad Pazych <vladpazych@gmail.com>
2021-08-17 15:22:21 +0300

    Simplify blueprint flow

--- tub/tub/blueprint.mk
+++ tub/tub/blueprint.mk
@@ -1,12 +1,11 @@
-sources/core: add/lib/basic add/lib/utils add/lib/hibon add/lib/p2p add/lib/crypto add/lib/dart add/lib/funnel add/lib/gossip add/lib/hashgraph add/lib/network add/lib/services add/lib/wallet add/lib/wasm add/lib/communication add/lib/monitor add/bin/node
+sources/core: add/lib/basic add/lib/utils add/lib/hibon add/lib/p2p-go-wrapper add/lib/crypto add/lib/dart add/lib/funnel add/lib/gossip add/lib/hashgraph add/lib/network add/lib/services add/lib/wallet add/lib/wasm add/lib/communication add/lib/monitor add/bin/node
 	@
 
-sources/core: add/lib/basic add/lib/utils add/lib/hibon add/lib/p2p-go-wrapper
+sources/public: add/lib/basic add/lib/utils add/lib/hibon add/lib/p2p-go-wrapper
 	@
 
-blueprint/core: add/wrap/secp256k1 add/wrap/openssl add/wrap/p2p-go-wrapper sources/core env/dependencies
-	$(PRECMD)meta git update
-	$(PRECMD)meta git checkout alpha
+blueprint/core: add/wrap/secp256k1 add/wrap/openssl add/wrap/p2p-go-wrapper sources/core
+	$(PRECMD)./git checkout alpha
 
-blueprint/public: add/wrap/p2p-go-wrapper env/dependencies
-	$(PRECMD)meta git update
+blueprint/public: add/wrap/p2p-go-wrapper
+	$(PRECMD)./git update
