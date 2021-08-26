276601f0d784a398c21ad0b4215ba9e87a0572dd
Vlad Pazych <vladpazych@gmail.com>
2021-08-26 13:31:42 +0300

    Fix compilation

--- src/wrap-secp256k1/context.mk
+++ src/wrap-secp256k1/context.mk
@@ -3,18 +3,21 @@ DIR_SECP256K1_SRC := ${call dir.self, secp256k1}
 DIR_SECP256K1_BUILD := $(DIR_BUILD)/wraps/secp256k1
 
 WAYS += $(DIR_SECP256K1_BUILD)/.way
-wrap/secp256k1: | ways wrap/openssl $(DIR_SECP256K1_BUILD)/libsecp256k1.a
+WAYS += $(DIR_SECP256K1_BUILD)/lib/.way
+WAYS += $(DIR_SECP256K1_SRC)/.libs/.way
+wrap/secp256k1: | ways wrap/openssl $(DIR_SECP256K1_BUILD)/lib/libsecp256k1.a
 	${eval WRAPS += secp256k1}
 	${eval WRAPS_STATIC += $(DIR_SECP256K1_BUILD)/libsecp256k1.a}
 
-$(DIR_SECP256K1_BUILD)/libsecp256k1.a: $(DIR_SECP256K1_SRC)/.libs/libsecp256k1.a
-	$(PRECMD)cp $(DIR_SECP256K1_SRC)/.libs/libsecp256k1.a $(DIR_SECP256K1_BUILD)/libsecp256k1.a
+$(DIR_SECP256K1_BUILD)/lib/libsecp256k1.a: $(DIR_SECP256K1_SRC)/.libs/libsecp256k1.a
+	$(PRECMD)cp $(DIR_SECP256K1_SRC)/.libs/libsecp256k1.a $(DIR_SECP256K1_BUILD)/lib/libsecp256k1.a
 
 $(DIR_SECP256K1_SRC)/.libs/libsecp256k1.a: $(DIR_SECP256K1_SRC)/Makefile
-	$(PRECMD)cd $(DIR_SECP256K1_SRC); make clean && make
+	$(PRECMD)cd $(DIR_SECP256K1_SRC); make clean
+	$(PRECMD)cd $(DIR_SECP256K1_SRC); make
 
 $(DIR_SECP256K1_SRC)/Makefile: $(DIR_SECP256K1_SRC)/configure
-	$(PRECMD)cd $(DIR_SECP256K1_SRC); ./configure
+	$(PRECMD)cd $(DIR_SECP256K1_SRC); ./configure CRYPTO_LIBS=$(DIR_BUILD_OPENSSL)/lib/ CRYPTO_CFLAGS=$(DIR_BUILD_OPENSSL)/include/
 
 $(DIR_SECP256K1_SRC)/configure: $(DIR_SECP256K1_SRC)/autogen.sh
 	$(PRECMD)cd $(DIR_SECP256K1_SRC); ./autogen.sh
