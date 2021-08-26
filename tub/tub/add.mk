74b3864dadcca355ac3fe59357ff42b8493e44ef
Vlad Pazych <vladpazych@gmail.com>
2021-08-26 11:13:07 +0300

    Simplify blueprints

--- tub/tub/add.mk
+++ tub/tub/add.mk
@@ -1,20 +1,43 @@
 # 
 # Source code cloning
 # 
-add/lib/%: $(DIR_LIBS)/%/context.mk
+add/%: $(DIR_SRC)/%/context.mk
 	@
 
-$(DIR_LIBS)/%/context.mk:
-	$(PRECMD)git clone $(GIT_ORIGIN)/core-lib-$(*) $(DIR_LIBS)/$(*)	
+$(DIR_SRC)/%/context.mk:
+	$(PRECMD)git clone $(GIT_ORIGIN)/core-$(*) $(DIR_SRC)/$(*)	
 
-add/bin/%: $(DIR_BINS)/%/context.mk
+# 
+# Blueprints
+# 
+_sources/core: add/lib-basic\
+			  add/lib-utils\
+			  add/lib-hibon\
+			  add/lib-p2p-go-wrapper\
+			  add/lib-crypto\
+			  add/lib-dart\
+			  add/lib-funnel\
+			  add/lib-gossip\
+			  add/lib-hashgraph\
+			  add/lib-network\
+			  add/lib-services\
+			  add/lib-wallet\
+			  add/lib-wasm\
+			  add/lib-communication\
+			  add/lib-monitor\
+			  add/lib-logger\
+			  add/lib-options\
+			  add/bin-node
 	@
 
-$(DIR_BINS)/%/context.mk:	
-	$(PRECMD)git clone $(GIT_ORIGIN)/core-bin-$(*) $(DIR_BINS)/$(*)
-
-add/wrap/%: $(DIR_WRAPS)/%/Makefile
+_sources/public: add/lib-basic\
+				add/lib-utils\
+				add/lib-hibon\
+				add/lib-p2p-go-wrapper
 	@
 
-$(DIR_WRAPS)/%/Makefile:
-	$(PRECMD)git clone $(GIT_ORIGIN)/core-wrap-$(*) $(DIR_WRAPS)/$(*)
+add/core: add/wrap-secp256k1 add/wrap-openssl add/wrap-p2p-go-wrapper _sources/core
+	$(PRECMD)./tub git checkout alpha
+
+add/public: add/wrap-p2p-go-wrapper _sources/public
+	$(PRECMD)./tub git update
