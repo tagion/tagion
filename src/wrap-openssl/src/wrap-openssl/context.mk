2bd8565c9e541477f74187f889501daf55d2b7c3
Vlad Pazych <vladpazych@gmail.com>
2021-08-26 13:31:44 +0300

    Fix compilation

--- src/wrap-openssl/src/wrap-openssl/context.mk
+++ src/wrap-openssl/src/wrap-openssl/context.mk
@@ -1,26 +1,20 @@
 REPO_OPENSSL ?= https://github.com/openssl/openssl.git
 BRANCH_OPENSSL_STABLE := OpenSSL_1_1_1-stable
 DIR_OPENSSL_SRC := ${call dir.self, openssl}
-DIR_INSTALL_OPENSSL := ${call dir.self, local}/$(ARCH)/openssl
-DIR_INSTALL_OPENSSL_EXTRA := ${call dir.self, local}/$(ARCH)/openssl-extra
 DIR_BUILD_OPENSSL := $(DIR_BUILD)/wraps/openssl
+DIR_BUILD_OPENSSL_EXTRA := $(DIR_BUILD)/wraps/openssl/extra
 
-WAYS += $(DIR_INSTALL_OPENSSL)/.way 
-WAYS += $(DIR_BUILD_OPENSSL)/.way
+WAYS += $(DIR_BUILD_OPENSSL)/.way 
+WAYS += $(DIR_BUILD_OPENSSL_EXTRA)/.way
 
-wrap/openssl: | ways ${DIR_BUILD_OPENSSL}/libcrypto.a $(DIR_BUILD_OPENSSL)/libssl.a
+wrap/openssl: | ways $(DIR_BUILD_OPENSSL)/lib/libcrypto.a
 	${eval WRAPS += opensssl}
 	${eval LDCFLAGS += -L-L$(DIR_OPENSSL_SRC)/build/openssl/lib/}
-	${eval export LD_LIBRARY_PATH=$(DIR_INSTALL_OPENSSL)/lib/:$(LD_LIBRARY_PATH)}
+	$(PRECMD)export LD_LIBRARY_PATH=$(DIR_BUILD_OPENSSL)/lib/:$(LD_LIBRARY_PATH)
+	$(PRECMD)export DYLD_LIBRARY_PATH=$(DIR_BUILD_OPENSSL)/lib/:$(DYLD_LIBRARY_PATH)
 
-$(DIR_BUILD_OPENSSL)/libcrypto.a: $(DIR_INSTALL_OPENSSL)/lib/libcrypto.a
-	$(PRECMD)cp $(DIR_INSTALL_OPENSSL)/lib/libcrypto.a $(DIR_BUILD_OPENSSL)
-
-$(DIR_BUILD_OPENSSL)/libssl.a: $(DIR_INSTALL_OPENSSL)/lib/libcrypto.a
-	$(PRECMD)cp $(DIR_INSTALL_OPENSSL)/lib/libssl.a $(DIR_BUILD_OPENSSL)
-
-$(DIR_INSTALL_OPENSSL)/lib/libcrypto.a: $(DIR_OPENSSL_SRC)/config
-	$(PRECMD)cd $(DIR_OPENSSL_SRC); ./config --shared --prefix=$(DIR_INSTALL_OPENSSL) --openssldir=$(DIR_INSTALL_OPENSSL_EXTRA)
+$(DIR_BUILD_OPENSSL)/lib/libcrypto.a: $(DIR_OPENSSL_SRC)/config
+	$(PRECMD)cd $(DIR_OPENSSL_SRC); ./config --shared --prefix=$(DIR_BUILD_OPENSSL) --openssldir=$(DIR_BUILD_OPENSSL_EXTRA)
 	${eval PARALLEL := ${shell [[ "$(MAKEFLAGS)" =~ "jobserver-fds" ]] && echo 1}}
 	${if $(PARALLEL), PARALLEL :=, ${eval PARALLEL := -j8}}
 	$(PRECMD)cd $(DIR_OPENSSL_SRC); make $(PARALLEL)
