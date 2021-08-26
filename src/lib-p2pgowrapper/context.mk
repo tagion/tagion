6e17d4d27a4136268b26d312300d8bc609fbd3ba
Vlad Pazych <vladpazych@gmail.com>
2021-08-26 15:14:56 +0300

    Add support for the new tub

--- src/lib-p2pgowrapper/context.mk
+++ src/lib-p2pgowrapper/context.mk
@@ -1,6 +1,8 @@
-# TODO: Describe the reason behind this flags
 ifeq ($(OS),Darwin)
+# We need these flags on Darwin (possibly only on arm64 macOS)
+# because when libp2p-go compiles, it can't find required dependencies.
+# This solution was found on StackOverflow
 LDCFLAGS += -L-framework -LCoreFoundation -L-framework -LSecurity
 endif
 
-ctx/lib/p2p-go-wrapper: ctx/wrap/p2p-go-wrapper
+p2p-go-wrapper.ctx: wrap/p2p-go-wrapper
