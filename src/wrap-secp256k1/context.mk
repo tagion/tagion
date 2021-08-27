e94661fcc90769fb1032f1ce80faf8cbe9d317c8
Vlad Pazych <vladpazych@gmail.com>
2021-08-27 15:51:21 +0300

    Fix dependency resolvement for parallel builds

--- src/wrap-secp256k1/context.mk
+++ src/wrap-secp256k1/context.mk
@@ -6,6 +6,7 @@ WAYS_PERSISTENT += $(DIR_SECP256K1_BUILD)/.way
 WAYS_PERSISTENT += $(DIR_SECP256K1_BUILD)/lib/.way
 WAYS_PERSISTENT += $(DIR_SECP256K1_SRC)/.libs/.way
 
+.PHONY: wrap-secp256k1
 wrap-secp256k1: | ways wrap-openssl $(DIR_SECP256K1_BUILD)/lib/libsecp256k1.a
 	${eval WRAPS += secp256k1}
 	${eval WRAPS_STATIC += $(DIR_SECP256K1_BUILD)/lib/libsecp256k1.a}
