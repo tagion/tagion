0c31e526d55cd1c6b436bcfb158fd9b7d258395a
Vlad Pazych <vladpazych@gmail.com>
2021-08-27 15:51:20 +0300

    Fix dependency resolvement for parallel builds

--- src/lib-monitor/context.mk
+++ src/lib-monitor/context.mk
@@ -1,1 +1,2 @@
-monitor.ctx: basic.o hashgraph.o hibon.o crypto.o communication.o gossip.o dart.o network.o funnel.o p2p-go-wrapper.o
+libtagionmonitor.ctx: libtagionbasic.o libtagionhashgraph.o libtagionhibon.o libtagioncrypto.o libtagioncommunication.o libtagiongossip.o libtagiondart.o libtagionnetwork.o libtagionfunnel.o libtagionp2p-go-wrapper.o
+	@
