d710c61027d009a6c893dc97b4f13cc14a0bfb44
Vlad Pazych <vladpazych@gmail.com>
2021-09-27 15:26:31 +0300

    Improve logging

--- tub/tub/add.mk
+++ tub/tub/add.mk
@@ -7,7 +7,9 @@ add-%: $(DIR_SRC)/%/context.mk
 # TODO: Add branch name
 # TODO: In isolated mode clone only --depth
 $(DIR_SRC)/%/context.mk:
-	$(PRECMD)git clone $(GIT_ORIGIN)/core-$(*) $(DIR_SRC)/$(*)	
+	${call log.header, Adding $(*)...}
+	$(PRECMD)git clone $(GIT_ORIGIN)/core-$(*) $(DIR_SRC)/$(*) || true
+	${call log.close}
 
 add-core: ${addprefix add-, $(UNIT_LIST)}
 	@
