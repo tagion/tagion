d708d06b03326df131a92f2e66510569c92ebea2
cbleser <cr@tagion.org>
2021-09-30 08:41:54 +0300

    Module dependency fix

--- src/lib-network/context.mk
+++ src/lib-network/context.mk
@@ -1,2 +1,2 @@
-libtagionnetwork.ctx: libtagionhibon.o libtagioncommunication.o libtagionservices.o libtagionhashgraph.o libtagionp2pgowrapper.o
+libtagionnetwork.ctx: libtagionhibon.o libtagioncommunication.o libtagionhashgraph.o libtagionp2pgowrapper.o
 	@
