c7a22139e2f95ecb852dfa9d55d1d764bfbb8f95
cbleser <cr@tagion.org>
2021-09-30 08:47:22 +0300

    Module dependency fix

--- src/bin-wave/src/bin-wave/context.mk
+++ src/bin-wave/src/bin-wave/context.mk
@@ -1,2 +1,2 @@
-tagionwave.ctx: libtagioncrypto.o libtagionp2pgowrapper.o libtagiongossip.o
+tagionwave.ctx: libtagioncrypto.o libtagionp2pgowrapper.o libtagiongossip.o libtagionservices.o libtagiontvm.o
 	@
