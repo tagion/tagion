5f91790a74aab08d3f9a5bd3a181e536f872976e
cbleser <cr@tagion.org>
2021-09-30 12:54:57 +0300

    tagionwave now depends on libtagionfoundation.o

--- src/bin-wave/context.mk
+++ src/bin-wave/context.mk
@@ -1,2 +1,2 @@
-tagionwave.ctx: libtagioncrypto.o libtagionp2pgowrapper.o libtagiongossip.o libtagionservices.o libtagiontvm.o
+tagionwave.ctx: libtagioncrypto.o libtagionp2pgowrapper.o libtagiongossip.o libtagionservices.o libtagiontvm.o libtagionfoundation.o
 	@
