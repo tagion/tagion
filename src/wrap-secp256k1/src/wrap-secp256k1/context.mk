0c62ce253d66b020e23b8be43d1c9e73b77d4e18
Vlad Pazych <vladpazych@gmail.com>
2021-10-13 12:02:42 +0300

    wip: Add hard-commit

--- src/wrap-secp256k1/src/wrap-secp256k1/context.mk
+++ src/wrap-secp256k1/src/wrap-secp256k1/context.mk
@@ -1,27 +1,28 @@
-REPO_SECP256K1 ?= https://github.com/bitcoin-core/secp256k1.git
-DIR_SECP256K1_SRC := ${call dir.self, secp256k1}
-DIR_SECP256K1_BUILD := $(DIR_BUILD)/wraps/secp256k1
+REPO_SECP256K1 ?= git@github.com:tagion/fork-secp256k1.git
+VERSION_SECP256k1 := cd07f5a72243a8f343679aa81ed0d0cb662ba90e
 
-WAYS_PERSISTENT += $(DIR_SECP256K1_BUILD)/.way
-WAYS_PERSISTENT += $(DIR_SECP256K1_BUILD)/lib/.way
+DIR_SECP256K1 := $(DIR_BUILD_WRAPS)/secp256k1
 
-.PHONY: wrap-secp256k1
-wrap-secp256k1: | ways wrap-openssl $(DIR_SECP256K1_BUILD)/lib/libsecp256k1.a
-	${eval WRAPS += secp256k1}
-	${eval WRAPS_STATIC += $(DIR_SECP256K1_BUILD)/lib/libsecp256k1.a}
+DIR_SECP256K1_PREFIX := $(DIR_SECP256K1)/src/.libs
+DIR_SECP256K1_SRC := $(DIR_SECP256K1)/src
 
-$(DIR_SECP256K1_BUILD)/lib/libsecp256k1.a: $(DIR_SECP256K1_SRC)/.libs/libsecp256k1.a
-	$(PRECMD)cp $(DIR_SECP256K1_SRC)/.libs/libsecp256k1.a $(DIR_SECP256K1_BUILD)/lib/libsecp256k1.a
+define unit.dep.wrap-secp256k1
+${eval UNIT_WRAPS_TARGETS += wrap-secp256k1}
+${eval UNIT_WRAPS_LINKFILES += $(DIR_SECP256K1_PREFIX)/libsecp256k1.a}
+${if ${findstring openssl, $(WRAPS_DEFINED)},,${eval include $(DIR_SRC)/wrap-openssl/context.mk}}
+endef
 
-$(DIR_SECP256K1_SRC)/.libs/libsecp256k1.a: $(DIR_SECP256K1_SRC)/Makefile
-	$(PRECMD)cd $(DIR_SECP256K1_SRC); make clean
-	$(PRECMD)cd $(DIR_SECP256K1_SRC); make
+wrap-secp256k1: $(DIR_SECP256K1_PREFIX)/libsecp256k1.a
+	@
 
-$(DIR_SECP256K1_SRC)/Makefile: $(DIR_SECP256K1_SRC)/configure
-	$(PRECMD)cd $(DIR_SECP256K1_SRC); ./configure CRYPTO_LIBS=$(DIR_BUILD_OPENSSL)/lib/ CRYPTO_CFLAGS=$(DIR_BUILD_OPENSSL)/include/
+clean-wrap-secp256k1:
+	${call unit.dep.wrap-secp256k1}
+	${call rm.dir, $(DIR_SECP256K1_SRC)}
 
-$(DIR_SECP256K1_SRC)/configure: $(DIR_SECP256K1_SRC)/autogen.sh
+$(DIR_SECP256K1_PREFIX)/%.a: wrap-openssl $(DIR_SECP256K1)/.way
+	$(PRECMD)git clone --depth 1 $(REPO_SECP256K1) $(DIR_SECP256K1_SRC) 2> /dev/null || true
+	$(PRECMD)git -C $(DIR_SECP256K1_SRC) fetch --depth 1 $(DIR_SECP256K1_SRC) $(VERSION_SECP256k1) &> /dev/null || true
 	$(PRECMD)cd $(DIR_SECP256K1_SRC); ./autogen.sh
-
-$(DIR_SECP256K1_SRC)/autogen.sh:
-	$(PRECMD)git -C $(DIR_SECP256K1_SRC) pull || git clone --depth 1 $(REPO_SECP256K1) $(DIR_SECP256K1_SRC)
+	$(PRECMD)cd $(DIR_SECP256K1_SRC); ./configure CRYPTO_LIBS=$(DIR_OPENSSL)/lib/ CRYPTO_CFLAGS=$(DIR_OPENSSL)/include/
+	$(PRECMD)cd $(DIR_SECP256K1_SRC); make clean
+	$(PRECMD)cd $(DIR_SECP256K1_SRC); make $(MAKE_PARALLEL)
