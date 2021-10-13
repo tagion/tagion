91ce288b454a97d06956592f5508a755f05b2a8a
Vlad Pazych <vladpazych@gmail.com>
2021-10-13 12:02:54 +0300

    wip: Add hard-commit

--- src/wrap-p2pgowrapper/src/wrap-p2pgowrapper/context.mk
+++ src/wrap-p2pgowrapper/src/wrap-p2pgowrapper/context.mk
@@ -1,29 +1,28 @@
-DIR_P2P_SRC := ${call dir.self, src}
-DIR_P2P_CGO := ${call dir.self, cgo}
-DIR_P2P_BUILD := $(DIR_BUILD)/wraps/p2p-go-wrapper
+REPO_P2PGOWRAPPER ?= git@github.com:tagion/p2pgowrapper.git
+VERSION_P2PGOWRAPPER := dfc77c04e0395e09652c0daa25f9f19b77c2b1d6
 
-WAYS_PERSISTENT += $(DIR_P2P_BUILD)/lib/.way
-WAYS_PERSISTENT += $(DIR_P2P_BUILD)/include/.way
-WAYS_PERSISTENT += $(DIR_P2P_CGO)/.way
+DIR_P2PGOWRAPPER := $(DIR_BUILD_WRAPS)/p2pgowrapper
 
-.PHONY: wrap-p2p-go-wrapper
-wrap-p2p-go-wrapper: $(DIR_P2P_BUILD)/lib/libp2p-go-wrapper.a $(DIR_P2P_BUILD)/include/libp2p.di
-	${eval WRAPS += p2p-go-wrapper}
-	${eval WRAPS_STATIC += $(DIR_P2P_BUILD)/lib/libp2p-go-wrapper.a}
-	${eval WRAPS_HEADERS += $(DIR_P2P_BUILD)/include/helper.di}
-	${eval WRAPS_HEADERS += $(DIR_P2P_BUILD)/include/libp2p.di}
+DIR_P2PGOWRAPPER_PREFIX := $(DIR_P2PGOWRAPPER)/lib
+DIR_P2PGOWRAPPER_SRC := $(DIR_P2PGOWRAPPER)/src
 
-$(DIR_P2P_BUILD)/lib/libp2p-go-wrapper.a: | ways $(DIR_P2P_CGO)/libp2p-go-wrapper.a
-	$(PRECMD)cp -r $(DIR_P2P_CGO)/*.a $(DIR_P2P_BUILD)/lib
+define unit.dep.wrap-p2pgowrapper
+${eval UNIT_WRAPS_TARGETS += wrap-p2pgowrapper}
+${eval UNIT_WRAPS_INCFLAGS += -I$(DIR_P2PGOWRAPPER_PREFIX)}
+${eval UNIT_WRAPS_LINKFILES += $(DIR_P2PGOWRAPPER_PREFIX)/libp2pgowrapper.a}
+endef
 
-$(DIR_P2P_BUILD)/include/libp2p.di: | ways $(DIR_P2P_CGO)/libp2p.di
-	$(PRECMD)cp -r $(DIR_P2P_CGO)/*.di $(DIR_P2P_BUILD)/include
+wrap-p2pgowrapper: $(DIR_P2PGOWRAPPER_PREFIX)/p2p/cgo/libp2p.di $(DIR_P2PGOWRAPPER_PREFIX)/p2p/cgo/libp2pgowrapper.di $(DIR_P2PGOWRAPPER_PREFIX)/libp2pgowrapper.a
+	@
 
-$(DIR_P2P_CGO)/libp2p.di: $(DIR_P2P_CGO)/libp2p-go-wrapper.a
-	$(PRECMD)dstep $(DIR_P2P_CGO)/libp2p-go-wrapper.h -o $(DIR_P2P_CGO)/libp2p.di --package p2p.cgo --global-import p2p.cgo.helper
-	$(PRECMD)dstep $(DIR_P2P_CGO)/c_helper.h -o $(DIR_P2P_CGO)/helper.di --package p2p.cgo
+clean-wrap-p2pgowrapper:
+	${call unit.dep.wrap-p2pgowrapper}
+	${call rm.dir, $(DIR_BUILD_WRAPS)}
 
-$(DIR_P2P_CGO)/libp2p-go-wrapper.a:
-	$(PRECMD)mkdir -p $(DIR_P2P_CGO)
-	cp $(DIR_P2P_SRC)/c_helper.h $(DIR_P2P_CGO)
-	$(PRECMD)cd $(DIR_P2P_SRC); go build -buildmode=c-archive -o $(DIR_P2P_CGO)/libp2p-go-wrapper.a
+$(DIR_P2PGOWRAPPER_PREFIX)/%: $(DIR_P2PGOWRAPPER_PREFIX)/.way $(DIR_P2PGOWRAPPER_PREFIX)/p2p/cgo/.way
+	$(PRECMD)git clone --depth 1 $(REPO_P2PGOWRAPPER) $(DIR_P2PGOWRAPPER_SRC) 2> /dev/null || true
+	$(PRECMD)git -C $(DIR_P2PGOWRAPPER_SRC) fetch --depth 1 $(REPO_P2PGOWRAPPER) $(VERSION_P2PGOWRAPPER) &> /dev/null || true
+	$(PRECMD)cp $(DIR_P2PGOWRAPPER_SRC)/c_helper.h $(DIR_P2PGOWRAPPER_PREFIX)
+	$(PRECMD)cd $(DIR_P2PGOWRAPPER_SRC); go build -buildmode=c-archive -o $(DIR_P2PGOWRAPPER_PREFIX)/libp2pgowrapper.a
+	$(PRECMD)dstep $(DIR_P2PGOWRAPPER_PREFIX)/libp2pgowrapper.h -o $(DIR_P2PGOWRAPPER_PREFIX)/p2p/cgo/libp2p.di --package p2p.cgo --global-import p2p.cgo.helper
+	$(PRECMD)dstep $(DIR_P2PGOWRAPPER_SRC)/c_helper.h -o $(DIR_P2PGOWRAPPER_PREFIX)/p2p/cgo/helper.di --package p2p.cgo
