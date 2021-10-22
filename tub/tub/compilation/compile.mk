2229b5f3fdee53940d70d9cc89e295fbd5540adf
Vlad Pazych <vladpazych@gmail.com>
2021-10-22 09:22:40 +0300

    wip: Add automated dependency file generation

--- tub/tub/compilation/compile.mk
+++ tub/tub/compilation/compile.mk
@@ -4,11 +4,13 @@ INCLFLAGS := ${addprefix -I$(DIR_ROOT)/,${shell ls -d src/*/ | grep -v wrap- | g
 
 # ${eval ${call debug.lines, INCLFLAGS: $(INCLFLAGS)}}
 
+ifndef DEPS_UNRESOLVED
 tagion%: $(DIR_BUILD)/bins/tagion%
 	@
 
 libtagion%: $(DIR_BUILD_LIBS_STATIC)/libtagion%.a
 	@
+endif
 
 # 
 # Targets
@@ -16,12 +18,7 @@ libtagion%: $(DIR_BUILD_LIBS_STATIC)/libtagion%.a
 $(DIR_BUILD_LIBS_STATIC)/libtagion%.a: $(DIR_BUILD_LIBS_STATIC)/.way $(DIR_BUILD_O)/libtagion%.o
 	${call log.line, archive - $(@)}
 
-ifdef SOURCE_INCLUDED
-$(DIR_BUILD_O)/libtagion%.o: $(DIR_BUILD_O)/.way
-	@
-else
-$(DIR_BUILD_O)/libtagion%.o:
-	${call log.info, Calling recursive from $(@)}
-endif
+$(DIR_BUILD_O)/libtagion%.o: $(DIR_BUILD_O)/.way $(DIR_SRC)/lib-%/$(FILENAME_SOURCE_MK)
+	@echo o target
 
 ${eval ${call debug.close, MAKE COMPILE - $(MAKECMDGOALS)}}
--- tub/tub/compilation/resolve.mk
+++ tub/tub/compilation/resolve.mk
@@ -1,14 +1,14 @@
 ${eval ${call debug.open, MAKE RESOLVE - $(MAKECMDGOALS)}}
 
 ifeq ($(MAKELEVEL),0)
-${shell rm -f $(DIR_SRC)/**/$(FILENAME_SOURCE_MK)}
+${shell rm -f $(DIR_SRC)/**/$(FILENAME_SOURCE_MK) || true}
 endif
 
-${eval ${call debug, DEPS: $(DEPS)}}
+${call debug, DEPS: $(DEPS)}
 
 DEPS := ${sort $(DEPS)}
 ${foreach DEP,$(DEPS),\
-	${eval ${call debug, Including $(DEP) context...}}\
+	${call debug, Including $(DEP) context...}\
 	${eval include $(DIR_SRC)/$(DEP)/context.mk}\
 	${eval DEPS_RESOLVED += $(DEP)}\
 }
@@ -18,26 +18,37 @@ $(DIR_SRC)/%/$(FILENAME_SOURCE_MK):
 DEPS := ${sort $(DEPS)}
 DEPS_UNRESOLVED := ${filter-out ${sort $(DEPS_RESOLVED)}, $(DEPS)}
 
-${eval ${call debug, Deps: $(DEPS)}}
-${eval ${call debug, Deps resolved: $(DEPS_RESOLVED)}}
-${eval ${call debug, Deps unresolved: $(DEPS_UNRESOLVED)}}
+${call debug, Deps: $(DEPS)}
+${call debug, Deps resolved: $(DEPS_RESOLVED)}
+${call debug, Deps unresolved: $(DEPS_UNRESOLVED)}
 
 ifdef DEPS_UNRESOLVED
-${eval ${call debug, Not all deps resolved, calling recursive make...}}
+${call debug, Not all deps resolved - calling recursive make...}
 resolve-lib-% libtagion% tagion%:
-	@$(MAKE) $(addprefix resolve-,$(DEPS_UNRESOLVED)) $(MAKECMDGOALS) | grep hello
+	@$(MAKE) $(addprefix resolve-,$(DEPS_UNRESOLVED)) $(MAKECMDGOALS)
 	
 else
-${eval ${call debug, All deps successfully resolved}}
-
-${call debug, Going to include source makefiles for $(DEPS)}
-${foreach DEP,$(DEPS),${eval include $(DIR_SRC)/$(DEP)/$(FILENAME_SOURCE_MK)}}
-
+${call debug, All deps successfully resolved}
 resolve-%:
 	${call log.line, Resolved dependencies for targets: $(MAKECMDGOALS)}
+
+# 
+# Generate and include gen.source.mk
+# 
+ALL_SOURCE_MK := ${addprefix $(DIR_SRC)/, $(DEPS)}
+ALL_SOURCE_MK := ${addsuffix /$(FILENAME_SOURCE_MK), $(ALL_SOURCE_MK)}
+
+${call debug, Including:}
+${call debug.lines, $(ALL_SOURCE_MK)}
+
+-include $(ALL_SOURCE_MK)
+
+$(DIR_SRC)/%/$(FILENAME_SOURCE_MK): source-%
+	@
+
+source-%:
+	@ldc2 ${foreach DEP,$(DEPS),-I$(DIR_SRC)/$(DEP)} --makedeps $(DIR_SRC)/$(*)/$(LOOKUP) -o- -of=$(DIR_BUILD_O)/$(subst lib-,libtagion,$(*)).o > $(DIR_SRC)/$(*)/$(FILENAME_SOURCE_MK)
 endif
 
-$(DIR_SRC)/%/$(FILENAME_SOURCE_MK):
-	@ldc2 ${foreach DEP,$(DEPS),-I$(DIR_SRC)/$(DEP)} --makedeps $(DIR_SRC)/$(*)/$(LOOKUP) -o- -of=$(DIR_BUILD_O)/$(subst lib-,libtagion,$(*)).o >> $(DIR_SRC)/$(*)/$(FILENAME_SOURCE_MK)
 
 ${eval ${call debug.close, MAKE RESOLVE - $(MAKECMDGOALS)}}
