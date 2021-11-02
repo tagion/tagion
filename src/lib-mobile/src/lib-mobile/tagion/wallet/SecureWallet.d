6fa3881dcf40a5b59ddec579a808cdd9871dc277
cbleser <cr@tagion.org>
2021-11-02 09:47:29 +0200

    WIP;SecureWallet.collect_bills added

--- src/lib-mobile/src/lib-mobile/tagion/wallet/SecureWallet.d
+++ src/lib-mobile/src/lib-mobile/tagion/wallet/SecureWallet.d
@@ -1,4 +1,4 @@
-module tagion.wallet.WalletWrapper;
+module tagion.wallet.SecureWallet;
 
 import std.format;
 import std.string: representation;
@@ -62,7 +62,6 @@ struct SecureWallet(Net) {
         assert(questions.length is answers.length, "Amount of questions should be same as answers");
     }
     do {
-        import std.string : representation;
         auto net = new Net;
 //        auto hashnet = new StdHashNet;
         auto recover = KeyRecover(net);
@@ -80,15 +79,10 @@ struct SecureWallet(Net) {
             auto R = new ubyte[net.hashSize];
 
             recover.findSecret(R, questions, answers);
-            //writefln("R=0x%s", R.idup.hex);
-            // import std.string : representation;
             auto pinhash = recover.checkHash(pincode.representation);
-            //writefln("R.length=%d pinhash.length=%d", R.length, pinhash.length);
             wallet.Y = xor(R, pinhash);
             wallet.check = recover.checkHash(R);
             net.createKeyPair(R);
-            pragma(msg, "fixme(cbr): wallet.pubkey is redundant");
-//            wallet.pubkey = net.pubkey;
 
             const seed_data = recover.toHiBON.serialize;
             const seed_doc = Document(seed_data);
@@ -103,7 +97,6 @@ struct SecureWallet(Net) {
     }
 
     bool recover(const(string[]) questions, const(string[]) answers, string pincode) {
-        import std.string : representation;
         net = new Net;
         auto recover = KeyRecover(net, wallet.generator);
         auto R = new ubyte[net.hashSize];
@@ -166,11 +159,11 @@ struct SecureWallet(Net) {
         string current_time = MonoTime.currTime.toString;
         scope seed = new ubyte[net.hashSize];
         scramble(seed);
-        details.drive_state = net.calcHash(seed ~ details.drive_state ~ current_time.representation);
+        details.derive_state = net.calcHash(seed ~ details.derive_state ~ current_time.representation);
         scramble(seed);
-        const pkey = net.derivePubkey(details.drive_state);
-        invoice.pkey = cast(Buffer) pkey;
-        details.account[pkey] = details.drive_state;
+        auto pkey = net.derivePubkey(details.derive_state);
+        invoice.pkey = pkey;
+        details.derives[pkey] = details.derive_state;
     }
 
     static Invoice createInvoice(string label, TagionCurrency amount = TagionCurrency.init) {
@@ -214,32 +207,22 @@ struct SecureWallet(Net) {
                 pragma(msg, "orders[] ", typeof(orders[0].amount.toDoc), " ", typeof(orders[0].pkey) );
                 orders.each!(o => {result.contract.output[o.pkey] = o.amount.toDoc;});
                 result.contract.script = Script("pay");
-            }
-            else {
-                return false;
-            }
 
-            immutable message = net.hashOf(result.contract.toDoc);
-            auto shared_net = (() @trusted {return cast(shared) net;})();
-            auto bill_net = new Net;
-            pragma(msg, "contract_bills " , typeof(contract_bills
-                    .filter!(b => b.owner in details.account)
+                immutable message = net.hashOf(result.contract.toDoc);
+                auto shared_net = (() @trusted {return cast(shared) net;})();
+                auto bill_net = new Net;
+                result.signs = contract_bills
+                    .filter!(b => b.owner in details.derives)
                     .map!(b => {
-                            immutable tweak_code = details.account[b.owner];
+                            immutable tweak_code = details.derives[b.owner];
                             bill_net.derive(tweak_code, shared_net);
                             return bill_net.sign(message);
                         }())
-                    .array));
-            result.signs = contract_bills
-                .filter!(b => b.owner in details.account)
-                .map!(b => {
-                        immutable tweak_code = details.account[b.owner];
-                        bill_net.derive(tweak_code, shared_net);
-                        return bill_net.sign(message);
-                    }())
-                .array;
-            return true;
-
+                    .array;
+                return true;
+            }
+            result = result.init;
+            return false;
         }
 
         return false;
@@ -269,8 +252,51 @@ struct SecureWallet(Net) {
     const(HiRPC.Sender) get_request_update_wallet() const {
         HiRPC hirpc;
         auto h = new HiBON;
-        h=details.account.byKey.map!(p => cast(Buffer)p);
+        h=details.derives.byKey.map!(p => cast(Buffer)p);
         return hirpc.search(h);
+    }
+
+    bool collect_bills(const TagionCurrency amount, out StandardBill[] active_bills) {
+        import std.algorithm.sorting : isSorted, sort;
+        import std.algorithm.iteration : cumulativeFold;
+        import std.range : takeOne, tee;
+
+        if (!details.bills.isSorted!"a.value > b.value") {
+            details.bills.sort!"a.value > b.value";
+        }
+
+        // Select all bills not in use
+        auto none_active = details.bills
+            .filter!(b => !(b.owner in details.active));
+
+        // Check if we have enough money
+        const enought = !none_active
+            .map!(b => b.value)
+            .cumulativeFold!((a, b) => a + b)
+            .filter!(a => a >= amount)
+            .takeOne
+            .empty;
+        if (enought) {
+            TagionCurrency rest = amount;
+            active_bills = none_active
+                .filter!(b => b.value <= rest)
+                .until!(b => rest <= 0)
+                .tee!(b => {rest -= b.value; details.active[b.owner] = true;})
+                .array;
+            if (rest > 0) {
+                // Take an extra larger bill if not enough
+                StandardBill extra_bill;
+                none_active
+                    .each!(b => extra_bill=b);
+                    // .retro
+                    // .takeOne;
+                details.active[extra_bill.owner] = true;
+                active_bills ~= extra_bill;
+            }
+            assert(rest > 0);
+            return true;
+        }
+        return false;
     }
 
     version(none)
@@ -313,12 +339,96 @@ struct SecureWallet(Net) {
     static TagionCurrency calcTotal(const(StandardBill[]) bills) pure {
         return bills.map!(b => b.value).sum;
     }
-}
-
 unittest {
     import std.stdio;
 
-    import tagion.crypto.SecureNet;
+    import std.range : iota;
+    import std.format;
+    // alias StdSecureWallet = SecureWallet!StdSecureNet;
+    // Create recovery
+    //   const hashnet = new StdHashNet;
+//    auto recover = KeyRecovery(hashnet);
+    const pin_code = "1234";
+
+//    Document create_secure_wallet_doc() {
+    // Create a new Wallet
+    enum {
+        num_of_questions = 5,
+        confidence = 3
+    }
+    const dummey_questions = num_of_questions.iota.map!(i => format("What %s", i)).array;
+    const dummey_amswers = num_of_questions.iota.map!(i => format("A %s", i)).array;
+    const wallet_doc = SecureWallet.createWallet(dummey_questions, dummey_amswers, confidence, pin_code).toDoc;
+//    }
+
+//    const wallet_doc = create_secure_wallet_doc;
+
+    auto secure_wallet = SecureWallet(wallet_doc);
+    const pin_code_2 = "3434";
+    { // Login test
+        assert(!secure_wallet.isLoggedin);
+        secure_wallet.login(pin_code);
+        assert(secure_wallet.isLoggedin);
+        secure_wallet.logout;
+        assert(!secure_wallet.isLoggedin);
+        secure_wallet.login(pin_code_2);
+        assert(!secure_wallet.isLoggedin);
+    }
+
+    { // Key Recover faild
+        auto test_answers = dummey_amswers.dup;
+        test_answers[0] = "Bad answer 0";
+        test_answers[3] = "Bad answer 1";
+        test_answers[4] = "Bad answer 2";
+
+        const result = secure_wallet.recover(dummey_questions, test_answers, pin_code_2);
+        assert(!result);
+        assert(!secure_wallet.isLoggedin);
+    }
+
+    { // Key Recover test
+        auto test_answers = dummey_amswers.dup;
+        test_answers[2] = "Bad answer 0";
+        test_answers[4] = "Bad answer 1";
+
+        const result = secure_wallet.recover(dummey_questions, test_answers, pin_code_2);
+        assert(result);
+        assert(secure_wallet.isLoggedin);
+    }
+
+    { // Re-login
+        secure_wallet.logout;
+        assert(!secure_wallet.isLoggedin);
+        secure_wallet.login(pin_code_2);
+        assert(secure_wallet.isLoggedin);
+    }
+
+    const new_pincode = "7851";
+    { // Fail to change pin-code
+        const result=secure_wallet.change_pincode(new_pincode, pin_code_2);
+        assert(!result);
+        assert(secure_wallet.isLoggedin);
+    }
+
+    { // Change pincode
+        const result = secure_wallet.change_pincode(pin_code_2, new_pincode);
+        assert(result);
+        assert(!secure_wallet.isLoggedin);
+        secure_wallet.login(new_pincode);
+        assert(secure_wallet.isLoggedin);
+    }
+}
+}
+    unittest {
+        import tagion.crypto.SecureNet;
+        alias StdSecureWallet = SecureWallet!StdSecureNet;
+
+    }
+
+    version(none)
+unittest {
+    import std.stdio;
+
     import std.range : iota;
     import std.format;
     alias StdSecureWallet = SecureWallet!StdSecureNet;
@@ -394,5 +504,6 @@ unittest {
         secure_wallet.login(new_pincode);
         assert(secure_wallet.isLoggedin);
     }
+
 
 }
