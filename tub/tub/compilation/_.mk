79d089f8b2743e202b180ff23435b24749f1706b
Vlad Pazych <vladpazych@gmail.com>
2021-11-03 10:52:05 +0200

    wip: Fix compilation flow

--- tub/tub/compilation/_.mk
+++ tub/tub/compilation/_.mk
@@ -3,5 +3,5 @@ include $(DIR_TUB)/compilation/ways.mk
 include $(DIR_TUB)/compilation/clean.mk
 include $(DIR_TUB)/compilation/clone.mk
 include $(DIR_TUB)/compilation/goals.mk
-include $(DIR_TUB)/compilation/resolve.mk
 include $(DIR_TUB)/compilation/compile.mk
+include $(DIR_TUB)/compilation/resolve.mk
--- tub/tub/compilation/resolve.mk
+++ tub/tub/compilation/resolve.mk
@@ -1,7 +1,8 @@
 ${eval ${call debug.open, MAKE RESOLVE LEVEL $(MAKELEVEL) - $(MAKECMDGOALS)}}
 
 ifeq ($(MAKELEVEL),0)
-${shell rm -f $(DIR_SRC)/**/$(FILENAME_SOURCE_MK) || true}
+${shell rm -f $(DIR_SRC)/**/$(FILENAME_SOURCE_PROD_MK) || true}
+${shell rm -f $(DIR_SRC)/**/$(FILENAME_SOURCE_TEST_MK) || true}
 endif
 
 DEPS := ${sort $(DEPS)}
@@ -15,7 +16,7 @@ ${foreach DEP,$(DEPS),\
 	${eval DEPS_RESOLVED += $(DEP)}\
 }
 
-$(DIR_SRC)/%/$(FILENAME_SOURCE_MK):
+$(DIR_SRC)/%/$(FILENAME_SOURCE_PROD_MK):
 
 DEPS := ${sort $(DEPS)}
 DEPS_UNRESOLVED := ${filter-out ${sort $(DEPS_RESOLVED)}, $(DEPS)}
@@ -26,9 +27,13 @@ ${call debug, Deps unresolved: $(DEPS_UNRESOLVED)}
 
 ifdef DEPS_UNRESOLVED
 ${call debug, Not all deps resolved - calling recursive make...}
-resolve-lib-% libtagion% tagion% test-libtagion%:
-	@$(MAKE) $(addprefix resolve-,$(DEPS_UNRESOLVED)) $(MAKECMDGOALS)
-	
+libtagion% tagion% test-libtagion%:
+	${call log.kvp, Recursive Make, ${addprefix resolve-,$(DEPS_UNRESOLVED)} $(MAKECMDGOALS)}
+	@$(MAKE) ${addprefix resolve-,$(DEPS_UNRESOLVED)} $(MAKECMDGOALS)
+
+resolve-%:
+	@
+
 else
 ${call debug, All deps successfully resolved}
 
@@ -36,25 +41,40 @@ resolve-%:
 	${call log.kvp, Ensured, $(*)}
 
 # 
-# Generate and include gen.source.mk
+# Generate and include gen.<mode>.source.mk
 # 
-ALL_SOURCE_MK := ${addprefix $(DIR_SRC)/, $(DEPS)}
-ALL_SOURCE_MK := ${addsuffix /$(FILENAME_SOURCE_MK), $(ALL_SOURCE_MK)}
+ALL_SOURCE_MK_BASE := ${addprefix $(DIR_SRC)/, $(DEPS)}
+ALL_SOURCE_MK := ${addsuffix /$(FILENAME_SOURCE_PROD_MK), $(ALL_SOURCE_MK_BASE)}
+ALL_SOURCE_MK += ${addsuffix /$(FILENAME_SOURCE_TEST_MK), $(ALL_SOURCE_MK_BASE)}
 
 ${call debug, Including:}
 ${call debug.lines, $(ALL_SOURCE_MK)}
 
 -include $(ALL_SOURCE_MK)
 
-$(DIR_SRC)/%/$(FILENAME_SOURCE_MK): source-%
+$(DIR_SRC)/%/$(FILENAME_SOURCE_PROD_MK): source-%
 	@
 
 source-%:
-	${eval _LOOKUP_PROD := $(LOOKUP) $(LOOKUP_PROD)}
-	${eval _LOOKUP_TEST := $(LOOKUP) $(LOOKUP_TEST)}
-	@ldc2 ${foreach DEP,$(DEPS),-I$(DIR_SRC)/$(DEP)} --makedeps ${foreach _LPROD,$(_LOOKUP_PROD),$(DIR_SRC)/$(*)/$(_LPROD)} -o- -of=$(DIR_BUILD_O)/$(subst lib-,libtagion,$(*)).o > $(DIR_SRC)/$(*)/$(FILENAME_SOURCE_MK)
-	@ldc2 ${foreach DEP,$(DEPS),-I$(DIR_SRC)/$(DEP)} --makedeps ${foreach _LTEST,$(_LOOKUP_TEST),$(DIR_SRC)/$(*)/$(_LTEST)} -o- -of=$(DIR_BUILD_O)/$(subst lib-,test-libtagion,$(*)).o >> $(DIR_SRC)/$(*)/$(FILENAME_SOURCE_MK)
+	@ldc2 ${foreach DEP,$(DEPS),-I$(DIR_SRC)/$(DEP)} --makedeps ${foreach _LPROD,$(LOOKUP) $(LOOKUP_PROD),$(DIR_SRC)/$(*)/$(_LPROD)} -o- -of=$(DIR_BUILD_O)/$(subst lib-,libtagion,$(*)).o > $(DIR_SRC)/$(*)/$(FILENAME_SOURCE_PROD_MK)
+	@echo "" >> $(DIR_SRC)/$(*)/$(FILENAME_SOURCE_PROD_MK)
+	${eval _DEPS_O_$* := ${shell ldc2 ${foreach DEP,$(DEPS),-I$(DIR_SRC)/$(DEP)} --makedeps ${foreach _LTEST,$(LOOKUP) $(LOOKUP_PROD),$(DIR_SRC)/$(*)/$(_LTEST)} -o- -of=$(DIR_BUILD_O)/$(subst lib-,test-libtagion,$(*)).o | grep $(DIR_SRC)}}
+	${eval _DEPS_O_$* := ${subst $(DIR_SRC)/,,$(_DEPS_O_$*)}}
+	${eval _DEPS_O_$* := ${subst /,.dir ,$(_DEPS_O_$*)}}
+	${eval _DEPS_O_$* := ${filter lib-%,$(_DEPS_O_$*)}}
+	${eval _DEPS_O_$* := ${subst .dir,,$(_DEPS_O_$*)}}
+	${eval _DEPS_O_$* := ${sort $(_DEPS_O_$*)}}
+	@echo $(DIR_BUILD_LIBS_STATIC)/$(subst lib-,libtagion,$(*)).a: $(foreach _DEP_O,$(_DEPS_O_$*),$(DIR_BUILD_O)/$(subst lib-,test-libtagion,$(_DEP_O)).o ) >> $(DIR_SRC)/$(*)/$(FILENAME_SOURCE_PROD_MK)
+	
+	@ldc2 ${foreach DEP,$(DEPS),-I$(DIR_SRC)/$(DEP)} --makedeps ${foreach _LTEST,$(LOOKUP) $(LOOKUP_TEST),$(DIR_SRC)/$(*)/$(_LTEST)} -o- -of=$(DIR_BUILD_O)/$(subst lib-,test-libtagion,$(*)).o > $(DIR_SRC)/$(*)/$(FILENAME_SOURCE_TEST_MK)
+	@echo "" >> $(DIR_SRC)/$(*)/$(FILENAME_SOURCE_PROD_MK)
+	${eval _DEPS_O_$* := ${shell ldc2 ${foreach DEP,$(DEPS),-I$(DIR_SRC)/$(DEP)} --makedeps ${foreach _LTEST,$(LOOKUP) $(LOOKUP_TEST),$(DIR_SRC)/$(*)/$(_LTEST)} -o- -of=$(DIR_BUILD_O)/$(subst lib-,test-libtagion,$(*)).o | grep $(DIR_SRC)}}
+	${eval _DEPS_O_$* := ${subst $(DIR_SRC)/,,$(_DEPS_O_$*)}}
+	${eval _DEPS_O_$* := ${subst /,.dir ,$(_DEPS_O_$*)}}
+	${eval _DEPS_O_$* := ${filter lib-%,$(_DEPS_O_$*)}}
+	${eval _DEPS_O_$* := ${subst .dir,,$(_DEPS_O_$*)}}
+	${eval _DEPS_O_$* := ${sort $(_DEPS_O_$*)}}
+	@echo $(DIR_BUILD_BINS)/test-$(subst lib-,libtagion,$(*)): $(foreach _DEP_O,$(_DEPS_O_$*),$(DIR_BUILD_O)/$(subst lib-,test-libtagion,$(_DEP_O)).o ) >> $(DIR_SRC)/$(*)/$(FILENAME_SOURCE_TEST_MK)
 endif
-
 
 ${eval ${call debug.close, MAKE RESOLVE LEVEL $(MAKELEVEL) - $(MAKECMDGOALS)}}
