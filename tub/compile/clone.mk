aabbb2421c1476b53e8be1a8de20a92d38efbb71
Vlad Pazych <vladpazych@gmail.com>
2021-11-08 12:31:29 +0200

    feat: Add branch name support when cloning

--- tub/compile/clone.mk
+++ tub/compile/clone.mk
@@ -2,11 +2,9 @@
 clone-%: $(DIR_SRC)/%/context.mk
 	@
 
-# TODO: Add branch name
-# TODO: In isolated mode clone only --depth
 $(DIR_SRC)/%/context.mk:
 	${call log.header, Cloning $(*)...}
-	$(PRECMD)git clone $(GIT_ORIGIN)/core-$(*) $(DIR_SRC)/$(*)
+	$(PRECMD)git clone ${if $(BRANCH),-b $(BRANCH) --single-branch} $(GIT_ORIGIN)/core-$(*) $(DIR_SRC)/$(*)
 	${call log.close}
 
 # Shortcut for Tagion Core developers
