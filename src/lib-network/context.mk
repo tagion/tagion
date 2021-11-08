dadf6d79561c5df75f708cd641e39901f6892d69
Vlad Pazych <vladpazych@gmail.com>
2021-11-08 10:22:27 +0200

    Add support for tub 0.8

--- src/lib-network/context.mk
+++ src/lib-network/context.mk
@@ -1,2 +1,13 @@
-libtagionnetwork.ctx: libtagionhibon.o libtagioncommunication.o libtagionservices.o libtagionhashgraph.o libtagionp2pgowrapper.o
-	@
+DEPS += lib-hibon
+DEPS += lib-communication
+DEPS += lib-crypto
+DEPS += lib-services
+DEPS += lib-hashgraph
+DEPS += lib-p2pgowrapper
+
+${call config.lib, network}: LOOKUP := tagion/**/*.d
+
+${call lib, network}: LINKFILES += $(DIR_BUILD_WRAPS)/secp256k1/lib/libsecp256k1.a
+${call lib, network}: LINKFILES += $(DIR_BUILD_WRAPS)/openssl/lib/libcrypto.a
+${call lib, network}: LINKFILES += $(DIR_BUILD_WRAPS)/openssl/lib/libssl.a
+${call lib, network}: LINKFILES += $(DIR_BUILD_WRAPS)/p2pgowrapper/lib/libp2pgowrapper.a
