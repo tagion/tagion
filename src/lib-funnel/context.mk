616a681f8793dc2c5f40c19351a1ad9e738562c4
Vlad Pazych <vladpazych@gmail.com>
2021-11-08 10:22:27 +0200

    Add support for tub 0.8

--- src/lib-funnel/context.mk
+++ src/lib-funnel/context.mk
@@ -1,2 +1,11 @@
-libtagionfunnel.ctx: libtagionhibon.o libtagioncrypto.o libtagionwallet.o libtagioncommunication.o
-	@
+DEPS += lib-hibon
+DEPS += lib-crypto
+DEPS += lib-wallet
+DEPS += lib-communication
+
+${call config.lib, funnel}: LOOKUP := tagion/**/*.d
+
+${call lib, funnel}: LINKFILES := $(DIR_BUILD_WRAPS)/secp256k1/lib/libsecp256k1.a
+${call lib, funnel}: LINKFILES += $(DIR_BUILD_WRAPS)/openssl/lib/libssl.a
+${call lib, funnel}: LINKFILES += $(DIR_BUILD_WRAPS)/openssl/lib/libcrypto.a
+${call lib, funnel}: LINKFILES += $(DIR_BUILD_WRAPS)/p2pgowrapper/lib/libp2pgowrapper.a
