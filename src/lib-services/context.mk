52a8ce05ca849cc2409e59c10848a7a183679a62
Vlad Pazych <vladpazych@gmail.com>
2021-11-08 10:22:27 +0200

    Add support for tub 0.8

--- src/lib-services/context.mk
+++ src/lib-services/context.mk
@@ -1,2 +1,17 @@
-libtagionservices.ctx: libtagionutils.o libtagionhashgraph.o libtagionhibon.o libtagioncrypto.o libtagioncommunication.o libtagiongossip.o libtagionp2pgowrapper.o libtagiondart.o libtagionmonitor.o
-	@
+DEPS += lib-utils
+DEPS += lib-hashgraph
+DEPS += lib-hibon
+DEPS += lib-crypto
+DEPS += lib-communication
+DEPS += lib-gossip
+DEPS += lib-p2pgowrapper
+DEPS += lib-dart
+DEPS += lib-monitor
+
+${call config.lib, services}: LOOKUP := tagion/**/*.d
+${call config.lib, services}: LOOKUP += tagion/*.d
+
+${call lib, services}: LINKFILES := $(DIR_BUILD_WRAPS)/secp256k1/lib/libsecp256k1.a
+${call lib, services}: LINKFILES += $(DIR_BUILD_WRAPS)/openssl/lib/libssl.a
+${call lib, services}: LINKFILES += $(DIR_BUILD_WRAPS)/openssl/lib/libcrypto.a
+${call lib, services}: LINKFILES += $(DIR_BUILD_WRAPS)/p2pgowrapper/lib/libp2pgowrapper.a
