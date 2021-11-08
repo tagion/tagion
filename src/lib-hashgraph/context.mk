b2337698cce11b8e4ac12efb6c572d90ad748d40
Vlad Pazych <vladpazych@gmail.com>
2021-11-08 10:22:27 +0200

    Add support for tub 0.8

--- src/lib-hashgraph/context.mk
+++ src/lib-hashgraph/context.mk
@@ -1,2 +1,13 @@
-libtagionhashgraph.ctx: libtagionbasic.o libtagionhibon.o libtagioncrypto.o libtagioncommunication.o
-	@
+DEPS += lib-basic
+DEPS += lib-hibon
+DEPS += lib-utils
+DEPS += lib-crypto
+DEPS += lib-communication
+DEPS += lib-gossip
+
+${call config.lib, hashgraph}: LOOKUP := tagion/**/*.d
+
+${call lib, hashgraph}: LINKFILES := $(DIR_BUILD_WRAPS)/secp256k1/lib/libsecp256k1.a
+${call lib, hashgraph}: LINKFILES += $(DIR_BUILD_WRAPS)/openssl/lib/libssl.a
+${call lib, hashgraph}: LINKFILES += $(DIR_BUILD_WRAPS)/openssl/lib/libcrypto.a
+${call lib, hashgraph}: LINKFILES += $(DIR_BUILD_WRAPS)/p2pgowrapper/lib/libp2pgowrapper.a
