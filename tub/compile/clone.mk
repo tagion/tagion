52a4c5816dca07d06a8e3b20763b47a401a7147c
Vlad Pazych <vladpazych@gmail.com>
2021-11-08 12:45:45 +0200

    refactor: Improve initialization process

--- tub/compile/clone.mk
+++ tub/compile/clone.mk
@@ -3,7 +3,7 @@ clone-%: $(DIR_SRC)/%/context.mk
 	@
 
 $(DIR_SRC)/%/context.mk:
-	${call log.header, Cloning $(*)...}
+	${call log.header, Cloning $(*), branch $(BRANCH)}
 	$(PRECMD)git clone ${if $(BRANCH),-b $(BRANCH) --single-branch} $(GIT_ORIGIN)/core-$(*) $(DIR_SRC)/$(*)
 	${call log.close}
 
