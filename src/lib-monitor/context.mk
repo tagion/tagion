48676edd6fd25c367af9df4e78d7bbc5a0f638d6
Vlad Pazych <vladpazych@gmail.com>
2021-11-08 10:22:27 +0200

    Add support for tub 0.8

--- src/lib-monitor/context.mk
+++ src/lib-monitor/context.mk
@@ -1,2 +1,17 @@
-libtagionmonitor.ctx: libtagionbasic.o libtagionhashgraph.o libtagionhibon.o libtagioncrypto.o libtagioncommunication.o libtagiongossip.o libtagiondart.o libtagionnetwork.o libtagionfunnel.o libtagionp2pgowrapper.o
-	@
+DEPS += lib-basic
+DEPS += lib-hashgraph
+DEPS += lib-hibon
+DEPS += lib-crypto
+DEPS += lib-communication
+DEPS += lib-gossip
+DEPS += lib-dart
+DEPS += lib-network
+DEPS += lib-funnel
+DEPS += lib-p2pgowrapper
+
+${call config.lib, monitor}: LOOKUP := tagion/**/*.d
+
+${call lib, monitor}: INFILES += $(DIR_BUILD_WRAPS)/secp256k1/lib/libsecp256k1.a
+${call lib, monitor}: INFILES += $(DIR_BUILD_WRAPS)/openssl/lib/libssl.a
+${call lib, monitor}: INFILES += $(DIR_BUILD_WRAPS)/openssl/lib/libcrypto.a
+${call lib, monitor}: INFILES += $(DIR_BUILD_WRAPS)/p2pgowrapper/lib/libp2pgowrapper.a
