4cede2792e8637d0a6183fdefdfd59b19428e400
Vlad Pazych <vladpazych@gmail.com>
2021-11-08 10:22:27 +0200

    Add support for tub 0.8

--- src/bin-wave/context.mk
+++ src/bin-wave/context.mk
@@ -1,2 +1,11 @@
-tagionwave.ctx: libtagioncrypto.o libtagionp2pgowrapper.o libtagiongossip.o
-	@
+DEPS += lib-crypto
+DEPS += lib-p2pgowrapper
+DEPS += lib-gossip
+DEPS += lib-services
+
+${call config.bin, wave}: LOOKUP := tagion/*.d
+
+${call bin, wave}: INFILES += $(DIR_BUILD_WRAPS)/secp256k1/lib/libsecp256k1.a
+${call bin, wave}: INFILES += $(DIR_BUILD_WRAPS)/openssl/lib/libssl.a
+${call bin, wave}: INFILES += $(DIR_BUILD_WRAPS)/openssl/lib/libcrypto.a
+${call bin, wave}: INFILES += $(DIR_BUILD_WRAPS)/p2pgowrapper/lib/libp2pgowrapper.a
