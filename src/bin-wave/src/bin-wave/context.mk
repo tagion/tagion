fcc5198d3d52baf51ea71cf3f8401c75681d7a94
cbleser <cr@tagion.org>
2021-11-10 09:20:44 +0200

    WIP: tagionwallet and tagionboot now links with updated tub flow

--- src/bin-wave/src/bin-wave/context.mk
+++ src/bin-wave/src/bin-wave/context.mk
@@ -2,10 +2,11 @@ DEPS += lib-crypto
 DEPS += lib-p2pgowrapper
 DEPS += lib-gossip
 DEPS += lib-services
+PROGRAM:=wave
 
-${call config.bin, wave}: LOOKUP := tagion/*.d
+${call config.bin, $(PROGRAM)}: LOOKUP := tagion/*.d
 
-${call bin, wave}: INFILES += $(DIR_BUILD_WRAPS)/secp256k1/lib/libsecp256k1.a
-${call bin, wave}: INFILES += $(DIR_BUILD_WRAPS)/openssl/lib/libssl.a
-${call bin, wave}: INFILES += $(DIR_BUILD_WRAPS)/openssl/lib/libcrypto.a
-${call bin, wave}: INFILES += $(DIR_BUILD_WRAPS)/p2pgowrapper/lib/libp2pgowrapper.a
+${call bin, $(PROGRAM)}: INFILES += $(DIR_BUILD_WRAPS)/secp256k1/lib/libsecp256k1.a
+${call bin, $(PROGRAM)}: INFILES += $(DIR_BUILD_WRAPS)/openssl/lib/libssl.a
+${call bin, $(PROGRAM)}: INFILES += $(DIR_BUILD_WRAPS)/openssl/lib/libcrypto.a
+${call bin, $(PROGRAM)}: INFILES += $(DIR_BUILD_WRAPS)/p2pgowrapper/lib/libp2pgowrapper.a
