6c765c515cb53e4d5d934fc3fa87015e5af0e606
cbleser <cr@tagion.org>
2021-11-10 09:20:44 +0200

    WIP: tagionwallet and tagionboot now links with updated tub flow

--- src/bin-boot/context.mk
+++ src/bin-boot/context.mk
@@ -1,2 +1,11 @@
-tagionutil.ctx: libtagiondart.o
-	@
+DEPS += lib-dart
+DEPS += lib-crypto
+DEPS += lib-logger
+PROGRAM:=boot
+
+${call config.bin, $(PROGRAM)}: LOOKUP := tagion/*.d
+
+${call bin, $(PROGRAM)}: INFILES += $(DIR_BUILD_WRAPS)/secp256k1/lib/libsecp256k1.a
+${call bin, $(PROGRAM)}: INFILES += $(DIR_BUILD_WRAPS)/openssl/lib/libssl.a
+${call bin, $(PROGRAM)}: INFILES += $(DIR_BUILD_WRAPS)/openssl/lib/libcrypto.a
+${call bin, $(PROGRAM)}: INFILES += $(DIR_BUILD_WRAPS)/p2pgowrapper/lib/libp2pgowrapper.a
