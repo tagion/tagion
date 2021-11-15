3657617b91765e2fe350d3e6990af9ccfc94c5e7
Vlad Pazych <vladpazych@gmail.com>
2021-11-15 13:20:29 +0200

    wip: Simplify build flow

--- src/wrap-openssl/context.mk
+++ src/wrap-openssl/context.mk
@@ -11,19 +11,18 @@ wrap-openssl: $(DIR_OPENSSL_PREFIX)/libcrypto.a $(DIR_OPENSSL_PREFIX)/libssl.a
 	@
 
 clean-wrap-openssl:
-	${call unit.dep.wrap-openssl}
 	${call rm.dir, $(DIR_OPENSSL)}
 
 $(DIR_OPENSSL_PREFIX)/%.a: $(DIR_OPENSSL)/.way
 	$(PRECMD)git clone --depth 1 $(REPO_OPENSSL) $(DIR_OPENSSL_SRC) 2> /dev/null || true
 	$(PRECMD)git -C $(DIR_OPENSSL_SRC) fetch --depth 1 $(DIR_OPENSSL_SRC) $(VERSION_OPENSSL) &> /dev/null || true
-	$(PRECMD)cd $(DIR_OPENSSL_SRC); ./config --shared --prefix=$(DIR_OPENSSL) --openssldir=$(DIR_OPENSSL_EXTRA)
-	$(PRECMD)cd $(DIR_OPENSSL_SRC); make $(MAKE_PARALLEL)
-	$(PRECMD)cd $(DIR_OPENSSL_SRC); make install
-	$(PRECMD)[ -d "$(DIR_OPENSSL)/lib64" ] && ln -s $(DIR_OPENSSL)/lib64 $(DIR_OPENSSL)/lib
-
-wrap-openssllink:
-	$(PRECMD)ln -s $(DIR_OPENSSL)/lib64 $(DIR_OPENSSL)/lib
+	$(PRECMD)cd $(DIR_OPENSSL_SRC); ./config no-shared -static --prefix=$(DIR_OPENSSL) --openssldir=$(DIR_OPENSSL_EXTRA)
+	$(PRECMD)cd $(DIR_OPENSSL_SRC); make build_generated $(MAKE_PARALLEL)
+	$(PRECMD)cd $(DIR_OPENSSL_SRC); make libcrypto.a $(MAKE_PARALLEL)
+	$(PRECMD)cd $(DIR_OPENSSL_SRC); make libssl.a $(MAKE_PARALLEL)
+	$(PRECMD)mkdir -p $(DIR_OPENSSL_PREFIX)
+	$(PRECMD)cp $(DIR_OPENSSL_SRC)/libssl.a $(DIR_OPENSSL_PREFIX)/libssl.a
+	$(PRECMD)cp $(DIR_OPENSSL_SRC)/libcrypto.a $(DIR_OPENSSL_PREFIX)/libcrypto.a
 
 # NOTE: Might need to export, but not sure. Will try without since we static link:
 # $(PRECMD)export LD_LIBRARY_PATH=$(DIR_OPENSSL_PREFIX)/:$(LD_LIBRARY_PATH)
