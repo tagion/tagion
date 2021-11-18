4449a1dc92428e2a94c6553e7f5c0736aa5262e4
Vlad Pazych <vladpazych@gmail.com>
2021-11-18 11:54:00 +0200

    fix: Fix compilation flow

--- src/lib-wallet/context.mk
+++ src/lib-wallet/context.mk
@@ -4,10 +4,8 @@ DEPS += lib-crypto
 DEPS += lib-funnel
 DEPS += lib-communication
 
-${call config.lib, wallet}: LOOKUP := tagion/**/*.d
+libwallet.configure: SOURCE := tagion/**/*.d
 
-ifdef TEST
-${call lib, wallet}: INFILES += $(DIR_BUILD_WRAPS)/secp256k1/lib/libsecp256k1.a
-${call lib, wallet}: INFILES += $(DIR_BUILD_WRAPS)/openssl/lib/libssl.a
-${call lib, wallet}: INFILES += $(DIR_BUILD_WRAPS)/openssl/lib/libcrypto.a
-endif
+$(DBIN)/libwallet.test: $(DTMP)/libsecp256k1.a
+$(DBIN)/libwallet.test: $(DTMP)/libssl.a
+$(DBIN)/libwallet.test: $(DTMP)/libcrypto.a
