cb4fd3bc9290098d73b18de0a209bb3aea71eae2
Vlad Pazych <vladpazych@gmail.com>
2021-11-18 11:46:08 +0200

    wip: Add wrap compilation

--- src/wrap-openssl/src/wrap-openssl/context.mk
+++ src/wrap-openssl/src/wrap-openssl/context.mk
@@ -1,32 +1,51 @@
 REPO_OPENSSL ?= git@github.com:tagion/fork-openssl.git
 VERSION_OPENSSL := 2e5cdbc18a1a26bfc817070a52689886fa0669c2 # OpenSSL_1_1_1-stable as of 09.09.2021
 
-DIR_OPENSSL := $(DIR_BUILD_WRAPS)/openssl
+DSRC_OPENSSL := ${call dir.resolve, src}
+DTMP_OPENSSL := $(DTMP)/openssl
 
-DIR_OPENSSL_PREFIX := $(DIR_OPENSSL)/lib
-DIR_OPENSSL_EXTRA := $(DIR_OPENSSL)/extra
-DIR_OPENSSL_SRC := $(DIR_OPENSSL)/src
+DPREFIX_OPENSSL := $(DTMP_OPENSSL)/install-lib
+DEXTRA_OPENSSL := $(DTMP_OPENSSL)/install-extra
 
-configure-wrap-openssl: wrap-openssl
+CONFIGUREFLAGS_OPENSSL += -static 
+CONFIGUREFLAGS_OPENSSL += --prefix=$(DPREFIX_OPENSSL)
+CONFIGUREFLAGS_OPENSSL += --openssldir=$(DEXTRA_OPENSSL)
+
+openssl.preconfigure: $(DSRC_OPENSSL)/.src
+openssl: $(DTMP)/libssl.a $(DTMP)/libcrypto.a
 	@
-	
-wrap-openssl: $(DIR_OPENSSL_PREFIX)/libcrypto.a $(DIR_OPENSSL_PREFIX)/libssl.a
+
+TOCLEAN_OPENSSL += $(DTMP)/libssl.a
+TOCLEAN_OPENSSL += $(DTMP)/libcrypto.a
+TOCLEAN_OPENSSL += $(DSRC_OPENSSL)
+TOCLEAN_OPENSSL += $(DTMP_OPENSSL)
+
+TOCLEAN += $(TOCLEAN_OPENSSL)
+
+clean-openssl: TOCLEAN := $(TOCLEAN_OPENSSL)
+clean-openssl: clean
 	@
 
-clean-wrap-openssl:
-	${call rm.dir, $(DIR_OPENSSL)}
+$(DTMP_OPENSSL)/.configured: $(DTMP)/.way $(DSRC_OPENSSL)/.src
+	$(PRECMD)$(CP) $(DSRC_OPENSSL) $(DTMP_OPENSSL)
+	$(PRECMD)cd $(DTMP_OPENSSL); ./config $(CONFIGUREFLAGS_OPENSSL)
+	$(PRECMD)cd $(DTMP_OPENSSL); make build_generated $(MAKE_PARALLEL)
+	$(PRECMD)touch $@
 
-$(DIR_OPENSSL_PREFIX)/%.a: $(DIR_OPENSSL)/.way
-	$(PRECMD)git clone --depth 1 $(REPO_OPENSSL) $(DIR_OPENSSL_SRC) 2> /dev/null || true
-	$(PRECMD)git -C $(DIR_OPENSSL_SRC) fetch --depth 1 $(DIR_OPENSSL_SRC) $(VERSION_OPENSSL) &> /dev/null || true
-	$(PRECMD)cd $(DIR_OPENSSL_SRC); ./config no-shared -static --prefix=$(DIR_OPENSSL) --openssldir=$(DIR_OPENSSL_EXTRA)
-	$(PRECMD)cd $(DIR_OPENSSL_SRC); make build_generated $(MAKE_PARALLEL)
-	$(PRECMD)cd $(DIR_OPENSSL_SRC); make libcrypto.a $(MAKE_PARALLEL)
-	$(PRECMD)cd $(DIR_OPENSSL_SRC); make libssl.a $(MAKE_PARALLEL)
-	$(PRECMD)mkdir -p $(DIR_OPENSSL_PREFIX)
-	$(PRECMD)cp $(DIR_OPENSSL_SRC)/libssl.a $(DIR_OPENSSL_PREFIX)/libssl.a
-	$(PRECMD)cp $(DIR_OPENSSL_SRC)/libcrypto.a $(DIR_OPENSSL_PREFIX)/libcrypto.a
+$(DTMP)/libcrypto.a: $(DTMP_OPENSSL)/.configured
+	$(PRECMD)cd $(DTMP_OPENSSL); make libcrypto.a $(MAKE_PARALLEL)
+	$(PRECMD)cp $(DTMP_OPENSSL)/libcrypto.a $(DTMP)/libcrypto.a
+
+
+$(DTMP)/libssl.a: $(DTMP_OPENSSL)/.configured
+	$(PRECMD)cd $(DTMP_OPENSSL); make libssl.a $(MAKE_PARALLEL)
+	$(PRECMD)cp $(DTMP_OPENSSL)/libssl.a $(DTMP)/libssl.a
+
+$(DSRC_OPENSSL)/.src:
+	$(PRECMD)git clone --depth 1 $(REPO_OPENSSL) $(DSRC_OPENSSL) 2> /dev/null || true
+	$(PRECMD)git -C $(DSRC_OPENSSL) fetch --depth 1 $(DSRC_OPENSSL) $(VERSION_OPENSSL) &> /dev/null || true
+	$(PRECMD)touch $@
 
 # NOTE: Might need to export, but not sure. Will try without since we static link:
-# $(PRECMD)export LD_LIBRARY_PATH=$(DIR_OPENSSL_PREFIX)/:$(LD_LIBRARY_PATH)
-# $(PRECMD)export DYLD_LIBRARY_PATH=$(DIR_OPENSSL_PREFIX)/:$(DYLD_LIBRARY_PATH)
+# $(PRECMD)export LD_LIBRARY_PATH=$(DPREFIX_OPENSSL)/:$(LD_LIBRARY_PATH)
+# $(PRECMD)export DYLD_LIBRARY_PATH=$(DPREFIX_OPENSSL)/:$(DYLD_LIBRARY_PATH)
