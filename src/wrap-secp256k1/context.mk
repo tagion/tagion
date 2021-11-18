aeeb13137e4639587d2325c28c5d4b2cdc3c7b34
Vlad Pazych <vladpazych@gmail.com>
2021-11-18 11:46:08 +0200

    wip: Add wrap compilation

--- src/wrap-secp256k1/context.mk
+++ src/wrap-secp256k1/context.mk
@@ -1,30 +1,39 @@
+DEPS += wrap-openssl
+
 REPO_SECP256K1 ?= git@github.com:tagion/fork-secp256k1.git
 VERSION_SECP256k1 := ea5e8a9c47f1d435e8f66913eb7f1293b85b43f9
 
 DSRC_SECP256K1 := ${call dir.resolve, src}
-DIR_SECP256K1 := $(DTMP)/secp256k1
+DTMP_SECP256K1 := $(DTMP)/secp256k1
 
-CONFIGUREFLAGS+=--enable-module-ecdh
-CONFIGUREFLAGS+=--enable-experimental
-CONFIGUREFLAGS+=--enable-module-recovery
-CONFIGUREFLAGS+=--enable-module-schnorrsig
-CONFIGUREFLAGS+=--enable-shared=no
-CONFIGUREFLAGS+= CRYPTO_LIBS=$(DIR_OPENSSL)/lib/ CRYPTO_CFLAGS=$(DIR_OPENSSL)/include/
+CONFIGUREFLAGS_SECP256K1 += --enable-module-ecdh
+CONFIGUREFLAGS_SECP256K1 += --enable-experimental
+CONFIGUREFLAGS_SECP256K1 += --enable-module-recovery
+CONFIGUREFLAGS_SECP256K1 += --enable-module-schnorrsig
+CONFIGUREFLAGS_SECP256K1 += --enable-shared=no
+CONFIGUREFLAGS_SECP256K1 += CRYPTO_LIBS=$(DTMP)/ CRYPTO_CFLAGS=$(DSRC_OPENSSL)/include/
 
 secp256k1.preconfigure: $(DSRC_SECP256K1)/.src
 secp256k1: $(DTMP)/libsecp256k1.a
 	@
 
-clean-secp256k1:
-	$(PRECMD)$(RMDIR) $(DTMP)/libsecp256k1.a
-	$(PRECMD)$(RMDIR) $(DSRC_SECP256K1)
+TOCLEAN_SECP256K1 += $(DTMP)/libsecp256k1.a
+TOCLEAN_SECP256K1 += $(DSRC_SECP256K1)
+TOCLEAN_SECP256K1 += $(DTMP_SECP256K1)
 
-$(DTMP)/%.a: $(DIR_SECP256K1)/.src
-	$(PRECMD)cd $(DIR_SECP256K1); ./autogen.sh
-	$(PRECMD)cd $(DIR_SECP256K1); ./configure $(CONFIGUREFLAGS)
-	$(PRECMD)cd $(DIR_SECP256K1); make clean
-	$(PRECMD)cd $(DIR_SECP256K1); make $(MAKE_PARALLEL)
-	$(PRECMD)cd $(DIR_SECP256K1); mv .libs/libsecp256k1.a $@
+TOCLEAN += $(TOCLEAN_SECP256K1)
+
+clean-secp256k1: TOCLEAN := $(TOCLEAN_SECP256K1)
+clean-secp256k1: clean
+	@
+
+$(DTMP)/libsecp256k1.a: $(DTMP)/.way $(DSRC_SECP256K1)/.src
+	$(PRECMD)$(CP) $(DSRC_SECP256K1) $(DTMP_SECP256K1)
+	$(PRECMD)cd $(DTMP_SECP256K1); ./autogen.sh
+	$(PRECMD)cd $(DTMP_SECP256K1); ./configure $(CONFIGUREFLAGS_SECP256K1)
+	$(PRECMD)cd $(DTMP_SECP256K1); make clean
+	$(PRECMD)cd $(DTMP_SECP256K1); make $(MAKE_PARALLEL)
+	$(PRECMD)cd $(DTMP_SECP256K1); mv .libs/libsecp256k1.a $@
 
 $(DSRC_SECP256K1)/.src:
 	$(PRECMD)git clone --depth 1 $(REPO_SECP256K1) $(DSRC_SECP256K1) 2> /dev/null || true
