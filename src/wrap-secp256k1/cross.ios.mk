3e4843ff78715e8d739f23b174fe4cb8f69fbdc8
Vlad Pazych <vladpazych@gmail.com>
2021-11-23 14:09:55 +0200

    wip: Update context.mk

--- src/wrap-secp256k1/cross.ios.mk
+++ src/wrap-secp256k1/cross.ios.mk
@@ -1,5 +1,5 @@
 
 
-${if $(CROSS_COMPILE),--host=$(MTRIPLE) --target=$(MTRIPLE) --with-sysroot=$(CROSS_SYSROOT)} 
+# ${if $(CROSS_COMPILE),--host=$(MTRIPLE) --target=$(MTRIPLE) --with-sysroot=$(CROSS_SYSROOT)} 
 
-${if $(CROSS_COMPILE),CC=/usr/bin/clang CFLAGS="-arch $(CROSS_ARCH) -fpic -g -Os -pipe -isysroot $(CROSS_SYSROOT) -mios-version-min=12.0"} 
+# ${if $(CROSS_COMPILE),CC=/usr/bin/clang CFLAGS="-arch $(CROSS_ARCH) -fpic -g -Os -pipe -isysroot $(CROSS_SYSROOT) -mios-version-min=12.0"} 
