764789db62cff6e4822a2c3b00fccc8280c606ca
Vlad Pazych <vladpazych@gmail.com>
2021-11-23 15:19:12 +0200

    fix: Fix dstep flow for p2pgowrapper

--- src/wrap-secp256k1/context.mk
+++ src/wrap-secp256k1/context.mk
@@ -13,12 +13,10 @@ CONFIGUREFLAGS_SECP256K1 += --enable-module-schnorrsig
 CONFIGUREFLAGS_SECP256K1 += --enable-shared=no
 CONFIGUREFLAGS_SECP256K1 += CRYPTO_LIBS=$(DTMP)/ CRYPTO_CFLAGS=$(DSRC_OPENSSL)/include/
 
-secp256k1.preconfigure: $(DSRC_SECP256K1)/.src
 secp256k1: $(DTMP)/libsecp256k1.a
 	@
 
 TOCLEAN_SECP256K1 += $(DTMP)/libsecp256k1.a
-TOCLEAN_SECP256K1 += $(DSRC_SECP256K1)
 TOCLEAN_SECP256K1 += $(DTMP_SECP256K1)
 
 TOCLEAN += $(TOCLEAN_SECP256K1)
@@ -27,16 +25,10 @@ clean-secp256k1: TOCLEAN := $(TOCLEAN_SECP256K1)
 clean-secp256k1: clean
 	@
 
-$(DTMP)/libsecp256k1.a: $(DTMP)/.way $(DSRC_SECP256K1)/.src
+$(DTMP)/libsecp256k1.a: $(DTMP)/.way 
 	$(PRECMD)$(CP) $(DSRC_SECP256K1) $(DTMP_SECP256K1)
 	$(PRECMD)cd $(DTMP_SECP256K1); ./autogen.sh
 	$(PRECMD)cd $(DTMP_SECP256K1); ./configure $(CONFIGUREFLAGS_SECP256K1)
 	$(PRECMD)cd $(DTMP_SECP256K1); make clean
 	$(PRECMD)cd $(DTMP_SECP256K1); make $(SUBMAKE_PARALLEL)
 	$(PRECMD)cd $(DTMP_SECP256K1); mv .libs/libsecp256k1.a $@
-
-$(DSRC_SECP256K1)/.src:
-	${call log.line, Cloning $(REPO_SECP256K1)...}
-	$(PRECMD)git clone --depth 1 $(REPO_SECP256K1) $(DSRC_SECP256K1)
-	$(PRECMD)git -C $(DSRC_SECP256K1) fetch --depth 1 $(DSRC_SECP256K1) $(VERSION_SECP256k1)
-	$(PRECMD)touch $@
