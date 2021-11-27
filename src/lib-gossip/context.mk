24e1165d01d09d4c96775cfe416389dde9f4ca99
cbleser <cr@tagion.org>
2021-11-27 17:27:52 +0200

    WIP: Update for the test-bench

--- src/lib-gossip/context.mk
+++ src/lib-gossip/context.mk
@@ -6,9 +6,9 @@ DEPS += lib-dart
 DEPS += lib-communication
 DEPS += lib-p2pgowrapper
 
-${call config.lib, gossip}: LOOKUP := tagion/gossip/*.d
+libgossip.configure: SOURCE := tagion/**/*.d
 
-${call lib, gossip}: LINKFILES := $(DIR_BUILD_WRAPS)/secp256k1/lib/libsecp256k1.a
-${call lib, gossip}: LINKFILES += $(DIR_BUILD_WRAPS)/openssl/lib/libssl.a
-${call lib, gossip}: LINKFILES += $(DIR_BUILD_WRAPS)/openssl/lib/libcrypto.a
-${call lib, gossip}: LINKFILES += $(DIR_BUILD_WRAPS)/p2pgowrapper/lib/libp2pgowrapper.a
+$(DBIN)/libgossip.test: $(DIR_BUILD_WRAPS)/secp256k1/lib/libsecp256k1.a
+$(DBIN)/libgossip.test: $(DIR_BUILD_WRAPS)/openssl/lib/libssl.a
+$(DBIN)/libgossip.test: $(DIR_BUILD_WRAPS)/openssl/lib/libcrypto.a
+$(DBIN)/libgossip.test: $(DIR_BUILD_WRAPS)/p2pgowrapper/lib/libp2pgowrapper.a
