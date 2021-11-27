4275d213cdf7b1b03b451c769331c951788e7f4e
cbleser <cr@tagion.org>
2021-11-27 17:27:52 +0200

    WIP: Update for the test-bench

--- src/lib-network/context.mk
+++ src/lib-network/context.mk
@@ -1,3 +1,4 @@
+DEPS += lib-options
 DEPS += lib-hibon
 DEPS += lib-communication
 DEPS += lib-crypto
@@ -5,9 +6,9 @@ DEPS += lib-services
 DEPS += lib-hashgraph
 DEPS += lib-p2pgowrapper
 
-${call config.lib, network}: LOOKUP := tagion/**/*.d
+libnetwork.configure: SOURCE := tagion/**/*.d
 
-${call lib, network}: LINKFILES += $(DIR_BUILD_WRAPS)/secp256k1/lib/libsecp256k1.a
-${call lib, network}: LINKFILES += $(DIR_BUILD_WRAPS)/openssl/lib/libcrypto.a
-${call lib, network}: LINKFILES += $(DIR_BUILD_WRAPS)/openssl/lib/libssl.a
-${call lib, network}: LINKFILES += $(DIR_BUILD_WRAPS)/p2pgowrapper/lib/libp2pgowrapper.a
+$(DBIN)/libnetwork.test: $(DIR_BUILD_WRAPS)/secp256k1/lib/libsecp256k1.a
+$(DBIN)/libnetwork.test: $(DIR_BUILD_WRAPS)/openssl/lib/libcrypto.a
+$(DBIN)/libnetwork.test: $(DIR_BUILD_WRAPS)/openssl/lib/libssl.a
+$(DBIN)/libnetwork.test: $(DIR_BUILD_WRAPS)/p2pgowrapper/lib/libp2pgowrapper.a
