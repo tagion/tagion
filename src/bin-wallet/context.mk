5c5fd05c9c77fe7194de7642313dceaad3271ca2
Vlad Pazych <vladpazych@gmail.com>
2021-11-29 19:37:19 +0200

    wip: Fix context files

--- src/bin-wallet/context.mk
+++ src/bin-wallet/context.mk
@@ -2,10 +2,12 @@ DEPS += lib-wallet
 DEPS += lib-communication
 DEPS += lib-network
 DEPS += lib-crypto
-PROGRAM:=tagionwallet
+
+PROGRAM := tagionwallet
+
+$(PROGRAM).configure: SOURCE := tagion/*.d
 
 $(DBIN)/$(PROGRAM): $(DTMP)/libsecp256k1.a
 $(DBIN)/$(PROGRAM): $(DTMP)/libssl.a
 $(DBIN)/$(PROGRAM): $(DTMP)/libcrypto.a
 
-$(PROGRAM).configure: SOURCE := tagion/*.d
