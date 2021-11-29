123a455a5edfb663d0deca5cf1974613ef9d1f1d
Vlad Pazych <vladpazych@gmail.com>
2021-11-29 19:37:19 +0200

    wip: Fix context files

--- src/bin-boot/context.mk
+++ src/bin-boot/context.mk
@@ -1,11 +1,12 @@
 DEPS += lib-dart
 DEPS += lib-crypto
 DEPS += lib-logger
-PROGRAM:=boot
 
-${call config.bin, $(PROGRAM)}: LOOKUP := tagion/*.d
+PROGRAM:=tagionboot
 
-${call bin, $(PROGRAM)}: INFILES += $(DIR_BUILD_WRAPS)/secp256k1/lib/libsecp256k1.a
-${call bin, $(PROGRAM)}: INFILES += $(DIR_BUILD_WRAPS)/openssl/lib/libssl.a
-${call bin, $(PROGRAM)}: INFILES += $(DIR_BUILD_WRAPS)/openssl/lib/libcrypto.a
-${call bin, $(PROGRAM)}: INFILES += $(DIR_BUILD_WRAPS)/p2pgowrapper/lib/libp2pgowrapper.a
+$(PROGRAM).configure: SOURCE := tagion/*.d
+
+$(DBIN)/$(PROGRAM): $(DTMP)/libsecp256k1.a
+$(DBIN)/$(PROGRAM): $(DTMP)/libssl.a
+$(DBIN)/$(PROGRAM): $(DTMP)/libcrypto.a
+$(DBIN)/$(PROGRAM): $(DTMP)/libp2pgowrapper.a
