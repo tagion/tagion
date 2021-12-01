a897bfbc2b2dbb47222f4a78c6c28160461a3e76
Vlad Pazych <vladpazych@gmail.com>
2021-12-01 18:54:12 +0200

    refactor: Improve context file format

--- src/wrap-secp256k1/cross.mk
+++ src/wrap-secp256k1/cross.mk
@@ -1,3 +1,15 @@
 CONFIGUREFLAGS_SECP256K1 += --host=$(MTRIPLE)
 CONFIGUREFLAGS_SECP256K1 += --target=$(MTRIPLE)
 CONFIGUREFLAGS_SECP256K1 += --with-sysroot=$(CROSS_SYSROOT)
+
+ifdef CROSS_ENABLED
+
+ifeq ($(findstring ios,$(CROSS_OS)),ios)
+include ${call dir.resolve, cross.ios.mk}
+endif
+
+ifeq ($(findstring android,$(CROSS_OS)),android)
+include ${call dir.resolve, cross.android.mk}
+endif
+
+endif
