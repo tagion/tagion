1b9f8578b26573f938f57303492fb0110a469f78
Vlad Pazych <vladpazych@gmail.com>
2021-12-01 18:54:12 +0200

    refactor: Improve context file format

---
+++ src/wrap-openssl/cross.mk
@@ -0,0 +1,15 @@
+CONFIGUREFLAGS_OPENSSL += --host=$(MTRIPLE)
+CONFIGUREFLAGS_OPENSSL += --target=$(MTRIPLE)
+CONFIGUREFLAGS_OPENSSL += --with-sysroot=$(CROSS_SYSROOT)
+
+ifdef CROSS_ENABLED
+
+ifeq ($(findstring ios,$(CROSS_OS)),ios)
+include ${call dir.resolve, cross.ios.mk}
+endif
+
+ifeq ($(findstring android,$(CROSS_OS)),android)
+include ${call dir.resolve, cross.android.mk}
+endif
+
+endif
