25b53df05545cfed74bd34d42d2469f6b07e46e4
Vlad Pazych <vladpazych@gmail.com>
2021-12-01 18:54:12 +0200

    refactor: Improve context file format

--- src/lib-network/context.mk
+++ src/lib-network/context.mk
@@ -6,9 +6,11 @@ DEPS += lib-services
 DEPS += lib-hashgraph
 DEPS += lib-p2pgowrapper
 
-libnetwork.configure: SOURCE := tagion/**/*.d
+PROGRAM := libnetwork
 
-$(DBIN)/libnetwork.test: $(DIR_BUILD_WRAPS)/secp256k1/lib/libsecp256k1.a
-$(DBIN)/libnetwork.test: $(DIR_BUILD_WRAPS)/openssl/lib/libcrypto.a
-$(DBIN)/libnetwork.test: $(DIR_BUILD_WRAPS)/openssl/lib/libssl.a
-$(DBIN)/libnetwork.test: $(DIR_BUILD_WRAPS)/p2pgowrapper/lib/libp2pgowrapper.a
+$(PROGRAM).configure: SOURCE := tagion/**/*.d
+
+$(DBIN)/$(PROGRAM).test: $(DTMP)/libsecp256k1.a
+$(DBIN)/$(PROGRAM).test: $(DTMP)/libssl.a
+$(DBIN)/$(PROGRAM).test: $(DTMP)/libcrypto.a
+$(DBIN)/$(PROGRAM).test: $(DTMP)/libp2pgowrapper.a
