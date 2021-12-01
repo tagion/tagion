86e0acfb1d54d4d2fa5c012df9cb937adf2f6182
Vlad Pazych <vladpazych@gmail.com>
2021-12-01 18:54:12 +0200

    refactor: Improve context file format

--- src/wrap-secp256k1/src/wrap-secp256k1/context.mk
+++ src/wrap-secp256k1/src/wrap-secp256k1/context.mk
@@ -1,8 +1,5 @@
 DEPS += wrap-openssl
 
-REPO_SECP256K1 ?= git@github.com:tagion/fork-secp256k1.git
-VERSION_SECP256k1 := cd07f5a72243a8f343679aa81ed0d0cb662ba90e
-
 DSRC_SECP256K1 := ${call dir.resolve, src}
 DTMP_SECP256K1 := $(DTMP)/secp256k1
 
@@ -13,15 +10,7 @@ CONFIGUREFLAGS_SECP256K1 += --enable-module-schnorrsig
 CONFIGUREFLAGS_SECP256K1 += --enable-shared=no
 CONFIGUREFLAGS_SECP256K1 += CRYPTO_LIBS=$(DTMP)/ CRYPTO_CFLAGS=$(DSRC_OPENSSL)/include/
 
-ifdef CROSS_ENABLED
 include ${call dir.resolve, cross.mk}
-ifeq ($(findstring ios,$(CROSS_OS)),ios)
-include ${call dir.resolve, cross.ios.mk}
-endif
-ifeq ($(findstring android,$(CROSS_OS)),android)
-include ${call dir.resolve, cross.android.mk}
-endif
-endif
 
 secp256k1: $(DTMP)/libsecp256k1.a
 	@
@@ -35,7 +24,7 @@ clean-secp256k1: TOCLEAN := $(TOCLEAN_SECP256K1)
 clean-secp256k1: clean
 	@
 
-$(DTMP)/libsecp256k1.a: $(DTMP)/.way 
+$(DTMP)/libsecp256k1.a: $(DTMP)/.way $(DTMP)/libssl.a $(DTMP)/libcrypto.a
 	$(PRECMD)$(CP) $(DSRC_SECP256K1) $(DTMP_SECP256K1)
 	$(PRECMD)cd $(DTMP_SECP256K1); ./autogen.sh
 	$(PRECMD)cd $(DTMP_SECP256K1); ./configure $(CONFIGUREFLAGS_SECP256K1)
--- src/wrap-secp256k1/src/wrap-secp256k1/cross.android.mk
+++ src/wrap-secp256k1/src/wrap-secp256k1/cross.android.mk
@@ -1,10 +1,10 @@
-CONFIGUREFLAGS_SECP256K1 += AR=$(TOOLCHAIN)/bin/llvm-ar
-CONFIGUREFLAGS_SECP256K1 += CC=$(TOOLCHAIN)/bin/$(MTRIPLE)$(CROSS_ANDROID_API)-clang
-CONFIGUREFLAGS_SECP256K1 += AS=$(TOOLCHAIN)/bin/$(MTRIPLE)$(CROSS_ANDROID_API)-clang
-CONFIGUREFLAGS_SECP256K1 += CXX=$(TOOLCHAIN)/bin/$(MTRIPLE)$(CROSS_ANDROID_API)-clang++
-CONFIGUREFLAGS_SECP256K1 += LD=$(TOOLCHAIN)/bin/ld
-CONFIGUREFLAGS_SECP256K1 += RANLIB=$(TOOLCHAIN)/bin/llvm-ranlib
-CONFIGUREFLAGS_SECP256K1 += STRIP=$(TOOLCHAIN)/bin/llvm-strip
-# CONFIGUREFLAGS_SECP256K1 += CFLAGS="$(CFLAGS) -sysroot $(CROSS_SYSROOT)"
-
-# CFLAGS="-mthumb -march=armv7-a" CCASFLAGS="-Wa,-mthumb -Wa,-march=armv7-a"
+# CONFIGUREFLAGS_SECP256K1 += --enable-ecmult-static-precomputation --enable-experimental --with-asm=arm
+# export AR=$(CROSS_TOOLCHAIN)/llvm-ar
+# export CC=$(CROSS_TOOLCHAIN)/$(MTRIPLE)$(CROSS_ANDROID_API)-clang
+# export AS=$(CROSS_TOOLCHAIN)/$(MTRIPLE)$(CROSS_ANDROID_API)-clang
+# export CXX="$(CROSS_TOOLCHAIN)/$(MTRIPLE)$(CROSS_ANDROID_API)-clang++"
+# export LD=$(CROSS_TOOLCHAIN)/ld
+# export RANLIB=$(CROSS_TOOLCHAIN)/llvm-ranlib
+# export STRIP=$(CROSS_TOOLCHAIN)/llvm-strip
+# # export CFLAGS="-mthumb -march=$(CROSS_ARCH)"
+# # export CCASFLAGS="-Wa,-mthumb -Wa,-march=$(CROSS_ARCH)"
