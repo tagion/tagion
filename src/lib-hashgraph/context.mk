4daa271e51993980a310149a486020b855246b3f
Vlad Pazych <vladpazych@gmail.com>
2021-12-01 16:13:49 +0200

    fix: Fix lib paths

--- src/lib-hashgraph/context.mk
+++ src/lib-hashgraph/context.mk
@@ -7,7 +7,7 @@ DEPS += lib-gossip
 
 libhashgraph.configure: SOURCE := tagion/**/*.d
 
-$(DBIN)/libhashgraph.test: $(DIR_BUILD_WRAPS)/secp256k1/lib/libsecp256k1.a
-$(DBIN)/libhashgraph.test: $(DIR_BUILD_WRAPS)/openssl/lib/libssl.a
-$(DBIN)/libhashgraph.test: $(DIR_BUILD_WRAPS)/openssl/lib/libcrypto.a
-$(DBIN)/libhashgraph.test: $(DIR_BUILD_WRAPS)/p2pgowrapper/lib/libp2pgowrapper.a
+$(DBIN)/libhashgraph.test: $(DTMP)/libsecp256k1.a
+$(DBIN)/libhashgraph.test: $(DTMP)/libssl.a
+$(DBIN)/libhashgraph.test: $(DTMP)/libcrypto.a
+$(DBIN)/libhashgraph.test: $(DTMP)/libp2pgowrapper.a
