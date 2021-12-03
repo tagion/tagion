3302f36757dc72f8e2f2c3b37d91b4291e30ccc6
Vlad Pazych <vladpazych@gmail.com>
2021-12-03 14:18:57 +0200

    refactor: Improve clean process

--- tub/tub/clean.mk
+++ tub/tub/clean.mk
@@ -1,10 +1,12 @@
-.PHONY: clean
-
-clean-build: TOCLEAN := $(DTMP)/*.o
-clean-build: TOCLEAN += $(DBIN)
-clean-build: clean
+proper: TOCLEAN := $(DTMP)
+proper: TOCLEAN += $(DBIN)
+proper: TOCLEAN += $(TOPROPER)
+proper: clean
 	@
 
+.PHONY: clean
+clean: TOCLEAN += $(DTMP)/*.o
+clean: TOCLEAN += $(DBIN)
 clean:
 	${call log.header, clean}
 	$(PRECMD)${foreach _, $(TOCLEAN), $(RMDIR) $(_) > /dev/null || true;}
