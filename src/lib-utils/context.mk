abb2c8095fdc4388d89052bd26d2f92f730e931f
Vlad Pazych <vladpazych@gmail.com>
2021-12-06 15:56:24 +0200

    fix: Use term.d only for desktop builds

--- src/lib-utils/context.mk
+++ src/lib-utils/context.mk
@@ -3,3 +3,9 @@ DEPS += lib-basic
 PROGRAM := libutils
 
 $(PROGRAM).configure: SOURCE := tagion/**/*.d
+
+# Since we only cross-compile for mobile, we can switch off
+# Term.d when cross compilation mode is enabled
+ifndef CROSS_ENABLED
+DCFLAGS+=$(DVERSION)=DESKTOP
+endif
