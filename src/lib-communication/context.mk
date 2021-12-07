35b78146e732a7f9ec6592b18f66834ad1f5b6ed
Vlad Pazych <vladpazych@gmail.com>
2021-12-07 18:12:54 +0200

    wip: Add files to link

--- src/lib-communication/context.mk
+++ src/lib-communication/context.mk
@@ -4,3 +4,6 @@ DEPS += lib-crypto
 PROGRAM := libcommunication
 
 $(PROGRAM).configure: SOURCE := tagion/**/*.d
+
+$(DBIN)/$(PROGRAM).test: $(DTMP)/libsecp256k1.a
+$(DBIN)/$(PROGRAM).test: $(DTMP)/libcrypto.a
