ba7af8cf9b1104fecfbd85466189b6b03accd661
cbleser <cr@tagion.org>
2021-12-09 10:33:20 +0200

    WIP: tagionwallet now links (dummy import of options package added)

--- src/bin-wallet/src/bin-wallet/context.mk
+++ src/bin-wallet/src/bin-wallet/context.mk
@@ -2,6 +2,7 @@ DEPS += lib-wallet
 DEPS += lib-communication
 DEPS += lib-network
 DEPS += lib-crypto
+DEPS += lib-options
 
 PROGRAM := tagionwallet
 
@@ -10,4 +11,3 @@ $(PROGRAM).configure: SOURCE := tagion/*.d
 $(DBIN)/$(PROGRAM): $(DTMP)/libsecp256k1.a
 $(DBIN)/$(PROGRAM): $(DTMP)/libssl.a
 $(DBIN)/$(PROGRAM): $(DTMP)/libcrypto.a
-
