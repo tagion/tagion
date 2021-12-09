2da3a258716eb028e2edf3b8c70fcac2503a6901
Vlad Pazych <vladpazych@gmail.com>
2021-12-09 09:19:34 +0200

    wip: Simplify first initialization

--- tub/help.mk
+++ tub/help.mk
@@ -11,15 +11,18 @@ main_help:
 	$(PRECMD)
 	echo
 	echo $(SEP) help
-	echo "make help: Show this help"
-	echo "make env: Show Make variables"
-	echo "make clone-* BRANCH=<branch>: Clone specific unit"
+	echo "make init: First-time tub initialization (required)"
+	echo 
 	echo "make configure: Configure units to compile"
 	echo "make lib*: Compile src/lib-*"
 	echo "make lib*.test: Compile and execute tests for src/lib-*"
 	echo "make tagion*: Compile src/bin-*"
 	echo "make *: compile wrapped library | example: make secp256k1"
 	echo "make clean: Clean built and generated files"
+	echo
+	echo "make help: Show this help"
+	echo "make env: Show Make variables"
+	echo "make clone-* BRANCH=<branch>: Clone specific unit"
 	echo
 	echo "README: $(DTUB)/README.md"
 	echo "Branch: $(TUB_BRANCH)"
