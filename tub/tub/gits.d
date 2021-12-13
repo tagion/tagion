68350963722f0f7e06a243ffb5b83e16a255d183
cbleser <cr@tagion.org>
2021-12-13 16:41:32 +0200

    WIP: gits now runs multi-processing

--- tub/tub/gits.d
+++ tub/tub/gits.d
@@ -199,7 +199,7 @@ struct Git {
         }
         void doit(ref PidInfo pid_info, const(string[]) cmds, const(string) name, const(string) path) {
             stdout.writefln("Git '%s'", name);
-            writefln("cmds %s", cmds);
+            //writefln("cmds %s", cmds);
             pid_info.pid =spawnProcess(
                 cmds,
                 stdin,
