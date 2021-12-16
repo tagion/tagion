tagion/core-lib-basic

refactor: Improve context file format

context.mk
@@ -1,3 +1,5 @@
 DEPS += lib-logger
 
-libbasic.configure: SOURCE := tagion/**/*.d tagion/*.d
+PROGRAM := libbasic
+
+$(PROGRAM).configure: SOURCE := tagion/**/*.d tagion/*.d

tagion/tagion

src/lib-logger/context.mk
@@ -1,3 +1,5 @@
 DEPS += lib-basic
 
-liblogger.configure: SOURCE := tagion/**/*.d
+PROGRAM := liblogger
+
+$(PROGRAM).configure: SOURCE := tagion/**/*.d