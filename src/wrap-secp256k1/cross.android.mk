dcec770fc5d29ae6edb2c8d5a0f8a919651f1bfd
Vlad Pazych <vladpazych@gmail.com>
2021-12-16 11:13:58 +0000

    fix: Restore cross-compilation flags for Android

--- src/wrap-secp256k1/cross.android.mk
+++ src/wrap-secp256k1/cross.android.mk
@@ -1,10 +1,14 @@
-# CONFIGUREFLAGS_SECP256K1 += --enable-ecmult-static-precomputation --enable-experimental --with-asm=arm
+CONFIGUREFLAGS_SECP256K1 += --enable-ecmult-static-precomputation --enable-experimental --with-asm=arm
+
+export CC=$(CROSS_TOOLCHAIN)/$(MTRIPLE)$(CROSS_ANDROID_API)-clang
+
+# Flags that are likely not needed, but kept as knowledge
+# in case something breaks:
 # export AR=$(CROSS_TOOLCHAIN)/llvm-ar
-# export CC=$(CROSS_TOOLCHAIN)/$(MTRIPLE)$(CROSS_ANDROID_API)-clang
 # export AS=$(CROSS_TOOLCHAIN)/$(MTRIPLE)$(CROSS_ANDROID_API)-clang
 # export CXX="$(CROSS_TOOLCHAIN)/$(MTRIPLE)$(CROSS_ANDROID_API)-clang++"
 # export LD=$(CROSS_TOOLCHAIN)/ld
 # export RANLIB=$(CROSS_TOOLCHAIN)/llvm-ranlib
 # export STRIP=$(CROSS_TOOLCHAIN)/llvm-strip
-# # export CFLAGS="-mthumb -march=$(CROSS_ARCH)"
-# # export CCASFLAGS="-Wa,-mthumb -Wa,-march=$(CROSS_ARCH)"
+# export CFLAGS="-mthumb -march=$(CROSS_ARCH)"
+# export CCASFLAGS="-Wa,-mthumb -Wa,-march=$(CROSS_ARCH)"
