d0610cbaa7ca32204035eba829971066e1845a1a
cbleser <cr@tagion.org>
2021-12-20 09:38:06 +0200

    WIP: secp256k1 now cross-compiles to Andriod-aarch64 on Linux

--- src/wrap-secp256k1/cross.android.mk
+++ src/wrap-secp256k1/cross.android.mk
@@ -1,6 +1,9 @@
-CONFIGUREFLAGS_SECP256K1 += --enable-ecmult-static-precomputation --enable-experimental --with-asm=arm
+CONFIGUREFLAGS_SECP256K1 += --enable-ecmult-static-precomputation --enable-experimental
+# Note. The asm pare does not compile for aarch64
+# So it is not enabled.
+#--with-asm=arm
 
-export CC=$(CROSS_TOOLCHAIN)/$(MTRIPLE)$(CROSS_ANDROID_API)-clang
+CONFIGUREFLAGS_SECP256K1 += CC=$(CROSS_TOOLCHAIN)/$(MTRIPLE)$(CROSS_ANDROID_API)-clang
 
 # Flags that are likely not needed, but kept as knowledge
 # in case something breaks:
