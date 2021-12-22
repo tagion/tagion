011a65c50bdc81075d768d386c92e18cb00e029c
Ivan Bilan <ib@i25s.com>
2021-12-22 13:53:20 +0200

    WIP: added fiters to LoggerService

--- src/lib-hashgraph/tagion/hashgraph/Event.d
+++ src/lib-hashgraph/tagion/hashgraph/Event.d
@@ -27,7 +27,7 @@ import tagion.utils.StdTime;
 import tagion.basic.Basic: this_dot, basename, Pubkey, Buffer, bitarray_clear, bitarray_change, EnumText, buf_idup;
 import tagion.Keywords: Keywords;
 
-import tagion.basic.Logger;
+import tagion.logger.Logger;
 import tagion.hashgraph.HashGraphBasic: isMajority, isAllVotes, EventBody, EventPackage, EvaPayload, Tides, EventMonitorCallbacks;
 import tagion.hashgraph.HashGraph: HashGraph;
 import tagion.utils.BitMask: BitMask;
