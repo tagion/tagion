aeae1a829b5a1cbbee96d9c0d90e68f7dd2c7859
cbleser <cr@tagion.org>
2022-01-14 12:23:25 +0200

    WIP: dstep.mk updated

--- src/lib-mobile/build.mk
+++ src/lib-mobile/build.mk
@@ -1,5 +1,15 @@
 CROSS_LD:=$(CROSS_TOOLCHAIN)/ld
 
+LDCRUNTIME:=$(HOME)/work/ldc-runtime/ldc-build-runtime.tmp/lib/
+LIBDRUNTIME+=libdruntime-ldc.a
+LIBDRUNTIME+=libphobos2-ldc.a
+LIBDRUNTIME_STATIC=${addprefix $(LDCRUNTIME),$(LIBDRUNTIME)}
+
+LDC2ALL+=${shell find /home/carsten/work/ldc-runtime/ldc-build-runtime.tmp/CMakeFiles/ -name "*.o" -not -path "*debug*"}
+
+LDC2ALL+=${shell find /home/carsten/work/ldc-runtime/ldc-build-runtime.tmp/objects/ -name "*.o"}
+
+
 LIBMOBILE:=libmobile.so
 LIBMOBILE_SRC_DIR:=$(DSRC)/lib-mobile
 LIBMOBILE_SRC:=$(LIBMOBILE_SRC_DIR)/tagion/mobile/package.d
@@ -15,11 +25,6 @@ DCFALGS_INC=${addprefix -I,$(DINC)}
 
 #DFILES=${shell find $(DSRC) -name "*.d" -o -name "*.di" }
 DFILES=${shell find $(DSRC) -path "*/lib-*" -a -name "*.d" }
-
-#DDFILES=${wildcard %.d}
-#DCFLAGS+=$(DVERSION)=TINY_AES
-#endef
-
 
 libmobileinfo:
 	$(PRECMD)
@@ -48,6 +53,7 @@ include $(LIBMOBILE_DEPS)
 
 #$(LIBMOBILE): LDCFLAGS+=$(LINKERFLAG)-L$(DTMP)/libsecp256k1.so
 
+
 $(LIBMOBILE): LDCFLAGS+=$(LINKERFLAG)-lsecp256k1
 
 #$(LIBMOBILE): LDCFLAGS+=-c
@@ -62,9 +68,13 @@ $(LIBMOBILE): $(DTMP)/libsecp256k1.so
 $(LIBMOBILE): $(LIBMOBILE_SRC)
 	$(PRECMD)
 	$(DC) -c $(DCFLAGS) $(DCCROSS_FLAGS) $(DCFALGS_INC) ${filter $(DFILES),${abspath $?}} ${filter %.a, $(abspath $?)}  $(LDCFLAGS) -od=/tmp/obj
-	$(CROSS_LD) /tmp/obj/*.o --shared -o $@
+	$(CROSS_LD) /tmp/obj/*.o $(LDC2ALL) \
+        --shared -o $@
 
-	#$(CROSS_LD) /tmp/obj/*.o --shared -o $@
+#-L-Wl,--whole-archive \
+#        $(LIBDRUNTIME_STATIC) \
+#        -L-Wl,--no-whole-archive \
+#        --shared -o $@
 
 endif
 
@@ -82,11 +92,6 @@ ${LIBMOBILE_TRIAL_DEPS:.mk=.knot.mk}: $(LIBMOBILE_SRC)
 
 %.mk: %.knot.mk
 
-#else
-#$(LIBMOBILE_TRIAL_DEPS):
-#	echo ok
-
-#endif
 else
 include $(LIBMOBILE_TRIAL_DEPS)
 
@@ -118,3 +123,7 @@ clean-trail:
 
 clean-libmobile:
 	rm -f $(LIBMOBILE_DEPS)
+
+
+yyyy:
+	@echo $(LDC2ALL)
