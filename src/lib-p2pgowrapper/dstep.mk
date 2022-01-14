b1aa98f77a4be2bcc7fff3b97f4647508fef5d3c
cbleser <cr@tagion.org>
2022-01-14 12:23:25 +0200

    WIP: dstep.mk updated

--- src/lib-p2pgowrapper/dstep.mk
+++ src/lib-p2pgowrapper/dstep.mk
@@ -1,21 +1,18 @@
+
+
 LP2PGOWRAPPER_DIROOT := ${call dir.resolve, p2p/cgo}
 LP2PGOWRAPPER_DSTEPINC := $(DTMP_P2PGOWRAPPER)
-LP2PGOWRAPPER_DIFILES := $(LP2PGOWRAPPER_DIROOT)/libp2pgowrapper.di $(LP2PGOWRAPPER_DIROOT)/c_helper.di
-LP2PGOWRAPPER_DSTEPFLAGS :=
 LP2PGOWRAPPER_PACKAGE := p2p.cgo
 
-$(LP2PGOWRAPPER_DIROOT)/libp2pgowrapper.di: $(LP2PGOWRAPPER_DSTEPINC)/libp2pgowrapper.h $(LP2PGOWRAPPER_DIROOT)/%.way
-	${call log.kvp, $(@F)}
-	${call log.lines, $<}
-	${call log.lines, $@}
-	$(PRECMD)$(DSTEP) $(LP2PGOWRAPPER) --package "$(LP2PGOWRAPPER_PACKAGE)" --global-import p2p.cgo.c_helper $< -o $@
+LP2PGOWRAPPER_DFILES := ${wildcard ${call dir.resolve, p2p}/*.d}
 
-$(LP2PGOWRAPPER_DIROOT)/c_helper.di: $(LP2PGOWRAPPER_DSTEPINC)/c_helper.h $(LP2PGOWRAPPER_DIROOT)/%.way
-	${call log.kvp, $@(F)}
-	${call log.lines, $<}
-	${call log.lines, $@}
-	$(PRECMD)$(DSTEP) $(LP2PGOWRAPPER) --package "$(LP2PGOWRAPPER_PACKAGE)" $< -o $@
+$(LP2PGOWRAPPER_DIROOT)/libp2pgowrapper.di: DSTEPFLAGS += --global-import=$(LP2PGOWRAPPER_PACKAGE).c_helper
+
+${call DSTEP_DO,$(LP2PGOWRAPPER_PACKAGE),$(DTMP_P2PGOWRAPPER),$(LP2PGOWRAPPER_DIROOT),$(LP2PGOWRAPPER_DFILES)}
 
 # libp2pgowrapper.h is generated during compilation of go library
 $(DTMP_P2PGOWRAPPER)/%.h: $(DTMP)/libp2pgowrapper.a
 	@
+
+test88:
+	echo $(DTMP_P2PGOWRAPPER)
