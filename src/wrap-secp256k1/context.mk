a0049e1638ca4e1d309bdfe1602fe7906b50baa1
cbleser <cr@tagion.org>
2022-01-21 08:36:37 +0200

    WIP: Make-flow dependency of  D .o-file on external .a

--- src/wrap-secp256k1/context.mk
+++ src/wrap-secp256k1/context.mk
@@ -1,12 +1,14 @@
 
 
 ifdef SHARED
-LIBSECP256K1:=libsecp256k1.so
+LIBSECP256K1_NAME:=libsecp256k1.$(DLLEXT)
 CONFIGUREFLAGS_SECP256K1 += --enable-shared=yes
 else
-LIBSECP256K1:=libsecp256k1.a
+LIBSECP256K1_NAME:=libsecp256k1.$(LIBEXT)
 CONFIGUREFLAGS_SECP256K1 += --enable-shared=on
 endif
+
+LIBSECP256K1:=$(DTMP)/$(LIBSECP256K1_NAME)
 
 DEPS += wrap-openssl
 DSRC_SECP256K1 := ${call dir.resolve, src}
@@ -16,34 +18,46 @@ CONFIGUREFLAGS_SECP256K1 += --enable-module-ecdh
 CONFIGUREFLAGS_SECP256K1 += --enable-experimental
 CONFIGUREFLAGS_SECP256K1 += --enable-module-recovery
 CONFIGUREFLAGS_SECP256K1 += --enable-module-schnorrsig
-#CONFIGUREFLAGS_SECP256K1 += --enable-shared=no
 CONFIGUREFLAGS_SECP256K1 += CRYPTO_LIBS=$(DTMP)/ CRYPTO_CFLAGS=$(DSRC_OPENSSL)/include/
-
+CONFIGUREFLAGS_SECP256K1 += --prefix=$(DLIB)
 include ${call dir.resolve, cross.mk}
 
-secp256k1: $(DTMP)/$(LIBSECP256K1)
+secp256k1: $(LIBSECP256K1)
 	@
 
-TOCLEAN_SECP256K1 += $(DTMP)/$(LIBSECP256K1)
-TOCLEAN_SECP256K1 += $(DTMP_SECP256K1)
+clean-secp256k1:
+	$(RM) $(LIBSECP256K1)
+	$(RMDIR) $(DTMP_SECP256K1)
 
-clean-secp256k1: TOCLEAN := $(TOCLEAN_SECP256K1)
-clean-secp256k1: clean
-	@
+proper: clean-secp256k1
 
-$(DTMP)/$(LIBSECP256K1): $(DTMP)/.way
-	echo $@
-	$(PRECMD)$(CP) $(DSRC_SECP256K1) $(DTMP_SECP256K1)
-	$(PRECMD)cd $(DTMP_SECP256K1); ./autogen.sh
-	$(PRECMD)cd $(DTMP_SECP256K1); ./configure $(CONFIGUREFLAGS_SECP256K1)
-	$(PRECMD)cd $(DTMP_SECP256K1); make clean
-	$(PRECMD)cd $(DTMP_SECP256K1); make $(SUBMAKE_PARALLEL)
-	$(PRECMD)cd $(DTMP_SECP256K1); mv .libs/$(LIBSECP256K1) $@
 
-MAKE_ENV += env-secp256k1
+$(LIBSECP256K1): $(DTMP)/.way $(DLIB)/.way
+	$(PRECMD)
+	${call log.kvp, $@}
+	$(CP) $(DSRC_SECP256K1) $(DTMP_SECP256K1)
+	$(CD) $(DTMP_SECP256K1)
+	./autogen.sh
+	./configure $(CONFIGUREFLAGS_SECP256K1)
+	$(MAKE) clean
+	$(MAKE) $(SUBMAKE_PARALLEL)
+#	ln -s .libs/lib* $(@D)
+
 env-secp256k1:
 	$(PRECMD)
 	$(call log.header, env :: secp256k1)
-	$(call log.kvp, CONFIGUREFLAGS_SECP256K1)
-	$(call log.lines, $(CONFIGUREFLAGS_SECP256K1))
+	$(call log.kvp, $(CONFIGUREFLAGS_SECP256K1))
 	$(call log.close)
+
+env: env-secp256k1
+
+help-secp256k1:
+	$(PRECMD)
+	${call log.header, $@ :: env}
+	${call log.help, "make help-secp256k1", "Will display this part"}
+	${call log.help, "make clean-secp256k1", "Clean all secp256k1 objects and libraries"}
+	${call log.help, "make env-secp256k1", "List all secp256k1 build environment"}
+	${call log.help, "make prober", "Also executes make clean-secp256k1"}
+	${call log.close}
+
+help: help-secp256k1
