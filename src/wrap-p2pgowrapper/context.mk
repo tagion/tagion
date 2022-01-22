f24c2b66192250ea22ab3c77aa149ab6fdfc7336
cbleser <cr@tagion.org>
2022-01-22 16:04:03 +0200

    WIP: c_helper.di and libp2pgowrapper.di now depends on libp2pgowrapper

--- src/wrap-p2pgowrapper/context.mk
+++ src/wrap-p2pgowrapper/context.mk
@@ -1,35 +1,66 @@
 DSRC_P2PGOWRAPPER := ${call dir.resolve, src}
 DTMP_P2PGOWRAPPER := $(DTMP)/p2pgowrapper
 
-
 CONFIGUREFLAGS_P2PGOWRAPPER :=
 
 include ${call dir.resolve, cross.mk}
 
-p2pgowrapper: $(DTMP)/libp2pgowrapper.a
+
 ifdef SHARED
+${error shared not implemented yet}
 LIBP2PGOWRAPPER:=$(DTMP)/libp2pgowrapper.$(DLLEXT)
 else
-LIBP2PGOWRAPPER:=$(DTMP)/libp2pgowrapper.
+LIBP2PGOWRAPPER:=$(DTMP)/libp2pgowrapper.$(LIBEXT)
 endif
-#@
 
-TOCLEAN_P2PGOWRAPPER += $(DTMP)/libp2pgowrapper.a
-TOCLEAN_P2PGOWRAPPER += $(DTMP_P2PGOWRAPPER)
+p2pgowrapper: $(LIBP2PGOWRAPPER)
 
-clean-p2pgowrapper: TOCLEAN := $(TOCLEAN_P2PGOWRAPPER)
-clean-p2pgowrapper: clean
-	@
+clean-p2pgowrapper:
+	$(RM) $(LIBP2PGOWRAPPER)
 
-$(DTMP)/libp2pgowrapper.a: $(DTMP_P2PGOWRAPPER)/.way
+clean: clean-p2pgowrapper
+
+proper-p2pgowrapper:
+	$(RMDIR) $(DTMP_P2PGOWRAPPER)
+
+proper: proper-p2pgowrapper
+
+# libp2pgowrapper.h is generated during compilation of go library
+$(DTMP_P2PGOWRAPPER)/c_helper.h: $(LIBP2PGOWRAPPER)
+
+$(DTMP_P2PGOWRAPPER)/libp2pgowrapper.h: $(LIBP2PGOWRAPPER)
+
+
+
+# $(DTMP_P2PGOWRAPPER)/%.h: $(LIBP2PGOWRAPPER)
+# 	@echo $@
+
+$(LIBP2PGOWRAPPER): $(DTMP_P2PGOWRAPPER)/.way
 	$(PRECMD)$(CP) $(DSRC_P2PGOWRAPPER)/* $(DTMP_P2PGOWRAPPER)
-	$(PRECMD)cd $(DTMP_P2PGOWRAPPER); go build -buildmode=c-archive -o $(DTMP_P2PGOWRAPPER)/libp2pgowrapper.a
-	$(PRECMD)cd $(DTMP_P2PGOWRAPPER); $(MV) $(DTMP_P2PGOWRAPPER)/libp2pgowrapper.a $(DTMP)/libp2pgowrapper.a
+	$(CD) $(DTMP_P2PGOWRAPPER); $(GO) build -buildmode=c-archive -o $(DTMP_P2PGOWRAPPER)/libp2pgowrapper.a
+	$(CD) $(DTMP_P2PGOWRAPPER); $(MV) $(DTMP_P2PGOWRAPPER)/libp2pgowrapper.a $(LIBP2PGOWRAPPER)
 
-# GOLIBP2P=$(DTMP)/libp2p_go.a
+env-p2pgowrapper:
+	$(PRECMD)
+	${call log.header, $@ :: p2pgowrapper}
+	${call log.env, LIBP2PGOWRAPPER, $(LIBP2PGOWRAPPER)}
 
-# ${GOLIBP2P}:
-# 	$(PRECMD)go build -buildmode=c-archive -o ${GOLIBP2P}
+env: env-p2pgowrapper
+
+help-p2pgowrapper:
+	$(PRECMD)
+	${call log.header, $@ :: p2pgowrapper}
+	${call log.help, "make p2pgowrapper", "Builds libp2pgowrapper library "}
+	${call log.help, "make clean-p2pgowrapper", "Cleans the library"}
+	${call log.help, "make prober-p2pgowrapper", "Erase all reletated to p2pgowrapper build"}
+	${call log.help, "make env-p2pgowrapper", " List all p2pgowrapper build environment"}
+	${call log.close}
+
+help: help-p2pgowrapper
 
 
-#test56: $(DTMP)/libp2pgowrapper.a
+test32:
+	@echo $(LIBP2PGOWRAPPER)
+	@echo $(HFILES.p2p.cgo)
+	@echo $(DTMP_P2PGOWRAPPER)
+	@echo $(DTMP_P2PGOWRAPPER)/*.h
