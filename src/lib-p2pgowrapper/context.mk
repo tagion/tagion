acb9fa5f8fb8f389ba9f93272640ee1939579edd
cbleser <cr@tagion.org>
2022-01-22 17:34:45 +0200

    WIP: Make-flow fixed some printouts

--- src/lib-p2pgowrapper/context.mk
+++ src/lib-p2pgowrapper/context.mk
@@ -11,28 +11,12 @@ ifeq ($(OS),darwin)
 $(DBIN)/$(PROGRAM).test: LDCFLAGS += -L-framework -LCoreFoundation -L-framework -LSecurity
 endif
 
-
-#$(LP2PGOWRAPPER_DFILES): ${addprefix $(DTMP_P2PGOWRAPPER)/, c_helper.h libp2pgowrapper.h}
-
+#
+# D files requires c_helper.di libp2pgowrapper.di
+#
 $(LP2PGOWRAPPER_DFILES): ${addprefix $(LP2PGOWRAPPER_DIROOT)/, c_helper.di libp2pgowrapper.di}
-
-#$(DIFILES.p2p.cgo)
-
-#/home/carsten/work/cross_regression/src/lib-p2pgowrapper/p2p/cgo/c_helper.di
-
-#.SECONDEXPANSION:
-
-test22:
-	@echo $(LIBP2PGOWRAPPER)
-	@echo $(LP2PGOWRAPPER_DFILES)
-	@echo $(DIFILES.p2p.cgo)
-	@echo $(addprefix $(LP2PGOWRAPPER_DIROOT), c_helper.di libp2pgowrapper.di)
 
 $(PROGRAM).preconfigure: $(LP2PGOWRAPPER_DIFILES)
 $(PROGRAM).configure: SOURCE := p2p/*.d p2p/cgo/*.di
 
 $(DBIN)/$(PROGRAM).test: $(DTMP)/libp2pgowrapper.a
-
-test31:
-	@echo $(LIBP2PGOWRAPPER)
-	@echo $(HFILES.p2p.cgo)
--- src/lib-p2pgowrapper/dstep.mk
+++ src/lib-p2pgowrapper/dstep.mk
@@ -9,21 +9,3 @@ LP2PGOWRAPPER_DFILES := ${wildcard ${call dir.resolve, p2p}/*.d}
 $(LP2PGOWRAPPER_DIROOT)/libp2pgowrapper.di: DSTEPFLAGS += --global-import=$(LP2PGOWRAPPER_PACKAGE).c_helper
 
 ${call DSTEP_DO,$(LP2PGOWRAPPER_PACKAGE),$(DTMP_P2PGOWRAPPER),$(LP2PGOWRAPPER_DIROOT),$(LP2PGOWRAPPER_DFILES)}
-
-#
-# Pre-declare the header files
-#
-#HFILES.p2p.cgo=${addprefix $(DTMP_P2PGOWRAPPER)/, c_helper.h libp2pgowrapper.h}
-
-#DIFILES.p2p.cgo=${addprefix $(LP2PGOWRAPPER_DIROOT)/, c_helper.di libp2pgowrapper.di}
-
-test34:
-	@echo $(LIBP2PGOWRAPPER)
-	@echo HFILES.p2p.cgo=$(HFILES.p2p.cgo)
-	@echo DIFILES_notdir.p2p.cgo $(DIFILES_notdir.p2p.cgo)
-	@echo DTMP_P2PGOWRAPPER=$(DTMP_P2PGOWRAPPER)
-	@echo "----" ${addprefix $(DTMP_P2PGOWRAPPER)/, c_helper.h libp2pgowrapper.h}
-	@echo $(DIFILES_notdir.p2p.cgo)
-	@echo DIFILES.p2p.cgo=$(DIFILES.p2p.cgo)
-	@echo LP2PGOWRAPPER_DIROOT = $(LP2PGOWRAPPER_DIROOT)
-	@echo ${addprefix $(LP2PGOWRAPPER_DIROOT)/, c_helper.di libp2pgowrapper.di}
