09fb20c812d0565baa09922bea56216c11e5b5d9
cbleser <cr@tagion.org>
2022-01-23 18:12:16 +0200

    WIP: make unittest added

--- src/wrap-secp256k1/context.mk
+++ src/wrap-secp256k1/context.mk
@@ -8,11 +8,14 @@ LIBSECP256K1_NAME:=libsecp256k1.$(LIBEXT)
 CONFIGUREFLAGS_SECP256K1 += --enable-shared=on
 endif
 
-LIBSECP256K1:=$(DTMP)/$(LIBSECP256K1_NAME)
 
 DEPS += wrap-openssl
 DSRC_SECP256K1 := ${call dir.resolve, src}
 DTMP_SECP256K1 := $(DTMP)/secp256k1
+
+LIBSECP256K1:=$(DTMP_SECP256K1)/.libs/$(LIBSECP256K1_NAME)
+
+$(UNITTEST_BIN): LIBS+=$(LIBSECP256K1)
 
 CONFIGUREFLAGS_SECP256K1 += --enable-module-ecdh
 CONFIGUREFLAGS_SECP256K1 += --enable-experimental
@@ -25,14 +28,11 @@ include ${call dir.resolve, cross.mk}
 secp256k1: $(LIBSECP256K1)
 	@
 
-clean-secp256k1:
+proper-secp256k1:
 	$(PRECMD)
 	${call log.header, $@ :: secp256k1}
 	$(RM) $(LIBSECP256K1)
 	$(RMDIR) $(DTMP_SECP256K1)
-
-proper: clean-secp256k1
-
 
 $(LIBSECP256K1): $(DTMP)/.way $(DLIB)/.way
 	$(PRECMD)
@@ -43,13 +43,14 @@ $(LIBSECP256K1): $(DTMP)/.way $(DLIB)/.way
 	./configure $(CONFIGUREFLAGS_SECP256K1)
 	$(MAKE) clean
 	$(MAKE) $(SUBMAKE_PARALLEL)
-#	ln -s .libs/lib* $(@D)
 
 env-secp256k1:
 	$(PRECMD)
-	$(call log.header, env :: secp256k1)
-	$(call log.kvp, $(CONFIGUREFLAGS_SECP256K1))
-	$(call log.close)
+	${call log.header, env :: secp256k1}
+	${call log.env, CONFIGUREFLAGS_SECP256K1, $(CONFIGUREFLAGS_SECP256K1)}
+	${call log.env, LIBSECP256K1, $(LIBSECP256K1)}
+	${call log.env, DTMP_SECP256K1, $(DTMP_SECP256K1)}
+	${call log.close}
 
 env: env-secp256k1
 
@@ -57,9 +58,8 @@ help-secp256k1:
 	$(PRECMD)
 	${call log.header, $@ :: secp256k1}
 	${call log.help, "make help-secp256k1", "Will display this part"}
-	${call log.help, "make clean-secp256k1", "Clean all secp256k1 objects and libraries"}
+	${call log.help, "make proper-secp256k1", "Erase all secp256k1 objects and libraries"}
 	${call log.help, "make env-secp256k1", "List all secp256k1 build environment"}
-	${call log.help, "make prober", "Also executes make clean-secp256k1"}
 	${call log.close}
 
 help: help-secp256k1
