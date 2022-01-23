e85a6db6cae0906f406abafc52dc5f4544b0498c
cbleser <cr@tagion.org>
2022-01-23 13:46:32 +0200

    WIP: Make flow working secp256k1 dependency

--- src/lib-p2pgowrapper/context.mk
+++ src/lib-p2pgowrapper/context.mk
@@ -11,10 +11,11 @@ ifeq ($(OS),darwin)
 $(DBIN)/$(PROGRAM).test: LDCFLAGS += -L-framework -LCoreFoundation -L-framework -LSecurity
 endif
 
+LP2PGOWRAPPER_DIFILES:=${addprefix $(LIBP2PGOWRAPPER_DIROOT)/, c_helper.di libp2pgowrapper.di}
 #
 # D files requires c_helper.di libp2pgowrapper.di
 #
-$(LP2PGOWRAPPER_DFILES): ${addprefix $(LP2PGOWRAPPER_DIROOT)/, c_helper.di libp2pgowrapper.di}
+$(LIBP2PGOWRAPPER_DFILES): ${addprefix $(LIBP2PGOWRAPPER_DIROOT)/, c_helper.di libp2pgowrapper.di}
 
 $(PROGRAM).preconfigure: $(LP2PGOWRAPPER_DIFILES)
 $(PROGRAM).configure: SOURCE := p2p/*.d p2p/cgo/*.di
