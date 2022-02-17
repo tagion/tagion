642b8d13855b1d9cbb3fd36a12dba927dce5986e
AlexeyP-tag <ap@i25s.com>
2022-02-17 12:22:58 +0200

    fix

--- src/lib-crypto/dstep.mk
+++ src/lib-crypto/dstep.mk
@@ -1,32 +1,32 @@
-LCRYPTO_DIROOT := ${call dir.resolve, tagion/crypto/secp256k1/c}
+#
+# Secp256k1 DSTEP headers
+#
 LCRYPTO_PACKAGE := tagion.crypto.secp256k1.c
+LCRYPTO_DIROOT := ${call dir.resolve, tagion/crypto/secp256k1/c}
 
-LCRYPTO_DSTEPINC += $(DSRC_SECP256K1)/include
-LCRYPTO_HFILES += ${wildcard $(LCRYPTO_DSTEPINC)/*.h}
-LCRYPTO_HNOTDIR := ${notdir $(LCRYPTO_HFILES)}
-LCRYPTO_DINOTDIR := ${LCRYPTO_HNOTDIR:.h=.di}
-LCRYPTO_DIFILES := ${addprefix $(LCRYPTO_DIROOT)/,$(LCRYPTO_DINOTDIR)}
+CRYPTO_DFILES := ${shell find ${call dir.resolve, tagion/crypto} -name "*.d"}
 
-LCRYPTO_DSTEPFLAGS += ${addprefix -I,$(LCRYPTO_DSTEPINC)}
-LCRYPTO_DSTEPFLAGS += --global-attribute=nothrow
-LCRYPTO_DSTEPFLAGS += --global-attribute=@nogc
+$(LCRYPTO_DIROOT)/secp256k1_ecdh.di: DSTEPFLAGS += --global-import=$(LCRYPTO_PACKAGE).secp256k1
 
-TOCLEAN += $(LCRYPTO_DIFILES)
+${call DSTEP_DO,$(LCRYPTO_PACKAGE),$(DSRC_SECP256K1)/include,$(LCRYPTO_DIROOT),$(CRYPTO_DFILES)}
 
-$(LCRYPTO_DIROOT)/secp256k1_ecdh.di: LCRYPTO_DSTEPFLAGS += --global-import=$(LCRYPTO_PACKAGE).secp256k1
 
-# Target for creating di local to this unit
-$(LCRYPTO_DIROOT)/%.di: $(LCRYPTO_DSTEPINC)/%.h $(LCRYPTO_DIROOT)/%.way
-	${call log.kvp, $*.di}
-	${call log.lines, $<}
-	${call log.lines, $@}
-	$(PRECMD)$(DSTEP) $(LCRYPTO_DSTEPFLAGS) --package "$(LCRYPTO_PACKAGE)" $< -o $@
 
-MAKE_SHOW_ENV += env-libcrypto-dstep
-env-libcrypto-dstep:
-	$(call log.header, env :: libscrypto :: dstep)
-	${call log.kvp, HFILES, $(HFILES)}
-	${call log.kvp, DESTROOT, $(DESTROOT)}
-	${call log.kvp, DIFILES, $(DIFILES)}
-	${call log.kvp, DSTEPFLAGS, $(DSTEPFLAGS)}
-	$(call log.close)
+
+#$(DIFILES_tagion.crypto.secp256k1.c)
+
+test33: /home/carsten/work/cross_regression/src/lib-crypto/tagion/crypto/SecureNet.d
+
+test34: $(DIFILES_tagion.crypto.secp256k1.c)
+
+test57:
+	@echo $(DSRC_SECP256K1)/include
+	@echo HFILES.tagion.crypto.secp256k1.c=$(HFILES.tagion.crypto.secp256k1.c)
+	@echo DIFILES.tagion.crypto.secp256k1.c=$(DIFILES.tagion.crypto.secp256k1.c)
+	@echo LCRYPTO_DIROOT=$(LCRYPTO_DIROOT)
+	@echo CRYPTO_DFILES $(CRYPTO_DFILES)
+
+#env-crypto:
+# 	$(PRECMD)
+# 	${call log.header, $@ :: env}
+# 	${call $(DSRC_SECP256K1)
