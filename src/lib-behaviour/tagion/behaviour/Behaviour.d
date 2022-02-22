f01db2cc9fcf7b123d6aec23b6467ea82580f8bf
cbleser <cr@tagion.org>
2022-02-22 10:12:57 +0200

    WIP: getFeature is split up into to functions (getScenario)

--- src/lib-behaviour/tagion/behaviour/Behaviour.d
+++ src/lib-behaviour/tagion/behaviour/Behaviour.d
@@ -15,36 +15,62 @@ version(unittest) {
 /**
    Returns:
    true if all the behavios has been runned
- */
+*/
 @safe
 auto scenario(T)(T test) if (isScenario!T) {
-    alias memberCode = format!(q{results ~= %s.%s;},string, string);
-    Document[] results;
-    Document[] run(S...)() {
+    alias memberCode = format!(q{
+            // Scenario group      %1$s
+            // Unique propery info %2$s
+            // Test scenario       %3$s
+            // Test member         %4$s
+            %1$s.%2$s.info.result = %3$s.%4$s;
+        },string, string, string, string);
+    alias underMemberCode = format!(q{
+            // Scenario group      %1$s
+            // Unique propery info %2$s
+            // Test scenario       %3$s
+            // Test member         %4$s
+            // Under index         %5$d
+            auto and = %3$s.%4$s;
+            %1$s.%2$s.ands[%5$d].result = and;
+        },string, string, string, string, size_t);
+//    auto
+//    Document[] results;
+    auto scenario_group=getScenarioGroup!T;
+    ScenarioGroup run(S...)() {
         static if (S.length is 0) {
-            return results;
+            return scenario_group;
         }
         else {
             alias behaviour = getBehaviour!(T, S[0]);
             static foreach(Property; AliasSeq!(Given, Then)) {
                 static if (is(S[0] == Property)) {
-                static assert(!is(behaviour),
-                    format!"%s of the Scenario %s is missing"
-                    (Property.stringof, T.stringof));
+                    static assert(!is(behaviour),
+                        format!"%s of the Scenario %s is missing"
+                        (Property.stringof, T.stringof));
                 }
             }
             static if (!is(behaviour == void)) {
+                import std.uni : toLower;
+                enum group_name = __traits(identifier, typeof(getProperty!(behaviour))).toLower;
+                pragma(msg, "group_name ", group_name);
                 enum code = memberCode(
+                    scenario_group.stringof, group_name,
                     test.stringof, __traits(identifier, behaviour));
+                pragma(msg, "code ", code);
                 mixin(code);
-                static foreach(under_behaviour; getUnderBehaviour!(T, S[0])) {{
-                        enum under_code = memberCode(
-                            test.stringof, __traits(identifier, under_behaviour));
+                static foreach(i, under_behaviour; getUnderBehaviour!(T, S[0])) {{
+                        // enum under_group_name = __traits(identifier, typeof(getProperty!(under_behaviour))).toLower;
+                        // pragma(msg, "unde_group_name ", under_group_name);
+                        enum under_code = underMemberCode(
+                            scenario_group.stringof, group_name,
+                            test.stringof, __traits(identifier, under_behaviour), i);
+                        pragma(msg, "under_code ", under_code);
                         mixin(under_code);
                     }}
-                run!(S[1..$])();
             }
-            return results;
+            return run!(S[1..$])();
+//            return scenario_group;
         }
     }
     return &run!UniqueBehaviourProperties;
@@ -54,9 +80,11 @@ unittest {
     import tagion.hibon.HiBONJSON;
     import std.algorithm.iteration : map;
     import std.algorithm.comparison : equal;
+    import std.array;
     import std.stdio;
     auto awesome = new Some_awesome_feature;
     const run_awesome=scenario(awesome);
+    ScenarioGroup scenario=getScenarioGroup!Some_awesome_feature;
     const result = run_awesome();
     auto expected = [
         "tagion.behaviour.BehaviourUnittest.Some_awesome_feature.is_valid",
@@ -68,8 +96,54 @@ unittest {
         ]
         .map!(a => Some_awesome_feature.result(a));
     assert(awesome.count == 6);
-    assert(equal(result, expected));
+    Document[] results;
+    results~=result.given.info.result;
+    results~=result.given.ands
+        .map!(a => a.result)
+        .array;
+    results~=result.when.info.result;
+    results~=result.when.ands
+        .map!(a => a.result)
+        .array;
+    results~=result.then.info.result;
+    results~=result.then.ands
+        .map!(a => a.result)
+        .array;
+
+    writefln("expected =%-(%s \n%)", expected.map!(a => a.toPretty));
+    writefln("results =%-(%s \n%)", results.map!(a => a.toPretty));
+
+    assert(equal(results, expected));
 }
+
+ScenarioGroup getScenarioGroup(T)() if (isScenario!T) {
+    ScenarioGroup scenario_group;
+    // scope(exit) {
+    //     result.scenarios~=scenario;
+    // }
+    scenario_group.info.property = getScenario!T;
+    scenario_group.info.name = T.stringof;
+    static foreach(_Property; UniqueBehaviourProperties) {{
+            alias behaviour=getBehaviour!(T, _Property);
+            static if (!is(behaviour == void)) {
+                import std.uni : toLower;
+                auto group = &__traits(getMember, scenario_group, _Property.stringof.toLower);
+
+                group.info.property = getProperty!behaviour;
+                group.info.name = __traits(identifier, behaviour);
+                static foreach(under_behaviour; getUnderBehaviour!(T, _Property)) {{
+                        Info!And and;
+                        scope(exit) {
+                            group.ands~=and;
+                        }
+                        and.property = getProperty!(under_behaviour);
+                        and.name = __traits(identifier, under_behaviour);
+                    }}
+            }
+        }}
+    return scenario_group;
+}
+
 
 //@safe
 FeatureGroup getFeature(alias M)() if (isFeature!M) {
@@ -78,43 +152,32 @@ FeatureGroup getFeature(alias M)() if (isFeature!M) {
     result.feature.property = obtainFeature!M;
     result.feature.name = moduleName!M;
     static foreach(_Scenario; Scenarios!M) {{
-        ScenarioGroup scenario;
-        scope(exit) {
-            result.scenarios~=scenario;
-        }
-        scenario.info.property = getScenario!(_Scenario);
-        scenario.info.name = _Scenario.stringof;
-//        ScenarioGroup scenario;
-//        scenario.info.
-        pragma(msg, "_Scenario ", _Scenario);
+            ScenarioGroup scenario_group=getScenarioGroup!_Scenario;
+            scope(exit) {
+                result.scenarios~=scenario_group;
+            }
+            version(none) {
+            scenario.info.property = getScenario!(_Scenario);
+            scenario.info.name = _Scenario.stringof;
+            static foreach(_Property; UniqueBehaviourProperties) {{
+                    alias behaviour=getBehaviour!(_Scenario, _Property);
+                    static if (!is(behaviour == void)) {
+                        import std.uni : toLower;
+                        auto group = &__traits(getMember, scenario, _Property.stringof.toLower);
 
-//        writefln("%J", scenario);
-        static foreach(_Property; UniqueBehaviourProperties) {{
-                alias behaviour=getBehaviour!(_Scenario, _Property);
-                static if (!is(behaviour == void)) {
-                    import std.uni : toLower;
-                    pragma(msg, "Property ", _Property);
-                    pragma(msg, "Property ", getProperty!behaviour);
-                    pragma(msg, "_Property.stringof.toLower ", _Property.stringof.toLower);
-                    auto group = &__traits(getMember, scenario, _Property.stringof.toLower);
-
-                    group.info.property = getProperty!behaviour;
-                    group.info.name = __traits(identifier, behaviour);
-                    static foreach(under_behaviour; getUnderBehaviour!(_Scenario, _Property)) {{
-                            Info!And and;
-                            scope(exit) {
-                                group.ands~=and;
-                            }
-                            and.property = getProperty!(under_behaviour);
-                            and.name = __traits(identifier, under_behaviour);
-//                        foreach(
+                        group.info.property = getProperty!behaviour;
+                        group.info.name = __traits(identifier, behaviour);
+                        static foreach(under_behaviour; getUnderBehaviour!(_Scenario, _Property)) {{
+                                Info!And and;
+                                scope(exit) {
+                                    group.ands~=and;
+                                }
+                                and.property = getProperty!(under_behaviour);
+                                and.name = __traits(identifier, under_behaviour);
                             }}
-//                    enum code = format!q{alias call=%}
-//                    pragma
-                    //BehaviourGroup behaviour = getBehaviour!Property;
-//                 behaviour = getBehaviour!Property;
-                }
-            }}
+                    }
+                }}
+            }
         }}
     return result;
 }
@@ -123,28 +186,14 @@ unittest { //
     import tagion.hibon.HiBONRecord;
     import tagion.basic.Basic : unitfile;
     import core.demangle : mangle;
-//    import std.array : replace;
     alias Module=tagion.behaviour.BehaviourUnittest;
     import std.path;
     enum filename=mangle!(FunctionTypeOf!(getFeature!Module))("unittest")
         .unitfile
         .setExtension("hibon");
-    pragma(msg, "filename ", filename);
-    import std.stdio;
-    // FeatureGroup expected;
-    // expected.feature.name= "tagion.behaviour.BehaviourUnittest";
-    // expected.feature.property = Feature("Some awesome feature should print some cash out of the blue");
-
     const feature = getFeature!(Module);
-//    const doc = filename.fread;
     const expected = filename.fread!FeatureGroup;
-    writefln("expected=%J", expected);
-    writefln("feature=%J", feature);
-    writefln("filename %s", filename);
-
-//    const
-//    filename.fwrite(feature);
-    assert(feature == expected);
+    assert(feature.toDoc == expected.toDoc);
 }
 
 @safe
