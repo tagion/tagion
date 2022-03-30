399c3ce196fc67bd07cbaa1701e624b12d56afc6
cbleser <cr@tagion.org>
2022-03-30 17:51:11 +0300

    secp256k1 dstep for hash.h added

--- src/lib-crypto/dstep.mk
+++ src/lib-crypto/dstep.mk
@@ -8,25 +8,15 @@ CRYPTO_DFILES := ${shell find ${call dir.resolve, tagion/crypto} -name "*.d"}
 
 $(LCRYPTO_DIROOT)/secp256k1_ecdh.di: DSTEPFLAGS += --global-import=$(LCRYPTO_PACKAGE).secp256k1
 
+
 ${call DSTEP_DO,$(LCRYPTO_PACKAGE),$(DSRC_SECP256K1)/include,$(LCRYPTO_DIROOT),$(CRYPTO_DFILES)}
+#${call DSTEP_DO,$(LCRYPTO_PACKAGE),$(DSRC_SECP256K1)/src,$(LCRYPTO_DIROOT),$(CRYPTO_DFILES)}
 
+dstep: $(DSRC_SECP256K1)/include/hash.h
 
+$(DSRC_SECP256K1)/include/hash.h: $(DSRC_SECP256K1)/src/hash.h
+	$(PRECMD)
+	ln -s $< $@
 
-
-#$(DIFILES_tagion.crypto.secp256k1.c)
-
-test33: /home/carsten/work/cross_regression/src/lib-crypto/tagion/crypto/SecureNet.d
-
-test34: $(DIFILES_tagion.crypto.secp256k1.c)
-
-test57:
-	@echo $(DSRC_SECP256K1)/include
-	@echo HFILES.tagion.crypto.secp256k1.c=$(HFILES.tagion.crypto.secp256k1.c)
-	@echo DIFILES.tagion.crypto.secp256k1.c=$(DIFILES.tagion.crypto.secp256k1.c)
-	@echo LCRYPTO_DIROOT=$(LCRYPTO_DIROOT)
-	@echo CRYPTO_DFILES $(CRYPTO_DFILES)
-
-#env-crypto:
-# 	$(PRECMD)
-# 	${call log.header, $@ :: env}
-# 	${call $(DSRC_SECP256K1)
+env-test34:
+	echo "DSRC_SECP256K1=$(DSRC_SECP256K1)"
