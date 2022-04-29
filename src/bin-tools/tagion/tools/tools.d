5311d0a0ca28cce19158f4075f9dcfd401a205d7
cbleser <cr@tagion.org>
2022-04-29 10:10:41 +0300

    WIP: Debug printout removed from tools

--- src/bin-tools/tagion/tools/tools.d
+++ src/bin-tools/tagion/tools/tools.d
@@ -23,35 +23,26 @@ int main(string[] args) {
     enum toolName(alias tool)=moduleName!tool.split(".").tail(1)[0];
 
     enum toolnames=staticMap!(toolName, alltools);
-    pragma(msg, "toolnames ", toolnames);
 
     auto tool=args[0].baseName;
 
     alias Result=Tuple!(int, "exit_code", bool, "executed");
     Result do_main(string tool, string[] args) {
-SelectTool:
-    switch (tool) {
-        static foreach(toolname; toolnames) {{
-        case toolname:
-            enum code =format(q{return Result(%s._main(args), true);}, toolname);
-            pragma(msg, code);
-            mixin(code);
-//            return Result(0, true);
-            break SelectTool;
-        // pragma(msg, tool);
-        // pragma(msg, tool.stringof);
-        // pragma(msg, moduleName!tool);
-        // pragma(msg, moduleName!tool.split("."));
-        // pragma(msg, moduleName!tool.split(".").tail(1)[0]);
-            }}
-    default:
-        // stderr.writefln("Invalid tool %s available %s", tool, toolnames);
-        return Result(0, false);
-    }
-    assert(0);
+    SelectTool:
+        switch (tool) {
+            static foreach(toolname; toolnames) {{
+                    case toolname:
+                        enum code =format(q{return Result(%s._main(args), true);}, toolname);
+                        // pragma(msg, code);
+                        mixin(code);
+                        break SelectTool;
+                }}
+        default:
+            return Result(0, false);
+        }
+        assert(0);
     }
     auto result=do_main(tool, args);
-    writefln("result=%s", result);
     if (!result.executed && args.length > 1) {
         tool=args[1];
         result=do_main(tool, args[1..$]);
@@ -60,7 +51,5 @@ SelectTool:
         stderr.writefln("Invalid tool %s available %-(%s, %)", tool, [toolnames]);
         return 1;
     }
-    writefln("result=%s", result);
-    pragma(msg, alltools);
     return result.exit_code;
 }
