8c114f0338b2403d02911fb356cccdc09718a60f
cbleser <cr@tagion.org>
2022-05-04 15:39:49 +0300

    WIP: In FileDiscoveryService and NetworkRecordDiscoveryService unused code removed

--- src/lib-services/tagion/services/FileDiscoveryService.d
+++ src/lib-services/tagion/services/FileDiscoveryService.d
@@ -28,10 +28,10 @@ import tagion.gossip.P2pGossipNet : ActiveNodeAddressBook;
 import tagion.gossip.AddressBook : addressbook, NodeAddress, AddressBook;
 
 void fileDiscoveryService(
-        Pubkey pubkey,
-        shared p2plib.Node node,
-        string task_name,
-        immutable(Options) opts) nothrow { //TODO: for test
+    Pubkey pubkey,
+    shared p2plib.Node node,
+    string task_name,
+    immutable(Options) opts) nothrow { //TODO: for test
     try {
         scope (success) {
             ownerTid.prioritySend(Control.END);
@@ -40,34 +40,6 @@ void fileDiscoveryService(
         string shared_storage = opts.path_to_shared_info;
 
         bool stop = false;
-
-        version(none)
-        bool checkTimestamp(SysTime time, Duration duration) {
-            return (Clock.currTime - time) > duration;
-        }
-
-        version(none)
-        void updateTimestamp(ref SysTime time) {
-            time = Clock.currTime;
-        }
-
-        version(none) {
-        SysTime mdns_start_timestamp;
-        updateTimestamp(mdns_start_timestamp);
-        }
-
-        version(none)
-        void notifyReadyAfterDelay() {
-            static bool owner_notified;
-            if (!owner_notified) {
-                const after_delay = checkTimestamp(mdns_start_timestamp,
-                        opts.discovery.delay_before_start.msecs);
-                if (after_delay) {
-                    ownerTid.send(DiscoveryState.READY);
-                    owner_notified = true;
-                }
-            }
-        }
 
         void initialize() {
             static uint count;
@@ -84,8 +56,6 @@ void fileDiscoveryService(
             count++;
             log.trace("update %d %s", count, pubkey.cutHex);
             addressbook.load(shared_storage, true);
-            // addressbook[pubkey] = NodeAddress(node.LlistenAddress, opts.dart, opts.port_base);
-            // addressbook.save(shared_storage, true);
         }
 
         initialize;
@@ -95,54 +65,48 @@ void fileDiscoveryService(
         bool addressbook_requested;
         while (!stop) {
             const message = receiveTimeout(
-                    500.msecs,// (immutable(Pubkey) key, Tid tid) {
-                    //     log("looking for key: %s", key.cutHex);
-                    //     tid.send(addressbook[key]);
-                    // },
-                    (Control control) {
-                if (control is Control.STOP) {
-                    log("stop");
-                    stop = true;
-                }
-            },
-                    (DiscoveryRequestCommand request) {
-                with (DiscoveryRequestCommand) {
-                    final switch (request) {
-                    case RequestTable:
-                        pragma(msg, "fixme(cbr):Address book request should not be used anymore (FileDiscoveryService)");
-
-                        //                        initialize();
-                        auto address_book = new ActiveNodeAddressBook(null); //node_addrses);
-                        //log("Requested: %d : %d", addressbook._data.length, address_book.data.length);
-                        addressbook_requested=true;
-                        ownerTid.send(address_book);
-                        break;
-                    case BecomeOnline:
-                        log("Becoming online..");
-                        break;
-                    case BecomeOffline:
-                        log("Becoming off-line");
-                        break;
-                    case UpdateTable:
-                        throw new TagionException(format("DiscoveryRequestCommand %s has not function", request));
-                        break;
-
+                500.msecs,
+                (Control control) {
+                    if (control is Control.STOP) {
+                        log("stop");
+                        stop = true;
                     }
-                }
-            });
+                },
+                (DiscoveryRequestCommand request) {
+                    with (DiscoveryRequestCommand) {
+                        final switch (request) {
+                        case RequestTable:
+                            pragma(msg, "fixme(cbr):Address book request should not be used anymore (FileDiscoveryService)");
+
+                            //                        initialize();
+                            auto address_book = new ActiveNodeAddressBook(null); //node_addrses);
+                            //log("Requested: %d : %d", addressbook._data.length, address_book.data.length);
+                            addressbook_requested=true;
+                            ownerTid.send(address_book);
+                            break;
+                        case BecomeOnline:
+                            log("Becoming online..");
+                            break;
+                        case BecomeOffline:
+                            log("Becoming off-line");
+                            break;
+                        case UpdateTable:
+                            throw new TagionException(format("DiscoveryRequestCommand %s has not function", request));
+                            break;
+
+                        }
+                    }
+                });
             log.trace("FILE NETWORK READY %d < %d ", addressbook.numOfNodes, opts.nodes);
-            //version(none) {
             if (!addressbook_done) {
-            if (!message) {
-                updateAddressbook;
+                if (!message) {
+                    updateAddressbook;
+                }
+                if (addressbook_requested && addressbook.isReady) {
+                    ownerTid.send(DiscoveryState.READY);
+                    addressbook_done=true;
+                }
             }
-            if (addressbook_requested && addressbook.isReady) {
-                ownerTid.send(DiscoveryState.READY);
-                addressbook_done=true;
-            }
-            }
-// }
-//                         notifyReadyAfterDelay();
         }
     }
     catch (Throwable t) {
--- src/lib-services/tagion/services/NetworkRecordDiscoveryService.d
+++ src/lib-services/tagion/services/NetworkRecordDiscoveryService.d
@@ -49,20 +49,14 @@ void networkRecordDiscoveryService(
             ownerTid.prioritySend(Control.END);
         }
         log.register(task_name);
-        const ADDR_TABLE = "address_table";
+        // const ADDR_TABLE = "address_table";
         immutable inner_task_name = format("%s-%s", task_name, "internal");
         const net = new StdHashNet();
         const internal_hirpc = HiRPC(null);
-        NodeAddress[Pubkey] internal_nodeaddr_table;
+//        NodeAddress[Pubkey] internal_nodeaddr_table;
 
-        //bool is_ready = false;
         void receiveAddrBook(ActiveNodeAddressBook address_book) {
             log.trace("updated addr book: %d", addressbook.numOfActiveNodes);
-            // if (is_ready) {
-            //     log("updated addr book internal: %d", address_book.data.length);
-            //     // update_internal_table(address_book.data);
-            //     // update_dart(address_book.data);
-            // }
             ownerTid.send(address_book);
         }
 
@@ -109,15 +103,18 @@ void networkRecordDiscoveryService(
         while(!stop) {
             receive(
                 &receiveAddrBook,
+                /*
                 (immutable(Pubkey) key, Tid tid) {
                     assert(0, "Should not be used");
-                log("looking for key: %s HASH: %s", key.cutHex, net.calcHash(cast(Buffer) key).cutHex);
-                const result_addr = addressbook[key]; //internal_nodeaddr_table.get(key, NodeAddress.init);
-                if (result_addr == NodeAddress.init) {
-                    log("Address not found in internal nodeaddr table");
-                }
-                tid.send(result_addr);
-            }, (DiscoveryRequestCommand request) {
+                    log("looking for key: %s HASH: %s", key.cutHex, net.calcHash(cast(Buffer) key).cutHex);
+                    const result_addr = addressbook[key]; //internal_nodeaddr_table.get(key, NodeAddress.init);
+                    if (result_addr == NodeAddress.init) {
+                        log("Address not found in internal nodeaddr table");
+                    }
+                    tid.send(result_addr);
+                },
+                */
+                (DiscoveryRequestCommand request) {
                 log("send request: %s", request);
                 bootstrap_tid.send(request);
             },
