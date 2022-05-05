1dabf323cffd9258d76c54d856a726d6bfc3b2a9
cbleser <cr@tagion.org>
2022-05-05 16:06:20 +0300

    Removing dead code NetworkRecordDiscoveryService

--- src/lib-services/tagion/services/NetworkRecordDiscoveryService.d
+++ src/lib-services/tagion/services/NetworkRecordDiscoveryService.d
@@ -2,39 +2,22 @@ module tagion.services.NetworkRecordDiscoveryService;
 
 import core.time;
 import std.datetime;
-import std.typecons;
-import std.conv;
 import std.concurrency;
-import std.stdio;
-import std.array;
-import std.algorithm.iteration;
 import std.format;
 
 import tagion.services.Options;
-import tagion.basic.Basic : Buffer, Control, nameOf, Pubkey;
+import tagion.basic.Basic : Buffer, Control, Pubkey;
 import tagion.logger.Logger;
 import tagion.utils.StdTime;
 
-import tagion.hibon.HiBON : HiBON;
-import tagion.hibon.Document : Document;
 import p2plib = p2p.node;
-import tagion.crypto.SecureInterfaceNet : HashNet;
-
-//import tagion.gossip.P2pGossipNet : ActiveNodeAddressBook;
-import tagion.gossip.AddressBook : addressbook, NodeAddress;
-import tagion.dart.DARTFile;
-import tagion.dart.DART;
-import tagion.dart.Recorder : RecordFactory;
-import tagion.script.StandardRecords;
+import tagion.gossip.AddressBook : addressbook;
 import tagion.communication.HiRPC;
 import tagion.services.ServerFileDiscoveryService;
 import tagion.services.FileDiscoveryService;
 import tagion.services.MdnsDiscoveryService;
-import tagion.utils.Miscellaneous : cutHex;
-import tagion.hibon.HiBONJSON;
 
-//import tagion.Keywords : NetworkMode;
-import tagion.basic.TagionExceptions : fatal, taskfailure, TagionException;
+import tagion.basic.TagionExceptions : fatal;
 import tagion.crypto.SecureNet;
 
 void networkRecordDiscoveryService(
@@ -52,13 +35,6 @@ void networkRecordDiscoveryService(
         immutable inner_task_name = format("%s-%s", task_name, "internal");
         const net = new StdHashNet();
         const internal_hirpc = HiRPC(null);
-
-        version(none)
-        void receiveAddrBook(ActiveNodeAddressBook address_book) {
-            assert(0, "Shoudl not be used");
-            log.trace("updated addr book: %d", addressbook.numOfActiveNodes);
-            //   ownerTid.send(address_book);
-        }
 
         Tid bootstrap_tid;
 
@@ -96,7 +72,6 @@ void networkRecordDiscoveryService(
             bootstrap_tid.send(Control.STOP);
             assert(receiveOnly!Control is Control.END);
         }
-
 
         ownerTid.send(Control.LIVE);
         bool stop = false;
