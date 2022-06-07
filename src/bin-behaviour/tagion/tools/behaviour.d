635a307e647fdbc6bcfd189429ccb056b4f05312
cbleser <cr@tagion.org>
2022-06-07 11:27:13 +0300

    WIP: behaviour

--- src/bin-behaviour/tagion/tools/behaviour.d
+++ src/bin-behaviour/tagion/tools/behaviour.d
@@ -1,6 +1,46 @@
 module tagion.tools.behaviour;
 
+import std.getopt;
+import std.stdio;
 
-int main(string[] args) {
+mixin Main!_main;
+
+int _main(string[] args) {
+    immutable program = args[0];
+    bool version_switch;
+    auto main_args = getopt(args,
+        std.getopt.config.caseSensitive,
+        std.getopt.config.bundling,
+        "version", "display the version", &version_switch,
+        "dartfilename|d", format("Sets the dartfile: default %s", dartfilename), &dartfilename,
+    );
+
+    if (version_switch) {
+        // writefln("version %s", REVNO);
+        // writefln("Git handle %s", HASH);
+        return 0;
+    }
+
+    if (main_args.helpWanted) {
+        defaultGetoptPrinter(
+                [
+            // format("%s version %s", program, REVNO),
+            "Documentation: https://tagion.org/",
+            "",
+            "Usage:",
+            format("%s <command> [<option>...]", program),
+            "",
+            "Where:",
+            "<command>           one of [--read, --rim, --modify, --rpc]",
+            "",
+
+            "<option>:",
+
+        ].join("\n"),
+        main_args.options);
+        return 0;
+    }
+
+
     return 0;
 }
