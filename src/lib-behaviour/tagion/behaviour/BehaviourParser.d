acb8326bf50d841e00cf57c69b7f7b7bc0c5a153
AlexeyP-tag <ap@i25s.com>
2022-06-22 00:47:02 +0300

    WIP: Behaviour parser

--- src/lib-behaviour/tagion/behaviour/BehaviourParser.d
+++ src/lib-behaviour/tagion/behaviour/BehaviourParser.d
@@ -7,182 +7,208 @@ import std.traits;
 import std.regex;
 import std.string : strip;
 import std.format;
-
+import tagion.hibon.HiBONRecord : RecordType, GetLabel;
+import std.traits : Fields;
+import std.meta;
+import std.uni : toLower;
 import tagion.behaviour.BehaviourException;
+import tagion.behaviour.BehaviourBase : UniqueBehaviourProperties;
 
 enum feature_regex = regex([
-        `Feature(?:\s+|\:)`, /// Feature
-        `Scenario(?:\s+|\:)`, /// Scenario
+        `feature(?:\s+|\:)`, /// Feature
+        `scenario(?:\s+|\:)`, /// Scenario
         r"\s*\*(\w+)\*", /// Action
+        r"\s*`(\w+)`", /// Name
         r"`((?:\w+\.?)+)`", /// Module
-        r"\s*`(\w+)`" /// Name
-    ]);
+    ], "i");
 
 enum Token {
     NONE,
     FEATURE,
     SCENARIO,
     ACTION,
+    NAME,
     MODULE,
-    NAME
 }
 
 enum State {
     Start,
     Feature,
     Scenario,
+    Action,
+    And_Action,
 }
 
+@trusted
 FeatureGroup parser(R)(R range) if (isInputRange!R && isSomeString!(ElementType!R)) {
     import std.stdio;
     import std.array;
-    import std.stdio: write, writeln, writef, writefln;
+    import std.stdio : write, writeln, writef, writefln;
     import std.algorithm.searching;
     import std.string;
+    import std.range : enumerate;
+
     FeatureGroup result;
     ScenarioGroup scenario_group;
 
     Info!Feature info_feature;
     Info!Scenario info_scenario;
+    writeln(typeid(scenario_group.given));
 
     State state;
-    string action_flag;
     writeln("STARTTTTTTT--------------------------------------------------------------------------------------------------------------");
-   // string flag = "";
-    foreach (line; range) {
+    int current_action_index = -1;
+    foreach (line_no, line; range.enumerate(1)) {
         writeln("______________________________________");
         auto match = range.front.matchFirst(feature_regex);
         writeln("match: ", match);
-        writeln("line: ", line);
-       // writeln("state: ", state);
-        //writeln("match.post: ", match.post);
-        //io.writefln("match %s : %s", match, line);
-
-        //if (match) {
-            // io.writefln("match %s '%s' whichPattern=%d", match, match.post.strip, match.whichPattern);
-            const token = cast(Token)(match.whichPattern);
-            writeln("Token: ", token);
-            with (Token) {
-                final switch (token) {
-                case NONE:
-                    string l = match.post.idup;
-                    writeln("Hi from NONE!!!");
-                    switch (state) {
-                    case State.Feature:
-                        writeln("1");
-                        info_feature.property.comments ~= strip(l);
-                        break;
-                    case State.Scenario:
-                        writeln("2");
-                        info_scenario.property.comments ~= strip(l);
-                        break;
-                    default:
-                        writeln("3");
-                        /// Empty
+        writefln("%s:%d ", line, line_no);
+        const Token token = cast(Token)(match.whichPattern);
+        writeln("Token: ", token);
+        with (Token) {
+        TokenSwitch:
+            final switch (token) {
+            case NONE:
+                immutable comment = match.post.strip.idup;
+                writeln("Hi from NONE!!!");
+            StateSwitch:
+                final switch (state) {
+                case State.Feature:
+                    writeln("1");
+                    info_feature.property.comments ~= comment;
+                    break;
+                case State.Scenario:
+                    writeln("2");
+                    info_scenario.property.comments ~= comment;
+                    break;
+                case State.Action:
+                case State.And_Action:
+                    static foreach (index, Field; Fields!ScenarioGroup) {
+                        static if (hasMember!(Field, "info")) {
+                            if (current_action_index is index) {
+                                if (state == State.And_Action) {
+                                    scenario_group.tupleof[index].ands[$ - 1].property.comments ~= comment;
+                                    break StateSwitch;
+                                }
+                                scenario_group.tupleof[index].info.property.comments ~= comment;
+                            }
+                        }
                     }
                     break;
-                case FEATURE:
-                    check(state is State.Start, format("Feature has already been declared in line %d", line));
-                    if(canFind(line, "Feature: ")) {
-                        string description = cast(string)line.replace("## Feature: ", "");
-                        info_feature.property.description = description;
-                    }
-                    else {assert(0);}
-                    state = State.Feature;
-                    break;
-                case MODULE:
-                    writeln("STATE: ", state, " ", action_flag);
-                   // check(state is State.Feature, format("Module name can only be declare after the Feature declaration :%d", line)); HERE!!!
-                    if(state is State.Feature) {
-                        info_feature.name = match[1].idup;
-                        break;
-                    }
-                    if(state is State.Scenario) {
-                        switch(action_flag) {
-                            case "Given":
-                                scenario_group.given.info.name = match[1].idup;
-                                break;
-                            case "When":
-                                scenario_group.when.info.name = match[1].idup;
-                            break;
-                            case "Then":
-                                scenario_group.then.info.name = match[1].idup;
-                                break;
-                            case "And":
-                                // todo
-                                break;
-                            default:
-                                info_scenario.name = match[1].idup;
-                                break;
-                       }
-                       break;
-                    }
-                    
-                    writeln("STATEEEE: ", state);
-                    //                    result.info.name=match[1];
-                    //io.writefln("%s %s '%s' whichPattern=%d", token, match, match.post.strip, match.whichPattern);
-                    break;
-                case SCENARIO:
-                    check(state is State.Feature || state is State.Scenario, format("Scenario must be declared after a Feature :%d", line));
-                    writeln("Hi from SCENARIO!!! ", line);
-                    if(canFind(line, "Scenario: ")) {
-                        string description = cast(string)line.replace("### Scenario: ", "");
-                        info_scenario.property.description = description;
-                    }
-                    else {assert(0);}
-                    state = State.Scenario;
-                    //                    result.scenarios ~= Scenario(match.post.strip);
-                   // io.writefln("%s %s '%s' whichPattern=%d", token, match, match.post.strip, match.whichPattern);
-                    break;
-                case ACTION:
-                    writeln("Hi from action!!!!!!!!!!!!");
-                    assert(state == State.Scenario, "State should be scenario");
-                    writefln("Action match %s", match);
-                    action_flag = match[1].idup;
-                    switch (match[1]) {
-                        case "Given":
-                            writefln("%s match.post = %s %s", match[1], match.post, typeof(match.post).stringof);
-                            scenario_group.given.info.property.description = match.post.idup;
-                            break;
-                        case "When" :
-                            writefln("%s match.post = %s %s", match[1], match.post, typeof(match.post).stringof);
-                            //writefln("%s match.post = %s ", match[1], match.post);
-                            scenario_group.when.info.property.description = match.post.idup;
-                            break;
-                        case "Then":
-                            writefln("%s match.post = %s %s", match[1], match.post, typeof(match.post).stringof);
-                            scenario_group.then.info.property.description = match.post.idup;
-                            //writefln("%s match.post = %s ", match[1], match.post);
-                            break;
-                        case "And":
-                            writefln("%s match.post = %s %s", match[1], match.post, typeof(match.post).stringof);
-                            // todo
-                            break;
-                        default:
-                            break;
-                    }
-
-
-                   // io.writefln("%s %s '%s' whichPattern=%d", token, match, match.post.strip, match.whichPattern);
-                    break;
-                case NAME:
-                writeln("Hi from name!!!!!!!!!!!!");
-                    //io.writefln("%s %s '%s' whichPattern=%d", token, match, match.post.strip, match.whichPattern);
-
+                case State.Start:
+                    check(0, format("Missing feature declaration %s:%d", line, line_no));
                 }
-            }
-            //             range.popFront;
+                break;
+            case FEATURE:
+                current_action_index = -1;
+                check(state is State.Start, format("Feature has already been declared in line %d", line));
+                writeln("Hi from Feature!!! ", line);
+                info_feature.property.description = match.post.idup;
+                state = State.Feature;
+                break;
+            case NAME:
+            case MODULE:
+                check((token is MODULE) || (state !is State.Feature),
+                        format("Illegal (namespace) name %s for %s", match[1], match.pre));
+                // check(state is State.Feature, format("Module name can only be declare after the Feature declaration :%d", line)); HERE!!!
+                final switch (state) {
+                case State.Feature:
+                    info_feature.name = match[1].idup;
+                    break TokenSwitch;
+                case State.Scenario:
+                    info_scenario.name = match[1].idup;
+                    break TokenSwitch;
+                case State.Action:
+                case State.And_Action:
+                    static foreach (index, Field; Fields!ScenarioGroup) {
+                        static if (hasMember!(Field, "info")) {
+                            if (current_action_index is index) {
+                                if (state is State.And_Action) {
+                                    scenario_group.tupleof[index].ands[$ - 1].name = match[1].idup;
+                                    break TokenSwitch;
+                                }
+                                writefln("scenario_group.tupleof[index].info.name = %s", scenario_group.tupleof[index]
+                                        .info.name);
+                                check(scenario_group.tupleof[index].info.name.length == 0,
+                                     format("Action name has already been defined %s", match[0], scenario_group.tupleof[index].info.name));
 
-            // //auto module_match=
-            //             return;
-       // }
+                                scenario_group.tupleof[index].info.name = match[1].idup;
+                                break TokenSwitch;
+
+                            }
+                        }
+                    }
+                    break TokenSwitch;
+                case State.Start:
+                    writefln("Start %s", match);
+                    break TokenSwitch;
+                }
+                check(0, format("No valid action has %s", match[1]));
+                writeln("STATEEEE: ", state);
+                break;
+            case SCENARIO:
+                current_action_index = -1;
+                check(state is State.Feature || state is State.Scenario, format("Scenario must be declared after a Feature :%d", line));
+                writeln("Hi from SCENARIO!!! ", line);
+                info_scenario.property.description = match.post.idup;
+                state = State.Scenario;
+                break;
+            case ACTION:
+                writeln("Hi from action!!!!!!!!!!!!");
+                state = State.Action;
+                scope const action_word = match[1].toLower;
+                if (action_word == "and") {
+                    check(current_action_index >= 0, "Missing action Given, When or Then before And");
+                    static foreach (index, Field; Fields!ScenarioGroup) {
+                        static if (isBehaviourGroup!Field) {
+                            if (current_action_index == index) {
+                                Info!And and;
+                                and.property.description = match.post.idup;
+                                pragma(msg, "Field ", Fields!ScenarioGroup[index]);
+                                pragma(msg, ":::", FieldNameTuple!(typeof(scenario_group.tupleof[index])));
+                                scenario_group.tupleof[index].ands ~= and;
+                                pragma(msg, ":::", typeof(scenario_group.tupleof[index].ands));
+                            }
+                        }
+                    }
+                    state = State.And_Action;
+                    break;
+                }
+                alias BehaviourGroups = staticMap!(BehaviourGroup, UniqueBehaviourProperties);
+                pragma(msg, "BehaviourGroups ", BehaviourGroups);
+                writefln("Action match %s", match);
+                static foreach (index, Field; Fields!ScenarioGroup) {
+                    {
+                        enum field_index = staticIndexOf!(Field, BehaviourGroups);
+                        static if (field_index >= 0) {
+                            alias label = GetLabel!(scenario_group.tupleof[index]);
+                            pragma(msg, "___action_name ", label.name);
+                            enum action_name = label.name;
+                            // enum action_name=getUDAs!(UniqueBehaviourProperties[field_index], RecordType)[0].name.toLower;
+                            pragma(msg, "action_name ", action_name);
+
+                            writefln("action %s match = %s index=%d", action_name, match[1].toLower, index);
+
+                            if (match[1].toLower == label.name) {
+                                writefln("!!!! %s", label.name);
+                                current_action_index = index;
+                                scenario_group.tupleof[index].info.property.description = match.post.idup;
+
+                                break TokenSwitch;
+                            }
+                        }
+                    }
+                }
+                break;
+            }
+        }
     }
     scenario_group.info = info_scenario;
     result.info = info_feature;
     result.scenarios ~= scenario_group;
 
     writeln("***********************************************************");
-    writeln("FeatureGroup: "); 
+    writeln("FeatureGroup: ");
     writeln("              Feature:   ");
     writeln("                       name:        ", result.info.name);
     writeln("                       description: ", result.info.property.description);
@@ -193,11 +219,35 @@ FeatureGroup parser(R)(R range) if (isInputRange!R && isSomeString!(ElementType!
     writeln("                       comments:    ", result.scenarios[0].info.property.comments);
     writeln("               Given name:          ", result.scenarios[0].given.info.name);
     writeln("               Given description:   ", result.scenarios[0].given.info.property.description);
+    writeln("               Given comments:   ", result.scenarios[0].given.info.property.comments);
+    writeln("               Given ands:   ", result.scenarios[0].given.ands.length);
+    foreach (and; result.scenarios[0].given.ands) {
+        writeln("                  And name:          ", and.name);
+        writeln("                  And description:   ", and.property.description);
+        writeln("                  And comments:   ", and.property.comments);
+    }
     writeln("               When name:           ", result.scenarios[0].when.info.name);
     writeln("               When description:    ", result.scenarios[0].when.info.property.description);
+    writeln("               When comments:   ", result.scenarios[0].when.info.property.comments);
+    writeln("               When ands:   ", result.scenarios[0].when.ands.length);
+    foreach (and; result.scenarios[0].when.ands) {
+        writeln("                  And name:          ", and.name);
+        writeln("                  And description:   ", and.property.description);
+        writeln("                  And comments:   ", and.property.comments);
+    }
     writeln("               Then name:           ", result.scenarios[0].then.info.name);
     writeln("               Then description:    ", result.scenarios[0].then.info.property.description);
+    writeln("               Then comments:   ", result.scenarios[0].then.info.property.comments);
+    writeln("               Then ands:   ", result.scenarios[0].then.ands.length);
+    foreach (and; result.scenarios[0].then.ands) {
+        writeln("                  And name:          ", and.name);
+        writeln("                  And description:   ", and.property.description);
+        writeln("                  And comments:   ", and.property.comments);
+    }
     writeln("FINISHHHHHHHH--------------------------------------------------------------------------------------------------------------");
+    import tagion.hibon.HiBONJSON : toPretty;
+
+    writefln("pretty %s", result.toPretty);
     return result;
 }
 
@@ -214,7 +264,7 @@ unittest { /// Convert ProtoBDD to Feature
     pragma(msg, "ElementType!ByLine ", ElementType!ByLine);
     pragma(msg, "isSomeString!(ElementType!ByLine) ", isSomeString!(ElementType!ByLine));
 
-    auto feature=parser(feature_byline);
+    auto feature = parser(feature_byline);
 } //failed! fix
 
 version (unittest) {
