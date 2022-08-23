748b7b8d00d28068991aa70ec0e7ebafe52da326
Dmitriy Nester <dn@decard.io>
2022-08-23 17:53:33 +0300

    Added wolf ssl module

--- platform.x86_64-linux.mk
+++ platform.x86_64-linux.mk
@@ -10,13 +10,13 @@ ANDROID_ABI=x86_64
 DINC+=${shell find $(DSRC) -maxdepth 1 -type d -path "*src/lib-*" }
 DFILES?=${shell find $(DSRC) -type f -name "*.d" -path "*src/lib-*" -a -not -path "*/tests/*" -a -not -path "*/unitdata/*"}
 
-WRAPS+=secp256k1 p2pgowrapper openssl
+WRAPS+=secp256k1 p2pgowrapper $(SSLIMPLEMENTATION)
 
 .PHONY: prebuild-linux
 
 $(UNITTEST_BIN): $(DFILES)
 
-unittest: LIBS+=$(LIBOPENSSL)
+unittest: LIBS+=$(SSLIMPLEMENTATION)
 unittest: LIBS+=$(LIBSECP256K1)
 unittest: LIBS+=$(LIBP2PGOWRAPPER)
 unittest: proto-unittest-run
---
+++ src/wrap-wolfssl/context.mk
@@ -0,0 +1,25 @@
+﻿#openssl
+#LIBOPENSSL
+DSRC_WOLFSSL := ${call dir.resolve, wolfssl}
+DTMP_WOLFSSL := $(DTMP)/wolfssl
+
+CONFIGUREFLAGS_WOLFSSL := --enable-static
+
+.PHONY: wolfssl
+
+LIBWOLFSSL := $(DTMP_WOLFSSL)/src/.libs/libwolfssl.a
+
+proper-wolfssl:
+	$(PRECMD)
+	${call log.header, $@ :: wolfssl}
+	$(RMDIR) $(DTMP_WOLFSSL)
+
+proper: proper-wolfssl
+
+wolfssl: $(DTMP)/.way
+	$(PRECMD)
+	${call log.kvp, $@}
+	$(CP) $(DSRC_WOLFSSL) $(DTMP_WOLFSSL)
+	$(PRECMD)cd $(DTMP_WOLFSSL); sh autogen.sh
+	$(PRECMD)cd $(DTMP_WOLFSSL); ./configure $(CONFIGUREFLAGS_WOLFSSL)
+	$(PRECMD)cd $(DTMP_WOLFSSL); make
