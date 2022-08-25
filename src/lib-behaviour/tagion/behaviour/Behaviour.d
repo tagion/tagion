874a5792fb5fe7e26bb6fa850700e8b3f0cf04da
cbleser <cr@tagion.org>
2022-08-25 15:19:50 +0300

    WIP: Behaviour some debug printout has been removed

--- src/lib-behaviour/tagion/behaviour/Behaviour.d
+++ src/lib-behaviour/tagion/behaviour/Behaviour.d
@@ -30,91 +30,21 @@ ScenarioGroup run(T)(T scenario) if (isScenario!T)
             // Test member         %5$s
             %1$s.%2$s.infos[%3$d].result = %4$s.%5$s;
         }, string, string, size_t,string, string);
-    // alias underMemberCode = format!(q{
-    //         // Scenario group      %1$s
-    //         // Unique propery info %2$s
-    //         // Test scenario       %3$s
-    //         // Test member         %4$s
-    //         // Under index         %5$d
-    //         auto and = %3$s.%4$s;
-    //         %1$s.%2$s.ands[%5$d].result = and;
-    //     }, string, string, string, string, size_t);
-    pragma(msg, "Dav do", T);
     ScenarioGroup scenario_group = getScenarioGroup!T;
-    io.writefln("scenario_group %s", scenario_group.toPretty);
-    // alias all_behaviours = getBehaviours_!(T);
-    // pragma(msg, "- - - all_behaviours ", all_behaviours);
     import std.uni : toLower;
     static foreach(_Property; BehaviourProperties) {
-        //pragma(msg, "FieldTupleNames!T ", FieldNameTuple!T);
         {
             alias all_behaviours = getBehaviour!(T, _Property);
-            pragma(msg, "all_behaviour ", all_behaviours, " ", _Property);
             static foreach(i, behaviour; all_behaviours) {{
-                pragma(msg, "i ", i, " behaviour ", typeof(behaviour));
-                // pragma(msg, "i ", i, " behaviour ", behaviour.stringof);
                 enum group_name = __traits(identifier, typeof(getProperty!(behaviour))).toLower;
                 enum code = memberCode(
                     scenario_group.stringof, group_name, i,
                     scenario.stringof, __traits(identifier, behaviour));
-                pragma(msg, "code: ", code);
                 mixin(code);
-//                behaviour();
                 }}
-        // static foreach(name; [ __traits(allMembers, T) ]) {
-        //     static if (__traits(compiles, typeof(__traits(getMember, scenario, name)))) {{
-        //         pragma(msg, "--- Name ", name);
-        //         const m = __traits(getMember, scenario, name);
-        //         static if (hasUDA!(m, _BehaviourProperty))  {
-        //             pragma(msg, "Member ", name, " has ", _BehaviourProperty);
-        //             }
-
-        //         }}
-        // }
             }
     }
-    version(none)
-    ScenarioGroup run(S...)()
-    {
-        static if (S.length is 0)
-        {
-            return scenario_group;
-        }
-        else
-        {
-            alias behaviours = getBehaviour!(T, S[0]);
-            static foreach (Property; AliasSeq!(Given, Then))
-            {
-                static if (is(S[0] == Property))
-                {
-                    static assert(!is(behaviour),
-                        format!"%s of the Scenario %s is missing"(Property.stringof, T.stringof));
-                }
-            }
-            static if (!is(behaviour == void))
-            {
-                import std.uni : toLower;
-
-                enum group_name = __traits(identifier, typeof(getProperty!(behaviour))).toLower;
-                enum code = memberCode(
-                        scenario_group.stringof, group_name,
-                        test.stringof, __traits(identifier, behaviour));
-                mixin(code);
-                static foreach (i, under_behaviour; getUnderBehaviour!(T, S[0]))
-                {
-                    {
-                        enum under_code = underMemberCode(
-                                scenario_group.stringof, group_name,
-                                test.stringof, __traits(identifier, under_behaviour), i);
-                        mixin(under_code);
-                    }
-                }
-            }
-            return run!(S[1 .. $])();
-        }
-    }
-
-    return scenario_group; //ScenarioGroup.init;
+    return scenario_group;
 }
 
 @safe
