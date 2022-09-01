8ad27edc978d4b6f88ba3e22281b4c7df6b4de2c
cbleser <cr@tagion.org>
2022-09-01 17:26:51 +0300

    Debug printout removed from Behaviour.d and BehaviourFeature.d

--- src/lib-behaviour/tagion/behaviour/Behaviour.d
+++ src/lib-behaviour/tagion/behaviour/Behaviour.d
@@ -1,4 +1,4 @@
-	module tagion.behaviour.Behaviour;
+module tagion.behaviour.Behaviour;
 
 public import tagion.behaviour.BehaviourFeature;
 import tagion.hibon.Document;
@@ -8,7 +8,7 @@ import std.format;
 import std.meta : AliasSeq;
 import std.range : only;
 import std.array : join;
-import std.algorithm.searching: any;
+import std.algorithm.searching : any;
 import std.exception : assumeWontThrow;
 
 import tagion.behaviour.BehaviourException;
@@ -22,44 +22,45 @@ import tagion.basic.Basic : isOneOf;
    The ScenarioGroup including the result of each action
 */
 @safe
-ScenarioGroup run(T)(T scenario) if (isScenario!T)
-{
+ScenarioGroup run(T)(T scenario) if (isScenario!T) {
     ScenarioGroup scenario_group = getScenarioGroup!T;
     try {
-    alias memberCode = format!(q{
+        alias memberCode = format!(q{
             // Scenario group      %1$s
             // Unique propery info %2$s
             // Info index          %3$d
             // Test scenario       %4$s
             // Test member         %5$s
             %1$s.%2$s.infos[%3$d].result = %4$s.%5$s;
-        }, string, string, size_t,string, string);
-    import std.uni : toLower;
-    .check(scenario !is null, format("The constructor must be called for %s before it's runned", T.stringof));
-    static foreach(_Property; BehaviourProperties) {
-        {
-            alias all_behaviours = getBehaviour!(T, _Property);
-            static if (is(all_behaviours == void)) {
-                static assert(!isOneOf!(_Property, MandatoryBehaviourProperties), format("%s is missing a @%s action", T.stringof, _Property.stringof));
-            }
-            else {
-            pragma(msg, "all_behaviours ", all_behaviours);
-            static foreach(i, behaviour; all_behaviours) {{
-                enum group_name = __traits(identifier, typeof(getProperty!(behaviour))).toLower;
-                enum code = memberCode(
-                    scenario_group.stringof, group_name, i,
-                    scenario.stringof, __traits(identifier, behaviour));
-                mixin(code);
-                }}
+        }, string, string, size_t, string, string);
+        import std.uni : toLower;
+        .check(scenario !is null,
+                format("The constructor must be called for %s before it's runned", T.stringof));
+        static foreach (_Property; BehaviourProperties) {
+            {
+                alias all_behaviours = getBehaviour!(T, _Property);
+                static if (is(all_behaviours == void)) {
+                    static assert(!isOneOf!(_Property, MandatoryBehaviourProperties),
+                            format("%s is missing a @%s action", T.stringof, _Property.stringof));
+                }
+                else {
+                    pragma(msg, "all_behaviours ", all_behaviours);
+                    static foreach (i, behaviour; all_behaviours) {
+                        {
+                            enum group_name = __traits(identifier, typeof(getProperty!(behaviour))).toLower;
+                            enum code = memberCode(
+                                        scenario_group.stringof, group_name, i,
+                                        scenario.stringof, __traits(identifier, behaviour));
+                            mixin(code);
+                        }
+                    }
+                }
             }
         }
-    }
-    scenario_group.info.result = result(ScenarioResult(true)).toDoc;
+        scenario_group.info.result = result(ScenarioResult(true)).toDoc;
     }
     catch (Exception e) {
-//        io.writefln("RUN %s", e.msg);
         scenario_group.info.result = BehaviourError(e).toDoc;
-//        io.writefln("scenario_group.info.result = %s", scenario_group.info.result.toPretty);
     }
     return scenario_group;
 }
@@ -74,22 +75,21 @@ struct ScenarioResult {
         });
 }
 
-private static Document scenario_ends=result(ScenarioResult(true)).toDoc;
+private static Document scenario_ends = result(ScenarioResult(true)).toDoc;
 
-version(none) {
-static this() {
-    // import tagion.hibon.HiBON;
-    // auto h=new HiBON;
-    // h["end"]=true;
-    Result result;
-//    auto x = ScenarioResult
-    result.outcome = ScenarioResult(true).toDoc;
-    scenario_ends = result.toDoc;
+version (none) {
+    static this() {
+        // import tagion.hibon.HiBON;
+        // auto h=new HiBON;
+        // h["end"]=true;
+        Result result;
+        //    auto x = ScenarioResult
+        result.outcome = ScenarioResult(true).toDoc;
+        scenario_ends = result.toDoc;
+    }
 }
-}
-	@safe
-unittest
-{
+@safe
+unittest {
     import std.algorithm.iteration : map;
     import std.algorithm.comparison : equal;
     import std.array;
@@ -98,15 +98,15 @@ unittest
     auto awesome = new Some_awesome_feature;
     const runner_result = run(awesome);
     auto expected = only(
-        "tagion.behaviour.BehaviourUnittest.Some_awesome_feature.is_valid",
-        "tagion.behaviour.BehaviourUnittest.Some_awesome_feature.in_credit",
-        "tagion.behaviour.BehaviourUnittest.Some_awesome_feature.contains_cash",
+            "tagion.behaviour.BehaviourUnittest.Some_awesome_feature.is_valid",
+            "tagion.behaviour.BehaviourUnittest.Some_awesome_feature.in_credit",
+            "tagion.behaviour.BehaviourUnittest.Some_awesome_feature.contains_cash",
 
-        "tagion.behaviour.BehaviourUnittest.Some_awesome_feature.request_cash",
-        "tagion.behaviour.BehaviourUnittest.Some_awesome_feature.is_debited",
-        "tagion.behaviour.BehaviourUnittest.Some_awesome_feature.is_dispensed",
-        "tagion.behaviour.BehaviourUnittest.Some_awesome_feature.swollow_the_card",
-        )
+            "tagion.behaviour.BehaviourUnittest.Some_awesome_feature.request_cash",
+            "tagion.behaviour.BehaviourUnittest.Some_awesome_feature.is_debited",
+            "tagion.behaviour.BehaviourUnittest.Some_awesome_feature.is_dispensed",
+            "tagion.behaviour.BehaviourUnittest.Some_awesome_feature.swollow_the_card",
+    )
         .map!(a => result(a));
     io.writefln("awesome.count = %d", awesome.count);
     assert(awesome.count == 7);
@@ -127,28 +127,27 @@ unittest
 }
 
 @safe
-ScenarioGroup getScenarioGroup(T)() if (isScenario!T)
-{
+ScenarioGroup getScenarioGroup(T)() if (isScenario!T) {
     ScenarioGroup scenario_group;
     scenario_group.info.property = getScenario!T;
     scenario_group.info.name = T.stringof;
-    static foreach (_Property; BehaviourProperties)
-    {
+    static foreach (_Property; BehaviourProperties) {
         {
             alias behaviours = getBehaviour!(T, _Property);
-            static if (!is(behaviours == void))
-            {
+            static if (!is(behaviours == void)) {
                 import std.uni : toLower;
+
                 enum group_name = _Property.stringof.toLower;
                 auto group = &__traits(getMember, scenario_group, group_name);
                 group.infos.length = behaviours.length;
-                static foreach (i, behaviour; behaviours)
-                {{
-                    Info!_Property info;
-                    info.property = getProperty!behaviour;
-                    info.name = __traits(identifier, behaviour);
-                    group.infos[i]=info;
-                    }}
+                static foreach (i, behaviour; behaviours) {
+                    {
+                        Info!_Property info;
+                        info.property = getProperty!behaviour;
+                        info.name = __traits(identifier, behaviour);
+                        group.infos[i] = info;
+                    }
+                }
             }
         }
     }
@@ -156,24 +155,21 @@ ScenarioGroup getScenarioGroup(T)() if (isScenario!T)
 }
 
 @safe
-FeatureGroup getFeature(alias M)() if (isFeature!M)
-{
+FeatureGroup getFeature(alias M)() if (isFeature!M) {
     //    import std.stdio;
     FeatureGroup result;
     result.info.property = obtainFeature!M;
     result.info.name = moduleName!M;
     alias ScenariosSeq = Scenarios!M;
     result.scenarios.length = ScenariosSeq.length;
-    static foreach (i, _Scenario; ScenariosSeq)
-    {
-        result.scenarios[i] =getScenarioGroup!_Scenario;
+    static foreach (i, _Scenario; ScenariosSeq) {
+        result.scenarios[i] = getScenarioGroup!_Scenario;
     }
     return result;
 }
 
 @safe
-unittest
-{ //
+unittest { //
     import tagion.hibon.HiBONRecord;
     import tagion.basic.Basic : unitfile;
     import core.demangle : mangle;
@@ -198,11 +194,11 @@ protected string _scenarioTupleCode(alias M, string tuple_name)() if (isFeature!
     string[] result;
     {
         result ~= format("alias %s = Tuple!(", tuple_name);
-        scope(exit) {
+        scope (exit) {
             result ~= ");";
         }
-        static foreach(_Scenario; Scenarios!M) {
-            result~=format(q{%1$s, "%1$s",}, _Scenario.stringof);
+        static foreach (_Scenario; Scenarios!M) {
+            result ~= format(q{%1$s, "%1$s",}, _Scenario.stringof);
         }
     }
     return result.join("\n");
@@ -211,6 +207,7 @@ protected string _scenarioTupleCode(alias M, string tuple_name)() if (isFeature!
 mixin template ScenarioTuple(alias M, string tuple_name) {
     import std.array : join;
     import std.format;
+
     enum code = _scenarioTupleCode!(M, tuple_name);
     mixin(code);
 }
@@ -218,6 +215,7 @@ mixin template ScenarioTuple(alias M, string tuple_name) {
 @safe
 auto automation(alias M)() if (isFeature!M) {
     import std.typecons;
+
     mixin(format(q{import %s;}, moduleName!M));
 
     static struct FeatureFactory {
@@ -226,33 +224,26 @@ auto automation(alias M)() if (isFeature!M) {
         mixin ScenarioTuple!(M, "ScenariosT");
         ScenariosT scenarios;
         void opDispatch(string scenario_name, Args...)(Args args) {
-            pragma(msg, "Scenarios.fieldNames ", ScenariosT.fieldNames, "Args ", Args);
-            pragma(msg, "ScenariosT ", ScenariosT);
-
             enum code_1 = format(q{alias Scenario=typeof(ScenariosT.%1$s);}, scenario_name);
-            pragma(msg, "code_1 ", code_1);
+            // pragma(msg, "code_1 ", code_1);
             mixin(code_1);
-            pragma(msg, "X ", Scenario, " ", is(Scenario == class));
-            //auto x= new Scenario();
-//            auto y= new Scenario(args);
-            alias PickCtorParams = ParameterTypeTuple!(__traits(getOverloads, Scenario,
-"__ctor")[0]);
-            pragma(msg, "PickCtorParams ", PickCtorParams);
-
+            alias PickCtorParams = ParameterTypeTuple!(
+                    __traits(getOverloads, Scenario, "__ctor")[0]);
             enum code = format(q{scenarios.%1$s = new typeof(ScenariosT.%1$s)(args);}, scenario_name);
-            pragma(msg, code);
+            // pragma(msg, code);
             mixin(code);
 
         }
+
         FeatureGroup run() nothrow {
             import tagion.behaviour.BehaviourException : BehaviourError;
+
             FeatureGroup result;
             result.info.property = obtainFeature!M;
             result.info.name = moduleName!M;
             alias ScenariosSeq = Scenarios!M;
             result.scenarios.length = ScenariosSeq.length;
-            static foreach (i, _Scenario; ScenariosSeq)
-            {
+            static foreach (i, _Scenario; ScenariosSeq) {
                 try {
                     io.writefln("run %s ", _Scenario.stringof);
                     static if (__traits(compiles, new _Scenario())) {
@@ -260,23 +251,20 @@ auto automation(alias M)() if (isFeature!M) {
                             result.scenarios[i] = new _Scenario();
                         }
                     }
-                    pragma(msg, "scenarios[i] ", typeof(scenarios[i]));
                     result.scenarios[i] = .run(scenarios[i]);
                 }
                 catch (Exception e) {
                     import std.exception : assumeWontThrow;
-                    // assumeWontThrow({
-                    //         io.writefln("FeatureGroup run %s", e.msg);
+
                     result.scenarios[i].info.result = assumeWontThrow(BehaviourError(e).toDoc);
-                        // });
                 }
             }
             return result;
         }
 
     }
+
     FeatureFactory result;
-//    auto feature
     return result;
 }
 
@@ -298,7 +286,7 @@ bool hasErrors(ref const FeatureGroup feature_group) nothrow {
  */
 @safe
 bool hasErrors(ref const ScenarioGroup scenario_group) nothrow {
-    static foreach(i, Type; Fields!ScenarioGroup) {
+    static foreach (i, Type; Fields!ScenarioGroup) {
         static if (__traits(isSame, TemplateOf!(Type), ActionGroup)) {
             if (scenario_group.tupleof[i].infos.any!(info => info.result.isRecordType!BehaviourError)) {
                 return true;
@@ -321,7 +309,7 @@ unittest {
     auto feature_with_ctor = automation!(WithCtor)();
 
     { // No constructor has been called for the scenarios, this means that scenarios and the feature will have errors
-        const feature_result=feature_with_ctor.run;
+        const feature_result = feature_with_ctor.run;
         assert(feature_result.scenarios[0].hasErrors);
         assert(feature_result.scenarios[1].hasErrors);
         assert(feature_result.hasErrors);
@@ -330,11 +318,8 @@ unittest {
     { // Fails in second scenario because the constructor has not been called
         // Calls the construction for the Some_awesome_feature scenario
         feature_with_ctor.Some_awesome_feature(42, "with_ctor");
-        const feature_result=feature_with_ctor.run;
-//        io.writefln("feature_result_with_ctor=%s", feature_result.toPretty);
-        // io.writefln("feature_result.scenarios[0]=%s", feature_result.scenarios[0].toPretty);
+        const feature_result = feature_with_ctor.run;
         assert(!feature_result.scenarios[0].hasErrors);
-        // io.writefln("feature_result.scenarios[1]=%s", feature_result.scenarios[1].toPretty);
         assert(feature_result.scenarios[1].hasErrors);
         assert(feature_result.hasErrors);
     }
@@ -343,23 +328,16 @@ unittest {
         // Calls the construction for the Some_awesome_feature scenario
         feature_with_ctor.Some_awesome_feature(42, "with_ctor");
         feature_with_ctor.Some_awesome_feature_bad_format_double_property(17);
-        const feature_result=feature_with_ctor.run;
-//        io.writefln("feature_result_with_ctor=%s", feature_result.toPretty);
-        // io.writefln("feature_result.scenarios[0]=%s", feature_result.scenarios[0].toPretty);
+        const feature_result = feature_with_ctor.run;
         assert(!feature_result.scenarios[0].hasErrors);
-        // io.writefln("feature_result.scenarios[1]=%s", feature_result.scenarios[1].toPretty);
         assert(!feature_result.scenarios[1].hasErrors);
         assert(!feature_result.hasErrors);
     }
 }
 
-version (unittest)
-{
-    //    import std.stdio;
-    // import WithoutCtor =tagion.behaviour.BehaviourUnittest;
-    // import WithCtor = tagion.behaviour.BehaviourUnittestWithCtor;
+version(unittest) {
     import tagion.hibon.Document;
-    import io=std.stdio;
+    import io = std.stdio;
     import tagion.hibon.HiBONJSON;
 }
 
@@ -377,14 +355,13 @@ bool hasPassed(ref const FeatureGroup feature_group) nothrow {
 
 @safe
 bool hasPassed(ref const ScenarioGroup scenario_group) nothrow {
-     static foreach(i, Type; Fields!ScenarioGroup) {
+    static foreach (i, Type; Fields!ScenarioGroup) {
         static if (__traits(isSame, TemplateOf!(Type), ActionGroup)) {
             if (scenario_group.tupleof[i].infos.any!(info => !info.result.isRecordType!Result)) {
                 return false;
             }
         }
         else static if (__traits(isSame, TemplateOf!(Type), Info)) {
-            //assumeWontThrow(io.writefln("scenario_group.tupleof[i].result = %s", scenario_group.tupleof[i].result.toPretty));
             if (!scenario_group.tupleof[i].result.isRecordType!Result) {
                 return false;
             }
@@ -392,7 +369,6 @@ bool hasPassed(ref const ScenarioGroup scenario_group) nothrow {
     }
     return true;
 }
-
 
 ///
 @safe
@@ -405,7 +381,7 @@ unittest {
     feature_with_ctor.Some_awesome_feature_bad_format_double_property(17);
 
     { // None of the scenario passes
-        const feature_result=feature_with_ctor.run;
+        const feature_result = feature_with_ctor.run;
         assert(!feature_result.scenarios[0].hasPassed);
         assert(!feature_result.scenarios[1].hasPassed);
         assert(!feature_result.hasPassed);
@@ -413,11 +389,9 @@ unittest {
 
     { // None of the scenario passes
         WithCtor.pass = true; /// Pass all tests!
-        const feature_result=feature_with_ctor.run;
+        const feature_result = feature_with_ctor.run;
         assert(feature_result.scenarios[0].hasPassed);
         assert(feature_result.scenarios[1].hasPassed);
         io.writefln("feature_result =%s", feature_result.toPretty);
-//        assert(!feature_result.hasPassed);
     }
-
 }
--- src/lib-behaviour/tagion/behaviour/BehaviourFeature.d
+++ src/lib-behaviour/tagion/behaviour/BehaviourFeature.d
@@ -10,7 +10,6 @@ import tagion.hibon.HiBONRecord;
 import tagion.hibon.Document;
 
 @safe:
-
 mixin template Property() {
     string description;
     @Label(VOID, true) string[] comments;
@@ -64,7 +63,7 @@ enum isInfo(alias I) = __traits(isSame, TemplateOf!I, Info);
 
 struct ActionGroup(Property) if (isOneOf!(Property, BehaviourProperties)) {
     Info!Property[] infos;
-//    @Label(VOID, true) Info!And[] ands;
+    //    @Label(VOID, true) Info!And[] ands;
     mixin HiBONRecord!();
 }
 
@@ -96,58 +95,6 @@ alias BehaviourProperties = AliasSeq!(Given, When, Then, But);
 /// The behaviour-properties which only occurrences once in a Scenario
 alias MandatoryBehaviourProperties = Erase!(When, Erase!(But, BehaviourProperties));
 
-// alias MemberProperty=Tuple!(string, "member", string, "goal");
-// alias PropertyFormat(T)=format!(T.stringof~".%s", string);
-
-// const(MemberProperty[]) memberSequency(T)() if (is(T==class) || is(T==struct)) {
-//     MemberProperty[] result;
-//     static foreach(name; __traits(allMembers, T)) {{
-//             enum code=format!q{alias member=%s.%s;}(T.stringof, name);
-//             mixin(code);
-//             static if (__traits(compiles, typeof(member))) {
-//                 alias hasProperty(Property) =hasUDA!(member, Property);
-//                 alias filterProperty=Filter!(hasProperty, BehaviourProperties);
-//                 static if (filterProperty.length == 1) {
-//                     result~=MemberProperty(
-//                         PropertyFormat!T(name),
-//                         filterProperty[0].stringof);
-//                 }
-//             }
-//         }}
-//     return result;
-// }
-
-// unittest { // Test of memberSequency
-//     alias SomeFormat=format!(Some_awesome_feature.stringof~".%s", string);
-
-//     const expected=
-//         zip(
-//             ["is_valid", "in_credit", "contains_cash", "request_cash", "is_debited", "is_dispensed"],
-//             ["Given", "And", "And", "When", "Then", "And"]
-//         )
-//         .map!(a => tuple(SomeFormat(a[0]), a[1]))
-//         .array;
-
-//     assert(equal(memberSequency!Some_awesome_feature,
-//             expected));
-// }
-
-// template getMemberAlias(string main, string name) {
-//     enum code = format!q{alias getMemberAlias=%s.%s;}(main, name);
-//     pragma(msg, "code ", code);
-//     mixin(code);
-// }
-
-// template getMemberAlias_(alias main, string name) {
-// //     pragma(msg, "modulename ", moduleName!main);
-// //     mixin(format(q{import %s;}, moduleName!main));
-// // //    mixin Import!main;
-// //     enum code = format!q{alias getMemberAlias=%s.%s;}(main.stringof, name);
-// //     pragma(msg, "code ", code);
-// //     mixin(code);
-//     alias getMemberAlias_= __traits(getMember, main, name);
-// }
-
 template getMethod(alias T, string name) {
     alias method = __traits(getOverloads, T, name);
     static if (method.length > 0) {
@@ -171,6 +118,7 @@ template getAllCallables(T) if (is(T == class) || is(T == struct)) {
 
 static unittest { // Test of getAllCallable
     import tagion.behaviour.BehaviourUnittest;
+
     alias all_callables = getAllCallables!(Some_awesome_feature);
     static assert(all_callables.length == 13);
     static assert(allSatisfy!(isCallable, all_callables));
@@ -192,6 +140,7 @@ template getBehaviours_(T) if (is(T == class) || is(T == struct)) {
     alias getBehaviours_ = Filter!(hasBehaviours, get_all_callable);
 }
 
+///
 static unittest { // Test of getBehaviours
     alias behaviours = getBehaviours_!(BehaviourUnittest.Some_awesome_feature);
     static assert(behaviours.length == 7);
@@ -217,6 +166,7 @@ template getBehaviour(T, Property) if (is(T == class) || is(T == struct)) {
 
 }
 
+///
 unittest {
     alias behaviour_with_given = getBehaviour!(BehaviourUnittest.Some_awesome_feature, Given);
     static assert(allSatisfy!(isCallable, behaviour_with_given));
@@ -275,12 +225,13 @@ unittest {
 
     alias behaviour_of_but = getBehaviour!(BehaviourUnittest.Some_awesome_feature, But);
     static assert(behaviour_of_but.length is 1);
-    static assert(getProperty!(behaviour_of_but[0]) == But("if the Customer does not take his card, then the card must be swollowed"));
+    static assert(getProperty!(behaviour_of_but[0]) ==
+            But("if the Customer does not take his card, then the card must be swollowed"));
 }
-
 
 enum isScenario(T) = hasUDA!(T, Scenario);
 
+/// 
 static unittest {
     static assert(isScenario!(BehaviourUnittest.Some_awesome_feature));
 }
@@ -304,6 +255,7 @@ template isFeature(alias M) if (__traits(isModule, M)) {
 @safe
 unittest {
     import tagion.behaviour.BehaviourUnittest;
+
     static assert(isFeature!(tagion.behaviour.BehaviourUnittest));
     static assert(!isFeature!(tagion.behaviour.BehaviourFeature));
 }
@@ -326,11 +278,11 @@ template obtainFeature(alias M) if (__traits(isModule, M)) {
 @safe
 unittest { // The obtainFeature of a module
     import tagion.behaviour.BehaviourUnittest;
+
     static assert(obtainFeature!(tagion.behaviour.BehaviourUnittest) ==
             Feature(
                 "Some awesome feature should print some cash out of the blue", null));
     static assert(!obtainFeature!(tagion.behaviour.BehaviourFeature));
-
 }
 
 protected template _Scenarios(alias M, string[] names) if (__traits(isModule, M)) {
@@ -341,7 +293,7 @@ protected template _Scenarios(alias M, string[] names) if (__traits(isModule, M)
         alias member = __traits(getMember, M, names[0]);
         enum is_object = is(member == class) || is(member == struct);
         static if (is_object) {
-            enum is_scenario  = hasUDA!(member, Scenario);
+            enum is_scenario = hasUDA!(member, Scenario);
         }
         else {
             enum is_scenario = false;
@@ -366,6 +318,7 @@ template Scenarios(alias M) if (__traits(isModule, M)) {
 ///
 static unittest { //
     import tagion.behaviour.BehaviourUnittest;
+
     alias scenarios = Scenarios!(tagion.behaviour.BehaviourUnittest);
     pragma(msg, "scenarios ", scenarios);
     alias expected_scenarios = AliasSeq!(
@@ -392,13 +345,14 @@ template getScenario(T) if (is(T == class) || is(T == struct)) {
 
 static unittest {
     import tagion.behaviour.BehaviourUnittest;
+
     enum scenario = getScenario!(Some_awesome_feature);
     static assert(is(typeof(scenario) == Scenario));
     static assert(scenario is Scenario("Some awesome money printer", null));
 }
 
 version (unittest) {
-    import BehaviourUnittest=tagion.behaviour.BehaviourUnittest;
+    import BehaviourUnittest = tagion.behaviour.BehaviourUnittest;
     import std.stdio;
     import std.algorithm.iteration : map, joiner;
     import std.algorithm.comparison : equal;
