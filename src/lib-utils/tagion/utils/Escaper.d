027aa13085fa3d71435f8db0aa21d8697153bae0
cbleser <cr@tagion.org>
2022-09-30 12:37:50 +0300

    WIP: Escaper

--- src/lib-utils/tagion/utils/Escaper.d
+++ src/lib-utils/tagion/utils/Escaper.d
@@ -4,35 +4,37 @@ import std.format;
 import std.algorithm.iterationi : map;
 import std.array : join;
 
-@safe 
+@safe
 struct Excaper(S) if (isInputRange!S && is(ElementType!S : const(char))) {
     protected {
-char escape_char;
-    S range;
-}
+        char escape_char;
+        S range;
+    }
     @disable this();
     this(S range) @nogc {
-    this.range = range;
-}
+        this.range = range;
+    }
 
     @nogc pure nothrow {
-    bool empty() const {
-    return range.empty;
-}
+        bool empty() const {
+            return range.empty;
+        }
 
-    char front() const {
-        if (escape_char is char.init) {
-        return escape_char;
-    }
-        return range.front;
-}
+        char front() const {
+            if (escape_char is char.init) {
+                return escape_char;
+            }
+            return range.front;
+        }
 
-    enum special_chars="ntr'\"\\";
-    enum code_esc_special_chars=format("enum code_esc_special=%s;", 
-special_chars.map!((char c) => '\'~c));
+        enum special_chars = "ntr'\"\\";
+        enum x =
+            special_chars.map!(c => c);
+        enum code_esc_special_chars = format("enum code_esc_special=%s;",
+                    special_chars.map!(c => c));
 
-//    pragma(msg, code_esc_special_chars);
-/+ 
+        //    pragma(msg, code_esc_special_chars);
+        /+ 
     void popFront() const {
         if (escape_char is char.init) {
             switch(range.front) {
@@ -51,4 +53,4 @@ special_chars.map!((char c) => '\'~c));
 
     }
 +/
-}
+    }
