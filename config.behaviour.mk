bc185401590fa28028b26543b7613cdc00712a65
cbleser <cr@tagion.org>
2022-10-10 08:34:20 +0300

    WIP: make bddtest

--- config.behaviour.mk
+++ config.behaviour.mk
@@ -1,5 +1,10 @@
 
-BDD_FLAGS+=${addprefix -I,$(DINC)}
+BDD_FLAGS+=${addprefix -I,$(BDD)}
 
 BDD_LOG=$(DLOG)/bdd
 
+BDDFILES+=${shell find $(BDD) -type f -name "*.d" -path "*/tagion/*" -a -not -name "*.gen.d"}
+BDDFILES+=${shell find $(BDD) -type f -name "*.d" -path "*/bin/*"}
+
+target-bdd_test: DFILES+=$(BDDFILES)
+${call BIN,bdd_test,BDD_TEST,$(LIBOPENSSL) $(LIBSECP256K1) $(LIBP2PGOWRAPPER)}
