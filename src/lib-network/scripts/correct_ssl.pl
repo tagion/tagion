69d833ec3bfabc83cde5921273ea159577d910ea
cbleser <cr@tagion.org>
2022-10-23 10:53:01 +0300

    WIP: dstep correcting of wolfssl ssl.di

--- src/lib-network/scripts/correct_ssl.pl
+++ src/lib-network/scripts/correct_ssl.pl
@@ -4,6 +4,7 @@
 while (<>) {
     s/(^\s+)d(\s+d;)/$1_D$2/;
     s/(^\s+struct\s+)d/$1_D/;
+    s/^(\s*)enum(\s+(wolfSSL_set_using_nonblock|wolfSSL_get_using_nonblock)\s+=)/$1alias$2/;
+    s/^(\s*(warning_return|fatal_return)\s+=\s+)/$1cast(int)/;
     print;
 }
-
