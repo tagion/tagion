ec5be48ccf80ca9451c7083e39bcaea21478b4db
cbleser <cr@tagion.org>
2022-10-30 13:27:25 +0200

    WIP: SSL.d dead code has been removed

--- src/lib-network/tagion/network/SSL.d
+++ src/lib-network/tagion/network/SSL.d
@@ -26,8 +26,6 @@ version (WOLFSSL) {
         private import tagion.network.wolfssl.c.error_ssl;
         private import tagion.network.wolfssl.c.ssl;
 
-        //                private import tagion.network.wolfssl.c.wolfcrypt.memory : wolfSSL_SetAllocators;
-
         package {
             alias SSL = WOLFSSL;
             alias SSL_CTX = WOLFSSL_CTX;
@@ -75,50 +73,10 @@ version (WOLFSSL) {
                 return format("enum ALL_SSLErrorCodes {\n%-(%s \n%)\n};", enum_list);
             }
         }
-        import core.memory;
-
-        //version(none)
-        extern (C) {
-            alias wolfSSL_Malloc_cb = void* function(size_t size);
-            alias wolfSSL_Free_cb = void function(void* ptr);
-            alias wolfSSL_Realloc_cb = void* function(void* ptr, size_t size);
-            void* wolfSSL_Malloc(size_t size);
-            void wolfSSL_Free(void* ptr);
-            void* wolfSSL_Realloc(void* ptr, size_t size);
-            /* WOLFSSL_DEBUG_MEMORY */
-            /* WOLFSSL_STATIC_MEMORY */
-
-            /* Public get/set functions */
-            int wolfSSL_SetAllocators(
-                    wolfSSL_Malloc_cb mf,
-                    wolfSSL_Free_cb ff,
-                    wolfSSL_Realloc_cb rf);
-            int wolfSSL_GetAllocators(
-                    wolfSSL_Malloc_cb* mf,
-                    wolfSSL_Free_cb* ff,
-                    wolfSSL_Realloc_cb* rf);
-        }
-
-        void* _wolfSSL_malloc(size_t size) {
-            return GC.malloc(size);
-        }
-
-        void _wolfSSL_free(void* ptr) {
-            GC.free(ptr);
-        }
-
-        void* _wolfSSL_realloc(void* ptr, size_t size) {
-            return GC.realloc(ptr, size);
-        }
-    }
+   }
     pragma(msg, generator_all_SSLErrorCodes);
     /// enum ALL_SSLErrprCodes
     mixin(generator_all_SSLErrorCodes);
-    version (none) static this() {
-
-        wolfSSL_SetAllocators(&_wolfSSL_malloc, &_wolfSSL_free, &_wolfSSL_realloc);
-        //       wolfSSL_SetAllocators(&GC.malloc, &GC.free, &GC.realloc);
-    }
 }
 else {
     extern (C) {
@@ -165,22 +123,6 @@ else {
             char* SSL_state_string_long(const SSL*);
         }
     }
-    /+
-    enum SSLErrorCodes {
-        SSL_ERROR_NONE = 0,
-        SSL_ERROR_SSL = 1,
-        SSL_ERROR_WANT_READ = 2,
-        SSL_ERROR_WANT_WRITE = 3,
-        SSL_ERROR_WANT_X509_LOOKUP = 4,
-        SSL_ERROR_SYSCALL = 5, /* look at error stack/return
-                                      * value/errno */
-        SSL_ERROR_ZERO_RETURN = 6,
-        SSL_ERROR_WANT_CONNECT = 7,
-        SSL_ERROR_WANT_ACCEPT = 8,
-        SSL_ERROR_WANT_ASYNC = 9,
-        SSL_ERROR_WANT_ASYNC_JOB = 10
-    }
-+/
-    alias SSLErrorCodes = _SSLErrorCodes;
 
+    alias SSLErrorCodes = _SSLErrorCodes;
 }
