b7f9816c88e31a8ae97ff7748d8c18fd4e9e3c72
cbleser <cr@tagion.org>
2022-11-05 17:58:34 +0200

    Version and help switch added to onetool

--- src/lib-tools/tagion/tools/OneMain.d
+++ src/lib-tools/tagion/tools/OneMain.d
@@ -4,18 +4,32 @@ import std.typecons : Tuple;
 
 alias Result = Tuple!(int, "exit_code", bool, "executed");
 mixin template doOneMain(alltools...) {
+    import std.getopt;
     import std.stdio;
     import std.traits;
     import std.array : split;
     import std.range : tail;
     import std.format;
     import std.path : baseName;
+    import tagion.tools.revision;
+    import std.array : join;
+    import std.algorithm.searching : canFind;
 
-    enum toolName(alias tool) = moduleName!tool.split(".").tail(1)[0];
+    /*
+    * Strips the non package name from a module-name
+    */
+    enum tailName(string name) = name.split(".").tail(1)[0];
+    enum toolName(alias tool) = tailName!(moduleName!tool);
 
+    /* 
+     * 
+     * Params:
+     *   tool = The name of the tool to be called
+     *   args = optarg for the tools
+     * Returns: 
+     *  The exit-code and if the tool has been executed
+     */
     Result call_main(string tool, string[] args) {
-
-        //    const tool = args[0].baseName;
     SelectTool:
         switch (tool) {
             static foreach (toolmod; alltools) {
@@ -25,7 +39,8 @@ mixin template doOneMain(alltools...) {
         case toolmod.alternative_name:
                     }
         case toolname:
-                    enum code = format(q{return Result(%s._main(args), true);}, toolname);
+                    enum code = format(q{return Result(%s.%s(args), true);},
+                                toolname, tailName!(toolmod.main_name));
                     mixin(code);
                     break SelectTool;
                 }
@@ -36,24 +51,97 @@ mixin template doOneMain(alltools...) {
         assert(0);
     }
 
+    enum alternative(alias mod) = mod.alternative_name;
+    enum notNull(string name) = name !is null;
+    /* 
+    * Lists all the toolnames inclusive the alternative name as a alias-sequency
+    */
+    enum toolnames = [
+            AliasSeq!(
+                    staticMap!(toolName, alltools),
+                    Filter!(notNull, staticMap!(alternative, alltools))
+            )
+        ];
+
+    /* 
+     * Handles the arguments for the onetool main
+     * Params:
+     *   args = cli arguments
+     * Returns: true if the onetool should continue to execute a tool
+     */
+    bool onetool_main(string[] args) {
+        immutable program = args[0];
+        bool version_switch;
+        bool link_switch;
+        try {
+            auto main_args = getopt(args,
+                    std.getopt.config.caseSensitive,
+                    "version", "display the version", &version_switch,
+                    std.getopt.config.bundling,
+                    "Q|link", "Creates soft links all the link", &link_switch,
+            );
+
+            if (version_switch) {
+                revision_text.writeln;
+                return false;
+            }
+
+            if (link_switch) {
+                return false;
+            }
+
+            if (main_args.helpWanted) {
+                defaultGetoptPrinter(
+                        [
+                    revision_text,
+                    "Documentation: https://tagion.org/",
+                    "Usage:",
+                    format("%s <program> [<option>...]", program),
+                    format("Tool programs %-(%s, %)", toolnames),
+                    "",
+                    "<option>:",
+
+                ].join("\n"),
+                main_args.options);
+                return false;
+            }
+        }
+        catch (GetOptException e) {
+            if (args.length > 0) {
+                return true;
+            }
+            stderr.writeln(e.msg);
+            return false;
+        }
+        return true;
+    }
+
+    /* 
+     * 
+     * Params:
+     *   args = optarg for the one tool
+     *          args[0] or arg[1] is the name of tool 
+     * Returns: exit-code for the program
+     */
     int do_main(string[] args) {
+        Result result;
         auto tool = args[0].baseName;
-        auto result = call_main(tool, args);
-        if (!result.executed && args.length > 1) {
+        if (toolnames.canFind(tool)) {
+            result = call_main(tool, args);
+        }
+        else if (args.length > 1) {
             tool = args[1];
-            result = call_main(tool, args[1 .. $]);
+            if (toolnames.canFind(tool)) {
+                result = call_main(tool, args[1 .. $]);
+            }
+        }
+        if (!result.executed) {
+            if (onetool_main(args)) {
+                stderr.writefln("Invalid tool %s, available tools are %-(%s, %)", tool, toolnames);
+                return 1;
+            }
         }
 
-        if (!result.executed) {
-            enum alternative(alias mod) = mod.alternative_name;
-            enum notNull(string name) = name !is null;
-            enum toolnames = AliasSeq!(
-                        staticMap!(toolName, alltools),
-                        Filter!(notNull, staticMap!(alternative, alltools))
-                );
-            stderr.writefln("Invalid tool %s available %-(%s, %)", tool, [toolnames]);
-            return 1;
-        }
         return result.exit_code;
     }
 }
