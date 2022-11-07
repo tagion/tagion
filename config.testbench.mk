7f8ff4a8b08611434a144eb8fe34464536892ffa
cbleser <cr@tagion.org>
2022-11-07 11:23:59 +0200

    FIX: testbench make flow

--- config.testbench.mk
+++ config.testbench.mk
@@ -8,11 +8,12 @@ BDD_DFLAGS+=${addprefix -I,$(BDD)}
 
 BDD_LOG=$(DLOG)/bdd
 
-BDD_DFILES+=${shell find $(BDD) -name "*.d" -a -path "*/testbench/*" -a -not -path "*/unitdata/*" $(NO_WOLFSSL) }
+BDD_DFILES+=${shell find $(BDD) -name "*.d" -a -not -name "*.gen.d" -a -path "*/testbench/*" -a -not -path "*/unitdata/*" $(NO_WOLFSSL) }
 
 #
 # Binary testbench 
 #
+testbench: bddfiles
 target-testbench: DFLAGS+=$(DVERSION)=ONETOOL
 target-testbench: LIBS+=$(SSLIMPLEMENTATION) $(LIBSECP256K1) $(LIBP2PGOWRAPPER)
 target-testbench: DFILES+=$(BDD_DFILES)
