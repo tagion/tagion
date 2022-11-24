7d9121851acfea65331ac87a0b9b443ded59a773
Philip Rying <philiprying@gmail.com>
2022-11-24 19:38:28 +0100

    WIP: bundle markdown files

--- tub/bundle_bdd_files.d
+++ tub/bundle_bdd_files.d
@@ -1,29 +1,37 @@
 #!/usr/bin/env rdmd
-
-import std.stdio;
-import std.file : dirEntries, readText, exists, mkdirRecurse, rmdir;
+import std.algorithm;
+import std.array;
+import std.file;
 import std.path;
+import std.stdio;
 import std.process;
+import core.stdc.string : strlen;
+import std.conv : to;
+import std.string;
 
+auto get_md_files(string pathname)
+{    
 
-string[] listdir(string pathname)
-{
-    import std.algorithm;
-    import std.array;
-    import std.file;
-    import std.path;
-
+    return dirEntries(pathname, SpanMode.depth)
+        .filter!(f => f.name.endsWith(".md"))
+        .filter!(f => f.name.canFind("gen.md"))
+        .array;
     
-    foreach(file; dirEntries(pathname, SpanMode.shallow)
-        .filter!(a => a.isFile)) {
-            writefln("file: %s", file);
-        }
-
 }
 
 int main(string[] args) {
-    const BDD = environment["BDD"];
-    
+    // const BDD = environment["BDD"];
+    const BDD_PATH = "/home/imrying/work/tagion/bdd";
+    const BDD_LENGTH = BDD_PATH.length;
+
+    auto md_files = get_md_files(BDD_PATH);
+
+    string[] paths;
+    foreach(i, file; md_files) {
+        paths ~= toStringz(file[BDD_LENGTH-3 .. file.length]).to!string;
+    }
+
+    writeln(paths);
     return 0;
 
 }
