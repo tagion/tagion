9a01396e4cadeac73cb2fb699c75f1f5d08d8fff
Philip Rying <philiprying@gmail.com>
2022-12-07 09:31:37 +0100

    rm server from main bdd file

--- bdd/tagion/testbench/ssl_echo_server.d
+++ bdd/tagion/testbench/ssl_echo_server.d
@@ -9,14 +9,14 @@ import tagion.hibon.HiBONRecord : fwrite;
 mixin Main!(_main);
 int _main(string[] args)
 {
-    // auto ssl_echo_feature = automation!(SSL_echo_test)();
-    // auto ssl_echo_context = ssl_echo_feature.run;
+    auto ssl_echo_feature = automation!(SSL_echo_test)();
+    auto ssl_echo_context = ssl_echo_feature.run;
 
-    // auto ssl_echo_d_client_feature = automation!(SSL_D_Client_test)();
-    // auto ssl_echo_d_client_context = ssl_echo_d_client_feature.run;
+    auto ssl_echo_d_client_feature = automation!(SSL_D_Client_test)();
+    auto ssl_echo_d_client_context = ssl_echo_d_client_feature.run;
 
-    auto ssl_echo_d_server_feature = automation!(SSL_D_Server_test)();
-    auto ssl_echo_d_server_context = ssl_echo_d_server_feature.run;
+    // auto ssl_echo_d_server_feature = automation!(SSL_D_Server_test)();
+    // auto ssl_echo_d_server_context = ssl_echo_d_server_feature.run;
 
     return 0;
 
