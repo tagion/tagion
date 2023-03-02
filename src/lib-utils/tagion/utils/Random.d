f165335837f15303e47e403477d778ea27e673cc
cbleser <cr@tagion.org>
2023-03-02 15:40:25 +0200

    WIP: Random is now a inputrange and forwardrange

--- src/lib-utils/tagion/utils/Random.d
+++ src/lib-utils/tagion/utils/Random.d
@@ -1,22 +1,27 @@
+/// Pseudo random range
 module tagion.utils.Random;
 
-@safe
+@safe @nogc
 struct Random(T = uint) {
     private T m_z;
     private T m_w;
-    this(const T seed_value) {
+    this(const T seed_value) pure nothrow {
         seed(seed_value);
     }
 
-    void seed(const T seed_value) {
+private this(T m_z, T m_w) pure nothrow {
+        this.m_z = m_z;
+        this.m_w = m_w;
+    }
+
+    void seed(const T seed_value) pure nothrow {
         m_z = 13 * seed_value;
         m_w = 7 * seed_value;
     }
 
     T value() {
-        m_z = 36969 * (m_z & T.max) + (m_z >> 16);
-        m_w = 18000 * (m_w & T.max) + (m_w >> 16);
-        return (m_z << 16) + m_w;
+        popFront;
+        return front;
     }
 
     T value(const(T) range) {
@@ -32,4 +37,37 @@ struct Random(T = uint) {
         return (value % range) + from;
     }
 
+    void popFront() pure nothrow {
+        m_z = 36_969 * (m_z & T.max) + (m_z >> 16);
+        m_w = 18_000 * (m_w & T.max) + (m_w >> 16);
+    }
+
+    T front() const pure nothrow {
+        return (m_z << 16) + m_w;
+    }
+
+    enum bool empty=false;
+
+    Random save() pure nothrow {
+       return Random(m_z, m_w); 
+    }
+import std.range.primitives : isInputRange, isForwardRange, isInfinite;
+
+static assert(isInputRange!(Random));
+static assert(isForwardRange!(Random));
+static assert(isInfinite!(Random));
+
 }
+@safe
+unittest {
+    import std.range : take;
+    import std.algorithm.comparison : equal;
+    auto r=Random!uint(1234);
+    auto r_forward=r.save;
+    assert(equal(r.take(5), r_forward.take(5)));
+
+    r.take(7);
+    r_forward=r.save;
+    assert(equal(r.take(4), r_forward.take(4)));
+}
+
