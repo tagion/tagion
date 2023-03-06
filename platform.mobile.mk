b000046e39a75a1cc64771cf1caf95a3e141f168
Lucas Rasmussen <lucasnethaj@protonmail.com>
2023-03-06 10:19:35 +0100

    fix: only add mobile flags to platform-mobile target

--- platform.mobile.mk
+++ platform.mobile.mk
@@ -1,9 +1,9 @@
 PLATFORMS+=MOBILE
-DINC+=${shell find $(DSRC) -maxdepth 1 -type d -path "*src/lib-*" }
+platform-mobile: DINC+=${shell find $(DSRC) -maxdepth 1 -type d -path "*src/lib-*" }
 # Betterc source files
 # DFILES?=${shell find $(DSRC) -type f -name "*.d" -path "*src/lib-betterc/*" -a -not -path "*/tests/*" -a -not -path "*/unitdata/*"}
-DFILES?=${shell fd -e d . src/lib-mobile}
-MTRIPLE=aarch64-android-linux
+platform-mobile: DFILES?=${shell fd -e d . src/lib-mobile}
+platform-mobile: MTRIPLE=aarch64-android-linux
 
 # We need the hosts precompiled runtime libraries and linker from android
 # LDCHOSTPATH?=/home/lucas/wrk/dondroid/ldc2-1.29.0-android-aarch64/
@@ -18,9 +18,9 @@ MTRIPLE=aarch64-android-linux
 # DFLAGS+=-linker=$(NDKPATH)/toolchains/llvm/prebuilt/linux-x86_64/bin/ld.lld
 
 # HACK: not define the arch flags -m32
-CROSS_OS=mobile
+platform-mobile: CROSS_OS=mobile
 
-DFLAGS+=-mtriple=$(MTRIPLE) 
+platform-mobile: DFLAGS+=-mtriple=$(MTRIPLE) 
 
 platform-mobile:
 	$(DC)  $(DFLAGS) ${addprefix -I,$(DINC)} --shared -of=libtagionmobile.so ${sort $(DFILES)}
