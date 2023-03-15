96f24857995fd5d4487059fce90f99084839406c
Lucas Rasmussen <lucasnethaj@protonmail.com>
2023-03-15 16:09:20 +0100

    small fixes to mobile target

--- src/lib-mobile/context.mk
+++ src/lib-mobile/context.mk
@@ -1,13 +1,13 @@
-MOBILE-DFILES?=${shell find $(DSRC) -name "*.d" -a -path "*/src/lib-mobile/*"}
+libmobile: DFILES?=${shell find $(DSRC) -name "*.d" -a -path "*/src/lib-mobile/*"}
 
-libmobile: LIBS += $(LIBSECP256K1_STATIC)
+libmobile: LIBS += $(LIBSECP256K1)
 libmobile: DFLAGS+=$(DEFAULTLIBSTATIC)
 libmobile: DFLAGS+=$(DVERSION)=TINY_AES
 libmobile: # secp256k1
 	$(PRECMD)
-	$(DC) $(DFLAGS) -i ${addprefix -I,$(DINC)} $(LIBS) ${sort $(MOBILE-DFILES)} --shared -of=$(DLIB)/libtagionmobile.so
+	$(DC) $(DFLAGS) -i ${addprefix -I,$(DINC)} $(LIBS) ${sort $(DFILES)} --shared -of=$(DLIB)/libtagionmobile.so
 
-mobile-bin-test: LIBS += $(LIBSECP256K1_STATIC)
-mobile-bin-test:
+libmobile-bin-test: LIBS += $(LIBSECP256K1_STATIC)
+libmobile-bin-test:
 	$(PRECMD)
-	$(DC) $(DFLAGS) -i ${addprefix -I,$(DINC)} $(LIBS) app.d ${sort $(MOBILE-DFILES)} -of=$(DBIN)/wallet_create
+	$(DC) $(DFLAGS) -i ${addprefix -I,$(DINC)} $(LIBS) app.d ${sort $(DFILES)} -of=$(DBIN)/wallet_create
