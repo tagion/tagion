abe712e4c91e1911dbad34be0d944617dbf4d1d0
Lucas Rasmussen <lucasnethaj@protonmail.com>
2023-03-15 16:29:16 +0100

    mk: mobile add include import flag

--- src/lib-mobile/context.mk
+++ src/lib-mobile/context.mk
@@ -1,13 +1,14 @@
-libmobile: DFILES?=${shell find $(DSRC) -name "*.d" -a -path "*/src/lib-mobile/*"}
+libmobile: MOBILE-DFILES?=${shell find $(DSRC) -name "*.d" -a -path "*/src/lib-mobile/*"}
 
 libmobile: LIBS += $(LIBSECP256K1)
-libmobile: DFLAGS+=$(DEFAULTLIBSTATIC)
+libmobile: DFLAGS+=$(DDEFAULTLIBSTATIC)
 libmobile: DFLAGS+=$(DVERSION)=TINY_AES
+libmobile: DFLAGS+=$(DINCIMPORT)
 libmobile: # secp256k1
 	$(PRECMD)
-	$(DC) $(DFLAGS) -i ${addprefix -I,$(DINC)} $(LIBS) ${sort $(DFILES)} --shared -of=$(DLIB)/libtagionmobile.so
+	$(DC) $(DFLAGS) ${addprefix -I,$(DINC)} $(LIBS) ${sort $(MOBILE-DFILES)} --shared -of=$(DLIB)/libtagionmobile.so
 
 libmobile-bin-test: LIBS += $(LIBSECP256K1_STATIC)
 libmobile-bin-test:
 	$(PRECMD)
-	$(DC) $(DFLAGS) -i ${addprefix -I,$(DINC)} $(LIBS) app.d ${sort $(DFILES)} -of=$(DBIN)/wallet_create
+	$(DC) $(DFLAGS) ${addprefix -I,$(DINC)} $(LIBS) app.d ${sort $(MOBILE-DFILES)} -of=$(DBIN)/wallet_create
