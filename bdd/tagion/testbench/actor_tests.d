64b723ed668567538411f33bceb685174f77967a
Lucas Rasmussen <lucasnethaj@protonmail.com>
2023-03-23 16:37:05 +0100

    WIP(#269): add skeleton

--- bdd/tagion/testbench/actor_tests.d
+++ bdd/tagion/testbench/actor_tests.d
@@ -18,8 +18,17 @@ mixin Main!(_main);
 
 int _main(string[] args) {
     if (env.stage == Stage.commit) {
+        // Sending messages between supervisor & children
+        version (282) {
         auto actor_supervisor_message_feature = automation!(actor_message)();
         auto actor_supervisor_message_context = actor_supervisor_message_feature.run();
+        }
+
+        // Supervisor with failing child
+        version (269) {
+        auto actor_supervisor_feature = automation!(actor_supervisor)();
+        auto actor_supervisor_context = actor_supervisor_feature.run();
+        }
     }
 
     return 0;
