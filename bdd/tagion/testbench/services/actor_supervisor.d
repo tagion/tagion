0948a436c07cae2a8bb6a3ff5ed28931c70e45d6
Lucas Rasmussen <lucasnethaj@protonmail.com>
2023-03-26 14:50:02 +0200

    WIP(#269): make sure the actors starts

--- bdd/tagion/testbench/services/actor_supervisor.d
+++ bdd/tagion/testbench/services/actor_supervisor.d
@@ -6,6 +6,7 @@ import std.typecons : Tuple;
 import tagion.testbench.tools.Environment;
 
 import core.time;
+import std.format: format;
 import tagion.actor.Actor;
 
 enum feature = Feature(
@@ -19,6 +20,10 @@ alias FeatureContext = Tuple!(
 
 /// Child Actor
 struct SetUpForFailure {
+    
+    @method void fail(int _i) {
+        throw new Exception("Child: I am a failure");
+    }
     @task void run() {
         alive; // Actor is now alive
         while (!stop) {
@@ -41,23 +46,32 @@ struct SetUpForDissapointment {
 }
 static assert(isActor!SetUpForDissapointment);
 
+enum supervisor_task_name = "supervisor";
+enum child_task_name = "child";
+
 @safe @Scenario("Supervisor with failing child",
         [])
 class SupervisorWithFailingChild {
+    ActorHandle!SetUpForDissapointment supervisor_handle;
 
     @Given("a actor #super")
     Document aActorSuper() {
-        return Document();
+        auto supervisor_factory = actor!SetUpForDissapointment;
+        supervisor_handle = supervisor_factory(supervisor_task_name);
+
+        return result_ok;
     }
 
     @Given("a actor #child")
     Document aActorChild() {
-        return Document();
+        return result_ok;
     }
 
     @When("the actor #super start it should start the #child.")
     Document startTheChild() {
-        return Document();
+        check(isRunning(supervisor_task_name), "Supervisor is not running");
+        check(isRunning(child_task_name), "Child is not running");
+        return result_ok;
     }
 
     @When("the #child has started then the #child should fail with an exception")
@@ -77,6 +91,9 @@ class SupervisorWithFailingChild {
 
     @Then("the #super should stop")
     Document superShouldStop() {
-        return Document();
+        supervisor_handle.stop;
+        check(!isRunning(child_task_name), "Child is still running");
+        check(!isRunning(supervisor_task_name), "Supervisor is still running");
+        return result_ok;
     }
 }
