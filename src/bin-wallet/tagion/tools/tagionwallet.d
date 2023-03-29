4d9aca3c7d531cb688aee41f7b5eb7ce6e995006
cbleser <cr@tagion.org>
2023-03-29 18:36:28 +0300

    FIX: tagionwallet fixed

--- src/bin-wallet/tagion/tools/tagionwallet.d
+++ src/bin-wallet/tagion/tools/tagionwallet.d
@@ -27,7 +27,7 @@ import tagion.script.StandardRecords;
 import tagion.script.TagionCurrency;
 import tagion.crypto.SecureNet : StdSecureNet, StdHashNet, scramble;
 import tagion.wallet.KeyRecover;
-import tagion.wallet.WalletRecords : RecoverGenerator, DevicePIN, Quiz;
+import tagion.wallet.WalletRecords : RecoverGenerator, DevicePIN, Quiz, AccountDetails;
 import tagion.wallet.SecureWallet;
 import tagion.utils.Term;
 import tagion.basic.Message;
@@ -55,6 +55,16 @@ Socket socket(AddressFamily af,
     }
     return new SSLSocket(af, type);
 }
+/**
+ * \struct Invoices
+ * Struct invoices array
+ */
+struct Invoices {
+    /** internal array */
+    Invoice[] list;
+    mixin HiBONRecord;
+}
+
 /**
  * \struct WalletOptions
  * Struct wallet options files and network status storage models
