f8565ced72aa5c12a305bf1870a6b88e10a2c944
Lucas Rasmussen <lucasnethaj@protonmail.com>
2023-04-04 19:44:13 +0200

    retab

--- source/app.d
+++ source/app.d
@@ -14,39 +14,39 @@ void exceptionHandler(Exception e) {
 }
 
 Nullable!Tid maybeOwnerTid() {
-	// tid is "null"
-	Nullable!Tid tid;
-	try {
-		// Tid is asigned
-		tid = ownerTid;
-	}
-	catch(TidMissingException) {
-	}
-	return tid;
+    // tid is "null"
+    Nullable!Tid tid;
+    try {
+        // Tid is asigned
+        tid = ownerTid;
+    }
+    catch(TidMissingException) {
+    }
+    return tid;
 }
 
 /// Send to the owner if there is one
 void sendOwner(T...)(T vals) {
-	if (!maybeOwnerTid.isNull) {
-		send(maybeOwnerTid.get, vals);
-	}
-	// Otherwise writr a message to the logger instead,
-	// Otherwise just write it to stdout;
-	else {
-		write("No owner, writing message to stdout instead: ");
-		writeln(vals);
-	}
+    if (!maybeOwnerTid.isNull) {
+        send(maybeOwnerTid.get, vals);
+    }
+    // Otherwise writr a message to the logger instead,
+    // Otherwise just write it to stdout;
+    else {
+        write("No owner, writing message to stdout instead: ");
+        writeln(vals);
+    }
 }
 
 /// send your state to your owner
 void setState(Control ctrl) {
-	if (!maybeOwnerTid.isNull) {
-		prioritySend(maybeOwnerTid.get, thisTid, ctrl);
-	}
-	else {
-		write("No owner, writing message to stdout instead: ");
-		writeln(ctrl);
-	}
+    if (!maybeOwnerTid.isNull) {
+        prioritySend(maybeOwnerTid.get, thisTid, ctrl);
+    }
+    else {
+        write("No owner, writing message to stdout instead: ");
+        writeln(ctrl);
+    }
 }
 
 // State messages send to the supervisor
@@ -86,16 +86,16 @@ struct Logger {
                 (M!1, string str) { writeln("Fatal: ", str); },
                 /* &exceptionHandler, */
 
-				(OwnerTerminated _e) {
-					writefln("%s, Owner stopped... nothing to life for... stoping self", thisTid);
-					stop = true;
-				},
+                (OwnerTerminated _e) {
+                    writefln("%s, Owner stopped... nothing to life for... stoping self", thisTid);
+                    stop = true;
+                },
 
                 // Default
                 (Variant message) {
                         // For unkown messages we assert,
                         // so we don't accidentally fill up our messagebox with garbage
-						setState(Control.FAIL);
+                        setState(Control.FAIL);
                         assert(0, "No delegate to deal with message: %s".format(message));
                     }
                 );
@@ -106,9 +106,9 @@ struct Logger {
             }
             // If we catch an exception we send it back to owner for them to deal with it.
             catch (shared(Exception) e) {
-				// Preferable FAIL would be able to carry the exception with it
+                // Preferable FAIL would be able to carry the exception with it
                 ownerTid.prioritySend(e);
-				setState(Control.FAIL);
+                setState(Control.FAIL);
                 stop = true;
             }
         }
@@ -120,7 +120,7 @@ void main() {
     auto logger_proto = Logger();
     alias logger_task = logger_proto.task;
     Tid logger = spawn(&logger_task);
-	register("logger", logger);
+    register("logger", logger);
 
     logger.send(M!0(), "hello");
     logger.send(M!0(), "momma");
