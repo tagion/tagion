22954baaf0dcdd3c7f336139282f57036f2f55a6
Lucas Rasmussen <lucasnethaj@protonmail.com>
2023-04-06 14:57:21 +0200

    define the atom types in the actor so we can use them as a member

--- example/app.d
+++ example/app.d
@@ -10,30 +10,47 @@ static class SuperVisor : Actor {
     }
 }
 
+alias SuperVisorHandle = ActorHandle!SuperVisor;
+
 static class Logger : Actor {
+    alias hell = Msg!"hell";
+    alias info = Msg!"info";
+    alias fatal = Msg!"fatal";
+
+    static void _hell(hell, string str) {
+        writeln("Hell: ", str);/// something else
+    }
+
+    static void _info(info, string str) {
+        writeln("Info: ", str);/// something else
+    }
+
+    static void _fatal(fatal, string str) {
+        writeln("Fatal: ", str);
+    }
 
     nothrow void task() {
         actorTask(
-            (Msg!"info", string str) {
-                writeln("Info: ", str);
-                /// something else
-            },
-            (Msg!"fatal", string str) { writeln("Fatal: ", str); },
+            &_info,
+            &_hell,
+            &_fatal,
         );
     }
 
 }
 
+alias LoggerHandle = ActorHandle!Logger;
+
 void main() {
-    alias LoggerHandle = ActorHandle!Logger;
+
     LoggerHandle logger = spawnActor!Logger("logger_task");
 
     assert(checkCtrl(Ctrl.STARTING));
     assert(checkCtrl(Ctrl.ALIVE));
 
-    logger.send(Msg!"info"(), "plana");
-    logger.send(Msg!"fatal"(), "submarina");
-    logger.send(Msg!"info"(), "tuna");
+    logger.send(Logger.fatal(), "plana");
+    logger.send(Logger.info(), "plana");
+    logger.send(Logger.fatal(), "tuna");
     logger.send(Sig.STOP);
     assert(checkCtrl(Ctrl.END));
 
