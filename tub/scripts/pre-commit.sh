17bd800c83813ec12a1f9761be22639e4037e1dc
Lucas Rasmussen <lr@decard.io>
2023-04-18 00:37:22 +0200

    fix issue with multiple files

--- tub/scripts/pre-commit.sh
+++ tub/scripts/pre-commit.sh
@@ -4,10 +4,13 @@
 # It automatically restages the files after formatting
 # So if you stage a file then change it. Then that change will also be committed.
 
-# install this by putting it in .git/hooks/ and making it executable
+# install this by putting it in .git/hooks/ and making it executable or by running make instal-git-hooks
  
 exec 1>&2
 STAGED_DFILES=$(git diff --name-only --cached --diff-filter=ACM | grep '\.d$')
+CHARS=$(echo STAGED_DFILES | wc -c)
 
-test -n $STAGED_DFILES && dfmt -i $STAGED_DFILES && git add $STAGED_DFILES
-
+if [[ ! $CHARS -eq 0 ]]; then
+    dfmt -i $STAGED_DFILES;
+    git add $STAGED_DFILES;
+fi
