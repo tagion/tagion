5ec82fd32e9a376ccc43c8da89d10e77ef8aff30
Lucas Rasmussen <lr@decard.io>
2023-04-18 10:21:48 +0200

    REWIP(#269): recreate the basic structure

--- bdd/tagion/testbench/services/actor_supervisor.d
+++ bdd/tagion/testbench/services/actor_supervisor.d
@@ -4,13 +4,10 @@ import tagion.behaviour;
 import tagion.hibon.Document;
 import std.typecons : Tuple;
 import tagion.testbench.tools.Environment;
+import tagion.actor.actor;
+import std.concurrency;
 
-import core.time;
-import core.thread;
-import concurrency = std.concurrency;
-import std.format : format;
-import tagion.actor.Actor;
-
+import std.meta;
 import std.stdio;
 
 enum feature = Feature(
@@ -22,116 +19,88 @@ alias FeatureContext = Tuple!(
         FeatureGroup*, "result"
 );
 
-enum sleep_time = 100.msecs;
-
 /// Child Actor
 struct SetUpForFailure {
-
-    @method void exceptionalMethod(int _i) {
-        debug writeln("child is failing");
-        fail(new Exception("Child: I am a failure"));
+static:
+    void exceptional(Msg!"exceptional") {
+        throw new Exception("I am fail");
     }
 
-    @task void run() {
-        alive; // Actor is now alive
-
-        /* Thread.sleep(300.msecs); */
-        /* fail(Exception("Child: i am a failure")); */
-        /* throw new Exception("Child: I am a failure"); */
-        while (!stop) {
-            receiveTimeout(100.msecs);
-        }
-    }
-
-    mixin TaskActor; /// Turns the struct into an Actor
+    mixin Actor!(&exceptional); /// Turns the struct into an Actor
 }
 
-static assert(isActor!SetUpForFailure);
+alias ChildHandle = ActorHandle!SetUpForFailure;
 
 /// Supervisor Actor
-struct SetUpForDissapointment {
+struct SetUpForDisapointment {
+static:
+    SetUpForFailure child;
 
-    ActorHandle!SetUpForFailure child_handle;
+    alias children = AliasSeq!(child);
 
-    @method void isChildRunning(string task_name) {
-        Thread.sleep(sleep_time);
-        sendSupervisor(isRunning(task_name));
+    void disapoint(Msg!"issapoint") {
+        ChildHandle dissapointee = actorHandle!SetUpForFailure(child_task_name);
+        dissapointee.send(Msg!"exceptional"());
     }
 
-    @task void run() {
-        auto actor_factory = actor!SetUpForFailure;
-        child_handle = actor_factory(child_task_name);
-        alive; // Actor is now alive
-        while (!stop) {
-            receiveTimeout(100.msecs);
-        }
-    }
-
-    mixin TaskActor; /// Turns the struct into an Actor
+    mixin Actor!(&disapoint); /// Turns the struct into an Actor
 }
 
-static assert(isActor!SetUpForDissapointment);
+alias SupervisorHandle = ActorHandle!SetUpForDisapointment;
 
 enum supervisor_task_name = "supervisor";
-enum child_task_name = "child";
+enum child_task_name = "0";
 
 @safe @Scenario("Supervisor with failing child",
         [])
 class SupervisorWithFailingChild {
-    ActorHandle!SetUpForDissapointment supervisor_handle;
+    SupervisorHandle supervisorHandle;
+    ChildHandle childHandle;
 
     @Given("a actor #super")
-    Document aActorSuper() {
-        auto supervisor_factory = actor!SetUpForDissapointment;
-        supervisor_handle = supervisor_factory(supervisor_task_name);
+    Document aActorSuper() @trusted {
+        supervisorHandle = spawnActor!SetUpForDisapointment(supervisor_task_name);
+
+        Ctrl ctrl = receiveOnly!CtrlMsg.ctrl;
+        check(ctrl is Ctrl.STARTING, "Supervisor is not starting");
+
+        ctrl = receiveOnly!CtrlMsg.ctrl;
+        check(ctrl is Ctrl.ALIVE, "Supervisor is not alive");
 
         return result_ok;
     }
 
     @Given("a actor #child")
     Document aActorChild() {
-        return result_ok;
+        return Document();
     }
 
     @When("the actor #super start it should start the #child.")
     Document startTheChild() @trusted {
-        check(isRunning(supervisor_task_name), "Supervisor is not running");
-        supervisor_handle.isChildRunning(child_task_name);
-        check(concurrency.receiveOnly!bool, "child has started");
-        return result_ok;
+        return Document();
     }
 
     @When("the #child has started then the #child should fail with an exception")
     Document withAnException() {
-        alias DissapointmentFactory = ActorFactory!SetUpForFailure;
-        auto child_handler = DissapointmentFactory.handler(child_task_name);
-        check(child_handler !is child_handler.init, "The child handler failed to initialise");
-
-        child_handler.exceptionalMethod(42);
-
-        return result_ok;
+        return Document();
     }
 
     @Then("the #super actor should catch the #child which failed")
     Document childWhichFailed() @trusted {
-        concurrency.receiveTimeout(
-                1.seconds,
-                (immutable(Exception) e) { writefln("%s", e); });
-        return result_ok;
+        return Document();
     }
 
     @Then("the #super actor should restart the child")
     Document restartTheChild() @trusted {
-        supervisor_handle.isChildRunning(
-                child_task_name);
-        check(concurrency.receiveOnly!bool, "child is running again");
-        return result_ok;
+        return Document();
     }
 
     @Then("the #super should stop")
-    Document superShouldStop() {
-        supervisor_handle.stop;
-        check(!isRunning(supervisor_task_name), "Supervisor is still running");
+    Document superShouldStop() @trusted {
+        supervisorHandle.send(Sig.STOP);
+        auto ctrl = receiveOnly!CtrlMsg;
+        check(ctrl.ctrl is Ctrl.END, "Supervisor did not stop");
+
         return result_ok;
     }
 }
