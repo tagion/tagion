2c2d43bf15552d645c5cac4202646b5f9f7c7292
Philip Rying <philiprying@gmail.com>
2023-04-18 13:48:57 +0200

    WIP: unittest rim 2/3/4 passing

--- src/lib-dart/tagion/dart/DARTFile.d
+++ src/lib-dart/tagion/dart/DARTFile.d
@@ -1822,9 +1822,7 @@ unittest {
     }
 
     { // Rim 2 & 3
-        writeln("rim 2/3 test");
         DARTFile.create(filename);
-        writefln("dartfilename=%s", filename);
         auto dart = new DARTFile(net, filename);
         RecordFactoryT!true.Recorder recorder;
 
@@ -1833,9 +1831,11 @@ unittest {
     }
 
     { // Rim 2 & 3 & 4
+        writeln("rim 2/3/4 test");
         DARTFile.create(filename);
+        writefln("dartfilename=%s", filename);
         auto dart = new DARTFile(net, filename);
-        RecordFactory.Recorder recorder;
+        RecordFactoryT!true.Recorder recorder;
 
         assert(DARTFile.validate(dart, table[0 .. 9] ~ table[17 .. $], recorder));
         // dart.dump;
