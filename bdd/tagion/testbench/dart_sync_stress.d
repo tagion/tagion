ef58149561bae47f6224dfed199a03f3543f8861
Philip Rying <philiprying@gmail.com>
2023-04-20 15:26:51 +0200

    WIP: update to use real hashes

--- bdd/tagion/testbench/dart_sync_stress.d
+++ bdd/tagion/testbench/dart_sync_stress.d
@@ -1,6 +1,5 @@
 module tagion.testbench.dart_sync_stress;
 
-
 import tagion.behaviour.Behaviour;
 import tagion.testbench.functional;
 import tagion.hibon.HiBONRecord : fwrite;
@@ -10,7 +9,7 @@ import std.traits : moduleName;
 import tagion.testbench.dart;
 import tagion.testbench.tools.BDDOptions;
 import tagion.testbench.tools.Environment;
-    
+
 import tagion.dart.DARTFakeNet : DARTFakeNet;
 import tagion.crypto.SecureInterfaceNet : SecureNet, HashNet;
 import tagion.crypto.SecureNet : StdSecureNet;
@@ -27,9 +26,7 @@ import tagion.testbench.dart.dartinfo;
 
 import tagion.basic.Version;
 
-
 mixin Main!(_main);
-
 
 int _main(string[] args) {
 
@@ -42,33 +39,28 @@ int _main(string[] args) {
         const string dartfilename = buildPath(module_path, "dart_sync_stress_test".setExtension(FileExtension.dart));
         const string dartfilename2 = buildPath(module_path, "dart_sync_start_slave".setExtension(FileExtension.dart));
 
-        
         SecureNet net;
         bool real_hashes;
 
-        if (real_hashes) {
+        if (!real_hashes) {
             net = new DARTFakeNet("very secret");
-        } else {
+        }
+        else {
             net = new StdSecureNet();
             net.generateKeyPair("very secret");
         }
-        
+
         const hirpc = HiRPC(net);
 
         DartInfo dart_info = DartInfo(dartfilename, module_path, net, hirpc, dartfilename2);
-       
 
         auto dart_sync_stress_feature = automation!(dart_sync_stress)();
         dart_sync_stress_feature.AddRemoveAndReadTheResult(dart_info, env.getSeed, 100_000, 1000, 1000);
 
         auto dart_sync_context = dart_sync_stress_feature.run();
 
-    } 
-
- 
-
+    }
 
     return 0;
-
 
 }
