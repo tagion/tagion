4dbbf5a5f34d4d9907fbffb3f08b3c47c125ab5d
Lucas Rasmussen <lr@decard.io>
2023-04-24 15:02:17 +0200

    revert tagion actor except

--- src/lib-actor/tagion/actor/exceptions.d
+++ src/lib-actor/tagion/actor/exceptions.d
@@ -1,6 +1,51 @@
 module tagion.actor.exceptions;
 
-import tagion.basic.tagionexceptions : TagionException, TaskFailure, Check;
+import tagion.basic.tagionexceptions : TagionException;
+import std.exception;
+
+struct TaskFailure {
+    immutable(Throwable) throwable;
+    string task_name;
+}
+
+/++
+ This function set the taskname set by the logger
+ The version LOGGER must be enabled for this to work
+ The function is used to send the exception to the task owner ownerTid
+ Returns:
+ The immutable version of the Exception
+ +/
+@trusted
+static immutable(TaskFailure) taskException(const(Throwable) e) @nogc nothrow { //if (is(T:Throwable) && !is(T:TagionExceptionInterface)) {
+    import tagion.logger.Logger;
+
+    return immutable(TaskFailure)(cast(immutable) e, log.task_name);
+}
+
+@safe
+static void fatal(const(Throwable) e) nothrow {
+    import tagion.logger.Logger;
+
+    immutable task_e = taskException(e);
+    log(task_e);
+    try {
+        task_e.taskfailure;
+    }
+    catch (Exception t) {
+        log.fatal(t.msg);
+    }
+}
+
+@trusted
+static void taskfailure(immutable(TaskFailure) t) nothrow {
+    import std.concurrency;
+
+    assumeWontThrow({
+        if (ownerTid != Tid.init) {
+            ownerTid.send(t);
+        }
+    });
+}
 
 /**
  Exception type used by tagion.actor.actor
