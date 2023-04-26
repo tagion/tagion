10ff363cc0eb5761fd131602fe886ce22c5e0887
Philip Rying <philiprying@gmail.com>
2023-04-26 13:32:00 +0200

    return fingerprint

--- integration/vibed-rest-api/source/routes/project/controller.d
+++ integration/vibed-rest-api/source/routes/project/controller.d
@@ -22,43 +22,37 @@ import services.dartService;
 import routes.project.model;
 
 import tagion.hibon.HiBONJSON : toPretty;
+import tagion.utils.Miscellaneous : toHexString;
 
 public Json[] projectList;
 public string filePath = "./source/routes/project/data.json";
 
-struct Controller
-{
+struct Controller {
     DartService dart_service;
     this(const(string) dart_filename, const(string) password) {
-      dart_service = DartService(dart_filename, password);
+        dart_service = DartService(dart_filename, password);
     }
 
-    void getAllProjects(HTTPServerRequest req, HTTPServerResponse res)
-    {
+    void getAllProjects(HTTPServerRequest req, HTTPServerResponse res) {
         getAll(req, res, projectList, filePath);
     }
 
-    void getOneProject(HTTPServerRequest req, HTTPServerResponse res)
-    {
+    void getOneProject(HTTPServerRequest req, HTTPServerResponse res) {
         getOne(req, res, projectList, filePath, "projectUUID");
     }
 
-    void postProject(HTTPServerRequest req, HTTPServerResponse res)
-    {
-        struct PostResponse
-        {
+    void postProject(HTTPServerRequest req, HTTPServerResponse res) {
+        struct PostResponse {
             string id;
         }
 
         Project project_data;
 
         // check that user submits correct body
-        try
-        {
+        try {
             project_data = deserializeJson!Project(req.json);
         }
-        catch (JSONException e)
-        {
+        catch (JSONException e) {
             res.statusCode = HTTPStatus.badRequest;
             res.writeBody(format("Request body does not match JSON struct error, %s", e.msg));
             return;
@@ -66,37 +60,34 @@ struct Controller
 
         writefln("document received: %s", project_data.toDoc.toPretty);
 
-        projectList = readFromFile(filePath);
+        // projectList = readFromFile(filePath);
+        // // throw error if object with provided ID already exists
+        // if (projectList.length > 0)
+        // {
+        //     foreach (key, value; projectList)
+        //     {
+        //         if (value["projectUUID"] == req.json["projectUUID"])
+        //         {
+        //             res.writeBody("Object with this ID exists");
+        //             return;
+        //         }
+        //     }
+        // }
+        // projectList ~= req.json;
+        // writeToFile(projectList, filePath);
 
-        // throw error if object with provided ID already exists
-        if (projectList.length > 0)
-        {
-            foreach (key, value; projectList)
-            {
-                if (value["projectUUID"] == req.json["projectUUID"])
-                {
-                    res.writeBody("Object with this ID exists");
-                    return;
-                }
-            }
-        }
-
-        projectList ~= req.json;
-
-        writeToFile(projectList, filePath);
         const fingerprint = dart_service.dartModify(project_data.toDoc);
 
         // TODO: return GUID representing data entry in DART db
         // return fingeprint of the archive
         PostResponse postResponse;
-        postResponse.id = "d76sc703-9334-46d1-93c9-5675656c0000";
+        postResponse.id = fingerprint.toHexString;
 
         res.statusCode = HTTPStatus.created;
         res.writeJsonBody(postResponse);
     }
 
-    void deleteOneProject(HTTPServerRequest req, HTTPServerResponse res)
-    {
+    void deleteOneProject(HTTPServerRequest req, HTTPServerResponse res) {
         deleteOne(req, res, projectList, filePath, "projectUUID");
     }
 }
