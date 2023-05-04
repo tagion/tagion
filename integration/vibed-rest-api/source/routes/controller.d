cfc2c9a64be555b2eb18c224e22f41dfce0317f0
Philip Rying <philiprying@gmail.com>
2023-05-04 12:32:30 +0200

    decode check

--- integration/vibed-rest-api/source/routes/controller.d
+++ integration/vibed-rest-api/source/routes/controller.d
@@ -35,7 +35,7 @@ struct ResponseModel {
 }
 
 enum ErrorCode {
-    dataIdWrongLength = 11,
+    dataIdnotValid = 11,
     dataNotFound = 12,
     dataNotCorrectType = 13,
     dataBodyNoMatch = 21,
@@ -44,7 +44,7 @@ enum ErrorCode {
 }
 
 enum ErrorDescription {
-    dataIdWrongLength = "Provided fingerprint is not valid",
+    dataIdnotValid = "Provided fingerprint is not valid",
     dataNotFound = "Archive with fingerprint not found in database",
     dataNotCorrectType = "Wrong document type",
     dataBodyNoMatch = "Request body does not match",
@@ -95,7 +95,8 @@ void handleServerError(HTTPServerResponse res, HTTPServerRequest req, Exception 
 
     const errorId = rnd.take(64).sum;
 
-    const err = ErrorResponse(HTTPStatus.internalServerError, "Internal Server Error, id: %s".format(errorId));
+    const err = ErrorResponse(HTTPStatus.internalServerError, "Internal Server Error, id: %s".format(
+            errorId));
     const errJson = serializeToJson(err);
 
     logError(format("%s", err));
@@ -158,13 +159,28 @@ struct Controller(T) {
         string id = req.params.get("entityId");
 
         // handle fingerprint exactly 64 characters
-        if (id.length != 64) {
-            const err = ErrorResponse(ErrorCode.dataIdWrongLength, ErrorDescription.dataIdWrongLength);
+        // if (id.length != 64) {
+        //     const err = ErrorResponse(ErrorCode.dataIdnotValid, ErrorDescription
+        //             .dataIdnotValid);
+        //     respondWithError(res, err);
+        //     return;
+        // }
+
+        DARTIndex fingerprint;
+        try {
+            if (id.length != 64) {
+                throw new Exception("Length is not correct");
+            }
+            fingerprint = DARTIndex(decode(id));
+        }
+        catch (Exception e) {
+            const err = ErrorResponse(ErrorCode.dataIdnotValid, ErrorDescription
+                    .dataIdnotValid);
             respondWithError(res, err);
             return;
         }
 
-        const fingerprint = DARTIndex(decode(id));
+        // const fingerprint = DARTIndex(decode(id));
         const doc = dart_service.read([fingerprint]);
         if (doc.empty) {
             const err = ErrorResponse(ErrorCode.dataNotFound, ErrorDescription.dataNotFound);
@@ -224,7 +240,8 @@ struct Controller(T) {
         const fingerprint = dart_service.modify(doc);
         const new_bullseye = dart_service.bullseye;
         if (new_bullseye == prev_bullseye) {
-            const err = ErrorResponse(ErrorCode.dataFingerprintNotAdded, ErrorDescription.dataFingerprintNotAdded);
+            const err = ErrorResponse(ErrorCode.dataFingerprintNotAdded, ErrorDescription
+                    .dataFingerprintNotAdded);
             writeln("ErrorDescription.dataFingerprintNotAdded");
             respondWithError(res, err);
             return;
@@ -257,7 +274,7 @@ struct Controller(T) {
 
     //     // handle fingerprint exactly 64 characters
     //     if (id.length != 64) {
-    //         const err = ErrorResponse(ErrorCode.dataIdWrongLength, ErrorDescription.dataIdWrongLength);
+    //         const err = ErrorResponse(ErrorCode.dataIdnotValid, ErrorDescription.dataIdnotValid);
     //         respondWithError(res, err);
     //         return;
     //     }
