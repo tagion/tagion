9f5d2ef09ea4e5030e8e8901fa0effcf4cf361a6
Aleksandr Bohdanov <aleksandr.bohdanov@gmail.com>
2023-05-08 16:01:28 +0300

    Add log import

--- integration/vibed-rest-api/source/helpers.d
+++ integration/vibed-rest-api/source/helpers.d
@@ -14,6 +14,7 @@ import std.range : take;
 import vibe.data.json;
 import vibe.http.server;
 import vibe.http.router;
+import vibe.core.log;
 
 import source.models.other : ResponseModel, ErrorResponse;
 
