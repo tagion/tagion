4e5b432300e430315d0ea74b91f58dcb8ada432a
Lucas Rasmussen <lr@decard.io>
2023-05-10 13:34:52 +0200

    add sum ios config

--- platform.aarch64-ios.mk
+++ platform.aarch64-ios.mk
@@ -3,39 +3,19 @@ IOS_AARCH64=aarch64-ios
 PLATFORMS+=$(IOS_AARCH64)
 
 ifeq ($(PLATFORM),$(IOS_AARCH64))
-ifndef CC_CROSS
-${error CC_CROSS not defined}
-endif
-ifndef CROSS_SYSROOT
-${error CROSS_SYSROOT not defined}
-endif
-ifndef CROSS_ARCH
-${error CROSS_ARCH not defined}
-endif
+
 CONFIGUREFLAGS_SECP256K1 += CC=$(CC_CROSS)
 CONFIGUREFLAGS_SECP256K1 += CFLAGS="-arch $(CROSS_ARCH) -fpic -g -Os -pipe -isysroot $(CROSS_SYSROOT) -mios-version-min=12.0"
 
 SHARED?=1
-#SPLIT_LINK?=1
+DLLEXT:=dylib
 IOS_ARCH=$(IOS_AARCH64)
-TRIPLET = $(ANDROID_ARCH)
-#include $(DROOT)/config.android.mk
+TRIPLET = $(IOS_ARCH)
+CROSS_OS=ios
+DFLAGS+=-mtriple=$(TRIPLET)
 DINC+=${shell find $(DSRC) -maxdepth 1 -type d -path "*src/lib-*" }
-DFILES?=${shell find $(DSRC) -type f -name "*.d" -path "*src/lib-betterc*" -not -path "*/tests/*"}
 
-CROSS_LIB=$(CROSS_SYSROOT)/usr/lib/$(ANDROID_ARCH)/$(ANDROID_NDK)
-OBJS+=$(CROSS_LIB)/crtbegin_so.o
-
-${call DDEPS,$(DBUILD),$(DFILES)}
-
-CROSS_LDFLAGS+=--fix-cortex-a53-843419
+env-ios:
+	echo THIS IS IOS
 
 endif
-#DCCROSS_FLAGS+=-mtriple=$(TRIPLET)
-
-ifdef SHARED
-DCCROSS_FLAGS+=-shared
-DCCROSS_FLAGS+=--relocation-model=pic
-endif
-
-#-link-defaultlib-shared=false"
