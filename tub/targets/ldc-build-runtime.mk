dd59218688e9996dff19273b901e03ad7b1a4af1
Lucas Rasmussen <lr@decard.io>
2023-05-16 16:58:14 +0200

    mk: ldc build runtime more generic and ad target for ios

--- tub/targets/ldc-build-runtime.mk
+++ tub/targets/ldc-build-runtime.mk
@@ -8,24 +8,23 @@ LDC_TAR_NAME=$(LDC_NAME).tar.xz
 LDC_URL=https://github.com/ldc-developers/ldc/releases/download/v1.29.0-beta1/$(LDC_TAR_NAME)
 LDC_TAR=$(TOOLS)/$(LDC_TAR_NAME)
 TOOLS_LDC_BIN=$(TOOLS)/$(LDC_NAME)/bin
-LDC_BUILD_RUNTIME:=$(TOOLS_LDC_BIN)/ldc-build-runtime
-LDC_BUILD_RUNTIME_TMP:=$(DBUILD)/ldc-build-runtime.tmp
+# LDC_BUILD_RUNTIME:=$(TOOLS_LDC_BIN)/ldc-build-runtime
+LDC_BUILD_RUNTIME:=$(shell which ldc-build-runtime)
+LDC_BUILD_RUNTIME_TMP:=$(DBUILD)/tmp/druntime/
 #https://github.com/ldc-developers/ldc/releases/download/v1.29.0-beta1/ldc2-1.29.0-beta1-linux-x86_64.tar.xz
 
-$(LDC_TAR): $(TOOLS)/.way
-	$(PRECMD)
-	echo $(LDC_TAR)
-	$(CD) $(TOOLS); wget $(LDC_URL)
-	$(TOUCH) $@
+# $(LDC_TAR): $(TOOLS)/.way
+# 	$(PRECMD)
+# 	echo $(LDC_TAR)
+# 	$(CD) $(TOOLS); wget $(LDC_URL)
+# 	$(TOUCH) $@
 
-$(TOOLS_LDC_BIN): $(LDC_TAR)
-	$(PRECMD)
-	$(CD) $(TOOLS); tar -xJvf $<
-	$(TOUCH) $@
+# $(TOOLS_LDC_BIN): $(LDC_TAR)
+# 	$(PRECMD)
+# 	$(CD) $(TOOLS); tar -xJvf $<
+# 	$(TOUCH) $@
 
-druntime: $(LDC_BUILD_RUNTIME_TMP)
-
-$(LDC_BUILD_RUNTIME_TMP): $(TOOLS_LDC_BIN)
+$(BUILD)/$(ARCH)-linux-android/tmp/druntime/: $(LDC_BUILD_RUNTIME)
 	$(LDC_BUILD_RUNTIME) \
 	--buildDir=$(LDC_BUILD_RUNTIME_TMP) \
 	--dFlags="-mtriple=$(TRIPLET) -flto=thin" \
@@ -36,6 +35,17 @@ $(LDC_BUILD_RUNTIME_TMP): $(TOOLS_LDC_BIN)
 	ANDROID_PLATFORM=android-$(ANDROID_API) \
 	MAKE_SYSTEM_VERSION=$(ANDROID_API) \
 	BUILD_LTO_LIBS=ON
+
+# $(REPOROOT)/build/$(ARCH)-ios/tmp/druntime/: $(LDC_BUILD_RUNTIME)
+# /home/lucas/wrk/tagion/build/aarch64-ios/tmp/druntime/
+$(BUILD)/$(ARCH)-ios/tmp/druntime/: $(LDC_BUILD_RUNTIME)
+	$(LDC_BUILD_RUNTIME) \
+	--buildDir=$(LDC_BUILD_RUNTIME_TMP) \
+	--dFlags="-mtriple=$(TRIPLET) -flto=thin" \
+	--targetSystem="Android;Linux;UNIX" \
+	BUILD_LTO_LIBS=ON
+
+druntime: $(LDC_BUILD_RUNTIME_TMP)
 
 druntime-bin: $(TOOLS_LDC_BIN)
 
@@ -65,5 +75,11 @@ help-druntime:
 
 help: help-druntime
 
-
 .PHONY: env-druntime help-druntime
+
+proper-druntime:
+	$(PRECMD)
+	$(RM) -r $(LDC_BUILD_RUNTIME_TMP)
+
+.PHONY: proper-druntime
+proper: proper-druntime
