374189944a65b25356c3776fae989d3c7a5cd57d
Philip Rying <philiprying@gmail.com>
2023-05-26 10:14:34 +0200

    signs now creates dummy contract

--- src/bin-signs/tagion/tools/signs.d
+++ src/bin-signs/tagion/tools/signs.d
@@ -23,6 +23,56 @@ import tagion.hibon.Document;
 import tagion.crypto.SecureInterfaceNet : SecureNet;
 import tagion.crypto.SecureNet : StdSecureNet;
 
+
+@recordType("DeliveryOrder")
+struct DeliveryOrder {
+    string vaccineType; // Vaccine Type - "Measels"
+    string packageID; // Package id - "1234ABC"
+    int numberOfVaccines; // Number of vaccines - 20
+    string destination; // Final destination - "Livingstone"
+    string pickuppoint; // Pickup point location - "copenhagen"
+    // TagionCurrency payment; // Payment - "20usd"
+
+    sdt_t startTime; // standard time
+    sdt_t endTime; // end time - should be delivered before this point    
+     
+    @label(OwnerKey) Pubkey originalOwner; // the owner of the delivery order
+    Pubkey receiver; // The receiver of the vaccines
+    mixin HiBONRecord!(q{
+        this(string vaccineType, 
+            string packageID, 
+            int numberOfVaccines, 
+            string destination, 
+            string pickuppoint,
+            sdt_t startTime,
+            sdt_t endTime,
+            Pubkey originalOwner,
+            Pubkey receiver
+        ) {
+            this.vaccineType = vaccineType;
+            this.packageID = packageID;
+            this.numberOfVaccines = numberOfVaccines;
+            this.destination = destination;
+            this.pickuppoint = pickuppoint;
+            this.startTime = startTime;
+            this.endTime = endTime;
+            this.originalOwner = originalOwner;
+            this.receiver = receiver;
+        }
+    });
+}
+
+@recordType("SignedDeliveryEvent")
+struct SignedDeliveryEvent {
+    Signature deliveryOrderChain; // signature ex. from unicef
+    Buffer deliveryEvent;
+    sdt_t timeStamp;
+    @label(OwnerKey) Pubkey tokenOwner; // owner of the vaccines
+    
+    mixin HiBONRecord;
+}
+
+
 mixin Main!_main;
 int _main(string[] args) {
     immutable program = args[0];
@@ -30,7 +80,9 @@ int _main(string[] args) {
     string password;
     bool standard_output;
     bool version_switch;
+    bool generate;
     string inputfilename;
+    string outputfilename = "delivery_order.hibon";
 
     GetoptResult main_args;
     try {
@@ -41,6 +93,8 @@ int _main(string[] args) {
                 "f|filename", "file to sign", &inputfilename, 
                 "p|password", "set the password for the signing", &password,
                 "c|stdout", "write the output to stdout", &standard_output,
+                "g|generate", "generates dummy delivery order", &generate,
+                "o|outputfilename", "filename to write to", &outputfilename,
         );
     }
     catch (std.getopt.GetOptException e) {
@@ -73,22 +127,45 @@ int _main(string[] args) {
         return 0;
     }
 
-    // if (args.length != 1) {
-    //     stderr.writefln("inputfilename not specified!");
-    //     return 0;
-    // }
-    SecureNet net = new StdSecureNet;
+   SecureNet net = new StdSecureNet;
     if (password.init) { 
         net.generateKeyPair("very secret"); 
     } else {
         net.generateKeyPair(password);
     }
 
-    if (inputfilename.fileExtension != FileExtension.hibon) {
+     // if (args.length != 1) {
+    //     stderr.writefln("inputfilename not specified!");
+    //     return 0;
+    // }
+    if (generate) {
+        writefln("generating dummy delivery order");
+        SecureNet receiver = new StdSecureNet;
+        receiver.generateKeyPair("receiver");
+        auto delivery_order = DeliveryOrder(
+            "Measels", 
+            "43a3efd", 
+            100, 
+            "Copenhagen", 
+            "Triesen Liechenstein", 
+            currentTime, 
+            currentTime, 
+            net.pubkey, 
+            receiver.pubkey,
+        );
+        if (standard_output) {
+            stdout.rawWrite(delivery_order.toDoc.serialize);
+            return 0;
+        }
+        outputfilename.setExtension(FileExtension.hibon).fwrite(delivery_order.toDoc.serialize);
+        return 0;       
+    
+    }
+     if (inputfilename.fileExtension != FileExtension.hibon) {
         stderr.writefln("Error: inputfilename not correct filetype. Must be %s", FileExtension.hibon);
         return 0;
     }
-    
+   
 
     immutable data = assumeUnique(cast(ubyte[]) fread(inputfilename));
     const doc = Document(data);
@@ -108,31 +185,4 @@ int _main(string[] args) {
 
 
 
-@recordType("DeliveryOrder")
-struct DeliveryOrder {
-    string vaccineType; // Vaccine Type - "Measels"
-    string packageID; // Package id - "1234ABC"
-    int numberOfVaccines; // Number of vaccines - 20
-    string destination; // Final destination - "Livingstone"
-    string pickuppoint; // Pickup point location - "copenhagen"
-    TagionCurrency payment; // Payment - "20usd"
-
-    sdt_t startTime; // standard time
-    sdt_t endTime; // end time - should be delivered before this point    
-    sdt_t timeStamp; // When the delivery order was created
-     
-    @label(OwnerKey) Pubkey originalOwner; // the owner of the delivery order
-    Pubkey receiver; // The receiver of the vaccines
-    mixin HiBONRecord;
-}
-
-@recordType("SignedDeliveryOrder")
-struct SignedDeliveryOrder {
-    Signature deliveryOrderChain; // signature ex. from unicef
-    Buffer deliveryOrder;
-    sdt_t timeStamp;
-    @label(OwnerKey) Pubkey tokenOwner; // owner of the vaccines
-    
-    mixin HiBONRecord;
-}
 
