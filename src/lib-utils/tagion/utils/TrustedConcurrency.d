73676de9421a57d516675ed78b8f27692d196b77
Lucas Rasmussen <lr@decard.io>
2023-05-30 15:29:35 +0200

    Checkout TrustedConcurrency from ib-trusted-concurrency

---
+++ src/lib-utils/tagion/utils/TrustedConcurrency.d
@@ -0,0 +1,58 @@
+/// \file TrustedConcurrency.d
+module tagion.utils.TrustedConcurrency;
+
+private import concurrency = std.concurrency;
+
+import core.time : Duration;
+
+/** @brief File contains functions of std.concurrency wrapped in @trusted
+ *         to use them in @safe code
+ */
+
+alias Tid = concurrency.Tid;
+alias thisTid = concurrency.thisTid;
+
+void sendTrusted(Args...)(Tid tid, Args args) @trusted
+{
+    concurrency.send(tid, args);
+}
+
+void prioritySendTrusted(Args...)(Tid tid, Args args) @trusted
+{
+    concurrency.prioritySend(tid, args);
+}
+
+void receiveTrusted(Args...)(Args args) @trusted
+{
+    concurrency.receive(args);
+}
+
+auto receiveOnlyTrusted(T...)() @trusted
+{
+    return concurrency.receiveOnly!T;
+}
+
+bool receiveTimeoutTrusted(T...)(Duration duration, T ops) @trusted
+{
+    return concurrency.receiveTimeout!T(duration, ops);
+}
+
+Tid ownerTidTrusted() @trusted
+{
+    return concurrency.ownerTid;
+}
+
+Tid spawnTrusted(F, Args...)(F fn, Args args) @trusted
+{
+    return concurrency.spawn(fn, args);
+}
+
+Tid locateTrusted(string name) @trusted
+{
+    return concurrency.locate(name);
+}
+
+bool registerTrusted(string name, Tid tid) @trusted
+{
+    return concurrency.register(name, tid);
+}
