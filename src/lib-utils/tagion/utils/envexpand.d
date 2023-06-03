5495c834660f64adf926010bd4b7ccecc790f12a
cbleser <cr@tagion.org>
2023-06-03 15:58:38 +0300

    DONE: envexpand

--- src/lib-utils/tagion/utils/envexpand.d
+++ src/lib-utils/tagion/utils/envexpand.d
@@ -4,9 +4,11 @@ import std.typecons;
 import std.algorithm;
 import std.range;
 
+enum ignore_env_start = "!";
 enum bracket_pairs = [
         ["$(", ")"],
         ["${", "}"],
+        ["$$", "$$", ignore_env_start], // "!" ignored as environment start
         ["$", ""],
 
     ];
@@ -27,14 +29,16 @@ string envExpand(string text, string[string] env, void delegate(string msg) erro
     }
 
     string innerExpand(string str) {
-        string result = str;
-
         auto begin = bracket_pairs
             .map!(bracket => BracketState(bracket, str.countUntil(bracket[0])))
             .filter!(state => state.index >= 0)
             .take(1);
         if (!begin.empty) {
             const state = begin.front;
+            if (state.bracket.length > 2) {
+                const next_index = state.index ~ state.bracket[1].length;
+                return str[0 .. state.index] ~ state.bracket[2] ~ innerExpand(str[next_index .. $]);
+            }
             const env_start_index = state.index + state.bracket[0].length;
             auto end_str = innerExpand(str[env_start_index .. $]);
             const env_end_index = envName(end_str, state.bracket[1]);
@@ -44,11 +48,11 @@ string envExpand(string text, string[string] env, void delegate(string msg) erro
                 end_str = end_str[env_end_index + state.bracket[1].length .. $];
                 env_value = env.get(env_name, null);
             }
-            result = str[0 .. state.index] ~
+            return str[0 .. state.index] ~
                 env_value ~
                 innerExpand(end_str);
         }
-        return result;
+        return str;
 
     }
 
