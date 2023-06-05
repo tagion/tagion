81a0391c01ac18b91fbf44c9f9630b96de307e55
Lucas Rasmussen <lr@decard.io>
2023-06-05 19:41:26 +0200

    add deps for libtagion

--- config.libs.mk
+++ config.libs.mk
@@ -1,6 +1,8 @@
-libtagion: $(DLIB)/libtagion.$(LIBEXT)
-libtagion: LIBS+=$(SSLIMPLEMENTATION) $(LIBSECP256K1) $(LIBP2PGOWRAPPER)
+LIBTAGION:=$(DLIB)/libtagion.$(LIBEXT)
+
 libtagion: DFILES:=${shell find $(DSRC) -name "*.d" -a -path "*/src/lib-*" -a -not -path "*/unitdata/*" -a -not -path "*/tests/*" -a -not -path "*/lib-betterc/*" $(NO_WOLFSSL) }
+libtagion: $(LIBTAGION) $(DFILES)
+libtagion: LIBS+=$(SSLIMPLEMENTATION) $(LIBSECP256K1) $(LIBP2PGOWRAPPER)
 
 libmobile: $(DLIB)/libmobile.$(LIBEXT)
 libmobile: LIBS+=$(LIBSECP256K1)
