fd231e1baa53321e99cd45c113c5d299bb8b54e8
Lucas Rasmussen <lr@decard.io>
2023-06-13 15:49:48 +0200

    fix import

--- src/bin-contracter/tagion/tools/contracter.d
+++ src/bin-contracter/tagion/tools/contracter.d
@@ -4,13 +4,14 @@ module tagion.tools.contracter;
 import std.socket;
 import tagion.dart.DARTcrud;
 import tagion.communication.HiRPC;
-import tagion.tools.neuewelle : contract_socket_path;
+import tagion.tools.Basic;
+import tagion.tools.neuewelle : contract_sock_path;
 import tagion.hibon.HiBON;
 import tagion.hibon.HiBONBase;
 
 mixin Main!(_main);
 
-int _main(void) {
+int _main() {
     HiRPC hirpc;
     HiBON hibon = new HiBON();
 
@@ -18,10 +19,12 @@ int _main(void) {
 
     const sender = hirpc.action("action", hibon);
 
-    Address addr = UnixAddress(contract_socket_path);
+    Address addr = new UnixAddress(contract_sock_path);
     Socket sock = new Socket(AddressFamily.UNIX, SocketType.STREAM);
 
     sock.connect(addr);
 
     sock.send(sender.toDoc.serialize);
+
+    return 0;
 }
