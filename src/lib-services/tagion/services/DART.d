960ac3599fb3673088de26098376dbd81ba4d94b
Lucas Rasmussen <lr@decard.io>
2023-06-19 15:22:16 +0200

    add receiver and dartRead

--- src/lib-services/tagion/services/DART.d
+++ src/lib-services/tagion/services/DART.d
@@ -1,30 +1,45 @@
+/// Tagion DART actor service
 module tagion.services.DART;
 
 import std.path : isValidPath;
 import std.format : format;
 import std.file;
-
-import tagion.actor;
+import std.algorithm : map;
+import std.array;
 import std.stdio;
+
+import tagion.utils.pretend_safe_concurrency;
+import tagion.actor;
 import tagion.crypto.Types;
 import tagion.crypto.SecureInterfaceNet;
 import tagion.dart.DART;
 import tagion.dart.Recorder;
 import tagion.dart.DARTBasic : DARTIndex;
+import tagion.hibon.Document;
+
+/// Response from a dart CRUD call
+alias dartResp = Msg!"dartResp";
 
 struct DARTService {
     static DART db;
-    static void dartRead(Msg!"dartRead", Fingerprint fingerprint) {
+
+    // Rssponds immutable Document[]
+    static void dartRead(Msg!"dartRead", Tid to, const(DARTIndex)[] fingerprints) {
+        auto read_recorder = db.loads(fingerprints);
+        const(Document)[] docs = read_recorder[].map!(a => a.filed).array;
+
+        send(to, dartResp(), cast(immutable) docs);
     }
 
-    static void dartRim(Msg!"dartRim", DART.Rims rims) {
+    static void dartRim(Msg!"dartRim", Tid to, DART.Rims rims) {
     }
 
-    static void dartModify(Msg!"dartModify", RecordFactory.Recorder recorder) {
+    static void dartModify(Msg!"dartModify", Tid to, RecordFactory.Recorder recorder) {
     }
 
-    static void dartBullseye(Msg!"dartBullseye") {
-        sendOwner(DARTIndex(db.bullseye));
+    // Responds DARTIndex
+    static void dartBullseye(Msg!"dartBullseye", Tid to) {
+        send(to, dartResp(), DARTIndex(db.bullseye));
     }
 
     static void task(string task_name, string dart_path, immutable SecureNet net) nothrow {
