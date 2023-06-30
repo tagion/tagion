1d6b8b57130c959068afcd909fb6c5014e822a49
cbleser <cr@tagion.org>
2023-06-30 09:24:51 +0300

    DONE: BitMask.count change to use popcnt

--- src/lib-utils/tagion/utils/BitMask.d
+++ src/lib-utils/tagion/utils/BitMask.d
@@ -382,26 +382,23 @@ struct BitMask {
     }
 
     size_t count() const pure nothrow @nogc {
-        static size_t local_count(size_t BIT_SIZE)(const size_t x) pure nothrow {
-            static if (BIT_SIZE is 1) {
-                return x & 1;
-            }
-            else {
-                if (x is 0) {
-                    return 0;
-                }
-                enum HALF_SIZE = BIT_SIZE >> 1;
-                enum MASK = (size_t(1) << HALF_SIZE) - 1;
-                return local_count!HALF_SIZE(x & MASK) + local_count!HALF_SIZE(
-                        (x >> HALF_SIZE) & MASK);
-            }
-        }
+        import core.bitop : popcnt;
 
-        size_t result;
-        foreach (m; mask) {
-            result += local_count!(WORD_SIZE)(m);
-        }
-        return result;
+        return mask.map!(m => m.popcnt).sum;
+    }
+
+    unittest {
+        BitMask bits;
+        assert(bits.count == 0);
+        bits[17] = true;
+        assert(bits.count == 1);
+        bits = ~bits;
+        assert(bits.count == WORD_SIZE - 1);
+        bits[WORD_SIZE * 3 / 2] = true;
+        assert(bits.count == WORD_SIZE);
+        bits[WORD_SIZE / 2] = false;
+        bits = ~bits;
+        assert(bits.count == WORD_SIZE + 1);
     }
 
     void toString(scope void delegate(scope const(char)[]) @safe sink,
@@ -557,7 +554,6 @@ struct BitMask {
 
     }
 
-    @trusted
     unittest {
         import std.algorithm : equal;
         import std.algorithm.sorting : merge, sort;
@@ -718,7 +714,7 @@ struct BitMask {
             { // Or
                 auto y = a.dup;
                 y |= b;
-                auto y_list = (a_list ~ b_list).dup.sort.uniq; //uniq;//.dup.sort;
+                auto y_list = (a_list ~ b_list).dup.sort.uniq;
                 assert(equal(y[], y_list));
                 assert(y.count is 10);
             }
