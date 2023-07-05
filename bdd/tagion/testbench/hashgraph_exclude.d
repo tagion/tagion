ea8f18b28dd0e262a464ed9b2ee07b09643dc8b1
Philip Rying <philiprying@gmail.com>
2023-07-05 15:56:39 +0200

    WIP: skeleton for exclude

---
+++ bdd/tagion/testbench/hashgraph_exclude.d
@@ -0,0 +1,33 @@
+module tagion.testbench.hashgraph_exclude;
+
+import tagion.behaviour.Behaviour;
+
+import tagion.behaviour.Behaviour;
+import tagion.testbench.functional;
+import tagion.hibon.HiBONRecord : fwrite;
+import tagion.tools.Basic;
+import tagion.testbench.hashgraph;
+import tagion.testbench.tools.Environment;
+import std.stdio;
+import std.path : buildPath;
+import std.file : mkdirRecurse;
+
+
+import tagion.testbench.hashgraph.hashgraph_test_network;
+
+
+mixin Main!(_main);
+
+int _main(string[] args) {
+
+    const module_path = env.bdd_log.buildPath(__MODULE__);
+    mkdirRecurse(module_path);
+    writeln(args);
+    auto node_names = args[1..$];
+
+    TestNetwork network = new TestNetwork(node_names);
+
+    auto hashgraph_exclude_feature = automation!(exclude_node);
+    hashgraph_exclude_feature.run;
+    return 0;
+}
