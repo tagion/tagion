e820b69b1b743cd1c62e1acee4793830ebbe20fb
cbleser <cr@tagion.org>
2023-07-07 10:16:51 +0300

    WIP: compiling secp256k1_sha256_wrapper

--- src/lib-crypto/context.mk
+++ src/lib-crypto/context.mk
@@ -7,3 +7,20 @@ DCFLAGS+=$(DVERSION)=TINY_AES
 endif
 
 DFILES_NATIVESECP256K1=${shell find $(DSRC)/lib-crypto -name "*.d"}
+
+SEC256k1_WRAPPER_SRC=$(DSRC)/lib-crypto/tagion/crypto/secp256k1/secp256k1_sha256_wrapper.c
+SEC256k1_WRAPPER_OBJ=$(DTMP)/secp256k1_sha256_wrapper.$(OBJEXT)
+
+$(SEC256k1_WRAPPER_OBJ): CFLAGS+=-I $(DSRC)/wrap-secp256k1/secp256k1/src/
+$(SEC256k1_WRAPPER_OBJ): CFLAGS+=-I $(DSRC)/wrap-secp256k1/secp256k1/include/
+
+secp256k1-wrapper: $(SEC256k1_WRAPPER_OBJ)
+
+test88:
+	echo $(SEC256k1_WRAPPER_OBJ)
+
+$(SEC256k1_WRAPPER_OBJ): $(SEC256k1_WRAPPER_SRC)
+	$(CC) $(CFLAGS) -c $< -o $@
+
+
+#src/lib-crypto/tagion/crypto/secp256k1/secp256k1_sha256_wrapper.c
--- src/lib-crypto/tagion/crypto/secp256k1/secp256k1_sha256_wrapper.c
+++ src/lib-crypto/tagion/crypto/secp256k1/secp256k1_sha256_wrapper.c
@@ -1,13 +1,15 @@
+//#define SECP256K1_INLINE /* empty */
+#include <secp256k1.h>
 #include "hash_impl.h"
 
 void secp256k1_sha256_initialize_w(secp256k1_sha256 *hash) {
-    secp256k1_sha256_initialize(&hash);
+    secp256k1_sha256_initialize(hash);
 }
 
 void secp256k1_sha256_write_w(secp256k1_sha256 *hash, const unsigned char *data, size_t size) {
-    secp256k1_sha256_write(&hash, data, size);
+    secp256k1_sha256_write(hash, data, size);
 }
 
 void secp256k1_sha256_finalize_w(secp256k1_sha256 *hash, unsigned char *out32) {
-    secp256k1_sha256_finalize(&hash, out32);
+    secp256k1_sha256_finalize(hash, out32);
 }
