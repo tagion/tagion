26d6ecb5c8e4c36b1862291dcb7107471ef299a7
Lucas Rasmussen <lr@decard.io>
2023-08-07 08:31:31 +0200

    update contract verifier and related tests

---
+++ bdd/tagion/testbench/services/contract.d
@@ -0,0 +1,209 @@
+/// Test for [tagion.services.contract]_
+module tagion.testbench.services.contract;
+// Default import list for bdd
+import tagion.behaviour;
+import tagion.hibon.Document;
+import std.typecons : Tuple;
+import tagion.testbench.tools.Environment;
+
+import core.time;
+import tagion.hibon.HiBON;
+import tagion.communication.HiRPC;
+import tagion.utils.pretend_safe_concurrency;
+import tagion.crypto.SecureNet;
+import tagion.actor;
+import tagion.actor.exceptions;
+import tagion.services.contract;
+
+import std.stdio;
+
+enum feature = Feature(
+            "ContractInterface service.",
+            [
+        "The transaction service should be able to receive HiRPC, validate data format and protocol rules before it sends to and send to the Collector services.",
+        "The HiRPC is package into a HiBON-Document in the following called doc."
+]);
+
+alias FeatureContext = Tuple!(
+        TheDocumentIsNotAHiRPC, "TheDocumentIsNotAHiRPC",
+        CorrectHiRPCFormatAndPermission, "CorrectHiRPCFormatAndPermission",
+        CorrectHiRPCWithPermissionDenied, "CorrectHiRPCWithPermissionDenied",
+        FeatureGroup*, "result"
+);
+
+@safe @Scenario("The Document is not a HiRPC",
+        [])
+class TheDocumentIsNotAHiRPC {
+    ContractServiceHandle contract_handle;
+    string contract_success;
+    string contract_reject;
+    this(ContractServiceHandle _contract_handle, string _success, string _reject) {
+        contract_handle = _contract_handle;
+        contract_success = _success; // The name of the service which successfull documents are sent to
+        contract_reject = _reject; // The name of the service which rejected documents are sent to
+    }
+
+    Document doc;
+
+    @Given("a doc with a correct document format but which is incorrect HiRPC format.")
+    Document format() {
+        writeln(thisTid);
+        check(waitforChildren(Ctrl.ALIVE), "ContractService never alived");
+
+        auto hibon = new HiBON();
+        hibon["$test"] = 5;
+        doc = Document(hibon);
+        contract_handle.send(inputDoc(), doc);
+
+        return result_ok;
+    }
+
+    @When("the doc should be received by the this services.")
+    Document services() {
+        return result_ok;
+    }
+
+    @Then("the doc should be checked that it is a correct HiRPC and if it is not it should be rejected.")
+    Document rejected() {
+        bool received = receiveTimeout(
+                1.seconds,
+                (RejectReason r, Document d) {
+            check(r is RejectReason.notAHiRPC, "Did not reject for the correct reason");
+            check(doc == d, "The rejected doc was not the same as was sent");
+        }
+        );
+        check(received, "Never received invalid contract");
+        return result_ok;
+    }
+
+    @But("the doc should not be sent to the Collector Service")
+    Document collectorService() {
+        return result_ok;
+    }
+
+}
+
+@safe @Scenario("Correct HiRPC format and permission.",
+        [
+    "The #permission scenario can be executed with and without correct permission."
+])
+class CorrectHiRPCFormatAndPermission {
+    ContractServiceHandle contract_handle;
+    string contract_success;
+    string contract_reject;
+    HiRPC hirpc;
+    this(ContractServiceHandle _contract_handle, string _success, string _reject) {
+        contract_handle = _contract_handle;
+        contract_success = _success; // The name of the service which successfull documents are sent to
+        contract_reject = _reject; // The name of the service which rejected documents are sent to
+        hirpc = HiRPC(new HiRPCNet("someObscurePassphrase"));
+    }
+
+    class HiRPCNet : StdSecureNet {
+        this(string passphrase) {
+            super();
+            generateKeyPair(passphrase);
+        }
+    }
+
+    Document doc;
+
+    @Given("a correctly formatted transaction.")
+    Document transaction() {
+        writeln(thisTid);
+        check(waitforChildren(Ctrl.ALIVE), "ContractService never alived");
+        auto params = new HiBON;
+        params["test"] = 42;
+        const sender = hirpc.action(ContractMethods.transaction, params);
+        doc = sender.toDoc;
+        contract_handle.send(sender);
+
+        return result_ok;
+    }
+
+    // All of these are invisible to the user,
+    // The test specification should be changed to reflect this
+    @When("the doc package has been verified that it is correct Document.")
+    Document document() {
+        return result_ok;
+    }
+
+    @When("the doc package has been verified that it is correct HiRPC.")
+    Document hiRPC() {
+        return result_ok;
+    }
+
+    @Then("the method of HiRPC should be checked that it is \'submit\'.")
+    Document submit() {
+        return result_ok;
+    }
+
+    @Then("the parameter for the send to the Collector service.")
+    Document service() {
+        return result_ok;
+    }
+
+    @Then("if check that the Collector services received the contract.")
+    Document contract() {
+        receiveTimeout(
+                1.seconds,
+                (inputHiRPC _, HiRPC.Sender s) {
+            const receiver = hirpc.receive(s.toDoc);
+            check(receiver.method.name == ContractMethods.transaction, "Incorrect method name");
+            check(s.toDoc == doc, "The received sender was not the same as was sent");
+        }
+        );
+        return result_ok;
+    }
+}
+
+@safe @Scenario("Correct HiRPC with permission denied.",
+        [])
+class CorrectHiRPCWithPermissionDenied {
+
+    ContractServiceHandle contract_handle;
+    string contract_success;
+    string contract_reject;
+    HiRPC bad_hirpc;
+    this(ContractServiceHandle _contract_handle, string _success, string _reject) {
+        contract_handle = _contract_handle;
+        contract_success = _success; // The name of the service which successfull documents are sent to
+        contract_reject = _reject; // The name of the service which rejected documents are sent to
+        bad_hirpc = HiRPC(new BadSecureNet("someLessObscurePassphrase"));
+    }
+
+    Document invalid_doc;
+    @Given("a HiPRC with incorrect permission")
+    Document incorrectPermission() {
+        auto params = new HiBON;
+        params["test"] = 42;
+        const invalid_sender = bad_hirpc.action(ContractMethods.transaction, params);
+        invalid_doc = invalid_sender.toDoc;
+        contract_handle.send(invalid_sender);
+        return result_ok;
+    }
+
+    @When("do scenario \'#permission\'")
+    Document scenarioPermission() {
+        receiveTimeout(
+                1.seconds,
+                (RejectReason r, Document d) {
+            check(d == invalid_doc, "The rejected doc was not the same as was sent");
+            check(r == RejectReason.notSigned, "The docuemnt was not rejected for the correct reason");
+        },
+        );
+        return result_ok;
+    }
+
+    @Then("check that the contract is not send to the Collector.")
+    Document theCollector() {
+        receiveTimeout(
+                Duration.zero,
+                (inputHiRPC _, HiRPC.Sender __) { check(false, "Should not have received a doc"); },
+        );
+
+        waitforChildren(Ctrl.END);
+        return result_ok;
+    }
+
+}
--- src/lib-services/tagion/services/contract.d
+++ src/lib-services/tagion/services/contract.d
@@ -4,35 +4,82 @@ module tagion.services.contract;
 
 import std.stdio;
 
-import tagion.utils.pretend_safe_concurrency;
-import tagion.actor;
+import tagion.logger.Logger;
 import tagion.basic.Debug : __write;
-import tagion.services.inputvalidator : inputDoc;
+import tagion.utils.JSONCommon;
+import tagion.utils.pretend_safe_concurrency;
+import tagion.actor.actor;
 import tagion.hibon.Document;
 import tagion.hibon.HiBONJSON;
 import tagion.hibon.HiBONRecord;
 import tagion.communication.HiRPC;
 import tagion.crypto.SecureInterfaceNet;
+public import tagion.services.inputvalidator : inputDoc;
 
 /// Msg type sent to receiver task along with a hirpc
 alias inputHiRPC = Msg!"inputHiRPC";
 
-/// ContractService actor
-/// Examples: [tagion.testbench.services.contract]
+struct ContractOptions {
+    /// Rejected documents won be discarded and instead sent to rejected_contracts_task
+    bool send_rejected_contracts = false;
+    /// Which task to send rejected document to;
+    string rejected_contracts_task = "";
+    mixin JSONCommon;
+}
+
+/// HiRPC methods
+enum ContractMethods {
+    transaction = "transaction",
+}
+
+/// used internally in combination with `send_rejected_contracts` optios for testing & tracing that contracts are correctly rejected
+enum RejectReason {
+    exception, // There was an internal exception in the contract task
+    notAHiRPC, // The Document received was not a vald HiRPC
+    invalidMethod, // The method was not one of the accepted methods
+    notSigned, // The rpc was not signed when it should have been
+}
+
+/**
+ * ContractService actor
+ * Examples: [tagion.testbench.services.contract]
+ * Receives: (inputDoc, Document)
+ * Sends: (inputHiRPC, HiRPC.Sender) to receiver_task, where Document is a correctly formatted HiRPC
+**/
 struct ContractService {
     string receiver_task;
-    void task(string receiver_task, immutable(SecureNet) net) {
+    void task(immutable(ContractOptions) opts, string receiver_task, immutable(SecureNet) net) {
         receiver_task = receiver_task;
         const hirpc = HiRPC(net);
 
+        void reject(RejectReason reason, lazy Document doc) {
+            if (opts.send_rejected_contracts) {
+                locate(opts.rejected_contracts_task).send(reason, doc);
+            }
+        }
+
         void contract(inputDoc, Document doc) {
-            __write("Received document \n%s", doc.toPretty);
-            if(!doc.isRecord!(HiRPC.Sender)) {
+            debug log("Received document \n%s", doc.toPretty);
+            scope (failure) {
+                reject(RejectReason.exception, doc);
+            }
+
+            if (!doc.isRecord!(HiRPC.Sender)) {
+                reject(RejectReason.notAHiRPC, doc);
                 return;
             }
-            HiRPC.Sender sender = hirpc.act(doc);
-            if (sender.isSigned) {
-                locate(receiver_task).send(inputHiRPC(), doc);
+
+            const receiver = hirpc.receive(doc);
+            with (ContractMethods) switch (receiver.method.name) {
+            case transaction:
+                HiRPC.Sender sender = hirpc.contract(doc);
+                if (sender.isSigned) {
+                    locate(receiver_task).send(inputHiRPC(), sender);
+                }
+                break;
+            default:
+                reject(RejectReason.invalidMethod, doc);
+                break;
             }
         }
 
