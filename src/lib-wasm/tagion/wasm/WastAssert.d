8e3cf742d9ee2c49e711ac77304d94271b66a460
cbleser <cr@tagion.org>
2023-08-11 16:27:34 +0300

    WIP: WastAssert.d added

---
+++ src/lib-wasm/tagion/wasm/WastAssert.d
@@ -0,0 +1,28 @@
+module tagion.wasm.WastAssert;
+
+import std.outbuffer;
+
+import tagion.basic.Types;
+import tagion.hibon.HiBONRecord;
+import tagion.wasm.WasmWriter;
+
+@safe
+struct Assert {
+    enum Method {
+        Return,
+        Invalid,
+        //Return_nan, same as Return
+        Trap,
+    }
+
+    Method method;
+    @label("assert") Buffer assert_code;
+    @label("*", true) Buffer result;
+    @label("*", true) string message;
+
+    mixin HiBONRecord;
+    void serialize(ref OutBuffer bout) const {
+    }
+}
+
+alias SectionAssert = WasmWriter.WasmSection.SectionT!Assert;
