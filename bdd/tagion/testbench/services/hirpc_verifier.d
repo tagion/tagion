326bb592f3544f6524ad763bed28dfdd7c9dbae7
lucas <lr@decard.io>
2023-08-15 16:21:15 +0200

    update documentation for Contract/HiRPCVerifier

--- bdd/tagion/testbench/services/hirpc_verifier.d
+++ bdd/tagion/testbench/services/hirpc_verifier.d
@@ -19,7 +19,7 @@ import tagion.services.hirpc_verifier;
 import std.stdio;
 
 enum feature = Feature(
-            "ontractInterface service.",
+            "HiRPCInterfaceService.",
             [
         "The transaction service should be able to receive HiRPC, validate data format and protocol rules before it sends to and send to the Collector services.",
         "The HiRPC is package into a HiBON-Document in the following called doc."
