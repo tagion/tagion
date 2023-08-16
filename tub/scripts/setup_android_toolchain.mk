219512c02da75736ff71263a6034c586fdf5e872
lucas <lr@decard.io>
2023-08-16 17:18:51 +0200

    update tool chain install

--- tub/scripts/setup_android_toolchain.mk
+++ tub/scripts/setup_android_toolchain.mk
@@ -1,4 +1,3 @@
-.ONESHELL:
 # tools directory
 TOOLS:=tools
 
@@ -12,34 +11,41 @@ ANDROID_NDK:=android-ndk-r21b
 ANDROID_NDK_ZIP:=$(ANDROID_NDK)-linux-x86_64.zip
 
 install: $(TOOLS)/.way
-install: $(TOOLS)/$(LDC_TARGET) $(TOOLS)/$(ANDROID_NDK) $(TOOLS)/$(LDC_HOST)
+install: $(LDC_TARGET) $(ANDROID_NDK) $(LDC_HOST)
 
 $(TOOLS)/.way:
 	mkdir -p $(TOOLS)
 	touch $(TOOLS)/.way
 
 $(TOOLS)/$(ANDROID_NDK_ZIP):
-	cd $(TOOLS)
-	wget https://dl.google.com/android/repository/${ANDROID_NDK_ZIP}
+	cd $(TOOLS); wget https://dl.google.com/android/repository/${ANDROID_NDK_ZIP}
 
 $(TOOLS)/$(LDC_TARGET_TAR):
-	cd $(TOOLS)
-	wget https://github.com/ldc-developers/ldc/releases/download/v1.29.0/${LDC_TARGET_TAR}
+	cd $(TOOLS); wget https://github.com/ldc-developers/ldc/releases/download/v1.29.0/${LDC_TARGET_TAR}
 
 $(TOOLS)/$(LDC_HOST_TAR):
-	cd $(TOOLS)
-	wget https://github.com/ldc-developers/ldc/releases/download/v1.29.0/${LDC_HOST_TAR}
+	cd $(TOOLS); wget https://github.com/ldc-developers/ldc/releases/download/v1.29.0/${LDC_HOST_TAR}
 
-$(TOOLS)/$(LDC_HOST): $(TOOLS)/$(LDC_HOST_TAR)
-	cd $(TOOLS)
-	tar xf $(LDC_HOST_TAR)
-	cd -
+$(TOOLS)/$(LDC_HOST)/.done: $(TOOLS)/$(LDC_HOST_TAR)
+	cd $(TOOLS); tar xf $(LDC_HOST_TAR)
 	cp tub/ldc2.conf $(TOOLS)/$(LDC_HOST)/etc/
+	touch $@
 
-$(TOOLS)/$(LDC_TARGET): $(TOOLS)/$(LDC_TARGET_TAR)
-	cd $(TOOLS)
-	tar xf $(LDC_TARGET_TAR)
+$(LDC_HOST): $(TOOLS)/$(LDC_HOST)/.done
 
-$(TOOLS)/$(ANDROID_NDK): $(TOOLS)/$(ANDROID_NDK_ZIP)
-	cd $(TOOLS)
-	unzip $(ANDROID_NDK_ZIP)
+$(TOOLS)/$(LDC_TARGET)/.done: $(TOOLS)/$(LDC_TARGET_TAR)
+	cd $(TOOLS); tar xf $(LDC_TARGET_TAR)
+	touch $@
+
+$(LDC_TARGET): $(TOOLS)/$(LDC_TARGET)/.done
+
+$(TOOLS)/$(ANDROID_NDK)/.done: $(TOOLS)/$(ANDROID_NDK_ZIP)
+	cd $(TOOLS); unzip $(ANDROID_NDK_ZIP)
+	touch $@
+
+$(ANDROID_NDK): $(TOOLS)/$(ANDROID_NDK)/.done
+
+clean:
+	rm -vr
+
+.PHONE: clean
