28122211c2110af109b85a7c56d74229551b1114
lucas <lr@decard.io>
2023-08-17 10:42:08 +0200

    mk: cross compilation works for android x86_64 and armv7a
    use
    PLATFORM=x86_64-linux-android
    and
    PLATFORM=armv7a-linux-android

--- platform.android.mk
+++ platform.android.mk
@@ -7,9 +7,9 @@ PLATFORMS+=$(ANDROID_AARCH64)
 
 ifeq ($(PLATFORM),$(ANDROID_AARCH64))
 
-ANDROID_ABI?=aarch64
 MTRIPLE:=aarch64-linux
 TRIPLET:=$(MTRIPLE)-android
+ANDROID_ABI?=$(TRIPLET)
 
 CROSS_GO_ARCH=arm64
 CROSS_ARCH=aarch64
@@ -18,6 +18,39 @@ ANDROID_ARCH=$(ANDROID_AARCH64)
 
 endif
 
+
+ANDROID_ARMV7A=armv7a-linux-android
+PLATFORMS+=$(ANDROID_ARMV7A)
+
+ifeq ($(PLATFORM),$(ANDROID_ARMV7A))
+
+MTRIPLE:=armv7a-linux
+TRIPLET:=$(MTRIPLE)-android
+ANDROID_ABI?=armv7a-linux-androideabi
+
+CROSS_GO_ARCH=arm64
+CROSS_ARCH=armv7a
+
+ANDROID_ARCH=$(ANDROID_ARMV7A)
+
+endif
+
+
+ANDROID_x86_64=x86_64-linux-android
+PLATFORMS+=$(ANDROID_x86_64)
+
+ifeq ($(PLATFORM),$(ANDROID_x86_64))
+
+MTRIPLE:=x86_64-linux
+TRIPLET:=$(MTRIPLE)-android
+ANDROID_ABI?=$(TRIPLET)
+
+CROSS_GO_ARCH=arm64
+CROSS_ARCH=x86_64
+
+ANDROID_ARCH=$(ANDROID_x86_64)
+
+endif
 
 # General android config
 ifneq (,$(findstring android,$(PLATFORM)))
@@ -29,13 +62,13 @@ HOST_OS:=${shell uname -s | tr '[:upper:]' '[:lower:]' }
 HOST_ARCH:=${shell uname -m}
 ANDROID_TOOLCHAIN:=$(abspath $(ANDROID_NDK)/toolchains/llvm/prebuilt/${HOST_OS}-${HOST_ARCH})
 
-export AR:=$(ANDROID_TOOLCHAIN)/bin/$(TRIPLET)-ar
-export AS:=$(ANDROID_TOOLCHAIN)/bin/$(TRIPLET)-as
-export CC:=$(ANDROID_TOOLCHAIN)/bin/$(TRIPLET)$(ANDROID_API)-clang
-export CXX:=$(ANDROID_TOOLCHAIN)/bin/$(TRIPLET)$(ANDROID_API)-clang++
-export LD:=$(ANDROID_TOOLCHAIN)/bin/$(TRIPLET)-ld
-export RANLIB:=$(ANDROID_TOOLCHAIN)/bin/$(TRIPLET)-ranlib
-export STRIP:=$(ANDROID_TOOLCHAIN)/bin/$(TRIPLET)-strip
+export AR:=$(ANDROID_TOOLCHAIN)/bin/llvm-ar
+export AS:=$(ANDROID_TOOLCHAIN)/bin/llvm-as
+export CC:=$(ANDROID_TOOLCHAIN)/bin/$(ANDROID_ABI)$(ANDROID_API)-clang
+export CXX:=$(ANDROID_TOOLCHAIN)/bin/$(ANDROID_ABI)$(ANDROID_API)-clang++
+export LD:=$(ANDROID_TOOLCHAIN)/bin/ld.ldd
+export RANLIB:=$(ANDROID_TOOLCHAIN)/bin/llvm-ranlib
+export STRIP:=$(ANDROID_TOOLCHAIN)/bin/llvm-strip
 
 CROSS_ENABLED=1
 CROSS_OS=android
