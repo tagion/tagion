0582269a478c79d4675617e3b17fc914b1b58438
cbleser <cr@tagion.org>
2023-08-23 15:54:20 +0300

    ADD: bdd/tagion/testbench/tvm_service.d

--- bdd/tagion/testbench/testbench.d
+++ bdd/tagion/testbench/testbench.d
@@ -28,6 +28,7 @@ int main(string[] args) {
     import hashgraph_swap = tagion.testbench.hashgraph_swap;
     import tvm_betterc = tagion.testbench.tvm_betterc;
     import epoch_creator = tagion.testbench.epoch_creator;
+    import tvm_service = tagion.testbench.tvm_service;
 
     alias alltools = AliasSeq!(
             ssl_server,
@@ -53,6 +54,7 @@ int main(string[] args) {
             hashgraph_swap,
             tvm_betterc,
             epoch_creator,
+            tvm_service,
     );
     mixin doOneMain!(alltools);
     return do_main(args);
