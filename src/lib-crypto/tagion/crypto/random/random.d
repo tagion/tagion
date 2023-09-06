5acc370e98410350c506163fe8650cba91b1c4bb
cbleser <cr@tagion.org>
2023-09-06 12:40:11 +0300

    DONE: True random has been moved to tagion.crypto.random.random

--- src/lib-crypto/tagion/crypto/random/random.d
+++ src/lib-crypto/tagion/crypto/random/random.d
@@ -1,38 +1,38 @@
 module tagion.crypto.random;
 
 import tagion.basic.Version;
+import std.system : os;
+import std.format;
 
-version (none) {
-    static if (ver.Linux || ver.Android) {
-        enum is_li = true;
-        extern (C) size_t getrandom(void* buf, size_t buflen, uint flags) @trusted;
-    }
-    else static if (ver.iOS) {
-        enum is_getrandom = false;
-        extern (C) void arc4random_buf(void* buf, size_t buflen) @trusted;
-    }
-    else {
-        //   static assert(0, "THR
-    }
+static if (ver.linux || ver.Android) {
+    enum is_getrandom = true;
+    extern (C) size_t getrandom(void* buf, size_t buflen, uint flags) @trusted;
+}
+else static if (ver.iOS) {
+    enum is_getrandom = false;
+    extern (C) void arc4random_buf(void* buf, size_t buflen) @trusted;
+}
+else {
+    static assert(0, format("Random function not support for %s", os));
+}
 
-    /++
+/++
      + getRandom - runs platform specific random function.
      +/
-    @trusted
-    ubyte[] getRandom() {
-        auto buf = new ubyte[20];
+@trusted
+ubyte[] getRandom(const size_t size) {
+    auto buf = new ubyte[size];
 
-        static if (is_getrandom) {
-            // GRND_NONBLOCK = 0x0001. Don't block and return EAGAIN instead
-            // GRND_RANDOM   = 0x0002. No effect
-            // GRND_INSECURE = 0x0004. Return non-cryptographic random bytes
+    static if (is_getrandom) {
+        // GRND_NONBLOCK = 0x0001. Don't block and return EAGAIN instead
+        // GRND_RANDOM   = 0x0002. No effect
+        // GRND_INSECURE = 0x0004. Return non-cryptographic random bytes
 
-            getrandom(&buf[0], buf.length, 0x0002);
-        }
-        else {
-            arc4random_buf(&buf[0], buf.length);
-        } // TODO: add other platforms
-
-        return buf;
+        getrandom(&buf[0], buf.length, 0x0002);
     }
+    else {
+        arc4random_buf(&buf[0], buf.length);
+    } // TODO: add other platforms
+
+    return buf;
 }
