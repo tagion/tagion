1223813f811e500e206eed4e3c5f99b5bd64352a
Lucas Rasmussen <lucasnethaj@gmail.com>
2023-09-07 16:37:56 +0200

    WIP: collector read reads from dart

--- src/lib-services/tagion/services/collector.d
+++ src/lib-services/tagion/services/collector.d
@@ -16,23 +16,42 @@ import tagion.utils.pretend_safe_concurrency;
 
 import std.typecons;
 
+version (none) struct Stack(T) {
+    import std.container.slist;
+
+    immutable(T) pop(uint id) {
+        return cast(immutable) t;
+    }
+
+    void push(uint id, immutable T item) {
+        stack[id] = cast(T) item;
+    }
+}
+
 @safe
 struct CollectorOptions {
     import tagion.utils.JSONCommon;
 
-    string task_name = "collector_task";
     mixin JSONCommon;
 }
 
 @safe
 struct CollectorService {
-    void task(immutable(CollectorOptions) opts, string dart_task_name, immutable SecureNet net) {
+    void task(immutable(CollectorOptions) opts, immutable SecureNet net, const string dart_task_name, const string tvm_task_name) {
         //            [req id]
-        SignedContract[uint] contracts;
+        // Slist!(immutable(SignedContract)) contracts;
+        import std.typecons : Unique;
+
+        CollectedSignedContract[uint] collections;
+        // Unique!CollectedSignedContract[uint] collections;
 
         void signed_contract(inputContract, immutable(SignedContract) s_contract) {
             const req = dartReadRR();
-            contracts[req.id] = cast(SignedContract) s_contract;
+
+            // collections[req.id] = new Unique!CollectorService;
+            // collections[req.id].contract = s_contract;
+            collections[req.id].contract = cast(SignedContract) s_contract;
+            locate(dart_task_name).send(req, s_contract.contract.reads);
             locate(dart_task_name).send(req, s_contract.contract.inputs);
         }
 
@@ -48,17 +67,42 @@ struct CollectorService {
 
         // bool verify(const Fingerprint message, const Signature signature, const Pubkey pubkey)
 
-        void recorder(dartReadRR.Response res, immutable(RecordFactory.Recorder) recorder) {
+        void recorder(dartReadRR.Response res, immutable(RecordFactory.Recorder) recorder) @safe {
             import std.range;
             import tagion.dart.DARTBasic : dartFingerprint;
+            import std.algorithm.iteration : map;
 
-            auto contract = contracts[res.id];
-            foreach (dartindex, sign, archive; zip(contract.contract.inputs, contract.signs, recorder[])) {
-                Pubkey pkey = archive.filed[OwnerKey].get!Buffer;
-                if (!net.verify(dartFingerprint(dartindex), sign, pkey)) {
-                    return;
-                }
+            auto collection = collections[res.id];
+
+            const archives = recorder[];
+            const s_contract = collection.contract;
+            if (s_contract.contract.reads == archives.map!(a => a.fingerprint).array) {
+                collection.reads = archives.map!(a => a.toDoc).array.dup;
             }
+            if (s_contract.contract.inputs == archives.map!(a => a.fingerprint).array) {
+                foreach (dartindex, sign, archive; zip(s_contract.contract.inputs, s_contract.signs, recorder[])) {
+                    Pubkey pkey = archive.filed[OwnerKey].get!Buffer;
+                    if (!net.verify(dartFingerprint(dartindex), sign, pkey)) {
+                        return;
+                    }
+                }
+                collection.inputs = archives.map!(a => a.toDoc).array.dup;
+            }
+            if (collection.reads !is Document[].init && collection.inputs !is Document[].init) {
+                return;
+            }
+
+            import std.exception;
+
+            const(Document[]) inputs = recorder[].map!(a => a.toDoc).array;
+            // immutable collected = 
+            //     CollectedSignedContract(
+            //             assumeUnique(inputs),
+            //             assumeUnique(inputs), // reads
+            //             s_contract,
+            //     );
+
+            // locate(tvm_task_name).send(signedContract(), collected);
         }
 
         run(&signed_contract, &recorder, &rpc_contract);
