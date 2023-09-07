7dbc4e634260bf14e39f661c3c1c181fcb90d278
cbleser <cr@tagion.org>
2023-09-07 12:34:22 +0300

    DONE: deprecation corrected in HiBON.d

--- src/lib-hibon/tagion/hibon/HiBON.d
+++ src/lib-hibon/tagion/hibon/HiBON.d
@@ -147,7 +147,7 @@ static size_t size(U)(const(U[]) array) pure {
         immutable Type type;
         Value value;
 
-        @nogc protected this(const string key) pure {
+        @nogc protected this(const string key) pure scope {
             this.key = key;
             type = Type.NONE;
         }
