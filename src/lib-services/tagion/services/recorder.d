59d3ff8b03c463863c115e1122a13bd9fabb35b2
Philip Rying <philiprying@gmail.com>
2023-09-24 11:51:28 +0200

    wip(recorderservice): initial recorder skeleton

---
+++ src/lib-services/tagion/services/recorder.d
@@ -0,0 +1,41 @@
+module tagion.services.recorder;
+
+import tagion.actor;
+import tagion.utils.Miscellaneous : cutHex;
+import tagion.logger.Logger : log;
+import tagion.recorderchain.RecorderChainBlock : RecorderChainBlock;
+import tagion.recorderchain.RecorderChain;
+import tagion.crypto.Types : Fingerprint;
+import tagion.dart.Recorder : RecordFactory;
+import tagion.crypto.SecureInterfaceNet;
+
+
+@safe
+struct RecorderOptions {
+    string folder_path = "/tmp/test/";
+}
+
+
+@safe
+struct RecorderService {
+
+    void task(immutable(RecorderOptions) opts, immutable(SecureNet) net) {
+        RecorderChainStorage storage = new RecorderChainFileStorage(opts.folder_path, net);
+        RecorderChain recorder_chain = new RecorderChain(storage);
+
+        void receiveRecorder(immutable(RecordFactory.Recorder) recorder, immutable(Fingerprint) bullseye, immutable(uint) epoch_number) {
+            auto last_block = recorder_chain.getLastBlock;
+            auto block = new RecorderChainBlock(
+                recorder.toDoc,
+                last_block ? last_block.fingerprint : Fingerprint.init,
+                bullseye,
+                net);
+            recorder_chain.append(block);
+            log.trace("Added recorder chain block with hash '%s'", block.getHash.cutHex);
+
+        }
+
+        run(&receiveRecorder);
+    }
+
+}
