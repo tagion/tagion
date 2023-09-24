ef68df3d5d49ee7dc276e533089bbe8d59abab27
Lucas Rasmussen <lucasnethaj@gmail.com>
2023-09-24 22:02:02 +0200

    mk: remove android abi decl

--- platform.x86_64-linux.mk
+++ platform.x86_64-linux.mk
@@ -6,7 +6,6 @@ LINUX_X86_64:=x86_64-linux
 
 PLATFORMS+=$(LINUX_X86_64)
 ifeq ($(PLATFORM),$(LINUX_X86_64))
-ANDROID_ABI=x86_64
 DINC+=${shell find $(DSRC) -maxdepth 1 -type d -path "*src/lib-*" }
 #DFILES?=${shell find $(DSRC) -type f -name "*.d" -path "*src/lib-*" -a -not -path "*/tests/*" -a -not -path "*/c/*" -a -not -path "*/unitdata/*"}
 
