541f4108dc0d6f00775dc1d6cd956903d15dbe1b
Lucas Rasmussen <lucasnethaj@gmail.com>
2023-09-24 20:57:40 +0200

    bdd: collector order inputs according to the nets

--- bdd/tagion/testbench/services/collector.d
+++ bdd/tagion/testbench/services/collector.d
@@ -28,6 +28,12 @@ import tagion.basic.Types : FileExtension, Buffer;
 import tagion.dart.Recorder;
 import tagion.dart.DARTBasic;
 
+debug {
+    import std.stdio;
+    import tagion.hibon.HiBONJSON;
+    import tagion.hibon.HiBONtoText;
+}
+
 enum feature = Feature(
             "collector services",
             []);
@@ -45,14 +51,18 @@ StdSecureNet[] createNets(uint count, string pass_prefix = "net") @safe {
     }).array;
 }
 
-TagionBill[] createBills(StdSecureNet[] bill_nets, uint amount) @safe {
+TagionBill[] createBills(StdSecureNet[] bill_nets, uint amount) @trusted {
+    import core.thread;
+    import core.time;
+
+    Thread.sleep(10.msecs);
     return bill_nets.map!((net) =>
             TagionBill(TGN(amount), currentTime, net.pubkey)
     ).array;
 }
 
 const(DARTIndex)[] insertBills(TagionBill[] bills, ref RecordFactory.Recorder rec) @safe {
-    rec.insert(bills.map!(bill => bill.toDoc), Archive.Type.ADD);
+    rec.insert(bills, Archive.Type.ADD);
     return rec[].map!((a) => a.fingerprint).array;
 }
 
@@ -63,6 +73,7 @@ class ItWork {
     CollectorServiceHandle collector_handle;
 
     immutable(DARTIndex)[] inputs;
+    TagionBill[] input_bills;
     StdSecureNet[] input_nets;
 
     immutable SecureNet node_net;
@@ -95,40 +106,69 @@ class ItWork {
 
         auto record_factory = RecordFactory(node_net);
         auto insert_recorder = record_factory.recorder;
-        auto output_recorder = record_factory.recorder;
 
         input_nets = createNets(10, "input");
-        inputs ~= input_nets.createBills(100_000).insertBills(insert_recorder);
+        input_bills = input_nets.createBills(100_000);
+        input_bills.insertBills(insert_recorder);
+        inputs ~= input_bills.map!(a => node_net.dartIndex(a.toDoc)).array;
+        check(inputs !is null, "Inputs were null");
         dart_handle.send(dartModifyRR(),
                 (() @trusted => cast(immutable) insert_recorder)()
         );
         receiveOnlyTimeout!(dartModifyRR.Response, immutable(DARTIndex));
 
-        // dart_handle.send(dartBullseyeRR());
         immutable collector = CollectorService(node_net, dart_service, thisActor.task_name);
         collector_handle = spawn(collector, "collector_task");
         check(waitforChildren(Ctrl.ALIVE), "CollectorService never alived");
 
-        return result_ok;
-    }
-
-    @When("i send a contract")
-    Document contract() @trusted {
+        //         return result_ok;
+        //     }
+        // 
+        //     @When("i send a contract")
+        //     Document contract() @trusted {
         import std.exception;
 
         immutable outputs = PayScript(iota(0, 10).map!(_ => TGN(100_000)).array).toDoc;
-        immutable contract = cast(immutable) Contract(inputs, immutable(DARTIndex[]).init, outputs);
-        immutable signs = {
-            immutable(Signature)[] signs;
-            foreach (fprint; inputs) {
-                signs ~= node_net.sign(node_net.calcHash(cast(Buffer) fprint));
-            }
-            return signs;
-        };
+        immutable contract = immutable(Contract)(inputs, immutable(DARTIndex[]).init, outputs);
+        check(Contract(contract.toDoc).inputs == contract.inputs, "Input bills and ordered inputs bills were not the same");
+        import std.algorithm.sorting;
 
-        // check(node_net.verify(insert_recorder[].take(1).dartFingerprint, signs()[0], node_net.pubkey));
-        immutable s_contract = cast(immutable) SignedContract(signs(), cast(immutable) contract);
-        collector_handle.send(inputContract(), s_contract);
+        immutable signs = {
+            Signature[] _signs;
+            const contract_hash = node_net.calcHash(contract.toDoc);
+            foreach (net; input_nets) {
+                _signs ~= net.sign(contract_hash);
+            }
+            return _signs.assumeUnique;
+        }();
+        check(signs !is null, "Signs is null");
+
+        immutable s_contract = immutable(SignedContract)(signs, contract);
+
+        writeln("Input order");
+        const contract_hash = node_net.calcHash(contract.toDoc);
+        foreach (index, sign; zip(s_contract.contract.inputs, s_contract.signs)) {
+            const archive = find(insert_recorder, index);
+            check(archive !is null, format("Archive %s, did not exist in recorder", index));
+
+            immutable bill = TagionBill(archive.filed);
+            writefln("f:%s", archive.fingerprint.encodeBase64);
+            writefln("s:%s", sign.encodeBase64);
+            writefln("p:%s", bill.owner.encodeBase64);
+
+            // bool verify(const fingerprint message, const signature signature, const pubkey pubkey)
+            if (!node_net.verify(contract_hash, sign, bill.owner)) {
+                writeln("could not be verified");
+                check(false, "could not be verified");
+            }
+        }
+
+        import tagion.communication.HiRPC;
+
+        const hirpc = HiRPC(node_net);
+        immutable sender = hirpc.sendDaMonies(s_contract);
+        collector_handle.send(inputHiRPC(), hirpc.receive(sender.toDoc));
+
         return result_ok;
     }
 
