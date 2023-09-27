9aeb9e8960497b12153913689b0a393bf00c4c13
Philip Rying <philiprying@gmail.com>
2023-09-27 17:06:27 +0200

    wip(send contract) create wallets

--- bdd/tagion/testbench/send_contract.d
+++ bdd/tagion/testbench/send_contract.d
@@ -8,7 +8,6 @@ import std.file;
 import std.path : setExtension, buildPath;
 import tagion.basic.Types : FileExtension;
 
-
 import tagion.services.options;
 import core.time;
 import core.thread;
@@ -25,6 +24,12 @@ void wrap_neuewelle(immutable(string)[] args) {
     neuewelle._main(cast(string[]) args);
 }
 
+
+
+
+
+
+
 int _main(string[] args) {
     auto module_path = env.bdd_log.buildPath(__MODULE__);
     if (module_path.exists) {
@@ -37,23 +42,59 @@ int _main(string[] args) {
     local_options.dart.folder_path = buildPath(module_path);
     local_options.replicator.folder_path = buildPath(module_path);
     local_options.save(config_file);
-    
-    auto send_contract_feature = automation!(sendcontract);
 
+    import std.format;
+    import std.range;
+    import std.array;
+    import std.algorithm;
+    import std.stdio;
+    import tagion.crypto.SecureNet : StdSecureNet;
+    import tagion.wallet.SecureWallet;
+    import tagion.script.common : TagionBill;
+    alias StdSecureWallet = SecureWallet!StdSecureNet;
+    import tagion.script.TagionCurrency;
+
+    StdSecureWallet[] wallets;
+    // create the wallets
+    foreach(i; 0..10) {
+        StdSecureWallet secure_wallet;
+        secure_wallet = StdSecureWallet.createWallet(
+            iota(0,5).map!(n => format("%dquestion%d", i, n)).array,
+            iota(0,5).map!(n => format("%danswer%d", i, n)).array,
+            4,
+            format("%04d", i),
+        );
+        wallets ~= secure_wallet;
+    }
+
+    TagionBill[] bills;
+    // bills for the dart
+    foreach(wallet; wallets) {
+        auto bill = wallet.requestBill(1000.TGN);
+        bills ~= bill;
+    }
+
+    writeln(bills);
+
+
+
+
+    
 
     immutable neuewelle_args = [config_file];
     auto tid = spawn(&wrap_neuewelle, neuewelle_args);
     Thread.sleep(10.seconds);
 
-
-
-    
-
+    auto send_contract_feature = automation!(sendcontract);
     send_contract_feature.run();
+
+
+    import tagion.hibon.HiBONRecord;
+    import tagion.hibon.HiBONRecord;
+
+
     stopsignal.set;
-    
 
     return 0;
 
 }
-
