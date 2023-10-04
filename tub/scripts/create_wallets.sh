844895af6c0fa97107140d25d8198a052a47db66
Philip Rying <philiprying@gmail.com>
2023-10-04 10:46:39 +0200

    wip(wallet) create script for initalizing wallets

--- tub/scripts/create_wallets.sh
+++ tub/scripts/create_wallets.sh
@@ -8,10 +8,14 @@ fi
 
 # Extract the number of wallets from the command line argument
 num_wallets=$1
+num_bills=10
+network_folder="network"
+number_of_nodes=5
 
 # Create the wallets in a loop
-for ((i = 0; i <= num_wallets; i++)); do
-  wallet_dir="wallet$i"
+for ((i = 1; i <= num_wallets; i++)); 
+do
+  wallet_dir="./wallet$i"
   wallet_config="wallet$i.json"
   password="password$i"
   pincode=$(printf "%04d" $i)
@@ -21,6 +25,26 @@ for ((i = 0; i <= num_wallets; i++)); do
 
   # Step 2: Generate wallet passphrase and pincode
   geldbeutel "$wallet_config" -P "$password" -x "$pincode"
-
   echo "Created wallet $i in $wallet_dir with passphrase: $password and pincode: $pincode"
+
+  for (( b=1; b <= num_bills; b++)); 
+  do
+    bill_name="bill$i-$b.hibon"
+    geldbeutel "$wallet_config" -x "$pincode" --amount 10000 -o "$bill_name" 
+    echo "Created bill $bill_name"
+  done 
+
+done
+
+bill_files=$(ls bill*.hibon)
+stiefel $bill_files -o dart_recorder.hibon
+
+mkdir -p $network_folder
+
+for ((i = 0; i <= number_of_nodes-1; i++)); 
+do
+  dartfilename="${network_folder}/Node_${i}_dart.drt"
+  echo "$dartfilename"
+  dartutil --initialize "$dartfilename"
+  dartutil "$dartfilename" dart_recorder.hibon -m
 done
