fc48c795055781d8bfebab59656c6544b5c13cd8
Lucas Rasmussen <lucasnethaj@gmail.com>
2023-10-06 23:58:38 +0200

    fix warnings in hibon

--- src/lib-hibon/tagion/hibon/HiBON.d
+++ src/lib-hibon/tagion/hibon/HiBON.d
@@ -324,7 +324,6 @@ static size_t size(U)(const(U[]) array) pure {
                                 const v = value.by!(E);
                                 return Document.sizeT(E, key, v);
                             }
-                            break TypeCase;
                         }
                     }
                 default:
