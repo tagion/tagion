897f2096d25d5b00229bf31e05af42948a665701
cbleser <cr@tagion.org>
2023-10-08 16:17:59 +0300

    WIP: DARTBasic.dartIndex unittest added including hibon type

--- src/lib-dart/tagion/dart/DARTBasic.d
+++ src/lib-dart/tagion/dart/DARTBasic.d
@@ -1,6 +1,7 @@
 /// Basic fuinction and types used in the DART database
 module tagion.dart.DARTBasic;
 
+@safe:
 import std.typecons : Typedef;
 
 import tagion.crypto.Types : BufferType, Fingerprint;
@@ -25,21 +26,54 @@ alias DARTIndex = Typedef!(Buffer, null, BufferType.HASHPOINTER.stringof);
  *   The DART fingerprint
  */
 
-@safe
 immutable(DARTIndex) dartIndex(const(HashNet) net, const(Document) doc) {
     if (!doc.empty && (doc.keys.front[0] is HiBONPrefix.HASH)) {
         if (doc.keys.front == STUB) {
             return doc[STUB].get!DARTIndex;
         }
         auto first = doc[].front;
-        immutable value_data = first.data[first.dataPos .. first.dataPos + first.dataSize];
+        immutable value_data = first.data[0 .. first.size];
         return DARTIndex(net.rawCalcHash(value_data));
     }
     return DARTIndex(cast(Buffer) net.calcHash(doc));
 }
 
 /// Ditto
-@safe
 immutable(DARTIndex) dartIndex(T)(const(HashNet) net, T value) if (isHiBONRecord!T) {
     return net.dartIndex(value.toDoc);
+}
+
+unittest { // Check the #key hash with types
+    import tagion.crypto.SecureNet : StdHashNet;
+    import tagion.crypto.SecureInterfaceNet : HashNet;
+    import tagion.hibon.HiBONRecord : label, HiBONRecord;
+
+    const(HashNet) net = new StdHashNet;
+    static struct HashU32 {
+        @label("#key") uint x;
+        string extra_name;
+        mixin HiBONRecord;
+    }
+
+    static struct HashU64 {
+        @label("#key") ulong x;
+        mixin HiBONRecord;
+    }
+
+    HashU32 hash_u32;
+    HashU64 hash_u64;
+    hash_u32.x = 42;
+    hash_u64.x = 42;
+    import std.stdio;
+
+    writefln("dart_index=%(%02x%)", net.dartIndex(hash_u32));
+    writefln("dart_index=%(%02x%)", net.dartIndex(hash_u64));
+    assert(net.dartIndex(hash_u32) != net.dartIndex(hash_u64));
+    auto other_hash_u32 = hash_u32;
+    other_hash_u32.extra_name = "extra";
+    writefln("dart_index=%(%02x%)", net.dartIndex(other_hash_u32));
+    assert(net.dartIndex(hash_u32) == net.dartIndex(other_hash_u32), "Archives with the same #key should have the same dart-Index");
+    writefln("fingerprint=%(%02x%)", net.calcHash(other_hash_u32));
+    writefln("fingerprint=%(%02x%)", net.calcHash(hash_u32));
+    assert(net.calcHash(hash_u32) != net.calcHash(other_hash_u32), "Two archives with same #key and different data should have different fingerprints");
 }
