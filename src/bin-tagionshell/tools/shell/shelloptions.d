16c44e3d6b92d3fa02742a522d36431dc04bde6c
Yuriy <yv@decard.io>
2023-10-10 03:04:27 +0000

    web shell draft

--- src/bin-tagionshell/tools/shell/shelloptions.d
+++ src/bin-tagionshell/tools/shell/shelloptions.d
@@ -7,9 +7,11 @@ enum mode0_prefix = "Node_1_";
 @safe
 struct ShellOptions {
     string tagion_sock_addr;
+    string contract_endpoint;
 
     void setDefault() nothrow {
         tagion_sock_addr = contract_sock_addr(mode0_prefix);
+        contract_endpoint = "http://localhost:8088";
     }
 
     mixin JSONCommon;
