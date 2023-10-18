757094ec82ca61ce0555f4c19d53e5313899890f
cbleser <cr@tagion.org>
2023-10-18 10:48:37 +0300

    DONE: dstep enabled as a target again (Note as default but just make dstep and make clean-dstep)

--- src/lib-crypto/context.mk
+++ src/lib-crypto/context.mk
@@ -4,28 +4,25 @@ include ${call dir.resolve, dstep.mk}
 
 DFILES_NATIVESECP256K1=${shell find $(DSRC)/lib-crypto -name "*.d"}
 
-ifdef SECP256K1_HASH
-SEC256k1_WRAPPER_SRC=$(DSRC)/lib-crypto/tagion/crypto/secp256k1/secp256k1_sha256_wrapper.c
-SEC256k1_WRAPPER_OBJ=$(DTMP)/secp256k1_sha256_wrapper.$(OBJEXT)
-
-$(SEC256k1_WRAPPER_OBJ): CFLAGS+=-I $(DSRC)/wrap-secp256k1/secp256k1/src/
-$(SEC256k1_WRAPPER_OBJ): CFLAGS+=-I $(DSRC)/wrap-secp256k1/secp256k1/include/
-
-secp256k1-sha256: $(SEC256k1_WRAPPER_OBJ)
-
-$(SEC256k1_WRAPPER_OBJ): $(SEC256k1_WRAPPER_SRC)
+env-crypto:
 	$(PRECMD)
-	$(call log.header, $(@F) :: compile)
-	$(CC) $(CFLAGS) -c $< -o $@
+	$(call log.header, $@ :: env)
+	$(call log.env, CRYPTO_DFILES,$(CRYPTO_DFILES))
+	$(call log.kvp, LCRYPTO_DIROOT,$(LCRYPTO_DIROOT))
+	$(call log.kvp, LCRYPTO_PACKAGE,$(LCRYPTO_PACKAGE))
+	$(call log.close)
 
+.PHONY: env-crypto
 
-proper-secp256k1-sha256:
+env: env-crypto
+
+help-crypto:
 	$(PRECMD)
-	${call log.header, $@ :: proper}
-	$(RM) $(SEC256k1_WRAPPER_OBJ)
+	$(call log.header, $@ :: help)
+	$(call log.help, "make env-dstep-$(LCRYPTO_PACKAGE)", "Display secp256k1 dstep env")
+	$(call log.close)
 
-.PHONY: proper-secp256k1-sha256
+help: help-crypto
 
-secp256k1: $(SEC256k1_WRAPPER_OBJ)
+.PHONY: help-crypto
 
-endif
