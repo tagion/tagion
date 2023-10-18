52029d003db2be14ff129ed3533ead86d3a60a90
Yuriy <yv@decard.io>
2023-10-18 23:41:03 +0000

    dart interface and invoice-to-pay endpoint added

--- src/bin-tagionshell/tools/shell/shelloptions.d
+++ src/bin-tagionshell/tools/shell/shelloptions.d
@@ -13,6 +13,9 @@ struct ShellOptions {
     string tagion_sock_addr;
     string tagion_dart_sock_addr;
     string tagion_subscription;
+    string i2p_endpoint;
+    string default_i2p_wallet;
+    string default_i2p_wallet_pin;
 
     void setDefault() nothrow {
         tagion_sock_addr = contract_sock_addr(mode0_prefix~"CONTRACT_");
@@ -22,6 +25,9 @@ struct ShellOptions {
         shell_api_prefix = "/api/v1";
         contract_endpoint = "/contract";
         dart_endpoint = "/dart";
+        i2p_endpoint = "/invoice2pay";
+        default_i2p_wallet = "./wallets/wallet1";
+        default_i2p_wallet_pin = "0001";
     }
 
     mixin JSONCommon;
--- src/bin-tagionshell/tools/tagionshell.d
+++ src/bin-tagionshell/tools/tagionshell.d
@@ -15,6 +15,27 @@ import tagion.tools.revision;
 import tagion.tools.shell.shelloptions;
 import tagion.hibon.Document;
 import tagion.hibon.HiBON;
+import tagion.hibon.HiBONFile : fread, fwrite;
+
+import tagion.script.common;
+import tagion.script.TagionCurrency;
+
+import tagion.basic.Types : FileExtension, Buffer, hasExtension;
+import tagion.basic.range : doFront;
+
+import tagion.utils.StdTime : currentTime;
+
+import tagion.crypto.SecureNet;
+import tagion.crypto.SecureInterfaceNet;
+
+import tagion.tools.wallet.WalletOptions;
+import tagion.tools.wallet.WalletInterface;
+
+import tagion.communication.HiRPC;
+import tagion.wallet.SecureWallet;
+import tagion.wallet.AccountDetails;
+
+
 import nngd.nngd;
 
 mixin Main!(_main, "shell");
@@ -86,7 +107,7 @@ WebData dart_handler ( WebData req, void* ctx ){
     }
     writeln(format("WH: dart: with %d bytes for %s",req.rawdata.length, opt.tagion_dart_sock_addr));
     NNGSocket s = NNGSocket(nng_socket_type.NNG_SOCKET_REQ);
-    s.recvtimeout = msecs(10000);
+    s.recvtimeout = msecs(60000);
     writeln(format("WH: dart: trying to dial %s", opt.tagion_dart_sock_addr));
     while(true){
         rc = s.dial(opt.tagion_dart_sock_addr);
@@ -120,6 +141,65 @@ WebData dart_handler ( WebData req, void* ctx ){
         type: "applicaion/octet-stream", rawdata: (doclen>0) ? docbuf[0..doclen] : null 
     };
     writeln("WH: dart: res ",res);
+    return res;
+}
+
+WebData i2p_handler ( WebData req, void* ctx ){
+    int rc;
+    ShellOptions* opt = cast(ShellOptions*) ctx;
+    if(req.type != "application/octet-stream"){
+        WebData res = { status: nng_http_status.NNG_HTTP_STATUS_BAD_REQUEST, msg: "invalid data type" };    
+        return res;
+    }
+    writeln(format("WH: invoice2pay: with %d bytes",req.rawdata.length));
+ 
+    WalletOptions options;
+    auto wallet_config_file = opt.default_i2p_wallet ~ "/wallet.json";
+    if (wallet_config_file.exists) {
+        options.load(wallet_config_file);
+    }else{
+        writeln("i2p: invalid wallet dir");
+        WebData res = { status: nng_http_status.NNG_HTTP_STATUS_BAD_REQUEST, msg: "invalid wallet dir" };    
+        return res;
+    }
+    auto wallet_interface = WalletInterface(options);
+    SecureWallet!StdSecureNet secure_wallet;
+    const wallet_doc = (opt.default_i2p_wallet ~ "/" ~ options.walletfile).fread;
+    const pin_doc = (opt.default_i2p_wallet ~ "/" ~ options.devicefile).exists ? (opt.default_i2p_wallet ~ "/" ~ options.devicefile).fread : Document.init;
+    if (wallet_doc.isInorder && pin_doc.isInorder) {
+        secure_wallet = WalletInterface.StdSecureWallet(wallet_doc, pin_doc);
+        secure_wallet.login(opt.default_i2p_wallet_pin);
+    }else{
+        writeln("i2p: invalid wallet load");
+        WebData res = { status: nng_http_status.NNG_HTTP_STATUS_BAD_REQUEST, msg: "invalid wallet load" };    
+        return res;
+    }   
+    if(!secure_wallet.isLoggedin){
+        writeln("i2p: invalid wallet login");
+        WebData res = { status: nng_http_status.NNG_HTTP_STATUS_BAD_REQUEST, msg: "invalid wallet login" };    
+        return res;
+    }
+    
+    Invoice[] invoices;
+    invoices ~=  Invoice(Document(cast(immutable(ubyte[]))req.rawdata));
+
+    SignedContract signed_contract;
+    TagionCurrency fees;
+    secure_wallet.payment(invoices, signed_contract, fees);
+    const message = secure_wallet.net.calcHash(signed_contract);
+    const contract_net = secure_wallet.net.derive(message);
+    const hirpc = HiRPC(contract_net);
+    const hirpc_submit = hirpc.submit(signed_contract);
+    secure_wallet.account.hirpcs ~= hirpc_submit.toDoc;
+
+    sendSubmitHiRPC(options.contract_address, hirpc_submit, contract_net);
+
+    writeln("i2p: payment sent");   
+
+    WebData res = {
+        status: nng_http_status.NNG_HTTP_STATUS_OK, 
+        type: "applicaion/octet-stream", rawdata: cast(ubyte[])(hirpc_submit.toDoc.serialize)
+    };
     return res;
 }
 
@@ -179,6 +259,7 @@ int _main(string[] args) {
 
     app.route(options.shell_api_prefix~options.contract_endpoint, &contract_handler, ["POST"]);
     app.route(options.shell_api_prefix~options.dart_endpoint, &dart_handler, ["POST"]);
+    app.route(options.shell_api_prefix~options.i2p_endpoint, &i2p_handler, ["POST"]);
 
     app.start();
 
@@ -189,7 +270,10 @@ int _main(string[] args) {
         ~"\t= POST contract hibon\n\t"
         ~options.shell_api_prefix
         ~options.dart_endpoint
-        ~"\t\t= POST dart request hibon\n"
+        ~"\t\t= POST dart request hibon\n\t"
+        ~options.shell_api_prefix
+        ~options.i2p_endpoint
+        ~"\t= POST invoice-to-pay hibon\n\t"
 
     );
 
