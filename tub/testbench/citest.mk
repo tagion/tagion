a5060a5639f3adb6b0ba5e8117658104f8554f01
Lucas Rasmussen <lucasnethaj@gmail.com>
2023-10-24 10:38:28 +0200

    ci: disable build ddoc because it no work

--- tub/testbench/citest.mk
+++ tub/testbench/citest.mk
@@ -5,7 +5,6 @@ ci:
 	$(MAKE) -S bins -f$(DTUB)/main.mk
 	$(MAKE) -S bddtest unittest-cov TESTBENCH_FLAGS=--silent -f$(DTUB)/main.mk
 	$(MAKE) -S release -f$(DTUB)/main.mk
-	$(MAKE) -S ddoc -f$(DTUB)/main.mk
 	cp $(REPOROOT)/collider_schedule.json $(DBIN) 
 	cp $(DSRC)/bin-wave/neuewelle.service $(DBIN)
 	cp $(DTUB)/scripts/create_wallets.sh $(DBIN)
