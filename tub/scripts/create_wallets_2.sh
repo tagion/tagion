17ae9055211a63258cb0deab94a23833f350511d
Philip Rying <philiprying@gmail.com>
2023-10-27 12:34:06 +0200

    fix(flow): force bill

--- tub/scripts/create_wallets_2.sh
+++ tub/scripts/create_wallets_2.sh
@@ -65,6 +65,8 @@ do
     bill_name=$(readlink -m "$wdir/bill$i-$b.hibon")
     $bdir/geldbeutel "$wallet_config" -x "$pincode" --amount 10000 -o "$bill_name" 
     echo "Created bill $bill_name"
+    $bdir/geldbeutel "$wallet_config" -x "$pincode" --force "$bill_name"
+    echo "Forced bill into wallet $bill_name"
   done 
 
 done
