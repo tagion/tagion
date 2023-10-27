1a7f5e3d85aa557d4799b656f8e813bda1668367
lucas rasmussen <lr@decard.io>
2023-10-27 12:58:35 +0200

    bdd: many contracts create invoice

--- bdd/tagion/testbench/e2e/manycontracts.d
+++ bdd/tagion/testbench/e2e/manycontracts.d
@@ -11,9 +11,12 @@ import std.format;
 import std.algorithm;
 import std.range;
 import std.stdio;
+import std.path;
 
 import tagion.tools.wallet.WalletOptions : WalletOptions;
 import tagion.tools.wallet.WalletInterface : WalletInterface;
+import tagion.script.common;
+import tagion.script.TagionCurrency;
 
 enum feature = Feature(
         "send multiple contracts through the network",
@@ -30,12 +33,16 @@ class SendNContractsFromwallet1Towallet2
 {
     WalletInterface[] wallets;
 
+    this(WalletInterface[] wallets) {
+        this.wallets = wallets;
+    }
+
     @Given("i have a network")
     Document network() @trusted
     {
-        auto wallet_switch = WalletInterface.Switch(update: true, send: true);
+        auto wallet_switch = WalletInterface.Switch(update: true, sendkernel: true);
 
-        foreach(w; wallets) {
+        foreach(ref w; wallets) {
             w.operate(wallet_switch, []);
         }
 
@@ -45,7 +52,14 @@ class SendNContractsFromwallet1Towallet2
     @When("i send N many valid contracts from `wallet1` to `wallet2`")
     Document wallet2()
     {
-        return Document();
+        const invoice = wallets[0].secure_wallet.createInvoice("Invoice", 1000.TGN);
+
+        SignedContract s_contract;
+        TagionCurrency fees;
+        auto result = wallets[1].secure_wallet.payment([invoice], s_contract, fees);
+        result.get;
+
+        return result_ok;
     }
 
     @When("all the contracts have been executed")
@@ -65,10 +79,6 @@ class SendNContractsFromwallet1Towallet2
 alias manycontracts = tagion.testbench.e2e.manycontracts;
 
 mixin Main!(_main);
-
-int _main(string[] a) {
-    return 1;
-}
 
 // import std.range;
 // import std.path : setExtension, buildPath;
@@ -169,7 +179,6 @@ int _main(string[] args) {
 import std.getopt;
 import std.file;
 
-version(none)
 int _main(string[] args) {
     const program = args[0];
     string[] wallet_config_files;
@@ -193,8 +202,10 @@ int _main(string[] args) {
         return 0;
     }
 
+    import std.process : environment;
+    const HOME = environment.get("HOME");
     if(wallet_config_files.empty) {
-        wallet_config_files = dirEntries("$HOME/.local/share/tagion/wallets/wallet*.json", SpanMode.shallow).map!(a => a.name).array;
+        wallet_config_files = dirEntries(buildPath(HOME, ".local/share/tagion/wallets/"), "wallet*.json", SpanMode.shallow).map!(a => a.name).array.sort.array;
         if(wallet_config_files.empty) {
             writeln("No wallet configs available");
             return 0;
@@ -203,7 +214,7 @@ int _main(string[] args) {
 
     if(wallet_pins.empty) {
         foreach(i, _; wallet_config_files) {
-            wallet_pins ~= format("%04d", i);
+            wallet_pins ~= format("%04d", i+1);
         }
     }
 
@@ -216,8 +227,10 @@ int _main(string[] args) {
         opts.load(c);
         wallet_options ~= opts;
         auto wallet_interface = WalletInterface(opts);
-        wallet_interface.secure_wallet.login(wallet_pins[i]);
+        check(wallet_interface.load, "Wallet %s could not be loaded".format(i));
+        check(wallet_interface.secure_wallet.login(wallet_pins[i]), "Wallet %s, %s, %s not logged in".format(i, wallet_pins[i], c));
         wallet_interfaces ~= wallet_interface;
+        writefln("Wallet logged in %s", wallet_interface.secure_wallet.isLoggedin);
     }
 
     auto manycontracts_feature = automation!manycontracts;
