746e47caa21b56fd20bceb122b3d5d497f42e5c5
Lucas Rasmussen <lucasnethaj@gmail.com>
2023-10-28 16:12:40 +0200

    ammend

--- flake.nix
+++ flake.nix
@@ -1,36 +1,37 @@
 {
   description = "Tagion is a decentrialized monetary system";
 
+  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
+
   outputs = { self, nixpkgs }: {
 
-    inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
     # packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
 
     # packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
 
 
+    formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixpkgs-fmt;
     defaultPackage.x86_64-linux =
       # Notice the reference to nixpkgs here.
       with import nixpkgs { system = "x86_64-linux"; };
       stdenv.mkDerivation {
 
-        configure = "";
         buildInputs = [
-            autoconf
-            automake
-            autoreconfHook
-            cmake
-            dtools
-            dfmt
-            dub
-            dmd
-            go
-            gnumake
-            git
-            glibcLocales
-            ldc
-            libtool
-            llvmPackages_15.clang-unwrapped
+          autoconf
+          automake
+          autoreconfHook
+          cmake
+          dub
+          dmd
+          dfmt
+          dtools
+          go
+          git
+          gnumake
+          glibcLocales
+          ldc
+          libtool
+          llvmPackages_15.clang-unwrapped
         ];
 
         name = "tagion";
