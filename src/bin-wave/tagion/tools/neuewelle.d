6ecf201548eee916fab46c47a4fb7f8df4e41bd1
imrying <philiprying@gmail.com>
2023-11-09 13:37:04 +0100

    add more timeout

--- src/bin-wave/tagion/tools/neuewelle.d
+++ src/bin-wave/tagion/tools/neuewelle.d
@@ -242,7 +242,7 @@ int _main(string[] args) {
         assert(0, "NetworkMode not supported");
     }
 
-    if (waitforChildren(Ctrl.ALIVE, 15.seconds)) {
+    if (waitforChildren(Ctrl.ALIVE, 30.seconds)) {
         log("alive");
         bool signaled;
         import tagion.utils.pretend_safe_concurrency : receiveTimeout;
