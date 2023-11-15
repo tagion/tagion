6a825eec15060607132e8fc0d82c79109f7b9dcc
imrying <philiprying@gmail.com>
2023-11-15 09:12:16 +0100

    wip(genesis_test): start the network

--- bdd/tagion/testbench/services/genesis_test.d
+++ bdd/tagion/testbench/services/genesis_test.d
@@ -9,7 +9,10 @@ import tagion.logger.LogRecords : LogInfo;
 import tagion.logger.Logger;
 import tagion.hashgraph.Refinement;
 import tagion.utils.pretend_safe_concurrency;
+import tagion.testbench.actor.util;
 import core.time;
+
+enum EPOCH_TIMEOUT = 15;
 
 enum feature = Feature(
             "Boot system with genesis block.",
@@ -25,24 +28,34 @@ alias FeatureContext = Tuple!(
         [])
 class NetworkRunningWithGenesisBlockAndEpochChain {
     bool epoch_on_startup;
+    long start_epoch;
 
     @Given("i have a network booted with a genesis block")
     Document block() {
         submask.subscribe(StdRefinement.epoch_created);
         writeln("waiting for epoch");
-        auto received = receiveTimeout(30.seconds, (LogInfo _, const(Document) __) {});
+        auto received = receiveTimeout(30.seconds, (LogInfo _, const(Document) doc) 
+            {
+                start_epoch = FinishedEpoch(doc).epoch;
+            });
         epoch_on_startup = received;
         check(epoch_on_startup, "No epoch on startup");
 
-        //check that network booted
-        // empty
         return result_ok;
     }
 
     @When("the network continues to run.")
     Document run() {
         check(epoch_on_startup, "No epoch on startup");
-        return Document();
+
+        // run the network for 20 epochs
+        long current_epoch_number;
+        while(current_epoch_number < start_epoch + 20) {
+            auto current_epoch = receiveOnlyTimeout!(LogInfo, const(Document))(EPOCH_TIMEOUT.seconds);
+            current_epoch_number = FinishedEpoch(current_epoch[1]).epoch;
+        }
+        submask.unsubscribe(StdRefinement.epoch_created);
+        return result_ok;
     }
 
     @Then("it should continue adding blocks to the _epochchain")
