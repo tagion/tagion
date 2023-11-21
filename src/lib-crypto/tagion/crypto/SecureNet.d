a0a1314a3a15696b75f8dce7081f211dc5fdf058
Lucas Rasmussen <lucasnethaj@gmail.com>
2023-11-21 16:06:13 +0100

    hmac do no cast

--- src/lib-crypto/tagion/crypto/SecureNet.d
+++ src/lib-crypto/tagion/crypto/SecureNet.d
@@ -34,15 +34,12 @@ class StdHashNet : HashNet {
         return Fingerprint(rawCalcHash(data));
     }
 
-    @trusted
     final immutable(Buffer) HMAC(scope const(ubyte[]) data) const pure {
         import std.digest.hmac : digestHMAC = HMAC;
         import std.digest.sha : SHA256;
-        import std.exception : assumeUnique;
 
-        scope hmac = digestHMAC!SHA256(data);
-        auto result = hmac.finish.dup;
-        return assumeUnique(result);
+        auto hmac = digestHMAC!SHA256(data);
+        return hmac.finish.idup;
     }
 
     Fingerprint calcHash(const(Document) doc) const {
