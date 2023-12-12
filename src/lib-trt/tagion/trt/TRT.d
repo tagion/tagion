535093881ac43c937565c5d7312d20ca6eedccf6
Ivan Bilan <ib@decard.io>
2023-12-12 16:40:00 +0200

    add few more TRT unittests

--- src/lib-trt/tagion/trt/TRT.d
+++ src/lib-trt/tagion/trt/TRT.d
@@ -54,7 +54,8 @@ void createTRTUpdateRecorder(
         auto archive = to_insert.require(bill.owner, TRTArchive(bill.owner, DARTIndex[].init));
 
         if (a_bill.type == Archive.Type.ADD) {
-            archive.indexes ~= DARTIndex(bill_index);
+            if (!archive.indexes.canFind(bill_index))
+                archive.indexes ~= DARTIndex(bill_index);
         }
         else if (a_bill.type == Archive.Type.REMOVE) {
             auto to_remove_index = archive.indexes.countUntil!(idx => idx == bill_index);
@@ -135,8 +136,6 @@ unittest {
 
     auto factory = RecordFactory(net);
 
-    auto empty_recorder = factory.recorder;
-
     auto initial_recorder = factory.recorder;
     initial_recorder.insert(bills, Archive.Type.ADD);
     immutable im_dart_recorder = factory.uniqueRecorder(initial_recorder);
@@ -144,6 +143,7 @@ unittest {
     // Test empty recorder input
     {
         auto trt_recorder = factory.recorder;
+        auto empty_recorder = factory.recorder;
 
         auto empty_dart_recorder = factory.recorder;
         createTRTUpdateRecorder(factory.uniqueRecorder(empty_dart_recorder), empty_recorder, trt_recorder, net);
@@ -154,6 +154,7 @@ unittest {
     // Test recorder without previous read
     {
         auto trt_recorder = factory.recorder;
+        auto empty_recorder = factory.recorder;
 
         createTRTUpdateRecorder(im_dart_recorder, empty_recorder, trt_recorder, net);
 
@@ -174,10 +175,68 @@ unittest {
 
     // Test recorder with some previous read
     {
+        auto trt_recorder = factory.recorder;
+
+        int count_read_bills = 2;
+        int number_of_dummy_indexes = count_read_bills;
+
+        auto read_recorder = factory.recorder;
+        read_recorder.insert(bills[0 .. count_read_bills].map!(b => TRTArchive(b.owner, [
+                    net.dartIndex(b), DARTIndex.init
+                ])), Archive.Type.ADD);
+
+        createTRTUpdateRecorder(im_dart_recorder, read_recorder, trt_recorder, net);
+
+        assert(countTRTRecorderIndexes(trt_recorder) - number_of_dummy_indexes == im_dart_recorder.length,
+            "Number of entries in recorders differs");
+
+        auto dart_archives = im_dart_recorder[]
+            .map!(a => TagionBill(a.filed))
+            .map!(b => TRTArchive(b.owner, [net.dartIndex(b)]));
+
+        auto trt_archives = trt_recorder[]
+            .map!(b => TRTArchive(b.filed));
+
+        foreach (a; dart_archives) {
+            assert(trt_archives.canFind!(trt_arch => trt_arch.indexes.canFind(a.indexes.front)),
+                "Some bills are missing");
+        }
+
+        assert(trt_archives.map!(a => a.indexes.canFind(DARTIndex.init))
+                .sum == number_of_dummy_indexes, "Read indexes are missing");
     }
 
     // Test recorder with duplicating read recorder
     {
+        auto trt_recorder = factory.recorder;
+
+        auto read_recorder = factory.recorder;
+        read_recorder.insert(im_dart_recorder[].map!(a => TagionBill(a.filed))
+                .map!(b => TRTArchive(b.owner, [
+                        net.dartIndex(b), DARTIndex.init
+                    ])), Archive.Type.ADD);
+
+        auto number_of_dummy_indexes = im_dart_recorder.length;
+
+        createTRTUpdateRecorder(im_dart_recorder, read_recorder, trt_recorder, net);
+
+        assert(countTRTRecorderIndexes(trt_recorder) == im_dart_recorder.length + number_of_dummy_indexes,
+            "Number of entries in recorders differs");
+
+        auto dart_archives = im_dart_recorder[]
+            .map!(a => TagionBill(a.filed))
+            .map!(b => TRTArchive(b.owner, [net.dartIndex(b)]));
+
+        auto trt_archives = trt_recorder[]
+            .map!(b => TRTArchive(b.filed));
+
+        foreach (a; dart_archives) {
+            assert(trt_archives.canFind!(trt_arch => trt_arch.indexes.canFind(a.indexes.front)),
+                "Some bills are missing");
+        }
+
+        assert(trt_archives.map!(a => a.indexes.canFind(DARTIndex.init))
+                .sum == number_of_dummy_indexes, "Read indexes are missing");
     }
 
     // Test recorder with duplicating owner field
