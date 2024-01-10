f30c9e3e298e7c0b94e4415e209554f17827f190
Lucas Rasmussen <lucasnethaj@gmail.com>
2024-01-10 16:54:10 +0100

    AddressBook remove unused function

--- src/lib-gossip/tagion/gossip/AddressBook.d
+++ src/lib-gossip/tagion/gossip/AddressBook.d
@@ -19,134 +19,11 @@ import tagion.hibon.HiBONRecord;
 import tagion.logger.Logger : log;
 import tagion.utils.Miscellaneous : cutHex;
 
-// @safe class AddressBookException : TagionException {
-//     this(string msg, string file = __FILE__, size_t line = __LINE__) pure {
-//         super(msg, file, line);
-//     }
-// }
-// 
-// /** check function used in the HiBON package */
-// alias check = Check!(AddressBookException);
-// 
-// enum lockext = "lock";
-
-// /**
-//  * Lock file
-//  * @param filename - file to lock
-//  */
-// void lock(string filename) {
-//     import std.file : fwrite = write;
-// 
-//     immutable file_lock = filename.setExtension(lockext);
-//     file_lock.fwrite(null);
-// }
-
 /** Address book for node p2p communication */
 @safe
 synchronized class AddressBook {
     /** Addresses for node */
     protected shared(string[Pubkey]) addresses;
-
-    // /** used for lock, unlock file */
-    // enum max_count = 3;
-    // /** used for lock, unlock file */
-    // protected int timeout = 300;
-
-    // /** nodes amount */
-    // protected size_t nodes;
-
-    // /**
-    //  * Set number of active nodes
-    //  * @param nodes - number of active nodes
-    //  */
-    // void number_of_active_nodes(const size_t nodes) pure nothrow
-    // in {
-    //     debug log.trace("this.nodes %s set to %s", this.nodes, nodes);
-    //     assert(this.nodes is size_t.init);
-    // }
-    // do {
-    //     this.nodes = nodes;
-    // }
-
-    // protected {
-    //     Random rnd;
-    // }
-    // this() {
-    //     rnd = shared(Random)(unpredictableSeed);
-    // }
-
-    /// /**
-    ///  * Create associative array with public keys and addresses of nodes
-    ///  * @return addresses of nodes
-    ///  */
-    /// immutable(NodeAddress[Pubkey]) _data() @trusted {
-    ///     pragma(msg, "fixme(cbr): AddressBook._data This function should be removed when the addressbook has been implemented");
-    ///     NodeAddress[Pubkey] result;
-    ///     foreach (pkey, addr; addresses) {
-    ///         result[pkey] = addr;
-    ///     }
-    ///     return cast(immutable) result;
-    /// }
-
-    /// /**
-    ///  * Overwrite node addresses associative array
-    ///  * @param addrs - array to overwrite
-    ///  */
-    /// private void overwrite(const(NodeAddress[Pubkey]) addrs) {
-    ///     addresses = null;
-    ///     foreach (pkey, addr; addrs) {
-    ///         addresses[pkey] = addr;
-    ///     }
-    /// }
-
-    // /**
-    //  * Load file if it's exist
-    //  * @param filename - file to load
-    //  * @param do_unlock - flag for unlock file
-    //  */
-    // void load(string filename, bool do_unlock = true) @trusted {
-    //     void local_read() @safe {
-    //         auto dir = filename.fread!AddressDirectory;
-    //         overwrite(dir.addresses);
-    //     }
-
-    //     if (filename.exists) {
-    //         int count_down = max_count;
-    //         while (filename.locked) {
-    //             Thread.sleep(timeout.msecs);
-    //             count_down--;
-    //             check(count_down > 0, format("The bootstrap file is locked. Timeout can't load file %s", filename));
-    //         }
-    //         filename.lock;
-    //         local_read;
-    //         if (do_unlock) {
-    //             filename.unlock;
-    //         }
-    //     }
-    // }
-
-    // /**
-    //  * Save addresses to file
-    //  * @param filename - file to save addresses
-    //  * @param nonelock - flag tolock file for save operation
-    //  */
-    // void save(string filename, bool nonelock = false) @trusted {
-    //     void local_write() {
-    //         AddressDirectory dir;
-    //         dir.addresses = cast(NodeAddress[Pubkey]) addresses;
-    //         filename.fwrite(dir);
-    //     }
-
-    //     int count_down = max_count;
-    //     while (!nonelock && filename.locked) {
-    //         Thread.sleep(timeout.msecs);
-    //         count_down--;
-    //         check(count_down > 0, format("The bootstrap file is locked. Timeout can't save file %s", filename));
-    //     }
-    //     filename.lock;
-    //     local_write;
-    //     filename.unlock;
-    // }
 
     /**
      * Init NodeAddress if public key exist
@@ -211,14 +88,6 @@ synchronized class AddressBook {
         return addresses[pkey];
     }
 
-    // /**
-    //  * Return amount of active nodes in network
-    //  * @return amount of active nodes
-    //  */
-    // size_t numOfActiveNodes() const pure nothrow {
-    //     return addresses.length;
-    // }
-
     /**
      * Return amount of nodes in networt
      * @return amount of nodes
@@ -226,41 +95,6 @@ synchronized class AddressBook {
     size_t numOfNodes() const pure nothrow {
         return addresses.length;
     }
-
-    // /**
-    //  * Check that nodes >= 4 and addresses >= nodes
-    //  * @return true if network ready
-    //  */
-    // bool isReady() const pure nothrow {
-    //     return (nodes >= 4) && (addresses.length >= nodes);
-    // }
-
-    // /**
-    //  * For random generation node pair
-    //  * @return node pair
-    //  */
-    // immutable(NodePair) random() @trusted const pure {
-    //     if (addresses.length) {
-    //         import std.range : dropExactly;
-
-    //         auto _addresses = cast(NodeAddresses) addresses;
-    //         const random_key_index = uniform(0, addresses.length, cast(Random) rnd);
-    //         return _addresses.byKeyValue.dropExactly(random_key_index).front;
-    //     }
-    //     return NodePair.init;
-    // }
-
-    // /**
-    //  * Select active channel by index
-    //  * @param index - index to select active channel
-    //  * @return active channel
-    //  */
-    // const(Pubkey) selectActiveChannel(const size_t index) @trusted const pure {
-    //     import std.range : dropExactly;
-
-    //     auto _addresses = cast(NodeAddresses) addresses;
-    //     return _addresses.byKey.dropExactly(index).front;
-    // }
 }
 
 static shared(AddressBook) addressbook;
@@ -336,9 +170,6 @@ struct NodeAddress {
 unittest {
     NodeAddress nnr;
     nnr.addr_type = MultiAddrProto.ip4;
-    nnr.host = [200, 185, 5, 5];
-    nnr.host = [200, 185, 5, 5];
-    nnr.host = [200, 185, 5, 5];
     nnr.host = [200, 185, 5, 5];
     nnr.port = 80;
     nnr.transport = MultiAddrProto.tcp;
