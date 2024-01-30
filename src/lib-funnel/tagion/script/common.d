6acadb7acfa65bf2d78edefe4012c600de08cdf2
Lucas Rasmussen <lucasnethaj@gmail.com>
2024-01-30 10:13:34 +0100

    fix: verify returns true when inputs is 0

--- src/lib-funnel/tagion/script/common.d
+++ src/lib-funnel/tagion/script/common.d
@@ -90,13 +90,16 @@ struct PayScript {
 
 Signature[] sign(const(SecureNet[]) nets, const(Contract) contract) {
     import std.algorithm : map;
+
     const message = nets[0].calcHash(contract);
     return nets
         .map!(net => net.sign(message))
         .array;
 }
+
 const(SignedContract) sign(const(SecureNet[]) nets, DARTIndex[] inputs, const(Document[]) reads, const(Document) script) {
     import std.algorithm : map, sort;
+
     check(nets.length > 0, "At least one input contract");
     check(nets.length == inputs.length, "Number of signature does not match the number of inputs");
     const net = nets[0];
@@ -115,21 +118,24 @@ const(SignedContract) sign(const(SecureNet[]) nets, DARTIndex[] inputs, const(Do
     result.signs = sign(sorted_inputs.map!((input) => nets[input.index]).array, result.contract);
     return result;
 }
+
 const(SignedContract) sign(
-    const(SecureNet[]) nets, 
-    const(Document[]) inputs, 
-    const(Document[]) reads, 
-    const(Document) script) {
+        const(SecureNet[]) nets,
+        const(Document[]) inputs,
+        const(Document[]) reads,
+        const(Document) script) {
     import std.algorithm : map;
+
     check(nets.length > 0, "At least one input contract");
     const net = nets[0];
     return sign(nets, inputs.map!((input) => cast(DARTIndex) net.dartIndex(input)).array, reads, script);
 }
 
 bool verify(const(SecureNet) net, const(SignedContract*) signed_contract, const(Pubkey[]) owners) nothrow {
-    import std.algorithm : all;
+    import std.algorithm;
+
     try {
-        if (signed_contract.contract.inputs.length == owners.length) {
+        if (!owners.empty && signed_contract.contract.inputs.length == owners.length) {
             const message = net.calcHash(signed_contract.contract);
             return zip(signed_contract.signs, owners)
                 .all!((a) => net.verify(message, a[0], a[1]));
@@ -143,6 +149,7 @@ bool verify(const(SecureNet) net, const(SignedContract*) signed_contract, const(
 
 bool verify(const(SecureNet) net, const(SignedContract*) signed_contract, const(Document[]) inputs) nothrow {
     import std.algorithm : map;
+
     try {
         return verify(net, signed_contract, inputs.map!(doc => doc[StdNames.owner].get!Pubkey).array);
     }
@@ -269,11 +276,11 @@ struct LockedArchives {
     });
 }
 
-version(WITHOUT_PAYMENT) {
-struct HashString {
-    string name;
-    mixin HiBONRecord!(q{this(string name) { this.name = name; }});
-}
-enum HashString snavs_record = HashString("snavs");
-}
+version (WITHOUT_PAYMENT) {
+    struct HashString {
+        string name;
+        mixin HiBONRecord!(q{this(string name) { this.name = name; }});
+    }
 
+    enum HashString snavs_record = HashString("snavs");
+}
