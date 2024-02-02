1c919b0b63a3e285e4453a5745e56b3ed2510297
cbleser <cr@tagion.org>
2024-02-02 13:30:57 +0200

    WIP: tool vergangenheit

--- src/bin-vergangenheit/tagion/tools/vergangenheit/Rebuild.d
+++ src/bin-vergangenheit/tagion/tools/vergangenheit/Rebuild.d
@@ -2,13 +2,20 @@ module tagion.tools.vergangenheit.Rebuild;
 import std.stdio;
 import std.algorithm;
 import std.range;
+import std.format;
 import tagion.basic.Types;
+import tagion.basic.basic;
 import tagion.crypto.SecureInterfaceNet;
 import tagion.crypto.Types : Fingerprint;
 import tagion.dart.DART;
+import tagion.dart.DARTBasic;
 import tagion.hibon.HiBONFile;
+import tagion.hibon.Document;
+import tagion.hibon.HiBONRecord;
 import tagion.replicator.RecorderBlock;
 import tagion.tools.Basic;
+import tagion.script.common;
+import tagion.tools.toolsexception;
 
 struct RebuildOptions {
 }
@@ -20,13 +27,14 @@ struct Rebuild {
         DART dst;
         string[] replicator_files;
     }
+    const(LockedArchives)[] locked_epochs;
     this(const RebuildOptions opt, DART src, DART dst) pure nothrow {
         this.opt = opt;
         this.src = src;
         this.dst = dst;
     }
 
-    static int checkReplicator(const HashNet net, const string file, ref Fingerprint previous) {
+    int checkReplicator(const HashNet net, const string file, ref Fingerprint previous) {
         auto fin = File(file, "r");
         scope (exit) {
             fin.close;
@@ -47,6 +55,33 @@ struct Rebuild {
 
                 }
             }
+            if (block.bullseye.length != net.hashSize) {
+                error("Bullseye is not a valid has in block %s for file %s", item.index, file);
+            }
+            const recorder = src.recorder(block.recorder_doc);
+
+            Epoch epoch;
+            LockedArchives locked_epoch;
+            foreach (archives; recorder[]) {
+                const doc = archives.filed;
+                if (Epoch.isRecord(doc)) {
+                    check(epoch.isinit, 
+                        format("More than one %s in recorder %d in replicator file %s", 
+                Epoch.stringof, item.index, file));
+                    epoch = Epoch(doc);
+                }
+                else if (LockedArchives.isRecord(doc)) {
+                    check(locked_epoch.isinit, 
+                        format("More than one %s in recorder %d in replicator file %s", 
+                LockedArchives.stringof, item.index, file));
+                    locked_epoch = LockedArchives(doc);
+                }
+
+            }
+            const epoch_dartindex=net.dartIndex(epoch);
+            const locked_epoch_dartindex=net.dartIndex(locked_epoch);
+            const not_in_dart=src.checkload([epoch_dartindex, locked_epoch_dartindex]); 
+            locked_epochs ~= locked_epoch;
             previous = Fingerprint(block.fingerprint);
         }
 
@@ -54,10 +89,20 @@ struct Rebuild {
     }
 
     void prepareReplicator(const HashNet hash_net, string[] replicator_files) {
+        locked_epochs = null;
         Fingerprint fingerprint;
-        foreach(file; replicator_files) {
-           checkReplicator(hash_net, file, fingerprint);
+        int result;
+        foreach (file; replicator_files) {
+            verbose("file %s", file);
+            const error_count = checkReplicator(hash_net, file, fingerprint);
+            result += error_count;
         }
+        if (result > 0) {
+            error("Counted %d errors", result);
+        }
+        auto locked_epoch_numbers = locked_epochs.map!(l => cast(long) l.epoch_number).array.sort;
+        writefln("locked_epoch_numbers=%s", locked_epoch_numbers);
+
     }
 
     //void rebuild(const RebuildOptions opt, DART src, DART dst, string[]
