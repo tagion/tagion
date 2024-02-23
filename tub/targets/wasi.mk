78e4b638a6984605e042a6684e6eadcd68b5ad5d
cbleser <cr@tagion.org>
2024-02-23 10:39:48 +0200

    WIP: wasi.mk

--- tub/targets/wasi.mk
+++ tub/targets/wasi.mk
@@ -32,9 +32,25 @@ help-wasi:
 	$(call log.header, $@ :: help)
 	$(call log.kvp, "make wasi", "Prebuild druntime")
 	$(call log.kvp, "make wasi-<tag>", "Execute the make <tag> on the wasi-druntime")
+	$(call log.kvp, "make wasi-run", "Will run the simple test program")
+	$(call log.kvp, "make proper-wasi", "Will remove wasi-druntime for the build")
 	$(call log.close)
+
+.PHONY: help-wasi
+
+help: help-wasi
 
 $(WASI_DRUNTIME_ROOT)/.git: $(TOOLS)/.way
 	$(PRECMD)
-	cd $(TOOLS); git clone --recurse-submodules $(WASI_DRUNTIME_REPO) 
+	cd $(TOOLS); git clone --recurse-submodules $(WASI_DRUNTIME_REPO)
+
+proper-wasi:
+	$(PRECMD)
+	$(call log.header, $@ :: proper)
+	$(RMDIR) $(WASI_DRUNTIME_ROOT)
+
+.PHONY: proper-wasi
+
+proper: proper-wasi
+
 	
