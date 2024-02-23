31bc740bafe250ff5035cf9c1f4a7cec0c6f27de
cbleser <cr@tagion.org>
2024-02-23 10:14:07 +0200

    WIP: wasi.mk

--- tub/targets/wasi.mk
+++ tub/targets/wasi.mk
@@ -10,7 +10,29 @@ test48:
 	echo $(WASI_DRUNTIME_ROOT)
 
 wasi: $(WASI_DRUNTIME_ROOT)/.git  
-	$(MAKE) -C $(WASI_DRUNTIME_ROOT) prebuild run
+	$(MAKE) -C $(WASI_DRUNTIME_ROOT) prebuild 
+
+wasi-%: $(WASI_DRUNTIME_ROOT)/.git 
+	$(MAKE) -C $(WASI_DRUNTIME_ROOT) $*
+
+env-wasi:
+	$(PRECMD)
+	$(call log.header, $@ :: env)
+	$(call log.kvp, WASI_DRUNTIME_ROOT, $(WASI_DRUNTIME_ROOT))
+	$(call log.kvp, WASI_DRUNTIME_REPO, $(WASI_DRUNTIME_REPO))
+	$(call log.close)
+
+.PHONY: env-wasi
+
+env: env-wasi
+		
+
+help-wasi:
+	$(PRECMD)
+	$(call log.header, $@ :: help)
+	$(call log.kvp, "make wasi", "Prebuild druntime")
+	$(call log.kvp, "make wasi-<tag>", "Execute the make <tag> on the wasi-druntime")
+	$(call log.close)
 
 $(WASI_DRUNTIME_ROOT)/.git: $(TOOLS)/.way
 	$(PRECMD)
