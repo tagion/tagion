942555cda2d9a5ee71ec4ada1ebed22958bdcef6
imrying <philiprying@gmail.com>
2024-03-14 17:23:10 +0100

    refactor(refinement): remove duplicate epoch function

--- src/lib-hashgraph/tagion/hashgraph/Refinement.d
+++ src/lib-hashgraph/tagion/hashgraph/Refinement.d
@@ -107,168 +107,143 @@ class StdRefinement : Refinement {
         // log.trace("epack.event_body.payload.empty %s", epack.event_body.payload.empty);
     }
 
-    version (NEW_ORDERING) {
-        static bool order_less(Event a, Event b, const(Event[]) famous_witnesses, const(Round) decided_round) pure {
-            import std.bigint;
-            import std.numeric : gcd;
-            import std.range : back, retro, tee;
+    version (NEW_ORDERING) 
+    static bool order_less(Event a, Event b, const(Event[]) famous_witnesses, const(Round) decided_round) pure {
+        import std.bigint;
+        import std.numeric : gcd;
+        import std.range : back, retro, tee;
 
-            struct PseudoTime {
-                BigInt num; //fraction representing the avg received round
-                BigInt denom;
-                BigInt order; //sum of received orders
-                sdt_t time; //avg received time
+        struct PseudoTime {
+            BigInt num; //fraction representing the avg received round
+            BigInt denom;
+            BigInt order; //sum of received orders
+            sdt_t time; //avg received time
 
-                this(BigInt num, BigInt denom, BigInt order, long time) pure nothrow {
-                    this.num = num;
-                    this.denom = denom;
-                    this.order = order;
-                    this.time = time;
-                }
-
-                this(int num, int denom, int order, sdt_t time, long round_number, ulong witness_count) pure nothrow {
-                    this.num = BigInt(num + denom * round_number);
-                    this.denom = BigInt(denom * witness_count);
-                    this.order = BigInt(order);
-                    this.time = time / witness_count;
-                }
-
-                PseudoTime opBinary(string op)(PseudoTime other) pure const nothrow if (op == "+") {
-                    BigInt d = gcd(denom, other.denom);
-                    return PseudoTime(other.denom / d * num + denom / d * other.num,
-                            denom / d * other.denom,
-                            order + other.order,
-                            time + other.time);
-                }
+            this(BigInt num, BigInt denom, BigInt order, long time) pure nothrow {
+                this.num = num;
+                this.denom = denom;
+                this.order = order;
+                this.time = time;
             }
-            PseudoTime calc_pseudo_time(Event event) pure const {
-                auto receivers = famous_witnesses
-                    .map!(e => e[].until!(e => !e.sees(event))
-                            .array.back);
 
-                return receivers.map!(e => PseudoTime(e.pseudo_time_counter,
-                        (e[].retro.filter!(e => e._witness)
-                        .front._mother.pseudo_time_counter + 1),
-                        e.order,
-                        e.event_body.time,
-                        e.round.number,
-                        decided_round.famous_mask.count))
-                    .array
-                    .reduce!((a, b) => a + b);
+            this(int num, int denom, int order, sdt_t time, long round_number, ulong witness_count) pure nothrow {
+                this.num = BigInt(num + denom * round_number);
+                this.denom = BigInt(denom * witness_count);
+                this.order = BigInt(order);
+                this.time = time / witness_count;
             }
-            PseudoTime at = calc_pseudo_time(a);
-            PseudoTime bt = calc_pseudo_time(b);
 
-            if (at.num * bt.denom == at.denom * bt.num) {
-                if (at.order == bt.order) {
-                    if (a.order == b.order) {
-                        return a.fingerprint < b.fingerprint;
-                    }
-                    return a.order < b.order;
-                }
-                return at.order < bt.order;
+            PseudoTime opBinary(string op)(PseudoTime other) pure const nothrow if (op == "+") {
+                BigInt d = gcd(denom, other.denom);
+                return PseudoTime(other.denom / d * num + denom / d * other.num,
+                        denom / d * other.denom,
+                        order + other.order,
+                        time + other.time);
             }
-            return at.num * bt.denom < at.denom * bt.num;
         }
+        PseudoTime calc_pseudo_time(Event event) pure const {
+            auto receivers = famous_witnesses
+                .map!(e => e[].until!(e => !e.sees(event))
+                        .array.back);
+
+            return receivers.map!(e => PseudoTime(e.pseudo_time_counter,
+                    (e[].retro.filter!(e => e._witness)
+                    .front._mother.pseudo_time_counter + 1),
+                    e.order,
+                    e.event_body.time,
+                    e.round.number,
+                    decided_round.famous_mask.count))
+                .array
+                .reduce!((a, b) => a + b);
+        }
+        PseudoTime at = calc_pseudo_time(a);
+        PseudoTime bt = calc_pseudo_time(b);
+
+        if (at.num * bt.denom == at.denom * bt.num) {
+            if (at.order == bt.order) {
+                if (a.order == b.order) {
+                    return a.fingerprint < b.fingerprint;
+                }
+                return a.order < b.order;
+            }
+            return at.order < bt.order;
+        }
+        return at.num * bt.denom < at.denom * bt.num;
+    }
     
-        void epoch(Event[] event_collection, const(Round) decided_round) {
-            import std.range : tee;
+    version (OLD_ORDERING) //SHOULD NOT BE DELETED SO WE CAN REVERT TO OLD ORDERING IF NEEDED
+    @safe static bool order_less(const Event a, const Event b, const(int) order_count) pure {
+        bool rare_less(Buffer a_print, Buffer b_print) {
+            // rare_order_compare_count++;
+            pragma(msg, "review(cbr): Consensus order changed");
+            return a_print < b_print;
+        }
 
-            pragma(msg, "fixme(bbh): move pseudotime out and add function labels");
+        if (order_count < 0) {
+            return rare_less(a.fingerprint, b.fingerprint);
+        }
+        if (a.order is b.order) {
+            if (a.father && b.father) {
+                return order_less(a.father, b.father, order_count - 1);
+            }
+            if (a.father) {
+                return true;
+            }
+            if (b.father) {
+                return false;
+            }
 
+            if (!a.isFatherLess && !b.isFatherLess) {
+                return order_less(a.mother, b.mother, order_count - 1);
+            }
+
+            return rare_less(a.fingerprint, b.fingerprint);
+        }
+        return a.order < b.order;
+    }
+
+    void epoch(Event[] event_collection, const(Round) decided_round) {
+        import std.range : tee;
+
+        sdt_t[] times;
+        auto events = event_collection
+            .tee!((e) => times ~= e.event_body.time)
+            .filter!((e) => !e.event_body.payload.empty)
+            .array;
+
+
+        version(OLD_ORDERING) {
+            auto sorted_events = events.sort!((a,b) => order_less(a, b, MAX_ORDER_COUNT));
+        }
+        version(NEW_ORDERING) {
             const famous_witnesses = decided_round
                 ._events
                 .filter!(e => e !is null)
                 .filter!(e => decided_round.famous_mask[e.node_id])
                 .array;
-
-            sdt_t[] times;
-            auto events = event_collection
-                .tee!((e) => times ~= e.event_body.time)
-                .filter!((e) => !e.event_body.payload.empty)
-                .array
-                .sort!((a, b) => order_less(a, b, famous_witnesses, decided_round))
-                .release;
-
-            times.sort;
-            const epoch_time = times[times.length / 2];
-
-            version (EPOCH_LOG) {
-                log.trace("%s Epoch round %d event.count=%d witness.count=%d event in epoch=%d time=%s",
-                        hashgraph.name, decided_round.number,
-                        Event.count, Event.Witness.count, events.length, epoch_time);
-            }
-            log.trace("event.count=%d witness.count=%d event in epoch=%d", Event.count, Event.Witness.count, events
-                    .length);
-
-            finishedEpoch(events, epoch_time, decided_round);
-            excludedNodes(hashgraph._excluded_nodes_mask);
+            auto sorted_events = events.sort!((a,b) => order_less(a,b, famous_witnesses, decided_round));
         }
-    }
-
-    version (OLD_ORDERING) //SHOULD NOT BE DELETED SO WE CAN REVERT TO OLD ORDERING IF NEEDED
-    {
-        @safe
-        static bool order_less(const Event a, const Event b, const(int) order_count) pure {
-            bool rare_less(Buffer a_print, Buffer b_print) {
-                // rare_order_compare_count++;
-                pragma(msg, "review(cbr): Consensus order changed");
-                return a_print < b_print;
-            }
-
-            if (order_count < 0) {
-                return rare_less(a.fingerprint, b.fingerprint);
-            }
-            if (a.order is b.order) {
-                if (a.father && b.father) {
-                    return order_less(a.father, b.father, order_count - 1);
-                }
-                if (a.father) {
-                    return true;
-                }
-                if (b.father) {
-                    return false;
-                }
-
-                if (!a.isFatherLess && !b.isFatherLess) {
-                    return order_less(a.mother, b.mother, order_count - 1);
-                }
-
-                return rare_less(a.fingerprint, b.fingerprint);
-            }
-            return a.order < b.order;
-        }
-
-        void epoch(Event[] event_collection, const(Round) decided_round) {
-            import std.algorithm;
-            import std.range;
-
-            sdt_t[] times;
-            auto events = event_collection
-                .tee!((e) => times ~= e.event_body.time)
-                .filter!((e) => !e.event_body.payload.empty)
-                .array
-                .sort!((a, b) => order_less(a, b, MAX_ORDER_COUNT))
-                .release;
-            times.sort;
+        times.sort;
+        
+        version(OLD_ORDERING) {
             const mid = times.length / 2 + (times.length % 1);
             const epoch_time = times[mid];
-
+        }
+        version(NEW_ORDERING) {
+            const epoch_time = times[times.length / 2];
+        }
+        version (EPOCH_LOG) {
             log.trace("%s Epoch round %d event.count=%d witness.count=%d event in epoch=%d time=%s",
                     hashgraph.name, decided_round.number,
                     Event.count, Event.Witness.count, events.length, epoch_time);
-
-            finishedEpoch(events, epoch_time, decided_round);
-            version (BDD) {
-                auto raw_event_payload = FinishedEpoch(event_collection, epoch_time, decided_round.number);
-                log.event(raw_epoch_events, "raw_epoch_successful", raw_event_payload);
-            }
-
-            excludedNodes(hashgraph._excluded_nodes_mask);
         }
 
-    }
+        log.trace("event.count=%d witness.count=%d event in epoch=%d", Event.count, Event.Witness.count, events
+                .length);
 
+        finishedEpoch(events, epoch_time, decided_round);
+        excludedNodes(hashgraph._excluded_nodes_mask);
+    }
 }
 
 @safe
--- src/lib-hashgraph/tagion/hashgraph/RefinementInterface.d
+++ src/lib-hashgraph/tagion/hashgraph/RefinementInterface.d
@@ -29,7 +29,7 @@ interface Refinement {
     void payload(immutable(EventPackage*) epack);
 
     version(NEW_ORDERING)
-    static bool order_less(Event a, Event b, const(Event[]) famous_witnesses, const(Round) decided_round);
+    static bool order_less(Event a, Event b, const(Event[]) famous_witnesses, const(Round) decided_round) pure;
 
     version(OLD_ORDERING)
     static bool order_less(const Event a, const Event b, const(int) order_count) pure;
