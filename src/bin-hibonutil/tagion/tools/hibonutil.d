cf2e1c15e0a5daadeed74b633b65eafdd36ecd8a
imrying <philiprying@gmail.com>
2024-03-25 14:08:10 +0100

    add samplehibon test

--- src/bin-hibonutil/tagion/tools/hibonutil.d
+++ src/bin-hibonutil/tagion/tools/hibonutil.d
@@ -28,6 +28,7 @@ import tools = tagion.tools.toolsexception;
 import tagion.crypto.SecureNet : StdHashNet;
 import tagion.dart.DARTBasic : dartIndex;
 import tagion.basic.Types;
+import tagion.tools.samplehibon : sampleHiBON;
 
 mixin Main!_main;
 /**
@@ -383,65 +384,4 @@ int _main(string[] args) {
     return 0;
 }
 
-Document sampleHiBON(const bool hibon_array = false) {
-    import std.datetime;
-    import std.typecons;
-    import tagion.hibon.BigNumber;
-    import tagion.utils.StdTime;
 
-    auto list = tuple!(
-            "BIGINT",
-            "BOOLEAN",
-            "FLOAT32",
-            "FLOAT64",
-            "INT32",
-            "INT64",
-            "UINT32",
-            "UINT64")(
-            BigNumber("-1234_1234_4678_4678_9876_8438_2345_1111"),
-            true,
-            float(0x1.3ae148p+0),
-            double(0x1.9b5d96fe285c6p+664),
-            int(-42),
-            long(-1234_1234_4678_4678),
-            uint(42),
-            ulong(1234_1234_4678_4678),
-    );
-
-    auto h = new HiBON;
-    foreach (i, value; list) {
-        if (hibon_array) {
-            h[i] = value;
-        }
-        else {
-            h[list.fieldNames[i]] = value;
-        }
-    }
-    immutable(ubyte)[] buf = [1, 2, 3, 4];
-    auto sub_list = tuple!(
-            "BINARY",
-            "STRING",
-            "TIME")(
-            buf,
-            "Text",
-            currentTime
-    );
-    auto sub_hibon = new HiBON;
-    foreach (i, value; sub_list) {
-        if (hibon_array) {
-            sub_hibon[i] = value;
-        }
-        else {
-            sub_hibon[sub_list.fieldNames[i]] = value;
-        }
-    }
-    if (hibon_array) {
-        h[list.length] = sub_hibon;
-    }
-    else {
-        h["sub_hibon"] = sub_hibon;
-    }
-
-    return Document(h.serialize);
-
-}
