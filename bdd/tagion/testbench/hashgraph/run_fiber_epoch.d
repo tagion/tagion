76abcceabc788f22c6b5c08e6e570698c03747ca
imrying <philiprying@gmail.com>
2024-03-27 16:11:06 +0100

    add todo hashgraph fiber

--- bdd/tagion/testbench/hashgraph/run_fiber_epoch.d
+++ bdd/tagion/testbench/hashgraph/run_fiber_epoch.d
@@ -9,6 +9,14 @@ import std.file : mkdirRecurse, rmdirRecurse, exists;
 import std.path : buildPath;
 import std.stdio;
 import tagion.testbench.hashgraph;
+import tagion.testbench.hashgraph.hashgraph_test_network;
+import std.range;
+import std.algorithm;
+import tagion.crypto.Types : Pubkey;
+import std.datetime.systime : SysTime;
+import tagion.hashgraph.Event;
+import std.format;
+import tagion.monitor.Monitor;
 
 enum feature = Feature(
             "Check hashgraph stability when runninng many epochs",
@@ -40,20 +48,43 @@ int _main(string[] args) {
 @safe @Scenario("Run passive fast hashgraph",
         [])
 class RunPassiveFastHashgraph {
+    string[] node_names;
+    TestNetwork network;
+    enum MAX_CALLS = 1000;
+    enum NUMBER_OF_NODES = 5;
+
+    this() {
+        this.node_names = NUMBER_OF_NODES.iota.map!(i => format("Node_%s")).array;
+
+        network = new TestNetwork(node_names);
+        network.networks.byValue.each!((ref _net) => _net._hashgraph.scrap_depth = 10);
+        network.random.seed(123456789);
+        writeln(network.random);
+        network.global_time = SysTime.fromUnixTime(1_614_355_286);
+    }
 
     @Given("i have a running hashgraph")
-    Document hashgraph() {
-        return Document();
+    Document hashgraph() @trusted {
+        uint i = 0;
+        while (i < MAX_CALLS) {
+            const channel_number = network.random.value(0, network.channels.length);
+            network.current = Pubkey(network.channels[channel_number]);
+            auto current = network.networks[network.current];
+            Event.callbacks = new FileMonitorCallBacks(format("%(%02x%)_graph.hibon", current), NUMBER_OF_NODES, network.channels);
+            (() @trusted { current.call; })();
+            i++;
+        }
+        return result_ok;
     }
 
     @When("the nodes creates epochs")
     Document epochs() {
-        return Document();
+        return result_ok;
     }
 
     @Then("the epochs should be the same")
     Document same() {
-        return Document();
+        return result_ok;
     }
 
 }
--- src/lib-monitor/tagion/monitor/Monitor.d
+++ src/lib-monitor/tagion/monitor/Monitor.d
@@ -63,7 +63,7 @@ class LogMonitorCallBacks : BaseMonitorCallbacks {
 class FileMonitorCallBacks : BaseMonitorCallbacks {
     File out_file;
     size_t[Pubkey] node_id_relocation;
-    this(string file_name, uint nodes, Pubkey[] node_keys) {
+    this(string file_name, uint nodes, const(Pubkey[]) node_keys) {
         out_file = File(file_name, "w");
         out_file.rawWrite(NodeAmount(nodes).toDoc.serialize);
 
