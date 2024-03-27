22423482c886798a221a7a2c401af6cf187452ec
imrying <philiprying@gmail.com>
2024-03-27 15:47:03 +0100

    create skeleton for fiber test

--- bdd/tagion/testbench/hashgraph/run_fiber_epoch.d
+++ bdd/tagion/testbench/hashgraph/run_fiber_epoch.d
@@ -4,6 +4,11 @@ import tagion.behaviour;
 import tagion.hibon.Document;
 import std.typecons : Tuple;
 import tagion.testbench.tools.Environment;
+import tagion.tools.Basic;
+import std.file : mkdirRecurse, rmdirRecurse, exists;
+import std.path : buildPath;
+import std.stdio;
+import tagion.testbench.hashgraph;
 
 enum feature = Feature(
             "Check hashgraph stability when runninng many epochs",
@@ -13,6 +18,24 @@ alias FeatureContext = Tuple!(
         RunPassiveFastHashgraph, "RunPassiveFastHashgraph",
         FeatureGroup*, "result"
 );
+
+
+mixin Main!(_main);
+int _main(string[] args) {
+    auto module_path = env.bdd_log.buildPath(__MODULE__);
+
+    if (module_path.exists) {
+        rmdirRecurse(module_path);
+    }
+    mkdirRecurse(module_path);
+
+    writeln("WE ARE RUNNING SOMETHING");
+
+    auto hashgraph_fiber_feature = automation!(run_fiber_epoch);
+    hashgraph_fiber_feature.run;
+
+    return 0;
+}
 
 @safe @Scenario("Run passive fast hashgraph",
         [])
