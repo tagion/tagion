d75134d41718fca6f6185c978fd0f863d11a7235
alabenskiy <al@decard.io>
2024-04-04 16:58:46 +0300

    wip: remove slash at logPath

--- src/lib-mobile/tagion/mobile/mobilelog.d
+++ src/lib-mobile/tagion/mobile/mobilelog.d
@@ -6,9 +6,8 @@ version(WRITE_LOGS)
 @safe void write_log(const(string) message) pure nothrow {
     if (!__ctfe) { 
         debug {
-            string logPath = "/data/user/0/io.decard.tagion_wallet_api_example/app_flutter/";
+            string logPath = "/data/user/0/io.decard.tagion_wallet_api_example/app_flutter";
             if (logPath.exists) {
-                assert(0);
                 string logFileName = "logfile.txt";
                 string logFile = buildPath(logPath, logFileName);
                 logFile.append(message);
