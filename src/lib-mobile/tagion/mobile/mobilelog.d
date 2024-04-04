141510455fa1a780da264c8e4fa0ad276d6e0601
alabenskiy <al@decard.io>
2024-04-04 17:43:23 +0300

    Revert "wip: try to simplify a log file path."
    This reverts commit 6f100aebec8b0ea4d9b4630fd6376dca74399b7e.

--- src/lib-mobile/tagion/mobile/mobilelog.d
+++ src/lib-mobile/tagion/mobile/mobilelog.d
@@ -6,9 +6,11 @@ version(WRITE_LOGS)
 @safe void write_log(const(string) message) pure nothrow {
     if (!__ctfe) { 
         debug {
-            string logPath = "/data/user/0/io.decard.tagion_wallet_api_example/app_flutter/logfile.txt";
+            string logPath = "/data/user/0/io.decard.tagion_wallet_api_example/app_flutter";
             if (logPath.exists) {
-                logPath.append(message);
+                string logFileName = "logfile.txt";
+                string logFile = buildPath(logPath, logFileName);
+                logFile.append(message);
             }
         }
     }
