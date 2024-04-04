2231af6a73e3582ad0467ec88dd44a10f46009a7
imrying <philiprying@gmail.com>
2024-04-04 18:06:20 +0200

    wip(log) all them logs

--- src/lib-mobile/tagion/mobile/mobilelog.d
+++ src/lib-mobile/tagion/mobile/mobilelog.d
@@ -9,7 +9,9 @@ version(WRITE_LOGS)
 @safe void write_log(const(string) message) pure nothrow {
     if (!__ctfe) { 
         debug {
-            import std.stdio;
+            import core.stdc.stdio;
+            printf("%.*s", cast(int) message.length, message.ptr);
+            fprintf(stderr, "%.*s", cast(int) message.length, message.ptr);
             if (log_file !is string.init && log_file.exists) {
                 log_file.append(message);
             }
