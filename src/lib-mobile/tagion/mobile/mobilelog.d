6f100aebec8b0ea4d9b4630fd6376dca74399b7e
alabenskiy <al@decard.io>
2024-04-04 17:13:24 +0300

    wip: try to simplify a log file path.

--- src/lib-mobile/tagion/mobile/mobilelog.d
+++ src/lib-mobile/tagion/mobile/mobilelog.d
@@ -6,11 +6,9 @@ version(WRITE_LOGS)
 @safe void write_log(const(string) message) pure nothrow {
     if (!__ctfe) { 
         debug {
-            string logPath = "/data/user/0/io.decard.tagion_wallet_api_example/app_flutter";
+            string logPath = "/data/user/0/io.decard.tagion_wallet_api_example/app_flutter/logfile.txt";
             if (logPath.exists) {
-                string logFileName = "logfile.txt";
-                string logFile = buildPath(logPath, logFileName);
-                logFile.append(message);
+                logPath.append(message);
             }
         }
     }
