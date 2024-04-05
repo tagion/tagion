99e9b47a00ab8c552a400d58890b76dd9579a83b
imrying <philiprying@gmail.com>
2024-04-05 09:12:33 +0200

    wip(log) disable logging for review

--- src/lib-mobile/tagion/mobile/mobilelog.d
+++ src/lib-mobile/tagion/mobile/mobilelog.d
@@ -4,17 +4,17 @@ import std.path;
 
 version(WRITE_LOGS) {
 static string log_file;
-}
-version(WRITE_LOGS) 
+
 @safe void write_log(const(string) message) pure nothrow {
     if (!__ctfe) { 
         debug {
             import core.stdc.stdio;
-            printf("%.*s", cast(int) message.length, message.ptr);
-            fprintf(stderr, "%.*s", cast(int) message.length, message.ptr);
+            // printf("%.*s", cast(int) message.length, message.ptr);
+            // fprintf(stderr, "%.*s", cast(int) message.length, message.ptr);
             if (log_file !is string.init && log_file.exists) {
                 log_file.append(message);
             }
         }
     }
+}
 }
