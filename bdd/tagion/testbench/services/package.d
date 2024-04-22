c9cf71d8e56ad48791641b255e4343654b1f873c
Lucas Rasmussen <lucasnethaj@gmail.com>
2024-04-22 15:17:08 +0200

    bdd: update service package.d

--- bdd/tagion/testbench/services/package.d
+++ bdd/tagion/testbench/services/package.d
@@ -9,7 +9,6 @@ public import genesis_test = tagion.testbench.services.genesis_test;
 public import hirpc_verifier = tagion.testbench.services.hirpc_verifier;
 public import inputvalidator = tagion.testbench.services.inputvalidator;
 public import malformed_contract = tagion.testbench.services.malformed_contract;
-public import node_interface = tagion.testbench.services.node_interface;
 public import sendcontract = tagion.testbench.services.sendcontract;
 public import spam_double_spend = tagion.testbench.services.spam_double_spend;
 public import subscription_test = tagion.testbench.services.subscription_test;
