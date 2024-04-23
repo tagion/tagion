c5c5a5e8c80c85025c3dcf65e8606777094871aa
Lucas Rasmussen <lucasnethaj@gmail.com>
2024-04-23 14:44:27 +0200

    AccountDetails: remove unneded array alloc in billHashes

--- src/lib-wallet/tagion/wallet/AccountDetails.d
+++ src/lib-wallet/tagion/wallet/AccountDetails.d
@@ -36,7 +36,7 @@ struct AccountDetails {
     void remove_bill_by_hash(const(DARTIndex) billHash) {
         import std.algorithm : remove, countUntil;
 
-        const billsHashes = bills.map!(b => cast(Buffer) hash_net.calcHash(b.toDoc.serialize)).array;
+        auto billsHashes = bills.map!(b => cast(Buffer) hash_net.calcHash(b.toDoc.serialize));
         const index = billsHashes.countUntil(billHash);
         if (index >= 0) {
             used_bills ~= bills[index];
@@ -52,7 +52,7 @@ struct AccountDetails {
     int check_contract_payment(const(DARTIndex)[] inputs, const(Document[]) outputs) const {
         import std.algorithm : countUntil;
 
-        auto billsHashes = bills.map!(b => cast(Buffer) hash_net.calcHash(b.toDoc.serialize)).array;
+        auto billsHashes = bills.map!(b => cast(Buffer) hash_net.calcHash(b.toDoc.serialize));
 
         // Look for input matches. Return 0 from func if found.
         foreach (inputHash; inputs) {
