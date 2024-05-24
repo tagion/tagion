802e43603ad8be63e90cb576a442186af17760d8
imrying <philiprying@gmail.com>
2024-05-24 10:02:10 +0200

    wip(hirpc utility)

---
+++ src/bin-hirpc/tagion/tools/hirpc.d
@@ -0,0 +1,14 @@
+/// HiRPC utility for generating HiRPC requests
+module tagion.tools.hirpc;
+
+
+import tagion.tools.Basic;
+import std.stdio;
+mixin Main!_main;
+
+
+int _main(string[] args) {
+    writefln("hello world!");
+
+    return 0;
+}
