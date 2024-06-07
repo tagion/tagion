6aea7b229eb8d29f52433e69879d089c89d6ec7f
Lucas Rasmussen <lucasnethaj@gmail.com>
2024-06-07 09:50:14 +0200

    add hirpc to capi

---
+++ src/lib-api/tagion/api/hirpc.d
@@ -0,0 +1,34 @@
+/// C-API for creating hirpcs
+module tagion.api.hirpc;
+
+import tagion.api.errors;
+
+import tagion.communication.HiRPC;
+import tagion.hibon.Document;
+
+extern(C):
+
+int tagion_hirpc_create_sender(
+        const char* method,
+        const size_t method_len,
+        ubyte* param,
+        const size_t param_len,
+        ubyte** out_doc,
+        size_t* out_doc_len) {
+    try {
+        const doc = Document(cast(immutable)param[0 .. param_len]);
+        string method_name = cast(immutable)method[0 .. method_len];
+
+        ubyte[] result_sender = cast(ubyte[])HiRPC(null).action(method_name, doc).serialize;
+        *out_doc = &result_sender[0];
+        *out_doc_len = result_sender.length;
+        return ErrorCode.none;
+    }
+    catch(Exception e) {
+        last_error = e;
+        return ErrorCode.exception;
+    }
+}
+
+version(none)
+int tagion_hirpc_create_sender_signed(const char*, );
