a9934c08044612560f998fb2c29abfaf06384052
Lucas Rasmussen <lucasnethaj@gmail.com>
2024-06-17 14:19:12 +0200

    capi: dart test add finalizer and HiBON class

--- src/lib-api/tests/dart/tagion.dart
+++ src/lib-api/tests/dart/tagion.dart
@@ -12,53 +12,38 @@ final class HiBONT extends Struct {
 typedef tagion_hibon_create_native = NativeFunction<Uint64 Function(Pointer<HiBONT>)>;
 typedef TagionHibonCreate = int Function(Pointer<HiBONT>);
 
+typedef tagion_hibon_free_native = NativeFunction<Uint64 Function(Pointer<HiBONT>)>;
+typedef TagionHibonFree = int Function(Pointer<HiBONT>);
+
 /// Dart types definitions for calling the C's foreign functions
 typedef DruntimeCallType = int Function();
 /// calling the function that returned d-runtime status
 typedef StatusCallNative = Int64 Function();
 
+// LibProvider
+final String _libPath = '../../../../build/x86_64-linux/lib/libtauonapi.so';
+final DynamicLibrary _dyLib = DynamicLibrary.open(_libPath);
+final TagionHibonCreate _hibonCreate = _dyLib.lookup<tagion_hibon_create_native>("tagion_hibon_create").asFunction();
+final TagionHibonCreate _hibonFree = _dyLib.lookup<tagion_hibon_free_native>("tagion_hibon_free").asFunction();
+final DruntimeCallType rtInitNative = _dyLib.lookup<NativeFunction<StatusCallNative>>('start_rt').asFunction();
+final DruntimeCallType rtStopNative = _dyLib.lookup<NativeFunction<StatusCallNative>>('stop_rt').asFunction();
 
-class LibProvider {
-  static final String _libPath = '../../../../build/';
-  static final DynamicLibrary dyLib = dyLibOpen('libtauonapi', path: _libPath);
+final class HiBON {
+    late Pointer<HiBONT> _data;
 
-  static final LibProvider _provider = LibProvider._internal();
-
-  factory LibProvider() {
-    return _provider;
-  }
-
-  LibProvider._internal();
-
-  /// Function for opening dynamic library
-  static DynamicLibrary dyLibOpen(String name, {String path = ''}) {
-    var fullPath = _platformPath(name, libpath: path);
-    if (Platform.isIOS) {
-      return DynamicLibrary.process();
-    } else {
-      return DynamicLibrary.open(fullPath);
+    HIBON() {
+          _data = calloc<HiBONT>();
+          _hibonCreate(_data);
     }
-  }
 
-  /// Function returning path to library object
-  static String _platformPath(String name, {String libpath = ''}) {
-    if (Platform.isLinux) {
-      return path.join(libpath, "x86_64-linux",  "lib", name + '.so');
-    }
-    throw Exception('Platform is not implemented');
-  }
-
-  final TagionHibonCreate hibonCreate = dyLib.lookup<tagion_hibon_create_native>("tagion_hibon_create").asFunction();
-  final DruntimeCallType rtInitNative = dyLib.lookup<NativeFunction<StatusCallNative>>('start_rt').asFunction();
-  final DruntimeCallType rtStopNative = dyLib.lookup<NativeFunction<StatusCallNative>>('stop_rt').asFunction();
+    // Destructor
+    static final Finalizer<HiBON> _finalizer = Finalizer((hibon) => _hibonFree(hibon._data));
 }
 
 main(){
-  LibProvider libProvider = LibProvider();
-  print(libProvider.rtInitNative());
+  print(rtInitNative());
 
-  final Pointer<HiBONT> hibon = calloc<HiBONT>();
-  libProvider.hibonCreate(hibon);
+  HiBON myhibon = HiBON();
 
-  libProvider.rtStopNative();
+  rtStopNative();
 }
