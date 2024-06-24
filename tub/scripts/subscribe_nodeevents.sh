99d2605860874b257d2f917125e2ab0528614754
lucasnethaj <47645665+lucasnethaj@users.noreply.github.com>
2024-06-24 09:35:09 +0200

    Update tub/scripts/subscribe_nodeevents.sh
    Co-authored-by: Philip Rying <36996706+imrying@users.noreply.github.com>

--- tub/scripts/subscribe_nodeevents.sh
+++ tub/scripts/subscribe_nodeevents.sh
@@ -62,4 +62,22 @@
 --address abstract://Mode1_60_SUBSCRIPTION_NEUEWELLE  \
 --address abstract://Mode1_61_SUBSCRIPTION_NEUEWELLE  \
 --address abstract://Mode1_62_SUBSCRIPTION_NEUEWELLE  \
---address abstract://Mode1_63_SUBSCRIPTION_NEUEWELLE
+#!/usr/bin/env bash
+
+if [ -z "$1" ]; then
+    echo "Usage: $0 <num_addresses>"
+    exit 1
+fi
+
+num_addresses=$1
+
+subscribe_command="./bin/subscriber --tag=node_action -o nodeevents.hibon"
+
+for ((i=0; i<$num_addresses; i++))
+do
+    address="abstract://Mode1_${i}_SUBSCRIPTION_NEUEWELLE"
+    subscribe_command="$subscribe_command --address $address"
+done
+
+eval $subscribe_command
+
