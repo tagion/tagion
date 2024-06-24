e3348f6e65a2a56c1ff90ab69d6ed5fc3576aa64
Lucas Rasmussen <lucasnethaj@gmail.com>
2024-06-24 15:54:30 +0200

    tvmutil: basic call tvm

--- src/lib-services/tagion/services/options.d
+++ src/lib-services/tagion/services/options.d
@@ -51,45 +51,7 @@ struct WaveOptions {
     mixin JSONCommon;
 }
 
-@safe
-struct TaskNames {
-    public import tagion.utils.JSONCommon;
-
-    string program = "tagion";
-    string supervisor = "supervisor";
-    string inputvalidator = "inputvalidator";
-    string dart = "dart";
-    string hirpc_verifier = "hirpc_verifier";
-    string collector = "collector";
-    string transcript = "transcript";
-    string tvm = "tvm";
-    string epoch_creator = "epoch_creator";
-    string replicator = "replicator";
-    string dart_interface = "dartinterface";
-    string trt = "trt";
-    string node_interface = "node_interface";
-
-    mixin JSONCommon;
-
-    /// Set a prefix for the default options
-    this(const string prefix) pure {
-        setPrefix(prefix);
-    }
-
-    /**
-        Inserts a prefix for all the task_names
-        This function is used in mode 0.
-    */
-    void setPrefix(const string prefix) pure nothrow {
-        alias This = typeof(this);
-        alias FieldsNames = FieldNameTuple!This;
-        static foreach (i, T; Fields!This) {
-            static if (is(T == string)) {
-                this.tupleof[i] = prefix ~ this.tupleof[i];
-            }
-        }
-    }
-}
+public import tagion.services.tasknames : TaskNames;
 
 public import tagion.logger.LoggerOptions : LoggerOptions;
 public import tagion.services.DART : DARTOptions;
