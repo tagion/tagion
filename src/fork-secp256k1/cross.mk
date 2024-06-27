57d33a3bdca1fc8aef08b1527b893729fba8e110
Lucas Rasmussen <lucasnethaj@gmail.com>
2024-06-27 14:16:46 +0200

    mk: simulator

--- src/fork-secp256k1/cross.mk
+++ src/fork-secp256k1/cross.mk
@@ -1,9 +1,5 @@
 ifdef CROSS_ENABLED
 
- # To keep it as simple as possible what we call TARGET is what autotools call HOST
-CONFIGUREFLAGS_SECP256K1 += --host=$(TRIPLET)
-# CONFIGUREFLAGS_SECP256K1 += --with-sysroot=$(CROSS_SYSROOT)
-
 ifeq ($(findstring ios,$(CROSS_OS)),ios)
 include ${call dir.resolve, cross.ios.mk}
 endif
