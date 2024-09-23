c3905e3652275a3c656d09b6c10e179f4ce4673f
cbleser <cr@tagion.org>
2024-09-23 14:20:33 +0300

    DONE: Error category check function added

--- src/lib-erros/tagion/errors/categories.d
+++ src/lib-erros/tagion/errors/categories.d
@@ -1,5 +1,6 @@
 module tagion.errors.categories;
 
+@safe:
 enum ERRORS {
     HIBON = 10_000, /// HiBON format errors 
     HASHGRAPH = 11_000, /// Hashgraph consensus errors
@@ -12,4 +13,55 @@ enum ERRORS {
     TVM = 18_000, /// Tagion virtual machine errors
 }
 
+
+void fwrite(Errors)(string filename) if (is(Errors == enum)) {
+    import std.json;
+    import std.traits;
+    import std.conv : to;
+    import std.file;
+    JSONValue json;
+    static foreach(E; EnumMembers!Errors) {
+        json[E.stringof] = E.to!int;
+    }
+    filename.write(json.toPrettyString);
+}
+
+void check_errors(Errors)(string filename) nothrow if (is(Errors == enum)) {
+    import std.json;
+    import std.traits;
+    import std.conv : to;
+    import std.file;
+    import std.exception;
+    import std.algorithm;
+    import tagion.basic.basic : NameOf;
+    import tagion.basic.Debug : __format;
+    import io=std.stdio;
+    void do_stuff() @safe {
+        const text=filename.readText;
+        auto json=parseJSON(text);
+        static foreach(E; EnumMembers!Errors) {{
+            const error_no=json[E.stringof].integer;
+            assert(E.to!int == error_no, 
+            __format("Error code %s has changes from %d to %d", E.stringof, E.to!int, error_no));
+        }}
+        string[] keys;
+        (() @trusted { 
+        foreach(string key, j; json) {
+            keys~=key;
+        }})();
+        auto error_names=[EnumMembers!Errors].map!(e => e.to!string);
+        auto undefined_errors = keys.filter!(key => !error_names.canFind(key));
+        assert(undefined_errors.empty, 
+        __format("Errors declared in %s file, but is not defined in %s (Not defined = %s)", 
+        filename, Errors.stringof, undefined_errors));
+    }
+    (() @trusted => assumeWontThrow(do_stuff))();
+}
+unittest {
+    import tagion.basic.testbasic;
+    import std.stdio;
+    const category_file=unitfile("categories.json");
+    version(UPDATE_ERROR_CATEGORIES) category_file.fwrite!ERRORS;
+    category_file.check_errors!ERRORS;
+}
 
