5f80ebb43d7a4bab6d9c248072e5ca5ba9959e6e
cbleser <cr@tagion.org>
2024-11-20 12:51:56 +0200

    Done: collider.check_reports function has been moved to collider.logger and the name has chaged to printReport

--- src/bin-collider/tagion/tools/collider/collider.d
+++ src/bin-collider/tagion/tools/collider/collider.d
@@ -185,134 +185,7 @@ void generate_packages(const(ModuleInfo[]) list_of_modules) {
     }
 }
 
-int check_reports(string[] paths) {
-
-    bool show(const TestCode test_code) nothrow {
-        return verbose_switch || test_code == TestCode.error || test_code == TestCode.started;
-    }
-
-    void show_report(Args...)(const TestCode test_code, string fmt, Args args) {
-        static if (Args.length is 0) {
-            const text = fmt;
-        }
-        else {
-            const text = format(fmt, args);
-        }
-        writefln("%s%s%s", testColor(test_code), text, RESET);
-    }
-
-    void report(Args...)(const TestCode test_code, string fmt, Args args) {
-        if (show(test_code)) {
-            show_report(test_code, fmt, args);
-        }
-    }
-
-    struct TraceCount {
-        uint passed;
-        uint errors;
-        uint started;
-        uint total;
-        void update(const TestCode test_code) nothrow pure {
-            final switch (test_code) {
-            case TestCode.none:
-                break;
-            case TestCode.passed:
-                passed++;
-                break;
-            case TestCode.error:
-                errors++;
-                break;
-            case TestCode.started:
-                started++;
-
-            }
-            total++;
-        }
-
-        TestCode testCode() nothrow pure const {
-            if (passed == total) {
-                return TestCode.passed;
-            }
-            if (errors > 0) {
-                return TestCode.error;
-            }
-            if (started > 0) {
-                return TestCode.started;
-            }
-            return TestCode.none;
-        }
-
-        int result() nothrow pure const {
-            final switch (testCode) {
-            case TestCode.none:
-                return 1;
-            case TestCode.error:
-                return cast(int) errors;
-            case TestCode.started:
-                return -cast(int)(started);
-            case TestCode.passed:
-                return 0;
-            }
-            assert(0);
-        }
-
-        void report(string text) {
-            const test_code = testCode;
-            if (test_code == TestCode.passed) {
-                show_report(test_code, "%d test passed BDD-tests", total);
-            }
-            else {
-                writef("%s%s%s: ", BLUE, text, RESET);
-                show_report(test_code, " passed %2$s/%1$s, failed %3$s/%1$s, started %4$s/%1$s",
-                        total, passed, errors, started);
-            }
-        }
-
-    }
-
-    TraceCount feature_count;
-    TraceCount scenario_count;
-    foreach (path; paths) {
-        foreach (string report_file; dirEntries(path, "*.hibon", SpanMode.breadth)
-                .filter!(f => f.isFile)) {
-            try {
-                const doc = report_file.fread;
-                if (doc.isRecord!FeatureGroup) {
-                    const feature_group = FeatureGroup(doc);
-                    const feature_test_code = testCode(feature_group);
-                    feature_count.update(feature_test_code);
-                    if (show(feature_test_code)) {
-                        writefln("Trace file %s", report_file);
-                    }
-
-                    report(feature_test_code, feature_group.info.property.description);
-                    const show_scenario = feature_test_code == TestCode.error
-                        || feature_test_code == TestCode.started;
-                    foreach (scenario_group; feature_group.scenarios) {
-                        const scenario_test_code = testCode(scenario_group);
-                        scenario_count.update(scenario_test_code);
-                        if (show_scenario) {
-                            report(scenario_test_code, "\t%s", scenario_group.info.property
-                                    .description);
-                            foreach (err; getBDDErrors(scenario_group)) {
-                                report(scenario_test_code, "\t\t%s", err.msg);
-                            }
-                        }
-                    }
-                }
-            }
-            catch (Exception e) {
-                error("Error: %s in handling report %s", e.msg, report_file);
-            }
-        }
-    }
-
-    feature_count.report("Features ");
-    if (feature_count.testCode !is TestCode.passed) {
-        scenario_count.report("Scenarios");
-    }
-    return feature_count.result;
-}
+import tagion.tools.collider.logger;
 
 SubTools sub_tools;
 static this() {
@@ -442,7 +315,7 @@ int main(string[] args) {
 
         check_reports_switch = Check_reports_switch || check_reports_switch;
         if (check_reports_switch) {
-            const ret = check_reports(args[1 .. $]);
+            const ret = stdout.printReport(args[1 .. $]);
             if (ret) {
                 writeln("Test result failed!");
             }
