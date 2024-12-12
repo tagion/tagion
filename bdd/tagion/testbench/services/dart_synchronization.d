3983a77b2ddd56e6e869467af4d183e0b50800e1
Anton Labenskyi <al@decard.io>
2024-12-12 18:10:18 +0200

    Add initial impementation to functions.

--- bdd/tagion/testbench/services/dart_synchronization.d
+++ bdd/tagion/testbench/services/dart_synchronization.d
@@ -5,13 +5,19 @@ import tagion.hibon.Document;
 import std.typecons : Tuple;
 import tagion.tools.Basic;
 import tagion.testbench.tools.Environment;
+import tagion.services.DART : DARTOptions;
+import std.file;
+import tagion.crypto.SecureNet;
+import tagion.dart.DART;
+import std.exception;
+import tagion.crypto.Types : Fingerprint;
 
 enum feature = Feature(
             "is a service that synchronizes the DART database with another one.",
             [
-            "It should be used on node start up to ensure that local database is up-to-date.",
-            "In this test scenario we require that the remote database is static (not updated)."
-            ]);
+        "It should be used on node start up to ensure that local database is up-to-date.",
+        "In this test scenario we require that the remote database is static (not updated)."
+]);
 
 alias FeatureContext = Tuple!(
         IsToConnectToRemoteDatabaseWhichIsUptodateAndReadItsBullseye, "IsToConnectToRemoteDatabaseWhichIsUptodateAndReadItsBullseye",
@@ -23,24 +29,72 @@ alias FeatureContext = Tuple!(
         [])
 class IsToConnectToRemoteDatabaseWhichIsUptodateAndReadItsBullseye {
 
+    DART local_db;
+    DART remote_db;
+    Fingerprint remote_b;
+
     @Given("we have a local database.")
     Document localDatabase() {
-        return Document();
+        auto net = new StdSecureNet;
+        local_db = new DART(net, "local_db.drt");
+        return result_ok;
     }
 
     @Given("we have a remote node with a database.")
     Document aDatabase() {
-        return Document();
+        // Note: Task is to connect to a remote db, but temporary
+        // for a test purpose a local filled database will be created.
+        import std.random;
+        import std.datetime.stopwatch;
+        import tagion.hibon.HiBONRecord;
+        import std.format;
+        import tagion.dart.DARTFakeNet : DARTFakeNet;
+        import tagion.utils.Term;
+
+        const number_of_archives = 10000;
+        const bundle_size = 1000;
+        auto net = new StdSecureNet;
+        remote_db = new DART(net, "remote_db.drt");
+
+        static struct TestDoc {
+            string text;
+            mixin HiBONRecord;
+        }
+
+        static const(Document) test_doc(const ulong x) {
+            TestDoc _test_doc;
+            _test_doc.text = format("Test document %d", x);
+            return _test_doc.toDoc;
+        }
+
+        size_t count;
+        auto rnd = Random(unpredictableSeed);
+
+        long prev_dart_time;
+        foreach (no; 0 .. (number_of_archives / bundle_size) + 1) {
+            count += bundle_size;
+            const N = (number_of_archives < count) ? number_of_archives % bundle_size : bundle_size;
+            auto recorder = remote_db.recorder;
+            foreach (i; 0 .. N) {
+                const random_doc_no = uniform(ulong.min, ulong.max, rnd);
+                recorder.add(test_doc(random_doc_no));
+            }
+            remote_db.modify(recorder);
+        }
+        return result_ok;
     }
 
     @When("we read the bullseye from the remote database.")
     Document remoteDatabase() {
-        return Document();
+        remote_b = Fingerprint(remote_db.bullseye);
+        return result_ok;
     }
 
     @Then("we check that the remote database is different from the local one.")
     Document localOne() {
-        return Document();
+        auto local_b = Fingerprint(local_db.bullseye);
+        // remote_b != local_b; write result to Doc.
+        return result_ok;
     }
 
 }
@@ -79,5 +133,9 @@ class IsToSynchronizeTheLocalDatabase {
 mixin Main!(_main);
 
 int _main(string[] args) {
+    auto dart_synchronization_feature = automation!(tagion.testbench.services.dart_synchronization);
+    DARTOptions opts; // specify a path.
+    dart_synchronization_feature.IsToConnectToRemoteDatabaseWhichIsUptodateAndReadItsBullseye;
+    dart_synchronization_feature.run;
     return 0;
 }
