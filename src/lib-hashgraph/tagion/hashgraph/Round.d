eae49e5ce6f318e26ae500eb7cdf83ea6078b467
Lucas Rasmussen <lucasnethaj@gmail.com>
2025-03-05 22:47:12 +0100

    renmame log.opCall(Throwable) to log.fatal

--- src/lib-hashgraph/tagion/hashgraph/Round.d
+++ src/lib-hashgraph/tagion/hashgraph/Round.d
@@ -696,7 +696,7 @@ class Round {
                     }
                 }
                 catch (Exception e) {
-                    log(e);
+                    log.fatal(e);
                 }
             }
         }
