f7dc21e040fc2b73a8061236cb15d4cf7fd50914
Lucas Rasmussen <lucasnethaj@gmail.com>
2025-03-12 14:29:51 +0100

    mirror_mode connect events

--- src/lib-hashgraph/tagion/hashgraph/HashGraph.d
+++ src/lib-hashgraph/tagion/hashgraph/HashGraph.d
@@ -130,9 +130,11 @@ class HashGraph {
 
     void initialize_witness(const(immutable(EventPackage)*[]) epacks)
     in {
-        assert(_nodes.length > 0 && (channel in _nodes),
-                "Own Eva event needs to be create before witness can be initialized");
-        assert(_owner_node !is null);
+        if(!mirror_mode) {
+            assert(_nodes.length > 0 && (channel in _nodes),
+                    "Own Eva event needs to be create before witness can be initialized");
+            assert(_owner_node !is null);
+        }
     }
     do {
         debug (EPOCH_LOG) {
@@ -416,8 +418,9 @@ class HashGraph {
                 _event_cache[fingerprint] = event;
                 return event;
             }
-            Event.check(0, ConsensusFailCode.EVENT_MISSING_IN_CACHE);
-            assert(0);
+            return null;
+            // Event.check(0, ConsensusFailCode.EVENT_MISSING_IN_CACHE);
+            // assert(0);
         }
 
         /**
@@ -438,10 +441,14 @@ class HashGraph {
 
             // event either from event_package_cache or event_cache.
             event = lookup(fingerprint);
-            //Event.check(event !is null, ConsensusFailCode.EVENT_MISSING_IN_CACHE);
-            //if (event !is null) {
-            event.connect(this.outer);
-            //}
+            if(!event) { // FIXME: this check should only be ignored in booting the network
+                if(mirror_mode) {
+                    return event;
+                }
+                Event.check(0, ConsensusFailCode.EVENT_MISSING_IN_CACHE);
+            } else {
+                event.connect(this.outer);
+            }
             return event;
         }
     }
@@ -834,6 +841,11 @@ class HashGraph {
                 connected_events_count++;
             }
             log("connected_events_count %s: %s", from_channel.encodeBase64, connected_events_count);
+
+            if (!areWeInGraph) {
+                break;
+            }
+            const from_front_seat = register_wavefront(received_wave, from_channel);
 
             /* return hirpc.result(received, buildWavefront(SECOND_WAVE, received_wave.tides)); */
             break;
