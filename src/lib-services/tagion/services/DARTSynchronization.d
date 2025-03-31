43b0abb9b8a52a9a4ef9baa189b6be797f950310
alabenskiy <al@decard.io>
2025-03-31 16:29:21 +0300

    Added a timeout. Made a clean up

--- src/lib-services/tagion/services/DARTSynchronization.d
+++ src/lib-services/tagion/services/DARTSynchronization.d
@@ -1,43 +1,42 @@
 module tagion.services.DARTSynchronization;
 
+import std.exception : enforce, assumeUnique;
+import std.format;
+import std.file;
+import std.path : baseName, buildPath, dirName, setExtension, stripExtension;
+import std.range;
+import std.stdio;
+import core.time;
+import core.thread;
+import core.memory : pageSize;
+
 import tagion.services.DART : DARTOptions, DARTService;
-import tagion.crypto.SecureNet;
-import tagion.dart.DART;
-import tagion.dart.DARTRemoteWorker;
-import tagion.crypto.Types : Fingerprint;
 import tagion.services.DARTInterface;
 import tagion.services.TRTService;
-import tagion.services.options : TaskNames;
-import tagion.utils.pretend_safe_concurrency : receiveOnly;
+import tagion.services.options : TaskNames, contract_sock_addr;
+import tagion.services.messages;
+import tagion.crypto.SecureNet;
+import tagion.crypto.Types : Fingerprint;
+import tagion.dart.DART;
 import tagion.dart.DARTcrud : dartBullseye;
-import tagion.communication.HiRPC;
-import tagion.hibon.Document;
-import tagion.Keywords;
+import tagion.dart.DARTRemoteWorker;
 import tagion.dart.DARTBasic : DARTIndex, Params;
 import tagion.dart.DARTRim;
 import tagion.dart.BlockFile : BlockFile, BLOCK_SIZE;
+import tagion.actor;
+import tagion.hibon.Document;
+import tagion.hibon.HiBONException;
+import tagion.json.JSONRecord;
+import tagion.Keywords;
 import tagion.utils.Term;
 import tagion.utils.pretend_safe_concurrency;
-import tagion.actor;
-import tagion.services.messages;
-import tagion.dart.DART;
-import tagion.json.JSONRecord;
-import tagion.services.options : contract_sock_addr;
-import tagion.hibon.HiBONException;
-
-import std.exception : enforce, assumeUnique;
-import std.format;
-import std.path : baseName, buildPath, dirName, setExtension, stripExtension;
-import std.file;
-import std.stdio;
-import std.range;
-import core.time;
-import core.thread;
+import tagion.utils.pretend_safe_concurrency : receiveOnly;
+import tagion.communication.HiRPC : HiRPC;
+import tagion.logger.Logger;
 
 @safe:
 
 struct SockAddresses {
-
     string[] sock_addrs;
     string dart_prefix = "DART_";
 
@@ -52,37 +51,25 @@ struct SockAddresses {
 
 pragma(msg, "fixme(cbr): Option cannot include a string array.");
 struct DARTSyncOptions {
-
     uint socket_timeout_mil = 10_000;
     uint socket_attempts_mil = 30_000;
     string journal_path;
-    // string dart_prefix;
-    // string[] sock_addrs;
-
-    // void setDefault() nothrow {
-    // socket_timeout_mil = 1000;
-    // socket_attempts_mil = 30_000;
-    // dart_prefix = "DART_";
-    // sock_addrs ~= contract_sock_addr(dart_prefix);
-    // }
-
-    // void setPrefix(string prefix) nothrow {
-    //     sock_addrs ~= contract_sock_addr(prefix ~ dart_prefix);
-    // }
 
     mixin JSONRecord;
 }
 
-/// Represents a DART Synchronization Service responsible for database sync tasks.
 struct DARTSynchronization {
 
     struct ReplayFiles {
         string[] files;
     }
 
-    /// Entry point for the synchronization task.
-    void task(immutable(DARTSyncOptions) opts, immutable(SockAddresses) sock_addrs, shared(
-            StdSecureNet) shared_net, string dst_dart_path) {
+    void task(
+        immutable(DARTSyncOptions) opts,
+        immutable(SockAddresses) sock_addrs,
+        shared(StdSecureNet) shared_net,
+        string dst_dart_path
+    ) {
         if (opts.journal_path.exists) {
             opts.journal_path.rmdirRecurse;
         }
@@ -92,36 +79,44 @@ struct DARTSynchronization {
         auto net = new StdSecureNet(shared_net);
         auto dest_db = new DART(net, dst_dart_path);
 
-        void compare(dartCompareRR req) @safe {
+        void compareTask(dartCompareRR req) {
             immutable result = bullseyesMatch(opts, sock_addrs, net, dest_db);
             req.respond(result);
         }
 
-        void sync(dartSyncRR req) @safe {
+        void synchronizeTask(dartSyncRR req) {
             immutable journal_filenames = synchronize(opts, sock_addrs, dest_db);
             req.respond(journal_filenames);
         }
 
-        void replay(dartReplayRR req, immutable(ReplayFiles) files) @safe {
+        void replayTask(dartReplayRR req, immutable(ReplayFiles) files) {
             replayWithFiles(dest_db, files);
             req.respond(true);
         }
 
-        void recorderSyncTask(syncRecorderRR req) @safe {
+        void recorderSynchronizeTask(syncRecorderRR req) {
             immutable result = recorderSynchronize(opts, sock_addrs, net, dest_db);
             req.respond(result);
         }
 
-        run(&compare, &sync, &replay, &recorderSyncTask);
+        run(&compareTask, &synchronizeTask, &replayTask, &recorderSynchronizeTask);
     }
 
 private:
-    immutable(bool) bullseyesMatch(immutable(DARTSyncOptions) opts, immutable(SockAddresses) sock_addrs,
-        const SecureNet net, DART destination) {
 
+    immutable(bool) bullseyesMatch(
+        immutable(DARTSyncOptions) opts,
+        immutable(SockAddresses) sock_addrs,
+        const SecureNet net,
+        DART destination
+    ) {
         try {
-            RemoteRequestSender sender = new RemoteRequestSender(opts.socket_timeout_mil, opts.socket_attempts_mil,
-                sock_addrs.sock_addrs[0], null);
+            RemoteRequestSender sender = new RemoteRequestSender(
+                opts.socket_timeout_mil,
+                opts.socket_attempts_mil,
+                sock_addrs.sock_addrs[0],
+                null
+            );
             HiRPC hirpc = HiRPC(net);
             auto bullseyeRequestDoc = dartBullseye(hirpc).toDoc;
             const bullseyeResponseDoc = sender.send(bullseyeRequestDoc);
@@ -136,11 +131,11 @@ private:
         }
     }
 
-    immutable(string[]) synchronize(immutable(DARTSyncOptions) opts,
-        immutable(SockAddresses) sock_addrs, DART destination) {
-
-        import core.memory : pageSize;
-
+    immutable(string[]) synchronize(
+        immutable(DARTSyncOptions) opts,
+        immutable(SockAddresses) sock_addrs,
+        DART destination
+    ) {
         enum stackPage = 256;
 
         const sz = pageSize * stackPage;
@@ -153,33 +148,26 @@ private:
 
         void synchronizeFiber(DARTRemoteWorker remote_worker, ushort current_rim) {
             auto dist_sync_fiber = destination.synchronizer(remote_worker, Rims(
-                    [
-                        cast(ubyte) current_rim
-                    ]), sz, guard_page_size);
+                    [cast(ubyte) current_rim]), sz, guard_page_size);
 
             while (!dist_sync_fiber.empty) {
                 (() @trusted => dist_sync_fiber.call)();
             }
-            // Ensure that fiber is really empty.
-            auto current_state = dist_sync_fiber.state;
-
-            if (current_state == Fiber.State.TERM) {
+            if (dist_sync_fiber.state == Fiber.State.TERM) {
                 (() @trusted => dist_sync_fiber.reset)();
             }
         }
 
         void assignWorkers() {
             foreach (sock_addr; sock_addr_arr) {
-                if (rim_range.empty) {
+                if (rim_range.empty)
                     break;
-                }
 
                 const ushort current_rim = rim_range.front;
                 const sector = current_rim << 8;
                 rim_range.popFront;
 
                 auto remote_worker = new DARTRemoteWorker(opts, sock_addr, destination);
-
                 remote_workers[sock_addr] = remote_worker;
 
                 immutable journal_filename = format("%s.%04x.dart_journal.hibon", opts.journal_path, sector);
@@ -217,9 +205,12 @@ private:
         }
     }
 
-    bool recorderSynchronize(immutable(DARTSyncOptions) opts, immutable(SockAddresses) sock_addrs,
-        const SecureNet net, DART destination) {
-
+    bool recorderSynchronize(
+        immutable(DARTSyncOptions) opts,
+        immutable(SockAddresses) sock_addrs,
+        const SecureNet net,
+        DART destination
+    ) {
         import tagion.replicator.RecorderCrud;
         import tagion.replicator.RecorderBlock;
         import tagion.dart.Recorder;
@@ -227,50 +218,55 @@ private:
         import tagion.dart.DARTFile;
         import tagion.actor.exceptions;
         import tagion.wave.common;
-        import tagion.logger.Logger;
         import tagion.hibon.HiBONRecord;
 
         bool bMatch = false;
         HiRPC hirpc = HiRPC(net);
+        uint node_timeout_mil = 30_000;
+        uint socket_addr_index = 0;
 
         while (!bMatch) {
-            // 1. Sync
             synchronize(opts, sock_addrs, destination);
-            // 2. Get a db head
             TagionHead tagion_head = getHead(destination, net);
+
+            string current_sock_addrs = sock_addrs.sock_addrs[socket_addr_index];
+            auto startTime = MonoTime.currTime();
 
             while (true) {
                 try {
-                    RemoteRequestSender sender = new RemoteRequestSender(opts.socket_timeout_mil, opts
-                            .socket_attempts_mil,
-                            sock_addrs.sock_addrs[0], null);
+                    if (MonoTime.currTime() - startTime > node_timeout_mil.msecs &&
+                        socket_addr_index + 1 < sock_addrs.sock_addrs.length) {
+                        ++socket_addr_index;
+                        break;
+                    }
+
+                    RemoteRequestSender sender = new RemoteRequestSender(
+                        opts.socket_timeout_mil,
+                        opts.socket_attempts_mil,
+                        current_sock_addrs,
+                        null
+                    );
 
                     const recorder_read_request = hirpc.readRecorder(
                         EpochParam(tagion_head.current_epoch));
                     const recorder_block_doc = sender.send(recorder_read_request.toDoc);
 
-                    if (recorder_block_doc.empty || !recorder_block_doc
-                        .isRecord!RecorderBlock) {
+                    if (recorder_block_doc.empty || !recorder_block_doc.isRecord!RecorderBlock)
                         break;
-                    }
 
                     const block = RecorderBlock(recorder_block_doc);
-                    
                     auto factory = RecordFactory(net);
                     auto recorder = factory.recorder(block.recorder_doc);
                     auto bullseye = destination.modify(recorder);
 
                     bMatch = bullseye == block.bullseye;
-                    if (bMatch) {
+                    if (bMatch)
                         break;
-                    }
                 }
                 catch (Exception e) {
                     break;
                 }
             }
-            // Check a timeout.
-            // Select another node if time out - TBD
         }
         return bMatch;
     }
@@ -279,37 +275,31 @@ private:
 class RemoteRequestSender {
 
     protected DART.SynchronizationFiber fiber;
-
     uint socket_timeout_mil;
     uint socket_attempts_mil;
     string sock_addr;
 
     this(uint socket_timeout_mil, uint socket_attempts_mil, string sock_addr, DART
             .SynchronizationFiber fiber = null) {
-        this.socket_attempts_mil = socket_attempts_mil;
         this.socket_timeout_mil = socket_timeout_mil;
+        this.socket_attempts_mil = socket_attempts_mil;
         this.sock_addr = sock_addr;
         this.fiber = fiber;
     }
 
     Document send(const Document request_doc) {
-
         import nngd;
         import std.range;
         import std.typecons;
 
         NNGSocket socket = NNGSocket(nng_socket_type.NNG_SOCKET_REQ);
-        scope (exit) {
-            writefln("-------- Close the socket at %s --------", sock_addr);
+        scope (exit)
             socket.close();
-        }
 
         socket.recvtimeout = socket_timeout_mil.msecs;
-        writefln("-------- Trying to dial a socket at %s --------", sock_addr);
         int rc = socket.dial(sock_addr);
         enforce(rc == 0, format("Failed to dial %s", nng_errstr(rc)));
 
-        writefln("-------- Send to the socket at %s --------", sock_addr);
         rc = socket.send!(immutable(ubyte[]))(request_doc.serialize);
         enforce(rc == 0, format("Failed to send %s", nng_errstr(rc)));
 
@@ -318,13 +308,10 @@ class RemoteRequestSender {
 
         while (true) {
             auto received = socket.receive!(immutable ubyte[])(Yes.Nonblock);
-
-            if (!received.empty) {
+            if (!received.empty)
                 return Document(received);
-            }
 
             if (MonoTime.currTime() - startTime > attempts_timeout) {
-                writefln("-------- send timeout --------");
                 return Document.init;
             }
 
--- src/lib-services/tagion/services/replicator.d
+++ src/lib-services/tagion/services/replicator.d
@@ -2,42 +2,37 @@
 /// https://docs.tagion.org/tech/architecture/Replicator
 module tagion.services.replicator;
 
+import std.algorithm;
+import std.exception;
+import std.file : append, exists, mkdirRecurse;
+import std.format;
+import std.path : buildPath, setExtension;
+import std.range;
+import std.stdio;
+
 import tagion.actor;
+import tagion.basic.Types : FileExtension;
+import tagion.communication.HiRPC;
 import tagion.crypto.SecureInterfaceNet;
 import tagion.crypto.SecureNet : StdHashNet;
 import tagion.crypto.Types : Fingerprint;
 import tagion.dart.Recorder : RecordFactory;
+import tagion.errors.tagionexceptions;
+import tagion.hibon.Document;
+import tagion.hibon.HiBONException;
+import tagion.hibon.HiBONFile;
 import tagion.logger.Logger;
 import tagion.replicator.RecorderBlock;
 import tagion.services.messages;
-
-//import tagion.utils.convert : cutHex;
-import tagion.basic.Types : FileExtension;
-import tagion.errors.tagionexceptions;
-import std.path : buildPath, setExtension;
-import std.stdio;
-import std.format;
-import std.file : append, exists, mkdirRecurse;
-import tagion.hibon.HiBONFile;
-import tagion.hibon.HiBONException;
-import std.algorithm;
-import tagion.hibon.Document;
-import tagion.communication.HiRPC;
+import tagion.json.JSONRecord;
 
 @safe
 struct ReplicatorOptions {
-    import std.format;
-    import tagion.json.JSONRecord;
-
     string folder_path = "./recorder";
     int new_file_interval = 10_000;
 
     void setPrefix(string prefix) nothrow {
-        import std.exception;
-        import std.path : buildPath;
-
-        folder_path = folder_path ~ prefix;
-        // assumeWontThrow(buildPath(folder_path, prefix));
+        folder_path ~= prefix;
     }
 
     mixin JSONRecord;
@@ -48,40 +43,34 @@ enum modify_log = "modify/replicator";
 @safe
 struct ReplicatorService {
     static Topic modify_recorder = Topic(modify_log);
-    const hirpc = HiRPC(null);
+    const HiRPC hirpc = HiRPC(null);
 
     void task(immutable(ReplicatorOptions) opts) {
         HashNet net = new StdHashNet;
         RecorderBlock last_block;
         string filepath;
-
         File file;
-        scope (exit) {
+
+        scope (exit)
             file.close;
-        }
 
         void receiveReplicatorFilePath(repFilePathRR req) {
             req.respond(filepath);
         }
 
         void readRecorder(readRecorderRR req, Document doc) {
-            import std.range;
             import tagion.replicator.RecorderCrud;
 
             try {
                 const receiver = hirpc.receive(doc);
-
-                // log("ReadRecorder receiver: %s", receiver.toPretty);
                 const epoch_number = receiver.params!(EpochParam).epoch_number;
 
                 auto fin = File(filepath, "r");
-                scope (exit) {
+                scope (exit)
                     fin.close;
-                }
 
                 foreach (item; HiBONRange(fin)) {
                     auto block = RecorderBlock(item, net);
-
                     if (block.epoch_number == epoch_number) {
                         req.respond(block.toDoc);
                         break;
@@ -89,48 +78,54 @@ struct ReplicatorService {
                 }
             }
             catch (Exception e) {
-                log("Received readRecorderRR request: %s", e.msg);
+                log("readRecorder error: %s", e.msg);
             }
         }
 
-        void receiveRecorder(SendRecorder, immutable(RecordFactory.Recorder) recorder, Fingerprint bullseye, immutable(
-                long) epoch_number) {
-
+        void receiveRecorder(
+            SendRecorder,
+            immutable(RecordFactory.Recorder) recorder,
+            Fingerprint bullseye,
+            immutable long epoch_number
+        ) {
             if (file is File.init || epoch_number % opts.new_file_interval == 0) {
-                log("going to create new file");
+                log("Creating new file for epoch %d", epoch_number);
+
                 if (file !is File.init) {
                     file.close;
                 }
+
                 const filename = format("%010d_epoch", epoch_number).setExtension(
                     FileExtension.hibon);
                 filepath = buildPath(opts.folder_path, filename);
-                log.trace("Creating new replicator file %s", filepath);
 
                 if (!opts.folder_path.exists) {
                     mkdirRecurse(opts.folder_path);
                 }
 
                 if (filepath.exists) {
-                    throw new TagionException(format("Error: File %s already exists", filepath));
+                    throw new TagionException(format("File already exists: %s", filepath));
                 }
+
                 file = File(filepath, "w");
             }
-            RecorderBlock block;
+
+            auto block = RecorderBlock(
+                recorder.toDoc,
+                last_block is RecorderBlock.init ? Fingerprint.init
+                    : last_block.fingerprint,
+                    bullseye,
+                    epoch_number,
+                    net
+            );
+
             scope (success) {
                 file.fwrite(block);
                 file.flush;
                 last_block = block;
             }
 
-            block = RecorderBlock(
-                recorder.toDoc,
-                last_block is RecorderBlock.init ? Fingerprint.init
-                    : last_block.fingerprint,
-                    bullseye,
-                    epoch_number,
-                    net);
-
-            log.trace("Added recorder chain block with hash '%(%02x%)'", block.fingerprint);
+            log.trace("Added recorder block with hash '%(%02x%)'", block.fingerprint);
             log.event(modify_recorder, "modify", recorder);
         }
 
