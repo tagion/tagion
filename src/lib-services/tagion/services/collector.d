tagion/tagion

commit 31691904814c85658024b8cc7c119c6b308ca17f

refactor collector service Spawn a new fiber task for each collected contract instead of keeping track of the state of each contract

src/lib-services/tagion/services/collector.d
@@ -23,7 +23,9 @@ import tagion.script.common;
 import tagion.script.execute;
 import tagion.services.messages;
 import tagion.services.options : TaskNames;
+import tagion.services.exception;
 import tagion.utils.pretend_safe_concurrency;
+import conc = tagion.utils.pretend_safe_concurrency;
 import tagion.hibon.HiBONJSON;
 import tagion.logger.ContractTracker;
 
@@ -34,7 +36,71 @@ struct CollectorOptions {
 }
 
 /// Topic for rejected collector inputs;
-enum reject_collector = "reject/collector";
+Topic reject_collector = "reject/collector";
+
+immutable(CollectedSignedContract)* collect_contract(ActorHandle dart_handle, immutable SecureNet net, immutable(SignedContract)* s_contract) {
+    immutable(Document)[] contract_reads;
+    /* Read indices */ 
+    if (s_contract.signs.length != s_contract.contract.inputs.length) {
+        throw new ServiceException("contract_mismatch_signature_length");
+    }
+
+    if (!s_contract.contract.reads.empty) {
+        auto reads_req = dartReadRR();
+        dart_handle.send(reads_req, (*s_contract).contract.reads);
+        receive((dartReadRR.Response res, immutable(RecordFactory.Recorder) recorder) {
+                assert(res.id == reads_req.id);
+                check(recorder[].map!(a => a.dart_index).array == (*s_contract).contract.reads, "missing_archives");
+                contract_reads = recorder[].map!(a => a.filed).array;
+            }
+        );
+    }
+
+    auto inputs_req = dartReadRR();
+    dart_handle.send(inputs_req, (*s_contract).contract.inputs);
+
+    // receive inputs recorder response
+    immutable(Document)[] contract_inputs;
+    receive((dartReadRR.Response res, immutable(RecordFactory.Recorder) recorder) {
+        assert(res.id == inputs_req.id);
+        check(recorder[].map!(a => a.dart_index).array == (*s_contract).contract.inputs, "missing_archives");
+        contract_inputs = recorder[].map!(a => a.filed).array;
+    });
+
+    if (!verify(net, s_contract, contract_inputs)) {
+        throw new ServiceException("contract_no_verify");
+    }
+
+    assert(contract_inputs !is Document[].init, "Recorder should've contained inputs at this point");
+
+    return new immutable(CollectedSignedContract)(s_contract, contract_inputs, contract_reads);
+}
+
+void collect_contract_hirpc(ActorHandle tvm_handle, ActorHandle dart_handle, immutable(SecureNet) net, immutable(SignedContract)* s_contract) {
+    try {
+        auto collected = collect_contract(dart_handle, net, s_contract);
+        if(collected) {
+            tvm_handle.send(signedContract(), collected);
+        }
+    }
+    catch(Exception e) {
+        log.event(reject_collector, e.msg, Document.init);
+        logContractStatus(s_contract.contract, ContractStatusCode.rejected, e.msg);
+    }
+}
+
+void collect_contract_consensus(ActorHandle tvm_handle, ActorHandle dart_handle, immutable(SecureNet) net, immutable(SignedContract)* s_contract) {
+    try {
+        auto collected = collect_contract(dart_handle, net, s_contract);
+        if(collected) {
+            tvm_handle.send(consensusContract(), collected);
+        }
+    }
+    catch(Exception e) {
+        log.event(reject_collector, e.msg, Document.init);
+        logContractStatus(s_contract.contract, ContractStatusCode.rejected, e.msg);
+    }
+}
 
 /**
  * Collector Service actor
@@ -46,59 +112,29 @@ enum reject_collector = "reject/collector";
 struct CollectorService {
     ActorHandle dart_handle;
     ActorHandle tvm_handle;
+    immutable(SecureNet) net;
 
+    @trusted
     this(immutable(TaskNames) tn) nothrow {
         dart_handle = ActorHandle(tn.dart);
         tvm_handle = ActorHandle(tn.tvm);
-    }
-
-    immutable(SignedContract)*[uint] contracts;
-    bool[uint] is_consensus_contract;
-    immutable(Document)[][uint] reads;
-
-    Topic reject = Topic(reject_collector);
-    SecureNet net;
-    void task() {
-        net = createSecureNet;
+        // Only used for verify function
+        net = cast(immutable)createSecureNet;
         assert(net !is null, "No secure net");
-        run(&receive_recorder, &signed_contract, &consensus_signed_contract, &rpc_contract);
     }
 
-    // Makes the read calls to the dart service;
-    void read_indices(dartReadRR req, immutable(SignedContract)* s_contract) {
-        if (s_contract.signs.length != s_contract.contract.inputs.length) {
-            log.event(reject, "contract_mismatch_signature_length", Document.init);
-            logContractStatus(s_contract.contract, ContractStatusCode.rejected, "Rejected contract mismatch signature length");
-            return;
-        }
-
-        contracts[req.id] = s_contract;
-        scope (failure) {
-            contracts.remove(req.id);
-            reads.remove(req.id);
-        }
-        log("Set the signed_contract %s", (req.id in contracts) !is null);
-        if (s_contract.contract.reads !is DARTIndex[].init) {
-            log("sending contract read request to dart");
-            dart_handle.send(req, (*s_contract).contract.reads);
-        }
-
-        log("sending contract input request to dart");
-        dart_handle.send(req, (*s_contract).contract.inputs);
+    @trusted
+    void task() {
+        auto scheduler = new FiberScheduler;
+        scheduler.start({
+            run(&signed_contract, &rpc_contract);
+        });
     }
 
-    void consensus_signed_contract(consensusContract, immutable(SignedContract*)[] signed_contracts) {
-        foreach (s_contract; signed_contracts) {
-            auto req = dartReadRR();
-            is_consensus_contract[req.id] = true;
-            read_indices(req, s_contract);
+    void signed_contract(consensusContract, immutable(SignedContract*)[] signed_contracts) {
+        foreach(s_contract; signed_contracts) {
+            conc.spawn(&collect_contract_consensus, tvm_handle, dart_handle, net, s_contract);
         }
-    }
-
-    void signed_contract(inputContract, immutable(SignedContract)* s_contract) {
-        auto req = dartReadRR();
-        is_consensus_contract[req.id] = false;
-        read_indices(req, s_contract);
     }
 
     // Input received directly from the HiRPC verifier
@@ -107,80 +143,10 @@ struct CollectorService {
         log("collector received receiver");
         try {
             immutable s_contract = new immutable(SignedContract)(doc);
-            signed_contract(inputContract(), s_contract);
+            conc.spawn(&collect_contract_hirpc, tvm_handle, dart_handle, net, s_contract);
         }
         catch (HiBONRecordException e) {
-            log.event(reject, "hirpc_invalid_signed_contract", doc);
+            log.event(reject_collector, "hirpc_invalid_signed_contract", doc);
         }
     }
-
-    private void clean(uint id) {
-        is_consensus_contract.remove(id);
-        contracts.remove(id);
-        reads.remove(id);
-    }
-
-    // Receives the read Documents from the dart and constructs the CollectedSignedContract
-    void receive_recorder(dartReadRR.Response res, immutable(RecordFactory.Recorder) recorder) {
-        import std.algorithm.iteration : map;
-        import std.range;
-
-        scope (failure) {
-            clean(res.id);
-        }
-        log("received dartresponse");
-
-        if (!(res.id in contracts)) {
-            return;
-        }
-
-        immutable s_contract = contracts[res.id];
-        auto fingerprints = recorder[].map!(a => a.dart_index).array;
-
-        // The collector can be in 1 of 2 states when receiving the recorder from the dart
-        // First if we requested any read documents and they all exist. Then we'll add it to out collected contract.
-        // When a request for the input archives is received, the inputs for each signature is checked.
-        // before it's sent to the tvm.
-        // if any archive is missing in inputs/reads or any of the signatures are incorrect. then the contract will be rejected.
-        if (s_contract.contract.reads !is null && fingerprints == contracts[res.id].contract.reads) {
-            reads[res.id] = recorder[].map!(a => a.filed).array;
-            return;
-        }
-        else if (fingerprints == contracts[res.id].contract.inputs) {
-            log("Received and input response");
-            scope (exit) {
-                clean(res.id);
-            }
-
-            immutable inputs = recorder[].map!(a => a.filed).array;
-            import tagion.hibon.HiBONJSON;
-
-            if (!verify(net, s_contract, inputs)) {
-                log.event(reject, "contract_no_verify", recorder);
-                logContractStatus(s_contract.contract, ContractStatusCode.rejected, "Rejected contract no verify");
-                return;
-            }
-
-            assert(inputs !is Document[].init, "Recorder should've contained inputs at this point");
-            immutable collection =
-                ((res.id in reads) !is null)
-                ? new immutable(CollectedSignedContract)(s_contract, inputs, reads[res.id]) : new immutable(
-                        CollectedSignedContract)(s_contract, inputs);
-
-            log("sending to tvm");
-            if (is_consensus_contract[res.id]) {
-                tvm_handle.send(consensusContract(), collection);
-            }
-            else {
-                tvm_handle.send(signedContract(), collection);
-            }
-            return;
-        }
-        else {
-            clean(res.id);
-            log.event(reject, "missing_archives", recorder);
-            return;
-        }
-    }
-
 }

src/lib-utils/tagion/utils/pretend_safe_concurrency.d
@@ -21,6 +21,8 @@ public import std.concurrency : Tid,
     MailboxFull,
     OnCrowding,
     MessageMismatch,
+    Scheduler,
+    FiberScheduler,
     Variant;
 
 void setMaxMailboxSize(Tid tid, size_t messages, OnCrowding doThis) @trusted {

bdd/tagion/testbench/services/collector.d
@@ -69,10 +69,8 @@ TagionBill createBill(const TagionCurrency tgn) pure nothrow @safe {
 
 @safe @Scenario("it work", [])
 class ItWork {
-    enum dart_service = "dart_service_task";
     ActorHandle dart_handle;
     ActorHandle collector_handle;
-    ActorHandle replicator_handle;
 
     TagionBill[] input_bills;
     SecureNet[] input_nets;
@@ -117,8 +115,7 @@ class ItWork {
 
             auto dart_net = createSecureNet;
             dart_net.generateKeyPair("dartnet");
-            dart_handle = (() @trusted => spawn!DARTService(task_names.dart, opts, task_names, cast(shared) dart_net, false))();
-            replicator_handle = (() @trusted => spawn!ReplicatorService(task_names.replicator, replicator_opts))();
+            dart_handle = (() @trusted => spawn!DARTService(task_names.dart, opts, cast(shared) dart_net))();
             check(waitforChildren(Ctrl.ALIVE), "dart service did not alive");
         }
 
@@ -128,7 +125,7 @@ class ItWork {
         input_nets = createNets(10, "input");
         input_bills = input_nets.createBills(100_000);
         input_bills.insertBills(insert_recorder);
-        dart_handle.send(dartModifyRR(), RecordFactory.uniqueRecorder(insert_recorder), immutable long(0));
+        dart_handle.send(dartModifyRR(), RecordFactory.uniqueRecorder(insert_recorder));
         receiveOnlyTimeout!(dartModifyRR.Response, Fingerprint);
 
         {

src/lib-services/tagion/services/messages.d
@@ -2,15 +2,18 @@
 module tagion.services.messages;
 import tagion.actor.actor : Msg, Request;
 
-/// Msg Type sent to actors who receive the document
+/// [FROM: rpcserver, TO: HiRPCVerifier] HiRPC Document
 alias inputDoc = Msg!"inputDoc";
-/// Msg type sent to receiver task along with a hirpc
+
+/// [FROM: HiRPCVerifier, TO: Collector] Signed hirpc
 alias inputHiRPC = Msg!"inputHiRPC";
 
-/// [FROM: HiRPC Verifier, TO: Collector] Contracts sent to the collector from HiRPC Verifier
-alias inputContract = Msg!"contract";
 
-/// [FROM: Epoch Creator, TO: Collector] Contract received from other nodes.
+/**
+[FROM: Epoch Creator, TO: Collector]
+[FROM: Collector, TO: TVM]
+Contracts that have gone through consensus
+ */
 alias consensusContract = Msg!"contract-C";
 
 /// [FROM: Collector, TO: TVM] Verified signed contract with inputs sent to TVM.
@@ -43,18 +46,16 @@ enum NodeAction {
 alias NodeError = Msg!"node_error";
 alias NNGError = Msg!"nng_error";
 
-/// [FROM: DART, TO: Replicator] Send the produced recorder for replication
+/// [FROM: EpochCommit, TO: Replicator] Send the produced recorder for recorderchain
 alias SendRecorder = Msg!"SendRecorder";
+
 alias readRecorderRR = Request!"readRecorder";
 alias syncRecorderRR = Request!"recorderSync";
 alias repHiRPCRR = Request!"replicatorHiRPCRequest";
 alias repFilePathRR = Request!"repFilePath";
 
-/// [FROM: DART, TO: TRT] send the recorder to the trt for update
+/// [FROM: EpochCommit, TO: TRT] send the recorder to the trt for update
 alias trtModify = Msg!"trtModify";
-
-/// [FROM: Transcript, TO: TRT] send the signed contract to the TRT for storing contract
-alias trtContract = Msg!"trtContract";
 
 alias trtHiRPCRR = Request!"trtRead"; // trt.dartCRUD: [dartRead, dartCheckRead, dartRim]
 alias dartReadRR = Request!"dartRead"; // dartRead Request
@@ -68,4 +69,8 @@ alias dartCompareRR = Request!"dartCompare";
 alias dartSyncRR = Request!"dartSync";
 alias dartReplayRR = Request!"dartReplay";
 
-alias EpochShutdown = Msg!"epoch_shutdown"; // Tell the transcript to stop at a specific epoch
+/// [FROM: Supervisor, TO: transcript] Tell the transcript to stop at a specific epoch
+alias EpochShutdown = Msg!"epoch_shutdown";
+
+/// [FROM: Transcript, TO: EpochCommit] Sent when finalizing an epoch
+alias EpochCommitRR = Request!("EpochCommit", immutable(long));

commit 458889f6752660d7a6aa466d6da714b61c1ae781

fix regression compiler parsing error when compiling libmobile error: Invalid type void delegate() caused by: 31691904814c85658024b8cc7c119c6b308ca17f

src/lib-services/tagion/services/collector.d
@@ -120,6 +120,7 @@ struct CollectorService {
 
     @trusted
     void task() {
+        import std.concurrency;
         auto scheduler = new FiberScheduler;
         scheduler.start({
             run(&signed_contract, &rpc_contract);

src/lib-utils/tagion/utils/pretend_safe_concurrency.d
@@ -21,8 +21,6 @@ public import std.concurrency : Tid,
     MailboxFull,
     OnCrowding,
     MessageMismatch,
-    Scheduler,
-    FiberScheduler,
     Variant;
 
 void setMaxMailboxSize(Tid tid, size_t messages, OnCrowding doThis) @trusted {