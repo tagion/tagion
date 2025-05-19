f24682618dbdc7f31fddbe9a99a9f1bfa655445b
Lucas Rasmussen <lucasnethaj@gmail.com>
2025-05-19 09:52:10 +0200

    fix create_address_file double dart dump

--- scripts/create_address_file.sh
+++ scripts/create_address_file.sh
@@ -18,17 +18,20 @@ fi
 
 dart_file="$1"
 
+tmp_nnr_file=$(mktemp)
 tmp_address_file=$(mktemp)
 tmp_key_file=$(mktemp)
 
 # dartutil --dump /tmp/tagion/wave/Node_0_dart.drt | hirep -r \$@NNR | hibonutil -p | jq -r '.["#$node"][1], .["$addr"]'
-dartutil --dump $dart_file | hirep -r \$@NNR | hibonutil -p | jq -r '.["#$node"][1]' > "$tmp_key_file"
+dartutil --dump "$dart_file" | hirep -r \$@NNR > "$tmp_nnr_file"
+hibonutil -p < "$tmp_nnr_file" | jq -r '.["#$node"][1]' > "$tmp_key_file"
 
 # Get node addr and replace with task name
 if [[ -z "$FIX_BROKEN_ADDRESS" ]]; then 
-    dartutil --dump $dart_file | hirep -r \$@NNR | hibonutil -p | jq -r '.["$addr"]' | sed 's/^./N/' | sed 's/$/_epoch_creator/' > "$tmp_address_file"
+    hibonutil -p < "$tmp_nnr_file" | jq -r '.["$addr"]' | sed 's/^./N/' | sed 's/$/_node_interface/' > "$tmp_address_file"
 else
-    dartutil --dump $dart_file | hirep -r \$@NNR | hibonutil -p | jq -r '.["$addr"]' > "$tmp_address_file"
+    hibonutil -p < "$tmp_nnr_file" | jq -r '.["$addr"]' > "$tmp_address_file"
 fi
 
 paste "$tmp_key_file" "$tmp_address_file"
+rm $tmp_key_file $tmp_address_file $tmp_nnr_file
