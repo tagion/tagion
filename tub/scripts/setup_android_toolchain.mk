0b614ce1fd7a683bfb892af60da9634ed710660d
Lucas Rasmussen <lucasnethaj@gmail.com>
2025-05-28 14:33:00 +0200

    mk: update android install script

--- tub/scripts/setup_android_toolchain.mk
+++ tub/scripts/setup_android_toolchain.mk
@@ -2,16 +2,8 @@
 TOOLS:=$(abspath $(REPOROOT)/tools)
 
 LDC_VERSION:=1.37.0
-# TARGET_ARCH:=aarch64
-ifeq ($(TARGET_ARCH),x86_64)
-# There is no distribution for android x86_64, however the libs are included with aarch64
-LDC_TARGET:=ldc2-${LDC_VERSION}-android-aarch64
-else
-LDC_TARGET:=ldc2-${LDC_VERSION}-android-$(TARGET_ARCH)
-endif
 LDC_HOST:=ldc2-${LDC_VERSION}-linux-x86_64
 LDC_HOST_TAR:=$(LDC_HOST).tar.xz
-LDC_TARGET_TAR:=$(LDC_TARGET).tar.xz
 
 ANDROID_NDK:=android-ndk-r21b
 ANDROID_NDK_ZIP:=$(ANDROID_NDK)-linux-x86_64.zip
@@ -19,19 +11,12 @@ ANDROID_NDK_ZIP:=$(ANDROID_NDK)-linux-x86_64.zip
 ANDROID_CMAKE_ZIP:=cmake-3.10.2-linux-x86_64.zip
 ANDROID_CMAKE:=android-cmake
 
-install-android-toolchain: $(LDC_HOST) $(LDC_TARGET) $(ANDROID_NDK) $(ANDROID_CMAKE)
+install-android-toolchain: $(LDC_HOST) $(ANDROID_NDK) $(ANDROID_CMAKE)
 .PHONY: install-android-toolchain
 
 $(TOOLS)/.way:
 	mkdir -p $(TOOLS)
 	touch $(TOOLS)/.way
-
-$(TOOLS)/$(LDC_HOST)/.done: $(TOOLS)/.way
-	cd $(TOOLS)
-	wget https://github.com/ldc-developers/ldc/releases/download/v${LDC_VERSION}/${LDC_HOST_TAR} -O ${LDC_HOST_TAR}
-	tar xf $(LDC_HOST_TAR)
-	cd -
-	touch $@
 
 $(TOOLS)/$(LDC_HOST)/etc/ldc2.conf: $(TOOLS)/$(LDC_HOST)/.done tub/ldc2.conf
 	mv $(TOOLS)/$(LDC_HOST)/etc/ldc2.conf $(TOOLS)/$(LDC_HOST)/etc/ldc2.conf.orig || true
@@ -41,18 +26,20 @@ $(LDC_HOST): $(TOOLS)/$(LDC_HOST)/.done
 $(LDC_HOST): $(TOOLS)/$(LDC_HOST)/etc/ldc2.conf
 .PHONY: $(LDC_HOST)
 
-$(TOOLS)/$(LDC_TARGET)/.done: $(TOOLS)/.way
+$(TOOLS)/$(LDC_HOST)/.done: $(TOOLS)/.way
 	cd $(TOOLS)
-	wget https://github.com/ldc-developers/ldc/releases/download/v${LDC_VERSION}/${LDC_TARGET_TAR} -O ${LDC_TARGET_TAR}
-	tar xf $(LDC_TARGET_TAR)
-	mkdir -p $(LDC_HOST)/android-$(TARGET_ARCH)/
-	mkdir -p $(LDC_HOST)/android-x86_64/
-	cp -r $(LDC_TARGET)/lib-x86_64 $(LDC_HOST)/android-x86_64/lib || true
-	cp -r $(LDC_TARGET)/lib $(LDC_HOST)/android-$(TARGET_ARCH)/
+	wget https://github.com/ldc-developers/ldc/releases/download/v${LDC_VERSION}/${LDC_HOST_TAR} -O ${LDC_HOST_TAR}
+	tar xf $(LDC_HOST_TAR)
+	wget https://github.com/ldc-developers/ldc/releases/download/v${LDC_VERSION}/ldc2-${LDC_VERSION}-android-aarch64.tar.xz -O ldc2-${LDC_VERSION}-android-aarch64.tar.xz
+	wget https://github.com/ldc-developers/ldc/releases/download/v${LDC_VERSION}/ldc2-${LDC_VERSION}-android-armv7a.tar.xz -O ldc2-${LDC_VERSION}-android-armv7a.tar.xz
+	tar xf ldc2-${LDC_VERSION}-android-aarch64.tar.xz
+	tar xf ldc2-${LDC_VERSION}-android-armv7a.tar.xz
+	mkdir -p $(LDC_HOST)/android-aarch64/ $(LDC_HOST)/android-x86_64/ $(LDC_HOST)/android-armv7a/
+	cp -r ldc2-${LDC_VERSION}-android-aarch64/lib-x86_64 $(LDC_HOST)/android-x86_64/lib
+	cp -r ldc2-${LDC_VERSION}-android-aarch64/lib $(LDC_HOST)/android-aarch64/
+	cp -r ldc2-${LDC_VERSION}-android-armv7a/lib $(LDC_HOST)/android-armv7a/
 	cd -
 	touch $@
-
-$(LDC_TARGET): $(TOOLS)/$(LDC_TARGET)/.done
 
 $(TOOLS)/$(ANDROID_NDK)/.done: $(TOOLS)/.way 
 	cd $(TOOLS)
