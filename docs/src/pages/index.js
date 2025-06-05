c168cce2ba038840f2536f5c3d8d500dd4afc1e6
lysanderbirkenwald <leander.bindewald@gmail.com>
2025-06-05 14:50:24 +0200

    Update index.js

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -32,7 +32,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/tech/guide">
+            to="/tech/intro">
             Tech Documentation
           </Link>
           <Link
