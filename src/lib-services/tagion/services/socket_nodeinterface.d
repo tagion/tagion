56e835d5627dc12e83bfbdc84be90237b70c3b7b
Lucas Rasmussen <lucasnethaj@gmail.com>
2025-06-06 10:47:56 +0200

    socket_nodeinterface wrap connection in trusted catch

--- src/lib-services/tagion/services/socket_nodeinterface.d
+++ src/lib-services/tagion/services/socket_nodeinterface.d
@@ -147,10 +147,10 @@ struct NodeInterface {
         import std.concurrency : FiberScheduler;
         auto scheduler = new FiberScheduler;
         event_listener_tid = spawn(&event_listener, tn.event_listener);
-        // scheduler.start({
+        scheduler.start({
                 listener_sock = Socket(listen_address);
                 listener_sock.bind();
-                listener_sock.listen(128);
+                listener_sock.listen(10);
                 log("listening on %s", listen_address);
                 listener_sock.blocking = false;
                 pollfd listener_poll = pollfd(listener_sock.handle, POLLIN, 0);
@@ -160,7 +160,7 @@ struct NodeInterface {
 
                 listener_sock.shutdown();
                 listener_sock.close();
-        // });
+        });
     }
 }
 
@@ -169,143 +169,147 @@ enum CONNECTION_STATE {
     receive,
 }
 
-
 @trusted
 void connection(Tid event_listener_tid, TaskNames tn , Socket sock, CONNECTION_STATE state) {
     try {
-        // count how many times this connection has sent or received some data
-        uint statistic_state_change;
-
-        scope(exit) {
-            event_listener_tid.send(RmPoll(), sock.handle);
-            sock.shutdown();
-            sock.close();
-            log("connection closed %s", statistic_state_change);
-        }
-
-        HiRPC hirpc = HiRPC(null);
-
-        log.task_name = format("%s(%s,%s)", tn.node_interface, thisTid, sock.handle);
-
-        string method;
-
-        while(true) {
-            statistic_state_change++;
-            final switch (state) {
-                case state.send:
-                    log("state sending");
-                    state = CONNECTION_STATE.receive;
-                    Document send_doc;
-                    receive(
-                        (NodeSend _, Pubkey channel, Document doc) { send_doc = doc; },
-                        (WavefrontReq _, Pubkey channel, Document doc) { send_doc = doc; },
-                        (dartHiRPCRR.Response _, Document doc) { send_doc = doc;  },
-                        (readRecorderRR.Response _, Document doc) { send_doc = doc; },
-                        (dartHiRPCRR.Error _, string msg) { log(msg); },
-                        (readRecorderRR.Error _, string msg) { log(msg); },
-                        // (Variant v) @trusted { throw new Exception(format("Unknown message %s", v)); },
-                    );
-                    if(send_doc.empty) {
-                        debug(nodeinterface) log("empty doc");
-                        return;
-                    }
-
-                    size_t total_sent;
-                    size_t sent;
-                    immutable(ubyte)[] serialized_doc = send_doc.serialize;
-                    do {
-                        sent = sock.send(serialized_doc[(sent == 0)? sent : sent+1 .. $]);
-                        // todo if the op would have blocked wait for it to be ready 
-                        socket_check(sent != -1, "Failed to send");
-                        total_sent += sent;
-                    } while(total_sent < serialized_doc.length);
-
-                    debug(nodeinterface) log("sent %s bytes", total_sent);
-                    const hirpcmsg = hirpc.receive(send_doc);
-
-                    if(hirpcmsg.isMethod) {
-                        method = hirpcmsg.method.name;
-                    }
-                    else {
-                        return; // close connection
-                    }
-
-                    break;
-                case state.receive:
-                    debug(nodeinterface) log("state recv");
-                    state = CONNECTION_STATE.send;
-                    ReceiveBuffer receive_buffer;
-                    // receive_buffer will keep calling the callback until the entire document has been received
-                    auto result_buffer = receive_buffer(
-                        (scope void[] buf) {
-                            ptrdiff_t rc = sock.receive(buf);
-                            if(sock.wouldHaveBlocked) {
-                                debug(nodeinterface) log("send wouldHaveBlocked");
-                                pollfd pfd = pollfd(sock.handle, POLLIN | POLLHUP);
-                                event_listener_tid.send(AddPoll(), thisTid(), pfd);
-                                pollfd poll_event;
-                                receive((PollEvent _, pollfd fd) { poll_event = fd; });
-                                check(!(poll_event.revents & POLLHUP), "remote closed while receiving");
-                                check(poll_event.revents & POLLIN, "socket event but was not ready to read");
-                                rc = sock.receive(buf);
-                            }
-                            return rc;
-                        }
-                    );
-
-                    if(result_buffer.size <= 0) {
-                            // err
-                            return;
-                    }
-                    debug(nodeinterface) log("recv %s bytes", result_buffer.size);
-
-                    Document doc = Document((() @trusted => receive_buffer.buffer.assumeUnique)());
-
-                    if (!doc.empty && !doc.isInorder(Document.Reserved.no)) {
-                        check(false, "doc not in order");
-                    }
-                    const hirpcmsg = hirpc.receive(doc);
-                    // if (hirpcmsg.pubkey == this.net.pubkey) {
-                    //     // err
-                    //     return;
-                    // }
-                    // if (!hirpcmsg.isSigned) {
-                    //     // err
-                    //     return;
-                    // }
-
-                    if(hirpcmsg.isMethod) {
-                        method = hirpcmsg.method.name;
-                    }
-
-                    switch(method) {
-                        case RPCMethods.dartRead:
-                        case RPCMethods.dartCheckRead:
-                        case RPCMethods.dartBullseye:
-                        case RPCMethods.dartRim:
-                            locate(tn.dart).send(dartHiRPCRR(), doc);
-                            break;
-                        case RPCMethods.readRecorder:
-                            locate(tn.replicator).send(readRecorderRR(), doc);
-                            break;
-                        case RPCMethods.wavefront:
-                            locate(tn.epoch_creator).send(WavefrontReq(), doc);
-                            break;
-                        default:
-                            check(false, format("Unsupported method %s", hirpcmsg.method.name));
-                    }
-
-                    if(hirpcmsg.isResponse || hirpcmsg.isError) {
-                        return; // close connection;
-                    }
-            }
-        }
+        connection_impl(event_listener_tid, tn , sock, state);
     }
     catch(OwnerTerminated e) {
         return;
     }
+    // We catch the throwable here because it's inside a seperate thread.
     catch(Throwable e) {
         log.fatal(e);
         return;
+    }
+}
+
+void connection_impl(Tid event_listener_tid, TaskNames tn , Socket sock, CONNECTION_STATE state) {
+    // count how many times this connection has sent or received some data
+    uint statistic_state_change;
+
+    scope(exit) {
+        event_listener_tid.send(RmPoll(), sock.handle);
+        sock.shutdown();
+        sock.close();
+        log("connection closed %s", statistic_state_change);
+    }
+
+    HiRPC hirpc = HiRPC(null);
+
+    log.task_name = format("%s(%s,%s)", tn.node_interface, thisTid, sock.handle);
+
+    string method;
+
+    while(true) {
+        statistic_state_change++;
+        final switch (state) {
+        case state.send:
+            log("state sending");
+            state = CONNECTION_STATE.receive;
+            Document send_doc;
+            receive(
+                (NodeSend _, Pubkey channel, Document doc) { send_doc = doc; },
+                (WavefrontReq _, Pubkey channel, Document doc) { send_doc = doc; },
+                (dartHiRPCRR.Response _, Document doc) { send_doc = doc;  },
+                (readRecorderRR.Response _, Document doc) { send_doc = doc; },
+                (dartHiRPCRR.Error _, string msg) { log(msg); },
+                (readRecorderRR.Error _, string msg) { log(msg); },
+                // (Variant v) @trusted { throw new Exception(format("Unknown message %s", v)); },
+            );
+            if(send_doc.empty) {
+                debug(nodeinterface) log("empty doc");
+                return;
+            }
+
+            size_t total_sent;
+            size_t sent;
+            immutable(ubyte)[] serialized_doc = send_doc.serialize;
+            do {
+                sent = sock.send(serialized_doc[(sent == 0)? sent : sent+1 .. $]);
+                // todo if the op would have blocked wait for it to be ready 
+                socket_check(sent != -1, "Failed to send");
+                total_sent += sent;
+            } while(total_sent < serialized_doc.length);
+
+            debug(nodeinterface) log("sent %s bytes", total_sent);
+            const hirpcmsg = hirpc.receive(send_doc);
+
+            if(hirpcmsg.isMethod) {
+                method = hirpcmsg.method.name;
+            }
+            else {
+                return; // close connection
+            }
+
+            break;
+        case state.receive:
+            debug(nodeinterface) log("state recv");
+            state = CONNECTION_STATE.send;
+            ReceiveBuffer receive_buffer;
+            // receive_buffer will keep calling the callback until the entire document has been received
+            auto result_buffer = receive_buffer(
+                (scope void[] buf) {
+                    ptrdiff_t rc = sock.receive(buf);
+                    if(sock.wouldHaveBlocked) {
+                        debug(nodeinterface) log("send wouldHaveBlocked");
+                        pollfd pfd = pollfd(sock.handle, POLLIN | POLLHUP);
+                        event_listener_tid.send(AddPoll(), thisTid(), pfd);
+                        pollfd poll_event;
+                        receive((PollEvent _, pollfd fd) { poll_event = fd; });
+                        check(!(poll_event.revents & POLLHUP), "remote closed while receiving");
+                        check(poll_event.revents & POLLIN, "socket event but was not ready to read");
+                        rc = sock.receive(buf);
+                    }
+                    return rc;
+                }
+            );
+
+            if(result_buffer.size <= 0) {
+                    // err
+                    return;
+            }
+            debug(nodeinterface) log("recv %s bytes", result_buffer.size);
+
+            Document doc = Document((() @trusted => receive_buffer.buffer.assumeUnique)());
+
+            if (!doc.empty && !doc.isInorder(Document.Reserved.no)) {
+                check(false, "doc not in order");
+            }
+            const hirpcmsg = hirpc.receive(doc);
+            // if (hirpcmsg.pubkey == this.net.pubkey) {
+            //     // err
+            //     return;
+            // }
+            // if (!hirpcmsg.isSigned) {
+            //     // err
+            //     return;
+            // }
+
+            if(hirpcmsg.isMethod) {
+                method = hirpcmsg.method.name;
+            }
+
+            switch(method) {
+                case RPCMethods.dartRead:
+                case RPCMethods.dartCheckRead:
+                case RPCMethods.dartBullseye:
+                case RPCMethods.dartRim:
+                    locate(tn.dart).send(dartHiRPCRR(), doc);
+                    break;
+                case RPCMethods.readRecorder:
+                    locate(tn.replicator).send(readRecorderRR(), doc);
+                    break;
+                case RPCMethods.wavefront:
+                    locate(tn.epoch_creator).send(WavefrontReq(), doc);
+                    break;
+                default:
+                    check(false, format("Unsupported method %s", hirpcmsg.method.name));
+            }
+
+            if(hirpcmsg.isResponse || hirpcmsg.isError) {
+                return; // close connection;
+            }
+        }
     }
 }
